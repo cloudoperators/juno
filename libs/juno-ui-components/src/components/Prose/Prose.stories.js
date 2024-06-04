6a14424285a1b59523ea3bbfffb622fbb97cb5c3
I577931 <martin.david.schulze@sap.com>
2024-06-04 10:27:01 +0200

    feat(ui): add new text example

--- libs/juno-ui-components/src/components/Prose/Prose.stories.js
+++ libs/juno-ui-components/src/components/Prose/Prose.stories.js
@@ -26,27 +26,30 @@ const Template = (args) => (
     <h4>Headline 4</h4>
     <h5>Headline 5</h5>
     <article>
+      <h2>h2: Lorem ipsum dolor sit amet</h2>
+      <h3>h3: Sunt in culpa qui officia</h3>
       <p>
-        Supernova is an alternative UI for Prometheus Alertmanager with some quality of life
-        improvements:
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint:
       </p>
       <ul>
         <li>
-          Micro frontend design based on
+          Consectetur adipiscing elit
           <a href="https://ui.juno.global.cloud.sap" rel="nofollow">
-            Juno UI components
+            Link
           </a>
         </li>
-        <li>Predefined filter categories</li>
-        <li>Easy filtering</li>
-        <li>Autodiscover of the support group and added automatically as filter</li>
-        <li>Aggregation and counting of alerts by region and severity</li>
-        <li>Automatic URL linking for URLs in descriptions</li>
-        <li>Parsing of alert labels for included external links</li>
-        <li>Display of active and expired silences for a given alert</li>
+        <li>Incididunt ut labore et dolore magna aliqua.</li>
+        <li>Nostrud exercitation ullamco laboris</li>
+        <li>Mollit anim id est laborum</li>
+        <li>Nostrud exercitation ullamco laboris</li>
+        <li>Ponsectetur adipiscing elit, sed do eiusmod</li>
+        <li>Gullamco laboris nisi ut aliquip ex ea commodo</li>
+        <li>Consectetur adipiscing elit</li>
         <li>
-          Warning of an existing silence displaying the exact expiration time when creating new
-          silences
+          Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
         </li>
       </ul>
       <div>
@@ -56,69 +59,93 @@ const Template = (args) => (
         <h3>Alerts</h3>
       </div>
       <p>
-        Alerting rules offer the capability to define alert conditions using expressions in the
-        Prometheus expression language. These rules enable you to specify conditions that trigger
-        alerts, and subsequently send notifications regarding the firing alerts to an external
-        service. Whenever the alert expression results in one or more vector elements at a given
-        point in time, the alert counts as active for these elements <code>label sets</code>.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum. <code>Some code</code>.
       </p>
-      <div>
-        <h4>Labels</h4>
-      </div>
+      <h2>h2: Lorem ipsum dolor sit amet</h2>
+      <h4>h4: Labels</h4>
       <p>
-        The labels clause allows specifying a set of additional labels to be attached to the alert.
-        Following is a live example of a set of labels from an alert of the{" "}
-        <code>support group:containers</code> with
-        <code>severity:info</code> in the <code>region:eu-de-2</code>.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+        anim id est laborum. <code>Some code</code> and
+        <code>more code</code> in <code>code code code ...</code>.
       </p>
     </article>
     <article>
-      <h2>Extract functionality out of the Component</h2>
+      <h2>h2: Lorem ipsum dolor sit amet</h2>
+      <h3>h3: Sunt in culpa qui officia</h3>
       <p>
-        Transferring functions from a component to a helper file or to the store preserves the
-        cleanliness of the component by extracting its functionalities to a separate file. This
-        approach facilitates the subsequent creation of unit tests without directly impacting the
-        component itself.
+        Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+        ex ea commodo consequat.
       </p>
       <p>
-        Consider the following example: the component is tasked with performing diverse filter
-        calculations. Based on these calculations, it determines which items should be filtered.
-        Ultimately, the filtered results are then stored within the application's data store and not
-        use in the return statement.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+        anim id est laborum.
       </p>
       <ol>
         <li>
           <p>
-            Modularity and Reusability: Dividing components into individual files allows you to
-            create smaller, self-contained modules that can be reused across different parts of your
-            application. This modular approach enhances reusability and reduces redundancy, as
-            components can be easily imported and integrated into various parts of the application.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </p>
         </li>
         <li>
           <p>
-            Testing: Isolating components in separate files simplifies testing. You can focus on
-            testing individual components with specific inputs and expected outputs, which leads to
-            more thorough and targeted test coverage.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat.
           </p>
         </li>
         <li>
           <p>
-            Readability and Maintainability: Smaller, focused files are easier to read and
-            understand. Each file is dedicated to a specific component, making it simpler to grasp
-            the purpose and functionality of that component. This clarity contributes to better code
-            maintenance and reduces the likelihood of errors when modifying or debugging.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
           </p>
         </li>
         <li>
           <p>
-            Easier Navigation: As your project grows, a modular structure makes it easier to
-            navigate through code. With distinct files for each component, locating and managing
-            code becomes more efficient. IDEs and code editors also offer better navigation features
-            for modular projects.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </p>
         </li>
       </ol>
+      <h3>h3: Sunt in culpa qui officia</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat.
+      </p>
+      <h4>h4: Nisi ut aliquip</h4>
+      <p>
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+      <h4>h4: Commodo consequat</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat.
+      </p>
+      <p>
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat.
+      </p>
+      <h5>h5: Duis aute irure dolor in reprehenderit</h5>
+      <p>
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
     </article>
   </Prose>
 )
