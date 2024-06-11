53c88ea0a9cada72cba3f778552cc0ee1a17a2ee
Franz Heidl <franz@franzheidl.de>
2024-06-11 11:13:32 +0200

    [ui] Secret: add more props

--- libs/juno-ui-components/src/components/Secret/Secret.component.js
+++ libs/juno-ui-components/src/components/Secret/Secret.component.js
@@ -22,8 +22,9 @@ const coverStyles = `
   jn-right-0
   jn-bottom-0
   jn-cursor-not-allowed
-  jn-backdrop-blur
-  jn-bg-theme-textinput/30
+  jn-backdrop-blur-[4px]
+  
+  jn-bg-theme-background-lvl-2/30
   jn-z-10
   jn-flex
   jn-justify-center
@@ -38,7 +39,10 @@ const copyConfirmMessageStyles = `
   jn-rounded
 `;
 
-const textareaStyles = ``;
+const textareaStyles = `
+  jn-break-all
+  jn-min-h-[7.5rem]
+`;
 
 const hintStyles = ``;
 
@@ -50,6 +54,8 @@ export const Secret = ({
   autoComplete,
   autoFocus,
   className,
+  clear,
+  copy,
   copyConfirmtext,
   disabled,
   errortext,
@@ -71,6 +77,7 @@ export const Secret = ({
   required,
   reveal,
   successtext,
+  toggle,
   valid,
   value,
   wrapperClassName,
@@ -99,7 +106,9 @@ export const Secret = ({
   };
 
   const handleToggleClick = () => {
+    isRevealed ? onHide && onHide() : onReveal && onReveal();
     setIsRevealed(!isRevealed);
+    onRevealToggle && onRevealToggle();
   };
 
   const handleClearClick = () => {
@@ -147,7 +156,7 @@ export const Secret = ({
         <Textarea
           autoComplete={autoComplete}
           autoFocus={isRevealed ? autoFocus : false}
-          className={`jn-secret-textarea jn-font-mono ${className}`}
+          className={`jn-secret-textarea jn-font-mono ${textareaStyles} ${className}`}
           disabled={disabled}
           id={id}
           invalid={invalid}
@@ -190,27 +199,39 @@ export const Secret = ({
         </div>
 
         <ButtonRow className="jn-secret-actions jn-justify-self-end jn-mt-2">
-          <Button
-            size="small"
-            className={`${actionStyles}`}
-            onClick={handleClearClick}
-          >
-            Clear
-          </Button>
-          <Button
-            size="small"
-            className={`${actionStyles}`}
-            onClick={handleToggleClick}
-          >
-            {isRevealed ? "Hide" : "Reveal"}
-          </Button>
-          <Button
-            size="small"
-            className={`${actionStyles}`}
-            onClick={handleCopyClick}
-          >
-            Copy
-          </Button>
+          {clear ? (
+            <Button
+              size="small"
+              className={`${actionStyles}`}
+              onClick={handleClearClick}
+            >
+              Clear
+            </Button>
+          ) : (
+            ""
+          )}
+          {toggle ? (
+            <Button
+              size="small"
+              className={`${actionStyles}`}
+              onClick={handleToggleClick}
+            >
+              {isRevealed ? "Hide" : "Reveal"}
+            </Button>
+          ) : (
+            ""
+          )}
+          {copy ? (
+            <Button
+              size="small"
+              className={`${actionStyles}`}
+              onClick={handleCopyClick}
+            >
+              Copy
+            </Button>
+          ) : (
+            ""
+          )}
         </ButtonRow>
       </Stack>
     </div>
@@ -228,15 +249,35 @@ Secret.propTypes = {
   copyConfirmtext: PropTypes.string,
   /** Whether the Secret's input is disabled. */
   disabled: PropTypes.bool,
-  /**  */
+  /** A small text to display information regarding any errors in the context of the Secret. */
   errortext: PropTypes.string,
+  /** A small text to display giving more information and context about the Secret. */
   helptext: PropTypes.string,
+  /** A handler to execute when the Secret's input area looses focus. */
   onBlur: PropTypes.func,
+  /** A handler to execute when the Secret's content changes. */
   onChange: PropTypes.func,
+  /** A handler to execute when the user clears the Secret's content using the Clear button. */
+  onClear: PropTypes.func,
+  /** A handler to execute when the user copies the Secret's content to the clipboard. */
   onCopy: PropTypes.func,
-  /** Whether the secret's content should be revealed or not. */
+  onFocus: PropTypes.func,
+  /** A handler to execute when the user hides the Secret's content. */
+  onHide: PropTypes.func,
+  /** A handler to execute when the user reveals the Secret's content. */
+  onReveal: PropTypes.func,
+  /** A handler to execute when the visibility of the Secret's content is toggled, i.e. this will be run when the content is revealed and when it is hidden. */
+  onRevealToggle: PropTypes.func,
+  /** Whether the secret's content is revealed / legible. */
   reveal: PropTypes.bool,
+  /** A small text to display giving information in the context of the secret, e.g. when it was successfully validated or matches specific requirements, etc.  */
   successtext: PropTypes.string,
+  /** Whether a button to toggle visibility of the Secret's content should be rendered. */
+  toggle: PropTypes.bool,
+  /** Whether the Secret's content was successfully validated. */
+  valid: PropTypes.bool,
+  /** The value of the Secret, i.e. the Secret's content.  */
+  value: PropTypes.string,
   /** Pass a className to the outer wrapper element */
   wrapperClassName: PropTypes.string,
 };
@@ -245,6 +286,8 @@ Secret.defaultProps = {
   autoComplete: "off",
   autoFocus: false,
   className: "",
+  clear: true,
+  copy: true,
   copyConfirmtext: "Secret copied to clipboard.",
   disabled: false,
   errortext: undefined,
@@ -254,8 +297,12 @@ Secret.defaultProps = {
   onClear: undefined,
   onCopy: undefined,
   onFocus: undefined,
+  onHide: undefined,
+  onReveal: undefined,
+  onRevealToggle: undefined,
   reveal: false,
   successText: undefined,
+  toggle: true,
   valid: false,
   value: "",
   wrapperClassName: "",
--- libs/juno-ui-components/src/components/Secret/Secret.stories.js
+++ libs/juno-ui-components/src/components/Secret/Secret.stories.js
@@ -20,3 +20,28 @@ export const Default = {
   render: Template,
   args: {},
 };
+
+export const withValue = {
+  render: Template,
+  args: {
+    value:
+      "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEArvLhT1yR4G3l0aTmUjP6ktK8xVJcQoZi/s5NQq5Z9ENJkZnQ+2lPkhslk/qf3AQHfw5QrI2vQjN5CvX0b8LS5yX+TYEkFpeQZD6b2CqY3OJVTzH1eENBoM1yTlgZe9UdU6wYuR+9p/JrDJ8KjzOG6uToINt6XodR9nMFLAnZHSqQoP/M2w1PCoWXgF5VtRt1UOcMZJX1C8KRdzvF4X6YBY4tMX1FfH5/Uj2Kn6h8u38fUqsn2wrAVzpPnSXPU1eHsdJZGn4dd+3Q8p0K9rwnX1LrF57ZxKxe/mKm",
+  },
+};
+
+export const revealWithValue = {
+  render: Template,
+  args: {
+    reveal: true,
+    value:
+      "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEArvLhT1yR4G3l0aTmUjP6ktK8xVJcQoZi/s5NQq5Z9ENJkZnQ+2lPkhslk/qf3AQHfw5QrI2vQjN5CvX0b8LS5yX+TYEkFpeQZD6b2CqY3OJVTzH1eENBoM1yTlgZe9UdU6wYuR+9p/JrDJ8KjzOG6uToINt6XodR9nMFLAnZHSqQoP/M2w1PCoWXgF5VtRt1UOcMZJX1C8KRdzvF4X6YBY4tMX1FfH5/Uj2Kn6h8u38fUqsn2wrAVzpPnSXPU1eHsdJZGn4dd+3Q8p0K9rwnX1LrF57ZxKxe/mKm",
+  },
+};
+
+export const NoClearNoToggle = {
+  render: Template,
+  args: {
+    clear: false,
+    toggle: false,
+  },
+};
