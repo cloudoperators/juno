4d240cf788112b738b50ae2dbdb42fe3fe107d08
d064310 <andreas.pfau@sap.com>
2024-07-18 13:33:26 +0200

    chore(eslint): update eslint to 9.7
    - change config file name to eslint.config.mjs
    - define global config
    - reuse global config in every package

--- packages/utils/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Juno contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-module.exports = {
-  root: true,
-  extends: ["@cloudoperators/juno/juno-libs.js"],
-}
