e075e30b0902f1dfc97b322409d50c1c89bfa913
Wowa Barsukov <pro.barsukov@gmail.com>
2024-07-18 07:49:31 +0200

    chore(eslint): integrate eslint into repo (#127)
    * chore(example): Fix lint on example app
    
    * chore(eslint): setup lint for juno
    
    * chore(eslint): integrate lint config for all packages
    
    * chore(eslint): eslint configs
    
    * Automatic application of license header
    
    ---------
    
    Co-authored-by: Wowa <vladimir.barsukov@sap.com>
    Co-authored-by: License Bot <license_bot@github.com>

---
+++ packages/esflint-config-juno/juno-libs.js
@@ -0,0 +1,51 @@
+/*
+ * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Juno contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+const { resolve } = require("node:path");
+/*
+ * This is a custom ESLint configuration for use with
+ * internal (bundled by their consumer) libraries
+ * that utilize React.
+ */
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "prettier",
+    "plugin:react/recommended",
+    // Turborepo custom eslint configuration configures the following rules:
+    //  - https://github.com/vercel/turbo/blob/main/packages/eslint-plugin-turbo/docs/rules/no-undeclared-env-vars.md
+    "eslint-config-turbo",
+  ],
+  plugins: ["only-warn","react"],
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  parserOptions: {
+    sourceType: "module",
+    allowImportExportEverywhere: true,
+    ecmaVersion: "latest",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    "node_modules/",
+    "build/",
+  ],
+  rules: {
+    "import/no-default-export": "off",
+  },
+  overrides: [
+    // Force ESLint to detect .tsx files
+    { files: ["*.js?(x)"] },
+  ],
+};
---
+++ packages/url-state-provider/.eslintrc.js
@@ -0,0 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Juno contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+module.exports = {
+    root: true,
+    extends: ["@cloudoperators/juno/juno-libs.js"],
+  };
