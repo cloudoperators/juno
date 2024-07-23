91e4c47d93c8d97a84c952ca37c29f3991f8eae5
Franz Heidl <franz@franzheidl.de>
2024-07-23 14:11:32 +0200

    feat(ThemeToggle) update story context to accept theme prop

--- packages/ui-components/src/components/ThemeToggle/ThemeToggle.stories.js
+++ packages/ui-components/src/components/ThemeToggle/ThemeToggle.stories.js
@@ -10,14 +10,17 @@ export default {
   
   argTypes: {},
   decorators: [
-    (Story) => (
-      <StyleProvider theme="theme-light">
-        <div className="jn-p-12" style={{ minHeight: "250px" }}>
-          <Story />
-          <p> Some content. </p>
-        </div>
-      </StyleProvider>
-    ),
+    (Story, context) => {
+      const theme = context.args.theme
+      return (
+        <StyleProvider theme={theme}>
+          <div className="jn-p-12" style={{ minHeight: "250px" }}>
+            <Story />
+            <p> Some content. </p>
+          </div>
+        </StyleProvider>
+      )
+    },
   ],
 };
 
@@ -27,6 +30,12 @@ export const Default = {
   parameters: {},
   args: {},
 };
+
+export const LightTheme = {
+  args: {
+    theme: "theme-light"
+  }
+}
 
 export const Disabled = {
   args: {
