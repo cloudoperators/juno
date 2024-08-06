9751252f064fa811fbc9358a91f54c48894e8ce3
d064310 <andreas.pfau@sap.com>
2024-08-06 13:59:16 +0200

    chore(oauth): use more specific email regex

--- packages/oauth/src/tokenHelpers.js
+++ packages/oauth/src/tokenHelpers.js
@@ -25,7 +25,7 @@ const capitalize = (str) => {
 const extractNameFromEmail = (email) => {
   if (!email) return null
   try {
-    const match = email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
+    const match = email.match(/^([a-zA-Z0-9._%+-]+)@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
     if (!match) return null
     const emailName = match[1]
     const index = emailName.indexOf(".")
