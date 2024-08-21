b25e024c8587b4bd29ff43347be78909002a2fc7
Wowa <vladimir.barsukov@sap.com>
2024-08-21 16:16:26 +0200

    chore(infra): husky for pre-push

--- commitlint.config.js
+++ commitlint.config.js
@@ -25,18 +25,14 @@ module.exports = {
         "url-state-provider",
         "url-state-router",
         "version",
+        /^ISSUE-\d+$/, // Regex pattern for ISSUE-<number>
       ],
     ],
-    "scope-case": [2, "always", "kebab-case"], // Ensure scopes are in kebab-case
-    "scope-regex": [
-      2,
-      "always",
-      "^(build|communicator|config|ci|core|deps|example|infra|juno|k8s|message-provider|npm|oauth|template|ui|utils|url-state-provider|url-state-router|version|ISSUE-\\d+)$",
-    ],
+    "scope-case": [2, "always", "kebab-case"], // Enforces kebab-case
     "subject-case": [
       2,
       "never",
-      ["start-case", "pascal-case", "upper-case"], // Disallow certain cases for subject
+      ["start-case", "pascal-case", "upper-case"], // Disallows certain cases for the subject
     ],
   },
 }
