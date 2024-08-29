f09f38f4bca7d1a3831fbc26114c011d2bc5bc8f
Wowa <vladimir.barsukov@sap.com>
2024-08-29 15:38:42 +0200

    chore(ui): delete default props

--- packages/ui-components/src/components/FormattedText/FormattedText.component.tsx
+++ packages/ui-components/src/components/FormattedText/FormattedText.component.tsx
@@ -2,7 +2,6 @@
  * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Juno contributors
  * SPDX-License-Identifier: Apache-2.0
  */
-
 import React from "react"
 
 const proseStyles = `
@@ -18,15 +17,14 @@ export interface FormattedTextProps {
 /**
  * A very basic layout container with padding.
  */
-export const FormattedText = ({ className, children, ...props }: FormattedTextProps) => {
+export const FormattedText = ({
+  className = "", // Default value for className
+  children = null, // Default value for children
+  ...props
+}: FormattedTextProps) => {
   return (
-    <div className={`formatted-text-container ${proseStyles} ${className} `} {...props}>
+    <div className={`formatted-text-container ${proseStyles} ${className}`} {...props}>
       {children}
     </div>
   )
-}
-
-FormattedText.defaultProps = {
-  className: "",
-  children: null,
 }
