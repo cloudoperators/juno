ed4bf96c7f6413a23674e46064e75a7b9b04fc68
Wowa <vladimir.barsukov@sap.com>
2024-08-29 14:11:12 +0200

    feat(ui): add typescript to FormattedText

---
+++ packages/ui-components/src/components/FormattedText/FormattedText.component.tsx
@@ -0,0 +1,32 @@
+/*
+ * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Juno contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React from "react"
+
+const proseStyles = `
+  jn-prose
+`
+
+export interface FormattedTextProps {
+  /** Add custom class name */
+  className?: string
+  children?: React.ReactNode
+}
+
+/**
+ * A very basic layout container with padding.
+ */
+export const FormattedText = ({ className, children, ...props }: FormattedTextProps) => {
+  return (
+    <div className={`formatted-text-container ${proseStyles} ${className} `} {...props}>
+      {children}
+    </div>
+  )
+}
+
+FormattedText.defaultProps = {
+  className: "",
+  children: null,
+}
