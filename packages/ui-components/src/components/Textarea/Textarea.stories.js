240bc9edbddc6fbcd9d5d693adb9446deba785f9
gjaskiewicz@objectivity.co.uk <gjaskiewicz@objectivity.co.uk>
2024-09-02 13:23:25 +0200

    fix(ui): fix textarea story

--- packages/ui-components/src/components/Textarea/Textarea.stories.js
+++ packages/ui-components/src/components/Textarea/Textarea.stories.js
@@ -7,7 +7,7 @@ import React from "react"
 import { Textarea } from "./index.js"
 
 export default {
-  title: "Forms/Textarea",
+  title: "Forms/Textarea (JS, deprecated)",
   component: Textarea,
   argTypes: {
     errortext: {
