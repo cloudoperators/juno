779b2cb060a736fd20b69c91c09abaa13df1ab18
Arturo Reuschenbach Puncernau <reuschenbach@gmail.com>
2024-09-05 16:01:02 +0200

    refactor(ui): migrate and adapt supernova from greenhouse-extension to juno

---
+++ apps/supernova/eslint.config.mjs
@@ -0,0 +1,22 @@
+/*
+ * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Juno contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import junoConfigs from "@cloudoperators/juno-config/eslint/juno.mjs"
+
+export default [
+  ...junoConfigs,
+  {
+    files: ["**/*.js", "**/*.mjs", "**/*.jsx"],
+    languageOptions: { sourceType: "module" },
+    rules: {
+      "no-unused-vars": "off",
+      "react/prop-types": "off",
+      "react/no-unescaped-entities": "off",
+    },
+  },
+  {
+    ignores: ["setupTests.js"],
+  },
+]
