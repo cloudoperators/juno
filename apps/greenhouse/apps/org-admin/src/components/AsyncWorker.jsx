2f011f7bb47de746bf2813908eefad7d32ca09b5
d064310 <andreas.pfau@sap.com>
2024-09-10 14:49:41 +0200

    chore(greenhouse): adds greenhouse dashboard and apps

---
+++ apps/greenhouse/apps/org-admin/src/components/AsyncWorker.jsx
@@ -0,0 +1,15 @@
+/*
+ * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import useUrlState from "../hooks/useUrlState"
+import useCommunication from "../hooks/useCommunication"
+
+const AsyncWorker = () => {
+  useUrlState()
+  useCommunication()
+  return null
+}
+
+export default AsyncWorker
