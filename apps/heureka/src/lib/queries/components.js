70aaa5515b69e2b5d435eaa798aa7be2c64e7393
d065895 <hoda.noori@sap.com>
2024-09-24 17:25:46 +0200

    fix(heureka): deletes the commented code

--- apps/heureka/src/lib/queries/components.js
+++ apps/heureka/src/lib/queries/components.js
@@ -3,62 +3,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/*import { gql } from "graphql-request"
-
+import { gql } from "graphql-request"
 // gql
 // It is there for convenience so that you can get the tooling support
 // like prettier formatting and IDE syntax highlighting.
 // You can use gql from graphql-tag if you need it for some reason too.
-export default () => gql`
-  query ($filter: ComponentFilter, $first: Int, $after: String) {
-    Components(filter: $filter, first: $first, after: $after) {
-      totalCount
-      edges {
-        node {
-          id
-          name
-          type
-          componentVersions {
-            totalCount
-            edges {
-              node {
-                id
-                version
-                issues {
-                  totalCount
-                }
-                componentInstances {
-                  totalCount
-                  edges {
-                    node {
-                      id
-                    }
-                  }
-                }
-              }
-              cursor
-            }
-          }
-        }
-        cursor
-      }
-      pageInfo {
-        hasNextPage
-        hasPreviousPage
-        isValidPage
-        pageNumber
-        nextPageAfter
-        pages {
-          after
-          isCurrent
-          pageNumber
-          pageCount
-        }
-      }
-    }
-  }
-`*/
-import { gql } from "graphql-request"
 
 // Main query for fetching Components data (excluding totalCount and pageInfo)
 export const componentsMainQuery = () => gql`
