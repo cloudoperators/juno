24517f577790c1457026b3f34ba381ab221d2213
TilmanHaupt <tilman.haupt@sap.com>
2024-09-24 16:33:03 +0200

    chore(supernova): delete useCommunication and auth store

--- apps/supernova/src/hooks/useAppStore.js
+++ apps/supernova/src/hooks/useAppStore.js
@@ -10,7 +10,6 @@ import { devtools } from "zustand/middleware"
 import createSilencesSlice from "../lib/createSilencesSlice"
 import createAlertsSlice from "../lib/createAlertsSlice"
 import createFiltersSlice from "../lib/createFiltersSlice"
-import createAuthDataSlice from "../lib/createAuthDataSlice"
 import createGlobalsSlice from "../lib/createGlobalsSlice"
 import createUserActivitySlice from "../lib/createUserActivitySlice"
 
@@ -22,7 +21,6 @@ export const StoreProvider = ({ options, children }) => {
       value={createStore(
         devtools((set, get) => ({
           ...createGlobalsSlice(set, get, options),
-          ...createAuthDataSlice(set, get),
           ...createUserActivitySlice(set, get),
           ...createAlertsSlice(set, get),
           ...createFiltersSlice(set, get, options),
@@ -48,12 +46,6 @@ export const useGlobalsApiEndpoint = () => useAppStore((state) => state.globals.
 export const useGlobalsActiveSelectedTab = () => useAppStore((state) => state.globals.activeSelectedTab)
 export const useGlobalsActions = () => useAppStore((state) => state.globals.actions)
 export const useGlobalsUser = () => useAppStore((state) => state.globals.user)
-
-// AUTH
-export const useAuthData = () => useAppStore((state) => state.auth.data)
-export const useAuthLoggedIn = () => useAppStore((state) => state.auth.loggedIn)
-export const useAuthActions = () => useAppStore((state) => state.auth.actions)
-export const useAuthUserEditable = () => useAppStore((state) => state.auth.userEditable)
 
 // UserActivity exports
 export const useUserIsActive = () => useAppStore((state) => state.userActivity.isActive)
