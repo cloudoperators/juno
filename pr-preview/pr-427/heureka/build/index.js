f8e1b2ff4687202ce887842a14318eed8b343a49
barsukov <barsukov@users.noreply.github.com>
2024-09-25 07:33:42 +0000

    Remove preview for PR 427 🛬

--- pr-preview/pr-427/heureka/build/index.js
+++
@@ -1,1 +0,0 @@
-import{a as R,b as i,i as l,j as m}from"./chunk-GIU6FERR.js";var a=R(e=>{"use strict";var u=m();e.createRoot=u.createRoot,e.hydrateRoot=u.hydrateRoot;var E});var c=i(a()),s=i(l()),t=(o,r={})=>{import("./App-AN327ONY.js").then(n=>{t.root=(0,c.createRoot)(o),t.root.render(s.default.createElement(n.default,r?.props))})},_=()=>t.root&&t.root.unmount();export{t as mount,_ as unmount};
