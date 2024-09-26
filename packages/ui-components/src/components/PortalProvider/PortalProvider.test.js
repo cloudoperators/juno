0160ea44b12a12fd3f2321d07b1d92a4a58e1e53
Franz Heidl <franz@franzheidl.de>
2024-09-26 18:09:05 +0200

    feat(ui): add test for usePortalRef hook

--- packages/ui-components/src/components/PortalProvider/PortalProvider.test.js
+++ packages/ui-components/src/components/PortalProvider/PortalProvider.test.js
@@ -1,6 +1,13 @@
 import * as React from "react"
+import { createPortal } from "react-dom"
 import { render, screen } from "@testing-library/react"
-import { PortalProvider } from "./"
+import { PortalProvider, usePortalRef } from "./"
+
+const TestComponent = () => {
+  const portalRef = usePortalRef()
+  if (!portalRef) return null
+  return createPortal(<button>Test</button>, portalRef)
+}
 
 describe("PortalProvider", () => {
   test("renders a portal root container", async () => {
@@ -8,7 +15,7 @@ describe("PortalProvider", () => {
     expect(document.getElementById("juno-portal-root")).toBeInTheDocument()
   })
 
-  test("renders children wrapped into a .juno-portal container into a portal", async () => {
+  test("renders children wrapped into a .juno-portal container into a portal using PortalProvider.Portal", async () => {
     render(
       <PortalProvider>
         <PortalProvider.Portal>
@@ -18,7 +25,26 @@ describe("PortalProvider", () => {
     )
     expect(document.getElementById("juno-portal-root")).toBeInTheDocument()
     expect(screen.getByRole("button")).toBeInTheDocument()
-    const parentElement = screen.getByRole("button").parentElement
-    expect(parentElement).toHaveClass("juno-portal")
+    const parentEl = screen.getByRole("button").parentElement
+    expect(parentEl).toHaveClass("juno-portal")
+    expect(parentEl.parentElement).toHaveClass("juno-portal-root")
   })
+
+  test("renders children wrapped into a .juno-portal-container into a portal using usePortalRef", async () => {
+    render(
+      <PortalProvider>
+        <TestComponent />
+      </PortalProvider>
+    )
+    expect(document.getElementById("juno-portal-root")).toBeInTheDocument()
+    expect(screen.getByRole("button")).toBeInTheDocument()
+    const parentEl = screen.getByRole("button").parentElement
+    expect(parentEl).toHaveClass("juno-portal")
+    expect(parentEl.parentElement).toHaveClass("juno-portal-root")
+  })
+
+  // ("removes the parent element when the portalled content unmounts")
+  // ("renders individual .juno-portal containers for multiple portalled components")
+  // ("render parent using usePortalRef")
+  // ("keep parent ref in context")
 })
