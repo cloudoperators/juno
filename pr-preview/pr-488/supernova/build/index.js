4fdf7567c054ef7d24ce6b0d86b84b2fdb264612
TilmanHaupt <TilmanHaupt@users.noreply.github.com>
2024-10-01 13:26:15 +0000

    Deploy preview for PR 488 🛫

---
+++ pr-preview/pr-488/supernova/build/index.js
@@ -0,0 +1,1 @@
+import{a as R,b as i,c as l,d as m}from"./chunk-34Z3OECK.js";var a=R(e=>{"use strict";var u=m();e.createRoot=u.createRoot,e.hydrateRoot=u.hydrateRoot;var E});var c=i(a()),s=i(l()),t=(o,r={})=>{import("./App-GLSNXZBT.js").then(n=>{t.root=(0,c.createRoot)(o),t.root.render(s.default.createElement(n.default,r?.props))})},_=()=>t.root&&t.root.unmount();export{t as mount,_ as unmount};
