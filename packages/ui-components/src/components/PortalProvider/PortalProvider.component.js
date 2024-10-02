beb4b45ebfd0593f7dcf5e78182c4599ac183947
Franz Heidl <franz@franzheidl.de>
2024-10-02 08:38:21 +0200

    feat(ui): try to remove license headers again to push

--- packages/ui-components/src/components/PortalProvider/PortalProvider.component.js
+++ packages/ui-components/src/components/PortalProvider/PortalProvider.component.js
@@ -1,8 +1,3 @@
-/*
- * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Juno contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
 import React, { createContext, useRef, useContext, useEffect, useState } from "react"
 import PropTypes from "prop-types"
 import { createPortal } from "react-dom"
