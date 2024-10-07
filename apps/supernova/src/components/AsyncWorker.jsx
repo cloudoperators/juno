d243b411575d0418d7757b34ce6f5354ce7e54f5
Tilman <147151040+TilmanHaupt@users.noreply.github.com>
2024-10-07 14:49:08 +0200

    feat(supernova): remove auth and pass initial filters per appProp (#488)
    * chore(supernova): document desired state initalFilters
    
    * feat(supernova): added initalFilters prop
    
    * feat(supernova): setActiveFilters as initialFilters if there are non in URL
    
    * feat(supernova): add user prop and initialFilter to docker
    
    * chore(supernova): delete useCommunication and auth store
    
    * chore(supernova): bump version
    
    * refactor(supernova): use username with a string instead of an object
    
    * test(supernova): tested parse initialFilters
    
    * chore(greenhouse): added secretProps to gitIgnore
    
    * fix(supernova): change array to obj apps/supernova/secretProps_sample.js
    
    Co-authored-by: Arturo Reuschenbach <reuschenbach@gmail.com>
    
    ---------
    
    Co-authored-by: Arturo Reuschenbach <reuschenbach@gmail.com>

--- apps/supernova/src/components/AsyncWorker.jsx
+++ apps/supernova/src/components/AsyncWorker.jsx
@@ -3,12 +3,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import useCommunication from "../hooks/useCommunication"
 import useAlertmanagerAPI from "../hooks/useAlertmanagerAPI"
 import useUrlState from "../hooks/useUrlState"
 
 const AsyncWorker = ({ endpoint }) => {
-  useCommunication()
   useAlertmanagerAPI(endpoint)
   useUrlState()
   return null
