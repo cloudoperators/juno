f8f9a91aff9933ec6bcac3252a0e2137db4b0e5f
edda <edda@users.noreply.github.com>
2024-10-09 14:28:40 +0000

    Remove preview for PR 500 🛬

--- pr-preview/pr-500/supernova/build/index.js
+++
@@ -1,1 +0,0 @@
-import{a as R,b as i,c as l,d as m}from"./chunk-34Z3OECK.js";var a=R(e=>{"use strict";var u=m();e.createRoot=u.createRoot,e.hydrateRoot=u.hydrateRoot;var E});var c=i(a()),s=i(l()),t=(o,r={})=>{import("./App-X6TSDUH4.js").then(n=>{t.root=(0,c.createRoot)(o),t.root.render(s.default.createElement(n.default,r?.props))})},_=()=>t.root&&t.root.unmount();export{t as mount,_ as unmount};
