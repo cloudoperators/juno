ea0bcee8b7faa980001318a982747ec85226b751
I531348 <guoda.puidokaite@sap.com>
2024-10-10 15:28:22 +0200

    chore(ui): organise tests and add zero/negative timeout test

--- packages/ui-components/src/components/Message/Message.test.tsx
+++ packages/ui-components/src/components/Message/Message.test.tsx
@@ -4,133 +4,38 @@
  */
 
 import * as React from "react"
-import { describe, expect, test, vi } from "vitest"
-
 import { render, screen, waitFor } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 
+import { MessageType } from "./Message.types"
 import { Message } from "./index"
 
 const mockOnDismiss = vi.fn()
 
-describe("Message", () => {
-  test("renders a Message", () => {
+describe("Message Component", () => {
+  // TEST BASIC RENDERING
+
+  test("renders the Message component", () => {
     render(<Message data-testid="my-message" />)
     expect(screen.getByTestId("my-message")).toBeInTheDocument()
   })
 
-  test("renders an info Message by default if no variant passed", () => {
-    render(<Message data-testid="my-message" />)
-    expect(screen.getByTestId("my-message")).toHaveClass("juno-message-info")
-    expect(screen.getByRole("img")).toHaveClass("jn-text-theme-info")
-  })
-
-  test("renders a Message where the icon is not allowed to shrink", () => {
-    render(<Message data-testid="my-message" />)
-    expect(screen.getByTestId("my-message")).toHaveClass("juno-message-info")
-    expect(screen.getByRole("img")).toHaveClass("jn-shrink-0")
-  })
-
-  test("renders a Message that can be dismissed", async () => {
-    render(<Message data-testid="my-message" dismissible={true} />)
-    // not checking specifically for the close button here. So if there is more than one button in the message this test will fail
-    // The reason is that it's hard to find specifically the close button because any classes added to a clickable Icon go to the image element, not the surrounding button
-    const user = userEvent.setup()
-    expect(screen.getByRole("button")).toBeInTheDocument()
-    await waitFor(async () => {
-      await user.click(screen.getByRole("button"))
-      expect(screen.queryByTestId("my-message")).not.toBeInTheDocument()
-    })
-  })
-
-  test("fires onDismiss handler when Message is manually dismissed", async () => {
-    const handleDismiss = vi.fn()
-    render(<Message data-testid="my-message" dismissible={true} onDismiss={handleDismiss} />)
-    // not checking specifically for the close button here. So if there is more than one button in the message this test will fail
-    // The reason is that it's hard to find specifically the close button because any classes added to a clickable Icon go to the image element, not the surrounding button
-    const user = userEvent.setup()
-    expect(screen.getByRole("button")).toBeInTheDocument()
-    const dismissButton = screen.getByRole("button")
-    await waitFor(async () => {
-      await user.click(dismissButton)
-      expect(handleDismiss).toHaveBeenCalledTimes(1)
-    })
-  })
-
-  test("renders a Message without dismiss button by default", () => {
-    render(<Message data-testid="my-message" />)
-    expect(screen.queryByRole("button")).not.toBeInTheDocument()
-  })
-
-  test("renders a Message without dismiss button", () => {
-    render(<Message data-testid="my-message" dismissible={false} />)
-    expect(screen.queryByRole("button")).not.toBeInTheDocument()
-  })
-
-  test("renders a Message that will be automatically dismissed", async () => {
-    render(<Message data-testid="my-message" autoDismiss={true} autoDismissTimeout={500} />)
-    await waitFor(
-      () => {
-        expect(screen.queryByTestId("my-message")).not.toBeInTheDocument()
-      },
-      { timeout: 1000 }
-    )
-  })
-
-  test("fires onDismiss handler when Message is automatically dismissed", async () => {
-    render(<Message data-testid="my-message" autoDismiss={true} autoDismissTimeout={500} onDismiss={mockOnDismiss} />)
-    await waitFor(() => {
-      expect(screen.queryByTestId("my-message")).not.toBeInTheDocument()
-      expect(mockOnDismiss).toHaveBeenCalledTimes(1)
-    })
-  })
-
-  test("renders an info Message as passed", () => {
-    render(<Message data-testid="my-message" variant="info" />)
-    expect(screen.getByTestId("my-message")).toHaveClass("juno-message-info")
-    expect(screen.getByRole("img")).toHaveClass("jn-text-theme-info")
-  })
-
-  test("renders a warning Message as passed", () => {
-    render(<Message data-testid="my-message" variant="warning" />)
-    expect(screen.getByTestId("my-message")).toHaveClass("juno-message-warning")
-    expect(screen.getByRole("img")).toHaveClass("jn-text-theme-warning")
-  })
-
-  test("renders a danger message as passed", () => {
-    render(<Message data-testid="my-message" variant="danger" />)
-    expect(screen.getByTestId("my-message")).toHaveClass("juno-message-danger")
-    expect(screen.getByRole("img")).toHaveClass("jn-text-theme-danger")
-  })
-
-  test("renders an error Message as passed", () => {
-    render(<Message data-testid="my-message" variant="error" />)
-    expect(screen.getByTestId("my-message")).toHaveClass("juno-message-error")
-    expect(screen.getByRole("img")).toHaveClass("jn-text-theme-error")
-  })
-
-  test("renders a success Message as passed", () => {
-    render(<Message data-testid="my-message" variant="success" />)
-    expect(screen.getByTestId("my-message")).toHaveClass("juno-message-success")
-    expect(screen.getByRole("img")).toHaveClass("jn-text-theme-success")
-  })
-
-  test("renders a title as passed", () => {
+  test("displays the provided title", () => {
     render(<Message data-testid="my-message" title="My Message Heading" />)
     expect(screen.getByTestId("my-message")).toHaveTextContent("My Message Heading")
   })
 
-  test("renders a text as passed", () => {
+  test("displays the provided text", () => {
     render(<Message data-testid="my-message" text="My Message text goes here." />)
     expect(screen.getByTestId("my-message")).toHaveTextContent("My Message text goes here.")
   })
 
-  test("renders text as passed as children", () => {
+  test("renders text from children, if provided", () => {
     render(<Message data-testid="my-message">{"My Message children text goes here!"}</Message>)
     expect(screen.getByTestId("my-message")).toHaveTextContent("My Message children text goes here!")
   })
 
-  test("renders text as passed as children if both children and 'text' prop were passed", () => {
+  test("prioritizes children over 'text' prop when both are provided", () => {
     render(
       <Message data-testid="my-message" text="I should not be here.">
         {"My Message children text goes here!"}
@@ -139,14 +44,104 @@ describe("Message", () => {
     expect(screen.getByTestId("my-message")).toHaveTextContent("My Message children text goes here!")
   })
 
-  test("renders custom classNames as passed", () => {
+  test("applies custom classNames, if provided", () => {
     render(<Message data-testid="my-message" className="my-custom-class" />)
     expect(screen.getByTestId("my-message")).toHaveClass("my-custom-class")
   })
 
-  test("renders all props as passed", () => {
+  test("renders the component with all provided props", () => {
     render(<Message data-testid="my-message" title="My shiny little Message" />)
-    expect(screen.getByText("My shiny little Message")).toBeInTheDocument()
+    expect(screen.getByTestId("my-message")).toHaveTextContent("My shiny little Message")
+  })
+
+  // TEST VARIANTS
+
+  test("renders an info Message by default if no variant is provided", () => {
+    render(<Message data-testid="my-message" />)
+    expect(screen.getByTestId("my-message")).toHaveClass("juno-message-info")
+    expect(screen.getByRole("img")).toHaveClass("jn-text-theme-info")
+  })
+
+  test.each`
+    variant      | cssClass                  | iconClass
+    ${"info"}    | ${"juno-message-info"}    | ${"jn-text-theme-info"}
+    ${"warning"} | ${"juno-message-warning"} | ${"jn-text-theme-warning"}
+    ${"danger"}  | ${"juno-message-danger"}  | ${"jn-text-theme-danger"}
+    ${"error"}   | ${"juno-message-error"}   | ${"jn-text-theme-error"}
+    ${"success"} | ${"juno-message-success"} | ${"jn-text-theme-success"}
+  `(
+    "renders a $variant Message with the correct classes",
+    ({ variant, cssClass, iconClass }: { variant: MessageType; cssClass: string; iconClass: string }) => {
+      render(<Message data-testid="my-message" variant={variant} />)
+      expect(screen.getByTestId("my-message")).toHaveClass(cssClass)
+      expect(screen.getByRole("img")).toHaveClass(iconClass)
+    }
+  )
+
+  test("ensures the icon cannot shrink", () => {
+    render(<Message data-testid="my-message" />)
+    expect(screen.getByTestId("my-message")).toHaveClass("juno-message-info")
+    expect(screen.getByRole("img")).toHaveClass("jn-shrink-0")
+  })
+
+  // TEST DISMISSABLE BEHAVIOUR
+
+  test("renders a Message without a dismiss button by default", () => {
+    render(<Message data-testid="my-message" />)
+    expect(screen.queryByRole("button")).not.toBeInTheDocument()
+  })
+
+  test("renders a dismissible Message when 'dismissible' is true", async () => {
+    render(<Message data-testid="my-message" dismissible={true} />)
+
+    // No specific check for the close button to avoid test failures if there are multiple buttons.
+    const user = userEvent.setup()
+    expect(screen.getByRole("button")).toBeInTheDocument()
+
+    // Ensure the message is dismissed after clicking the button
+    await waitFor(async () => {
+      await user.click(screen.getByRole("button"))
+      expect(screen.queryByTestId("my-message")).not.toBeInTheDocument()
+    })
+  })
+
+  test("renders a Message without a dismiss button when 'dismissible' is false", () => {
+    render(<Message data-testid="my-message" dismissible={false} />)
+    expect(screen.queryByRole("button")).not.toBeInTheDocument()
+  })
+
+  test("calls 'onDismiss' handler when the Message is manually dismissed", async () => {
+    render(<Message data-testid="my-message" dismissible={true} onDismiss={mockOnDismiss} />)
+
+    // No specific check for the close button to avoid test failures if there are multiple buttons.
+    const user = userEvent.setup()
+    expect(screen.getByRole("button")).toBeInTheDocument()
+    const dismissButton = screen.getByRole("button")
+
+    // Ensure onDismiss is called when the message is dismissed
+    await waitFor(async () => {
+      await user.click(dismissButton)
+      expect(mockOnDismiss).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  // TEST AUTO-DISMISS BEHAVIOUR
+
+  test("automatically dismisses the Message after the provided timeout", async () => {
+    render(<Message data-testid="my-message" autoDismiss={true} autoDismissTimeout={500} />)
+    expect(screen.getByTestId("my-message")).toBeInTheDocument()
+
+    // Wait to check that the message auto-dismissed
+    await new Promise((resolve) => setTimeout(resolve, 501))
+    expect(screen.queryByTestId("my-message")).not.toBeInTheDocument()
+  })
+
+  test("calls 'onDismiss' handler when the Message is automatically dismissed", async () => {
+    render(<Message data-testid="my-message" autoDismiss={true} autoDismissTimeout={500} onDismiss={mockOnDismiss} />)
+
+    await waitFor(() => {
+      expect(mockOnDismiss).toHaveBeenCalledTimes(1)
+    })
   })
 
   test("does not auto-dismiss when 'autoDismissTimeout' is set to zero", async () => {
