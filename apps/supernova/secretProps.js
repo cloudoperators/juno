5b9f9ab21ab8d2c9f83e856adc97ba2be9d49037
d064310 <andreas.pfau@sap.com>
2024-10-15 15:13:52 +0200

    chore(supernova): remove obsolete files

--- apps/supernova/secretProps.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  endpoint: "https://alertmanager-internal.scaleout.eu-de-1.cloud.sap/api/v2",
-  filterLabels: ["support_group"],
-  theme: "theme-light",
-  filterLabels: ["support_group", "service", "severity"],
-  embedded: false,
-}
