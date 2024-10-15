f7c8ef0bacd95997e53e939c77ae56d6a3fa9e84
andypf <andypf@users.noreply.github.com>
2024-10-15 09:38:09 +0000

    Deploy preview for PR 517 🛫

---
+++ pr-preview/pr-517/greenhouse/build/apps/cloudoperators/juno-app-doop/build/index.js
@@ -0,0 +1,5 @@
+import { m as u, u as n } from "./index-risb2qg-.mjs";
+export {
+  u as mount,
+  n as unmount
+};
---
+++ pr-preview/pr-517/greenhouse/build/apps/cloudoperators/juno-app-heureka/build/index.js
@@ -0,0 +1,5 @@
+import { m as u, u as n } from "./index-CF7pl4VP.mjs";
+export {
+  u as mount,
+  n as unmount
+};
