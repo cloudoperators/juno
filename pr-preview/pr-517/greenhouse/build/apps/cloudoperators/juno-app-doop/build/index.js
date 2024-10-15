481ba53b2012bcfb5c9f253fea514f6493b1b9b1
andypf <andypf@users.noreply.github.com>
2024-10-15 09:40:57 +0000

    Remove preview for PR 517 🛬

--- pr-preview/pr-517/greenhouse/build/apps/cloudoperators/juno-app-doop/build/index.js
+++
@@ -1,5 +0,0 @@
-import { m as u, u as n } from "./index-risb2qg-.mjs";
-export {
-  u as mount,
-  n as unmount
-};
--- pr-preview/pr-517/greenhouse/build/apps/cloudoperators/juno-app-heureka/build/index.js
+++
@@ -1,5 +0,0 @@
-import { m as u, u as n } from "./index-CF7pl4VP.mjs";
-export {
-  u as mount,
-  n as unmount
-};
