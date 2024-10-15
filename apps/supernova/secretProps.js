019182150202f38b47f95bebc771a6c683aebdae
Wowa <vladimir.barsukov@sap.com>
2024-10-15 11:57:09 +0200

    chore(npm): delete url-state-router package

---
+++ apps/supernova/secretProps.js
@@ -0,0 +1,7 @@
+module.exports = {
+  endpoint: "https://alertmanager-internal.scaleout.eu-de-1.cloud.sap/api/v2",
+  filterLabels: ["support_group"],
+  theme: "theme-light",
+  filterLabels: ["support_group", "service", "severity"],
+  embedded: false,
+}
