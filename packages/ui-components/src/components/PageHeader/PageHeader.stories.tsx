f307966ada303ecd6b71402bb1e5f9cf5297d86b
Franz Heidl <franz@franzheidl.de>
2024-10-16 16:15:28 +0200

    fix(ui): fix broken pageheader layout (#506, #473) (#510)
    * fix(ui): fix broken PageHeader
    
    * fix(ui): fix story template
    
    * fix(ui): remove obsolete
    
    * fix(ui): fix story es-lint error
    
    remove explicit destructuring of children, leave type-checking to TypeScript
    
    * Create blue-news-wash.md
    
    * fix(ui): rename Pageheader story
    
    * fix(ui): always render heading div
    
    * fix(ui): prevent rendering null, false or undefined as page header title at all cost
    
    Fixes #506 , Fixes #473
    ---------
    
    Co-authored-by: Wowa Barsukov <pro.barsukov@gmail.com>

--- packages/ui-components/src/components/PageHeader/PageHeader.stories.tsx
+++ packages/ui-components/src/components/PageHeader/PageHeader.stories.tsx
@@ -12,10 +12,20 @@ import CustomLogoLandscapePNG from "./custom-logo-placeholders/custom-logo-lands
 import CustomLogoPortraitPNG from "./custom-logo-placeholders/custom-logo-portrait.png"
 import CustomLogoSquarePNG from "./custom-logo-placeholders/custom-logo-square.png"
 import { PageHeader, PageHeaderProps } from "./index"
+import { Button } from "../Button/"
 
 const CustomLogoImagePNGSquare = () => <img src={CustomLogoSquarePNG} alt="Custom Logo Square" />
 const CustomLogoImagePNGLandscape = () => <img src={CustomLogoLandscapePNG} alt="Custom Logo Landscape" />
 const CustomLogoImagePNGPortrait = () => <img src={CustomLogoPortraitPNG} alt="Custom Logo Portrait" />
+const HeaderOptions = () => {
+  const userNameStyles = { marginRight: "1rem" }
+  return (
+    <>
+      <span style={userNameStyles}>Jane Doe</span>
+      <Button size="small">Log Out</Button>
+    </>
+  )
+}
 
 type Story = StoryObj<PageHeaderProps>
 
@@ -34,23 +44,18 @@ const meta: Meta<PageHeaderProps> = {
 }
 export default meta
 
-const Template: StoryFn<PageHeaderProps> = (args) => <PageHeader {...args} />
+const Template: StoryFn<PageHeaderProps> = (args) => {
+  return <PageHeader {...args} />
+}
 
-export const Default = Template.bind({
+export const Default: Story = {
   render: Template,
-  parameters: {
-    docs: {
-      description: {
-        story: "The PageHeader component renders a header at the top of the website. Place as first child of AppBody.",
-      },
-    },
-  },
 
   args: {
     heading: "My App",
     logo: true,
   },
-})
+}
 
 export const WithHeading: Story = {
   parameters: {
@@ -64,10 +69,24 @@ export const WithHeading: Story = {
   },
 }
 
+export const WithHeadingAndChildren: Story = {
+  args: {
+    heading: "My Awesome App",
+    children: <HeaderOptions />,
+  },
+}
+
+export const NoHeadingWithChildren: Story = {
+  args: {
+    children: <HeaderOptions />,
+  },
+}
+
 export const NoLogo: Story = {
   args: {
     logo: false,
     heading: "My Awesome App",
+    children: <HeaderOptions />,
   },
 }
 
@@ -75,6 +94,7 @@ export const WithCustomLogoSquareInline: Story = {
   args: {
     logo: <CustomLogoSquare alt={""} />,
     heading: "My Awesome App",
+    children: <HeaderOptions />,
   },
 }
 
@@ -82,6 +102,7 @@ export const WithCustomLogoLandscapeInline: Story = {
   args: {
     logo: <CustomLogoLandscape alt={""} />,
     heading: "My Awesome App",
+    children: <HeaderOptions />,
   },
 }
 
@@ -96,6 +117,7 @@ export const WithCustomLogoPNGSquare: Story = {
   args: {
     logo: <CustomLogoImagePNGSquare />,
     heading: "My Awesome App",
+    children: <HeaderOptions />,
   },
 }
 
@@ -103,6 +125,7 @@ export const WithCustomLogoPNGLandscape: Story = {
   args: {
     logo: <CustomLogoImagePNGLandscape />,
     heading: "My Awesome App",
+    children: <HeaderOptions />,
   },
 }
 
@@ -110,5 +133,6 @@ export const WithCustomLogoPNGPortrait: Story = {
   args: {
     logo: <CustomLogoImagePNGPortrait />,
     heading: "My Awesome App",
+    children: <HeaderOptions />,
   },
 }
