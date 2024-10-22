0ffbf612122e53234123ce65824ee18438dd55ba
ArtieReus <ArtieReus@users.noreply.github.com>
2024-10-22 09:41:00 +0000

    Remove preview for PR 543 🛬

--- pr-preview/pr-543/example/appProps.template.js
+++
@@ -1,8 +0,0 @@
-// please copy this file to appProps.json (note: .json not .js)
-// and add props for your app
-// example:
-// {
-//   "theme": "theme-dark",
-//   "endpoint": "https://example.com",
-//   "embedded": "true"
-// }
