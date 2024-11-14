962ca3ca86d88b21f61af3fa8dcc01f669ebbe1c
andypf <andypf@users.noreply.github.com>
2024-11-14 13:56:16 +0000

    Deploy preview for PR 600 🛫

---
+++ pr-preview/pr-600/greenhouse/assets/Container.component-B8M3UgGG-z1zvyKLt.js
@@ -0,0 +1,53 @@
+import{_ as h,a as f}from"./Shell-BApd2zbU.js";import{R as r,r as a}from"./index-imqT1cba.js";import{A as z,M as y,a as C,C as b}from"./App-HCPH7r3W.js";var x,_,Z,w,N,S,O,M,F,P,L,A,B,H,I,R,k,D,U,J=["title","titleId"];function g(){return g=Object.assign?Object.assign.bind():function(e){for(var l,n=1;n<arguments.length;n++)for(var t in l=arguments[n],l)({}).hasOwnProperty.call(l,t)&&(e[t]=l[t]);return e},g.apply(null,arguments)}function T(e,l){if(e==null)return{};var n,t,i=q(e,l);if(Object.getOwnPropertySymbols){var o=Object.getOwnPropertySymbols(e);for(t=0;t<o.length;t++)n=o[t],l.includes(n)||{}.propertyIsEnumerable.call(e,n)&&(i[n]=e[n])}return i}function q(e,l){if(e==null)return{};var n={};for(var t in e)if({}.hasOwnProperty.call(e,t)){if(l.includes(t))continue;n[t]=e[t]}return n}var K=function(e){var l=e.title,n=e.titleId,t=T(e,J);return a.createElement("svg",g({xmlns:"http://www.w3.org/2000/svg",width:49,height:41,fill:"none",viewBox:"0 0 49 41",alt:"Juno UI","aria-labelledby":n},t),l?a.createElement("title",{id:n},l):null,x||(x=a.createElement("path",{fill:"#0FA7B4",d:"M11.093 16 4 20.095l7.093 4.105 7.093-4.105L11.093 16Z"})),_||(_=a.createElement("path",{fill:"#000",fillRule:"evenodd",d:"m11.093 16.346 6.793 3.922v7.844l-6.793 3.922L4.3 28.112v-7.844l6.793-3.922ZM4 20.095 11.093 16l7.093 4.095v8.19l-7.093 4.095L4 28.285v-8.19Z",clipRule:"evenodd"})),Z||(Z=a.createElement("path",{fill:"#05646D",d:"M11.093 24.19v8.19l7.093-4.095v-8.19l-7.093 4.095Z"})),w||(w=a.createElement("path",{fill:"#05646D",d:"M11.093 24.19v8.19l7.093-4.095v-8.19l-7.093 4.095Z"})),N||(N=a.createElement("path",{fill:"#098590",d:"M11.093 24.19v8.19L4 28.285v-8.19l7.093 4.095Z"})),S||(S=a.createElement("path",{fill:"#15D0E0",fillRule:"evenodd",d:"M18.186 20.095 11.093 16 4 20.095v8.19l7.093 4.095 7.093-4.095v-8.19Zm-7.093-3.749 6.493 3.75-6.493 3.757L4.6 20.095l6.494-3.749Zm-.3 8.017L4.3 20.614v7.498l6.493 3.748v-7.497Zm7.093-3.749-6.493 3.749v7.497l6.492-3.748v-7.498Z",clipRule:"evenodd"})),O||(O=a.createElement("path",{fill:"#098590",d:"M24.402 36.76V20.38l-14.185-8.19v8.226l7.065 4.079v8.155l7.12 4.11Z"})),M||(M=a.createElement("path",{fill:"#0FA7B4",d:"m38.587 20.388-7.08 4.087v-8.19l7.08-4.087v-.008l-14.185 8.19v16.38l14.185-8.19v-8.182Z"})),F||(F=a.createElement("path",{fill:"#05646D",d:"M31.591 16.337v.064-8.254l-7.093 4.095 7.093 4.095Z"})),P||(P=a.createElement("path",{fill:"#02454B",d:"M31.51 16.285v8.19l7.092-4.095-7.093-4.095Z"})),L||(L=a.createElement("path",{fill:"#15D0E0",d:"M37.092 8 30 12.095v8.19l7.092 4.095 7.093-4.095v-8.19L37.092 8Z"})),A||(A=a.createElement("path",{fill:"#0FA7B4",d:"M37.092 16.19v8.19l7.093-4.095v-8.19l-7.093 4.095Z"})),B||(B=a.createElement("path",{fill:"#098590",d:"M37.092 16.19v8.19L30 20.285v-8.19l7.092 4.095Z"})),H||(H=a.createElement("path",{fill:"url(#a)",fillRule:"evenodd",d:"M44.185 12.095 37.092 8 30 12.095v8.19l7.092 4.095 7.093-4.095v-8.19Zm-7.093-3.749 6.493 3.75-6.493 3.757-6.493-3.758 6.493-3.749Zm-.3 8.017-6.493-3.749v7.497l6.493 3.75v-7.498Zm7.093-3.749-6.493 3.749v7.497l6.493-3.748v-7.498Z",clipRule:"evenodd"})),I||(I=a.createElement("path",{fill:"#0FA7B4",d:"m31.502 32.65-7.1 4.15V20.4l7.1-4.11v16.36Z"})),R||(R=a.createElement("path",{fill:"#15D0E0",d:"m24.502 12.25 7 4.04-7.1 4.11-14.2-8.2 14.2-8.2 7.2 4.15-7.1 4.1Z"})),k||(k=a.createElement("path",{fill:"url(#b)",fillRule:"evenodd",d:"M31.602 8.15 24.402 4l-14.184 8.191h-.001v.001l-.014.008.013.008v8.147l7.066 4.08v8.19l7.12 4.135V28.7v8.1l5.52-3.226 8.665-5.003v-5.054l-.3.173v4.707l-6.77 3.91.033-7.51 3.133-1.808-.3-.173-2.831 1.634v-.011h-.044v-8.154l1.19-.687v-.008l-1.203.695-6.773-3.908.006-.01-.214-.124 7.088-4.093Zm-6.9 28.127v-7.576l6.506-3.717v.011l.042-.024-.034 7.509-1.446.835-5.068 2.962Zm0-7.921 6.506-3.718v-7.84l-6.506 3.756v7.802Zm6.3-20.206-6.787 3.92-6.577-3.817 6.764-3.907 6.6 3.804Zm-7.096 4.087-6.568-3.811-6.535 3.774 6.199 3.58 6.904-3.543Zm.294.185-6.889 3.537 7.09 4.095 6.501-3.764-6.702-3.868Zm-6.618 20.055V24.26l-7.066-4.08v-7.47l13.586 7.844V36.24l-6.52-3.764Z",clipRule:"evenodd"})),D||(D=a.createElement("path",{fill:"url(#c)",d:"m34.376 14.622.007.004 4.204-2.428v-.008l-4.21 2.432Z"})),U||(U=a.createElement("defs",null,a.createElement("linearGradient",{id:"a",x1:24.093,x2:23.823,y1:36.799,y2:3.998,gradientUnits:"userSpaceOnUse"},a.createElement("stop",{stopColor:"#05646D"}),a.createElement("stop",{offset:.613,stopColor:"#15D0E0"}),a.createElement("stop",{offset:1,stopColor:"#098590"})),a.createElement("linearGradient",{id:"b",x1:26.402,x2:15.344,y1:-1,y2:32.206,gradientUnits:"userSpaceOnUse"},a.createElement("stop",{stopColor:"#127E88"}),a.createElement("stop",{offset:.539,stopColor:"#44F0FF"}),a.createElement("stop",{offset:1,stopColor:"#0B717A"})),a.createElement("linearGradient",{id:"c",x1:26.402,x2:15.344,y1:-1,y2:32.206,gradientUnits:"userSpaceOnUse"},a.createElement("stop",{stopColor:"#127E88"}),a.createElement("stop",{offset:.539,stopColor:"#44F0FF"}),a.createElement("stop",{offset:1,stopColor:"#0B717A"})))))},Q=`
+  jn-min-h-[3.25rem]
+  jn-bg-juno-grey-blue-11
+  jn-sticky
+  jn-top-0
+  jn-px-6
+  jn-py-3
+  jn-z-50
+`,X=`
+  jn-grid
+  jn-grid-cols-[minmax(0,max-content),1fr]
+  jn-gap-3
+  jn-h-7
+  jn-w-full
+  jn-items-center
+`,Y=`
+  jn-h-7
+  jn-max-w-xs
+  [&>*]:jn-w-min
+  [&>*]:jn-max-w-xs
+  [&>*]:jn-h-7
+  [&>*]:jn-object-contain
+`,ee=`
+  jn-grid
+  jn-grid-cols-[1fr,minmax(0,max-content)]
+`,ne=`
+  jn-flex
+  jn-flex-row
+  jn-items-center
+`,te=`
+  jn-text-lg
+  jn-text-theme-high
+`,W=function(e){var l=e.heading,n=l===void 0?null:l,t=e.className,i=t===void 0?"":t,o=e.children,p=o===void 0?null:o,d=e.logo,c=d===void 0?void 0:d,s=e.onClick,u=h(e,["heading","className","children","logo","onClick"]);return r.createElement("div",f({className:"juno-pageheader theme-dark ".concat(Q," ").concat(i),role:"banner"},u),r.createElement("div",{className:"juno-pageheader-inner ".concat(X)},r.createElement("div",{className:"juno-pageheader-logo-container ".concat(Y)},typeof c=="function"||r.isValidElement(c)&&c||(c===!0||c===void 0)&&r.createElement(K,{"data-testid":"default-logo",alt:""})),r.createElement("div",{className:"juno-pageheader-content-container ".concat(ee)},r.createElement("div",{className:"juno-pageheader-heading ".concat(te," ").concat(typeof s=="function"?"jn-cursor-pointer":""),onClick:s},n&&n),r.createElement("div",{className:"juno-pageheader-options ".concat(ne)},p))))},V,le=["title","titleId"];function j(){return j=Object.assign?Object.assign.bind():function(e){for(var l,n=1;n<arguments.length;n++)for(var t in l=arguments[n],l)({}).hasOwnProperty.call(l,t)&&(e[t]=l[t]);return e},j.apply(null,arguments)}function ae(e,l){if(e==null)return{};var n,t,i=re(e,l);if(Object.getOwnPropertySymbols){var o=Object.getOwnPropertySymbols(e);for(t=0;t<o.length;t++)n=o[t],l.includes(n)||{}.propertyIsEnumerable.call(e,n)&&(i[n]=e[n])}return i}function re(e,l){if(e==null)return{};var n={};for(var t in e)if({}.hasOwnProperty.call(e,t)){if(l.includes(t))continue;n[t]=e[t]}return n}var oe=function(e){var l=e.title,n=e.titleId,t=ae(e,le);return a.createElement("svg",j({width:59,height:42,viewBox:"0 0 59 42",fill:"none",xmlns:"http://www.w3.org/2000/svg","aria-labelledby":n},t),l?a.createElement("title",{id:n},l):null,V||(V=a.createElement("path",{d:"M59.3825 11.1608C59.3545 11.1402 59.3267 11.1209 59.299 11.1029C56.8161 9.48915 53.8572 8.55235 50.6806 8.55235C49.5105 8.55235 48.3701 8.67944 47.272 8.92064C44.3685 3.66326 38.8166 0.108856 32.4447 0.108856C23.4894 0.108856 16.1537 7.12988 15.514 16.032L15.3487 16.0309L15.1066 16.0329C7.13468 16.1633 0.712036 22.728 0.712036 30.807C0.712036 35.0932 2.69502 38.9531 5.7492 41.6521H10.3029C6.42919 39.7347 3.75866 35.6852 3.75866 30.9998C3.75866 24.4639 8.95513 19.1655 15.3653 19.1655C16.2819 19.1655 17.1736 19.2738 18.0286 19.4792C17.9603 18.8925 17.9251 18.2952 17.9251 17.6896C17.9251 9.4705 24.4057 2.80763 32.4 2.80763C38.5297 2.80763 43.7695 6.72481 45.8801 12.2573C47.317 11.7139 48.8714 11.417 50.4937 11.417C53.699 11.417 56.6389 12.5758 58.9323 14.505C59.0819 14.6309 59.2321 14.8185 59.3825 15.0614V11.1608Z",fill:"#F0AB00"})))},ie=`
+  jn-flex
+  jn-shrink-0
+  jn-grow-0
+  jn-basis-auto
+  jn-relative
+  jn-bg-theme-global-bg
+  jn-min-h-[3.25rem]
+  jn-pl-6
+  jn-pr-24
+  jn-py-5
+  jn-z-50
+`,ce=`
+  jn-h-[2.625rem]
+  jn-absolute
+  jn-right-0
+  jn-bottom-0
+`,$=function(e){var l=e.className,n=l===void 0?"":l,t=e.children,i=t===void 0?null:t,o=h(e,["className","children"]);return r.createElement("div",f({className:"juno-pagefooter ".concat(ie," ").concat(n),role:"contentinfo"},o),i,r.createElement(oe,{className:ce,alt:"cloud shape"}))},ve=function(e){var l=e.children,n=e.className,t=n===void 0?"":n,i=e.contentHeading,o=i===void 0?"":i,p=e.embedded,d=e.fullWidthContent,c=e.pageHeader,s=c===void 0?r.createElement(W,null):c,u=e.pageFooter,E=u===void 0?r.createElement($,null):u,m=e.sideNavigation,v=e.topNavigation,G=h(e,["children","className","contentHeading","embedded","fullWidthContent","pageHeader","pageFooter","sideNavigation","topNavigation"]);return o&&o.length&&console.warn("AppShell: The contentHeading prop is obsolete and will be removed in a future version. In order to render a content heading, use a ContentHeading element as a child in your main content."),r.createElement(z,f({className:t},G),o||"",p!==void 0&&p?r.createElement(r.Fragment,null,v&&v,r.createElement(y,null,r.createElement(C,{fullWidth:d!==!1,hasSideNav:!!m,className:"".concat(v?"jn-mt-[3.875rem]":"")},m&&m,r.createElement(b,null,l)))):r.createElement(r.Fragment,null,s&&(typeof s=="string"||s instanceof String)?r.createElement(W,{heading:s}):s,v&&v,r.createElement(y,null,r.createElement(C,{fullWidth:d===!0,hasSideNav:!!m,className:"jn-mt-[3.875rem]"},m&&m,r.createElement(b,{className:m?"":"jn-ml-8"},l))),E||r.createElement($,null)))},se=function(e,l){return`
+    `.concat(e?"jn-px-6 ":" ",` 
+    `).concat(l?" jn-py-6":"",`
+  `)},ue=function(e){var l=e.px,n=e.py,t=e.className,i=t===void 0?"":t,o=e.children,p=o===void 0?null:o,d=h(e,["px","py","className","children"]);return r.createElement("div",f({className:"juno-container ".concat(se(l===void 0||l,n!==void 0&&n)," ").concat(i)},d),p)};export{ve as A,ue as C};
