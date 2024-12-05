707cc3901324d9d4c22a7cda70fe95d09d7921d2
Taimoor Aslam <taimoor.aslam@sap.com>
2024-12-05 14:47:13 +0100

    chore(example): remove @cloudoperators/juno-utils dependency

---
+++ apps/example/src/mocks/db/index.js
@@ -0,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Juno contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import peaks from "./peaks.json"
+
+export default {
+  peaks,
+  // add more mocks here
+}
