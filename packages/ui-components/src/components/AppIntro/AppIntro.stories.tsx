4ffc47baa699ff0216cf463af716255eeee985ea
gjaskiewicz@objectivity.co.uk <gjaskiewicz@objectivity.co.uk>
2024-12-08 23:45:55 +0100

    chore(ui): migrate AppIntro to TypeScript

--- packages/ui-components/src/components/AppIntro/AppIntro.stories.tsx
+++ packages/ui-components/src/components/AppIntro/AppIntro.stories.tsx
@@ -5,7 +5,8 @@
 
 import React from "react"
 
-import { AppIntro } from "./index.js"
+import { AppIntro } from "./index"
+import { AppIntroProps } from "./AppIntro.component"
 
 export default {
   title: "Layout/AppIntro",
@@ -17,7 +18,7 @@ export default {
   },
 }
 
-const Template = (args) => (
+const Template = (args: AppIntroProps) => (
   <AppIntro {...args}>
     Intro text here. Explain what this app is in a short lead text. Lorem ipsum dolor sit amet. At vero eos et accusam
     et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est. Lorem ipsum dolor sit
