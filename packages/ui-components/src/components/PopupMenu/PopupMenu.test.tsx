7bcc6d0d1ffb06f5f4453119faa35a4b9b96a8d2
Franz Heidl <franz@franzheidl.de>
2024-12-10 14:42:33 +0100

    feat(ui): add more tests

--- packages/ui-components/src/components/PopupMenu/PopupMenu.test.tsx
+++ packages/ui-components/src/components/PopupMenu/PopupMenu.test.tsx
@@ -3,12 +3,25 @@ import { render, screen, act } from "@testing-library/react"
 import { describe, expect, test, vi } from "vitest"
 import { PopupMenu } from "./index"
 
-// Mock the PortalProvider so the menu is render4ded directly into a Dom node that is easily accessible for testing:
+// Mock the PortalProvider so the menu is rendered directly into a Dom node that is easily accessible in our tests:
 vi.mock("../PortalProvider", () => ({
   PortalProvider: {
-    Portal: ({ children }: { children: React.ReactNode }) => <div>{children}</div>,
+    Portal: ({ children }: { children: React.ReactNode }) => <div data-testid="portal-container">{children}</div>,
   },
 }))
+
+interface CustomToggleProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string
+}
+
+const CustomToggle = React.forwardRef<HTMLButtonElement, CustomToggleProps>(
+  ({ children = null, className = "", onClick, ...props }, ref) => (
+    <button ref={ref} className={`my-custom-toggle ${className}`} onClick={onClick} {...props}>
+      {children}
+    </button>
+  )
+)
+CustomToggle.displayName = "CustomToggle"
 
 describe("PopupMenu", () => {
   test("renders a PopupMenu default toggle", () => {
@@ -52,6 +65,16 @@ describe("PopupMenu", () => {
     expect(screen.getByRole("button")).toHaveClass("juno-popupmenu-toggle")
     expect(screen.getByRole("button")).toHaveTextContent("Toggle Me")
   })
+  test("renders a toggle as passed and ignores icon prop on parent", () => {
+    render(
+      <PopupMenu icon="warning">
+        <PopupMenu.Toggle>Toggle Me</PopupMenu.Toggle>
+      </PopupMenu>
+    )
+    expect(screen.getByRole("button")).toBeInTheDocument()
+    expect(screen.getByRole("button")).toHaveTextContent("Toggle Me")
+    expect(screen.queryByRole("img")).not.toBeInTheDocument()
+  })
   test("renders a toggle with a custom className as passed", () => {
     render(
       <PopupMenu>
@@ -61,6 +84,19 @@ describe("PopupMenu", () => {
     expect(screen.getByRole("button")).toBeInTheDocument()
     expect(screen.getByRole("button")).toHaveClass("juno-popupmenu-toggle")
     expect(screen.getByRole("button")).toHaveClass("my-custom-toggle")
+  })
+  test("renders a Toggle as a custom component as passed", () => {
+    render(
+      <PopupMenu>
+        <PopupMenu.Toggle as={CustomToggle} className="my-custom-toggle">
+          Toggle Me
+        </PopupMenu.Toggle>
+      </PopupMenu>
+    )
+    const toggle = screen.getByRole("button")
+    expect(toggle).toBeInTheDocument()
+    expect(toggle).toHaveTextContent("Toggle Me")
+    expect(toggle).toHaveClass("my-custom-toggle")
   })
   test("executes an onOpen handler as passed when the menu opens", () => {
     const onOpenSpy = vi.fn()
@@ -83,10 +119,21 @@ describe("PopupMenu", () => {
         <PopupMenu.Menu />
       </PopupMenu>
     )
-
     act(() => screen.getByRole("button").click())
     expect(screen.getByRole("menu")).toBeInTheDocument()
     expect(screen.getByRole("menu")).toHaveClass("juno-popupmenu-menu")
+  })
+  test("renders the menu into a portal container", () => {
+    render(
+      <PopupMenu>
+        <PopupMenu.Menu />
+      </PopupMenu>
+    )
+    act(() => screen.getByRole("button").click())
+    const portal = screen.getByTestId("portal-container")
+    const menu = screen.getByRole("menu")
+    expect(menu).toBeInTheDocument()
+    expect(portal).toContainElement(menu)
   })
   test("renders a normal size menu by default", () => {
     render(
@@ -110,7 +157,6 @@ describe("PopupMenu", () => {
     expect(screen.getByRole("menu")).toHaveClass("juno-popupmenu-menu")
     expect(screen.getByRole("menu")).toHaveClass("juno-popupmenu-menu-size-normal")
   })
-
   test("renders a menu with a custom className as passed", () => {
     render(
       <PopupMenu>
@@ -122,16 +168,177 @@ describe("PopupMenu", () => {
     expect(screen.getByRole("menu")).toHaveClass("juno-popupmenu-menu")
     expect(screen.getByRole("menu")).toHaveClass("my-custom-menu")
   })
-  // renders a menu section as passed
-  // renders a menu section with a title as passed
-  // renders a menu section with a custom className as passed
-  // renders the menu into a portal as passed
-  // renders all menu items as passed
-  // renders a disabled menu item as passed
-  // renders a menu item with an icon as passed
-  // renders only one toggle in case multiple toggles are passed
-  // renders only one menu in case multiple menus are passed
-  // when both icon and a toggle subcomponent are passed, expect toggle subcomponent to be rendered (and icon ingnored)
+  test("renders menu items as passed", () => {
+    render(
+      <PopupMenu>
+        <PopupMenu.Menu>
+          <PopupMenu.Item></PopupMenu.Item>
+        </PopupMenu.Menu>
+      </PopupMenu>
+    )
+    act(() => screen.getByRole("button").click())
+    expect(screen.getByRole("menuitem")).toBeInTheDocument()
+    expect(screen.getByRole("menuitem")).toHaveClass("juno-popupmenu-item")
+  })
+  test("renders menu items with a label as passed", () => {
+    render(
+      <PopupMenu>
+        <PopupMenu.Menu>
+          <PopupMenu.Item label="My Menu Item"></PopupMenu.Item>
+        </PopupMenu.Menu>
+      </PopupMenu>
+    )
+    act(() => screen.getByRole("button").click())
+    expect(screen.getByRole("menuitem")).toBeInTheDocument()
+    expect(screen.getByRole("menuitem")).toHaveClass("juno-popupmenu-item")
+    expect(screen.getByRole("menuitem")).toHaveTextContent("My Menu Item")
+  })
+  test("renders menu items with children as passed", () => {
+    render(
+      <PopupMenu>
+        <PopupMenu.Menu>
+          <PopupMenu.Item>Item With Children</PopupMenu.Item>
+        </PopupMenu.Menu>
+      </PopupMenu>
+    )
+    act(() => screen.getByRole("button").click())
+    expect(screen.getByRole("menuitem")).toBeInTheDocument()
+    expect(screen.getByRole("menuitem")).toHaveClass("juno-popupmenu-item")
+    expect(screen.getByRole("menuitem")).toHaveTextContent("Item With Children")
+  })
+  test("renders a menu item with label when both label and children were passed", () => {
+    render(
+      <PopupMenu>
+        <PopupMenu.Menu>
+          <PopupMenu.Item label="123">abc</PopupMenu.Item>
+        </PopupMenu.Menu>
+      </PopupMenu>
+    )
+    act(() => screen.getByRole("button").click())
+    expect(screen.getByRole("menuitem")).toBeInTheDocument()
+    expect(screen.getByRole("menuitem")).toHaveClass("juno-popupmenu-item")
+    expect(screen.getByRole("menuitem")).toHaveTextContent("123")
+    expect(screen.getByRole("menuitem")).not.toHaveTextContent("abc")
+  })
+  test("renders a menu item with a custom className as passed", () => {
+    render(
+      <PopupMenu>
+        <PopupMenu.Menu>
+          <PopupMenu.Item className="my-custom-item"></PopupMenu.Item>
+        </PopupMenu.Menu>
+      </PopupMenu>
+    )
+    act(() => screen.getByRole("button").click())
+    expect(screen.getByRole("menuitem")).toBeInTheDocument()
+    expect(screen.getByRole("menuitem")).toHaveClass("juno-popupmenu-item")
+    expect(screen.getByRole("menuitem")).toHaveClass("my-custom-item")
+  })
+  test("renders a disabled menu item as passed", () => {
+    render(
+      <PopupMenu>
+        <PopupMenu.Menu>
+          <PopupMenu.Item disabled></PopupMenu.Item>
+        </PopupMenu.Menu>
+      </PopupMenu>
+    )
+    act(() => screen.getByRole("button").click())
+    expect(screen.getByRole("menuitem")).toBeInTheDocument()
+    expect(screen.getByRole("menuitem")).toHaveAttribute("aria-disabled", "true")
+  })
+  test("renders a menu item with an icon as passed", () => {
+    render(
+      <PopupMenu>
+        <PopupMenu.Menu>
+          <PopupMenu.Item icon="warning"></PopupMenu.Item>
+        </PopupMenu.Menu>
+      </PopupMenu>
+    )
+    act(() => screen.getByRole("button").click())
+    expect(screen.getByRole("menuitem")).toBeInTheDocument()
+    // access the icon using the title attribute that establishes its accessible name:
+    expect(screen.getByRole("img", { name: "Warning" })).toBeInTheDocument()
+  })
+  test("renders an item with arbitrary props", () => {
+    render(
+      <PopupMenu>
+        <PopupMenu.Menu>
+          <PopupMenu.Item data-lolol="123" />
+        </PopupMenu.Menu>
+      </PopupMenu>
+    )
+    act(() => screen.getByRole("button").click())
+    expect(screen.getByRole("menuitem")).toBeInTheDocument()
+    expect(screen.getByRole("menuitem")).toHaveAttribute("data-lolol", "123")
+  })
+  test("renders a menu section as passed", () => {
+    render(
+      <PopupMenu>
+        <PopupMenu.Menu>
+          <PopupMenu.Section data-testid="my-menu-section"></PopupMenu.Section>
+        </PopupMenu.Menu>
+      </PopupMenu>
+    )
+    act(() => screen.getByRole("button").click())
+    expect(screen.getByTestId("my-menu-section")).toBeInTheDocument()
+  })
+  test("renders a menu section with a title as passed", () => {
+    render(
+      <PopupMenu>
+        <PopupMenu.Menu>
+          <PopupMenu.Section data-testid="my-menu-section" title="My Own Menu Section"></PopupMenu.Section>
+        </PopupMenu.Menu>
+      </PopupMenu>
+    )
+    act(() => screen.getByRole("button").click())
+    expect(screen.getByTestId("my-menu-section")).toBeInTheDocument()
+    expect(screen.getByTestId("my-menu-section")).toHaveTextContent("My Own Menu Section")
+  })
+  test("renders a menu section with a custom className as passed", () => {
+    render(
+      <PopupMenu>
+        <PopupMenu.Menu>
+          <PopupMenu.Section data-testid="my-menu-section" className="my-custom-class"></PopupMenu.Section>
+        </PopupMenu.Menu>
+      </PopupMenu>
+    )
+    act(() => screen.getByRole("button").click())
+    expect(screen.getByTestId("my-menu-section")).toBeInTheDocument()
+    expect(screen.getByTestId("my-menu-section")).toHaveClass("my-custom-class")
+  })
+  test("renders a menu section with children as passed", () => {
+    render(
+      <PopupMenu>
+        <PopupMenu.Menu>
+          <PopupMenu.Item data-testid="child-outside">Item outside of a section</PopupMenu.Item>
+          <PopupMenu.Section data-testid="my-menu-section">
+            <PopupMenu.Item data-testid="child-inside">Item inside a section</PopupMenu.Item>
+          </PopupMenu.Section>
+        </PopupMenu.Menu>
+      </PopupMenu>
+    )
+    act(() => screen.getByRole("button").click())
+    const section = screen.getByTestId("my-menu-section")
+    const insideChild = screen.getByTestId("child-inside")
+    const outsideChild = screen.getByTestId("child-outside")
+    expect(section).toContainElement(insideChild)
+    // double-check the other child is not a child of the section:
+    expect(section).not.toContainElement(outsideChild)
+  })
+  test("renders a section with arbitrary props", () => {
+    render(
+      <PopupMenu>
+        <PopupMenu.Menu>
+          <PopupMenu.Section data-testid="my-menu-section" data-lolol="123"></PopupMenu.Section>
+        </PopupMenu.Menu>
+      </PopupMenu>
+    )
+    act(() => screen.getByRole("button").click())
+    expect(screen.getByTestId("my-menu-section")).toBeInTheDocument()
+    expect(screen.getByTestId("my-menu-section")).toHaveAttribute("data-lolol", "123")
+  })
+
+  // TODO: renders only one toggle in case multiple toggles are passed
+  // TODO: renders only one menu in case multiple menus are passed
   // renders a functional toggle with only text as child
   // renders a functional toggle with a custom component passed as 'as'
   // renders a functional toggle with a custom component passed as a child to the toggle subcomponent
