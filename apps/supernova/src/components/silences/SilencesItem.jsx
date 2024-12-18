2bb2d9e3574f46f32e08de973ab4cb093f9f7548
TilmanHaupt <tilman.haupt@sap.com>
2024-12-18 15:56:06 +0100

    fix(supernova): Allow pending silences to expire

--- apps/supernova/src/components/silences/SilencesItem.jsx
+++ apps/supernova/src/components/silences/SilencesItem.jsx
@@ -42,7 +42,8 @@ const SilencesItem = ({ silence }, ref) => {
       <DataGridCell className="overflow-hidden">
         {
           /// show the expire button only if the silence is active or pending
-          silence?.status?.state === constants.SILENCE_ACTIVE ? (
+          silence?.status?.state === constants.SILENCE_ACTIVE ||
+          silence?.status?.state === constants.SILENCE_PENDING ? (
             <ExpireSilence silence={silence} />
           ) : (
             <RecreateSilence silence={silence} />
