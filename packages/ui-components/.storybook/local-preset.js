815a1a2884722400a8f874d83f15f610f8de6c8b
d064310 <andreas.pfau@sap.com>
2024-12-19 14:02:30 +0100

    chore(ui): update package-lock.json and fix jsx issues

--- packages/ui-components/.storybook/local-preset.js
+++ packages/ui-components/.storybook/local-preset.js
@@ -2,14 +2,14 @@
  * to load the built addon in this test Storybook
  */
 function previewAnnotations(entry = []) {
-    return [...entry, require.resolve("../src/juno-addon/preview.js")];
-  }
-  
-  function managerEntries(entry = []) {
-    return [...entry, require.resolve("../src/juno-addon/manager.js")];
-  }
-  
-  module.exports = {
-    managerEntries,
-    previewAnnotations,
-  };
+  return [...entry, require.resolve("../src/juno-addon/preview.js")]
+}
+
+function managerEntries(entry = []) {
+  return [...entry, require.resolve("../src/juno-addon/manager.js")]
+}
+
+module.exports = {
+  managerEntries,
+  previewAnnotations,
+}
