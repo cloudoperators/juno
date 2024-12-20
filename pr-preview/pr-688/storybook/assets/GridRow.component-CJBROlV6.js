8a93e1b55870b6166b79c2a48bf44084b69585af
andypf <andypf@users.noreply.github.com>
2024-12-20 09:56:51 +0000

    Remove preview for PR 688 🛬

--- pr-preview/pr-688/storybook/assets/GridRow.component-CJBROlV6.js
+++
@@ -1,7 +0,0 @@
-import{j as t}from"./jsx-runtime-D6fbYt3N.js";const a=`
-    jn-flex
-    jn-flex-wrap
-    jn-m-grid-row
-`,r=({children:e=null,className:o="",...n})=>t.jsx("div",{className:`juno-grid-row ${a} ${o}`,...n,children:e});try{r.displayName="GridRow",r.__docgenInfo={description:`A grid row container to hold GridColumn elements inside a Grid.
-This component ensures that its children are wrapped correctly in a flexbox layout.`,displayName:"GridRow",props:{children:{defaultValue:{value:"null"},description:`Elements to be rendered within the grid row.
-Typically, these would be GridColumn components.`,name:"children",required:!1,type:{name:"ReactNode"}},className:{defaultValue:{value:""},description:"Additional CSS classes to apply to the grid row for custom styling.",name:"className",required:!1,type:{name:"string"}}}}}catch{}export{r as G};
