219127001b8c336a14fafb0edf7fb40b65a274a1
Grzegorz Jaśkiewicz <gj.jaskiewicz@gmail.com>
2024-12-20 10:56:19 +0100

    chore(ui): migrate storybook to vite (#688)
    * chore(ui): migrate storybook build to vite
    
    * chore(ui): update package-lock.json and fix jsx issues
    
    * chore(ui): update package-lock.json and fix jsx issues
    
    * chore(ui): add storybook-static to lint ignore
    
    * chore(ui): remove jest from package.json
    
    ---------
    
    Co-authored-by: gjaskiewicz@objectivity.co.uk <gjaskiewicz@objectivity.co.uk>
    Co-authored-by: d064310 <andreas.pfau@sap.com>

---
+++ packages/ui-components/.storybook/local-preset.js
@@ -0,0 +1,15 @@
+/**
+ * to load the built addon in this test Storybook
+ */
+function previewAnnotations(entry = []) {
+  return [...entry, require.resolve("../src/juno-addon/preview.js")]
+}
+
+function managerEntries(entry = []) {
+  return [...entry, require.resolve("../src/juno-addon/manager.js")]
+}
+
+module.exports = {
+  managerEntries,
+  previewAnnotations,
+}
