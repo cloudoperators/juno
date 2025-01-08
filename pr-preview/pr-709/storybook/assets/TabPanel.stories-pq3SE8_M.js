c12504114a875c4b7b553c59cb1912b64dded54c
gjaskiewicz <gjaskiewicz@users.noreply.github.com>
2025-01-08 13:20:55 +0000

    Deploy preview for PR 709 🛫

--- pr-preview/pr-709/storybook/assets/TabPanel.stories-pq3SE8_M.js
+++
@@ -1,5 +0,0 @@
-import{T as t}from"./TabPanel.component-D5P2tUK3.js";import"./jsx-runtime-D6fbYt3N.js";import"./index-DysCNOs_.js";import"./TabPanel-SXlHHxNZ.js";const p={title:"Layout/Tabs/TabPanel",component:t,argTypes:{children:{control:!1}}},e={args:{children:"Tab panel content goes here."}};var a,r,o;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    children: "Tab panel content goes here."
-  }
-}`,...(o=(r=e.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const i=["Default"];export{e as Default,i as __namedExportsOrder,p as default};
