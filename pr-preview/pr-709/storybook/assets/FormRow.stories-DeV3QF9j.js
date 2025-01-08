e7e202f57be0d6c363d50fff1a737435d05b8f1d
gjaskiewicz <gjaskiewicz@users.noreply.github.com>
2025-01-08 13:42:22 +0000

    Deploy preview for PR 709 🛫

--- pr-preview/pr-709/storybook/assets/FormRow.stories-DeV3QF9j.js
+++
@@ -1,6 +0,0 @@
-import{j as a}from"./jsx-runtime-D6fbYt3N.js";import{F as s}from"./FormRow.component-DeN_H-xw.js";import{T as p}from"./TextInput.component-CLE03gg2.js";import"./index-DysCNOs_.js";import"./Label.component-D1jWKEjF.js";import"./Icon.component-BzIpr4Ej.js";import"./widgets-BR6-ubtP.js";import"./FormHint.component-Bac6eIcI.js";const F={title:"Forms/FormRow",component:s,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},l=({children:m,...n})=>a.jsx(s,{...n,children:m}),e={render:l,args:{children:[a.jsx(p,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  render: Template,
-  args: {
-    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
-  }
-}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const R=["Default"];export{e as Default,R as __namedExportsOrder,F as default};
---
+++ pr-preview/pr-709/storybook/assets/Tab.stories-Be8zbOFp.js
@@ -0,0 +1,11 @@
+import{T as i}from"./Tab.component-B2nrnc_G.js";import{K as l}from"./Icon.component-C0iYw0aN.js";import"./jsx-runtime-D6fbYt3N.js";import"./index-DysCNOs_.js";import"./TabPanel-SXlHHxNZ.js";import"./widgets-BR6-ubtP.js";const g={title:"Layout/Tabs/Tab",component:i,argTypes:{icon:{options:["default",...Object.keys(l)],control:{type:"select"}},children:{control:!1}}},r={args:{label:"A Tab Label",children:"Tab 1"}},e={args:{children:"Tab with Icon",icon:"danger"}};var a,o,n;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    label: "A Tab Label",
+    children: "Tab 1"
+  }
+}`,...(n=(o=r.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};var s,t,c;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    children: "Tab with Icon",
+    icon: "danger"
+  }
+}`,...(c=(t=e.parameters)==null?void 0:t.docs)==null?void 0:c.source}}};const h=["Default","WithIcon"];export{r as Default,e as WithIcon,h as __namedExportsOrder,g as default};
