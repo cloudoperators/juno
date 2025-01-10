09861f257ac7b4e1f609b58b5b7004ee9e6d71ef
ArtieReus <ArtieReus@users.noreply.github.com>
2025-01-10 08:40:59 +0000

    Deploy preview for PR 710 🛫

---
+++ pr-preview/pr-710/storybook/assets/PageFooter.stories-ByJaDPny.js
@@ -0,0 +1,11 @@
+import{j as n}from"./jsx-runtime-D6fbYt3N.js";import{P as s}from"./PageFooter.component-NMWcR93m.js";import"./index-DysCNOs_.js";const i={title:"Layout/PageFooter",component:s,argTypes:{children:{control:!1}}},p=a=>n.jsx(s,{...a}),e={render:p,parameters:{docs:{description:{story:"The page footer component renders a footer at the bottom of the website. Place as last child of AppBody."}}},args:{}};var o,t,r;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: Template,
+  parameters: {
+    docs: {
+      description: {
+        story: "The page footer component renders a footer at the bottom of the website. Place as last child of AppBody."
+      }
+    }
+  },
+  args: {}
+}`,...(r=(t=e.parameters)==null?void 0:t.docs)==null?void 0:r.source}}};const l=["Simple"];export{e as Simple,l as __namedExportsOrder,i as default};
