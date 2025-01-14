749d406af5eb7a702c84b61b96f1065448f621ab
gjaskiewicz@objectivity.co.uk <gjaskiewicz@objectivity.co.uk>
2025-01-14 11:19:05 +0100

    fix(ui): extract function for rendering a panel

--- packages/ui-components/src/components/Panel/Panel.test.tsx
+++ packages/ui-components/src/components/Panel/Panel.test.tsx
@@ -8,117 +8,87 @@ import { render, screen, waitFor } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import { describe, expect, test, vi } from "vitest"
 
-import { Panel } from "./Panel.component"
+import { Panel, PanelProps } from "./Panel.component"
 import { PortalProvider } from "../PortalProvider"
 
 const closedClass = "jn-translate-x-[100%]"
 
+const renderPanel = async (props?: PanelProps, extraProps?: any) => {
+  return waitFor(() =>
+    render(
+      <PortalProvider>
+        <Panel {...(props || {})} {...extraProps} />
+      </PortalProvider>
+    )
+  )
+}
+
 describe("Panel", () => {
   describe("Basic Rendering", () => {
     test("renders a panel", async () => {
-      await waitFor(() =>
-        render(
-          <PortalProvider>
-            <Panel />
-          </PortalProvider>
-        )
-      )
+      await renderPanel()
       expect(screen.getByRole("dialog")).toBeInTheDocument()
       expect(screen.getByRole("dialog")).toHaveClass("juno-panel")
     })
 
     test("renders a closed panel by default", async () => {
-      await waitFor(() =>
-        render(
-          <PortalProvider>
-            <Panel />
-          </PortalProvider>
-        )
-      )
+      await renderPanel()
       expect(screen.getByRole("dialog")).toBeInTheDocument()
       expect(screen.getByRole("dialog")).toHaveClass(closedClass)
     })
 
     test("renders a panel without any props", async () => {
-      await waitFor(() =>
-        render(
-          <PortalProvider>
-            <Panel />
-          </PortalProvider>
-        )
-      )
+      await renderPanel()
       expect(screen.getByRole("dialog")).toBeInTheDocument()
     })
 
     test("renders an opened panel", async () => {
-      await waitFor(() =>
-        render(
-          <PortalProvider>
-            <Panel opened />
-          </PortalProvider>
-        )
-      )
+      await renderPanel({
+        opened: true,
+      })
       expect(screen.getByRole("dialog")).toBeInTheDocument()
       expect(screen.getByRole("dialog")).not.toHaveClass(closedClass)
     })
 
     test("renders a panel with heading", async () => {
-      await waitFor(() =>
-        render(
-          <PortalProvider>
-            <Panel heading="My heading" opened />
-          </PortalProvider>
-        )
-      )
+      await renderPanel({
+        heading: "My heading",
+        opened: true,
+      })
       expect(screen.getByRole("dialog")).toBeInTheDocument()
       expect(screen.getByRole("dialog")).toHaveTextContent("My heading")
     })
 
     test("renders a custom classname", async () => {
-      await waitFor(() =>
-        render(
-          <PortalProvider>
-            <Panel className="my-custom-classname" />
-          </PortalProvider>
-        )
-      )
+      await renderPanel({
+        className: "my-custom-classname",
+      })
       expect(screen.getByRole("dialog")).toBeInTheDocument()
       expect(screen.getByRole("dialog")).toHaveClass("my-custom-classname")
     })
 
     test("renders all props as passed", async () => {
-      await waitFor(() =>
-        render(
-          <PortalProvider>
-            <Panel data-xyz={true} />
-          </PortalProvider>
-        )
+      await renderPanel(
+        {},
+        {
+          "data-xyz": true,
+        }
       )
       expect(screen.getByRole("dialog")).toBeInTheDocument()
       expect(screen.getByRole("dialog")).toHaveAttribute("data-xyz")
     })
 
     test("renders a panel with undefined className", async () => {
-      await waitFor(() =>
-        render(
-          <PortalProvider>
-            <Panel className={undefined} />
-          </PortalProvider>
-        )
-      )
+      await renderPanel({
+        className: undefined,
+      })
       expect(screen.getByRole("dialog")).toBeInTheDocument()
     })
   })
 
   describe("Conditional Rendering", () => {
     test("renders a panel with close button by default", async () => {
-      await waitFor(() =>
-        render(
-          <PortalProvider>
-            <Panel />
-          </PortalProvider>
-        )
-      )
+      await renderPanel()
       expect(screen.getByRole("dialog")).toBeInTheDocument()
       expect(screen.getByRole("button")).toBeInTheDocument()
       expect(screen.getByLabelText("close")).toBeInTheDocument()
@@ -127,26 +97,18 @@ describe("Panel", () => {
     })
 
     test("renders a panel without a close button", async () => {
-      await waitFor(() =>
-        render(
-          <PortalProvider>
-            <Panel closeable={false} />
-          </PortalProvider>
-        )
-      )
+      await renderPanel({
+        closeable: false,
+      })
       expect(screen.getByRole("dialog")).toBeInTheDocument()
       expect(screen.queryByRole("button")).not.toBeInTheDocument()
       expect(screen.queryByLabelText("close")).not.toBeInTheDocument()
     })
 
     test("renders a panel without a heading", async () => {
-      await waitFor(() =>
-        render(
-          <PortalProvider>
-            <Panel opened />
-          </PortalProvider>
-        )
-      )
+      await renderPanel({
+        opened: true,
+      })
       expect(screen.getByRole("dialog")).toBeInTheDocument()
       expect(screen.getByRole("dialog")).not.toHaveTextContent("My heading")
     })
@@ -154,39 +116,26 @@ describe("Panel", () => {
 
   describe("Events", () => {
     test("on click on close button closes panel", async () => {
-      await waitFor(() =>
-        render(
-          <PortalProvider>
-            <Panel />
-          </PortalProvider>
-        )
-      )
+      await renderPanel()
       await waitFor(() => userEvent.click(screen.getByRole("button")))
       expect(screen.getByRole("dialog")).toHaveClass(closedClass)
     })
 
     test("on click on close button fires onClose handler as passed", async () => {
       const handleClose = vi.fn()
-      await waitFor(() =>
-        render(
-          <PortalProvider>
-            <Panel onClose={handleClose} />
-          </PortalProvider>
-        )
-      )
+      await renderPanel({
+        onClose: handleClose,
+      })
       await waitFor(() => userEvent.click(screen.getByRole("button")))
       expect(handleClose).toHaveBeenCalledTimes(1)
     })
 
     test("on click on close button when panel is already closed", async () => {
       const handleClose = vi.fn()
-      await waitFor(() =>
-        render(
-          <PortalProvider>
-            <Panel opened={false} onClose={handleClose} />
-          </PortalProvider>
-        )
-      )
+      await renderPanel({
+        opened: false,
+        onClose: handleClose,
+      })
       const button = screen.queryByRole("button")
       await waitFor(() => (button ? userEvent.click(button) : null))
       expect(screen.getByRole("dialog")).toHaveClass(closedClass)
@@ -195,13 +144,9 @@ describe("Panel", () => {
 
     test("double-click on close button fires onClose handler twice", async () => {
       const handleClose = vi.fn()
-      await waitFor(() =>
-        render(
-          <PortalProvider>
-            <Panel onClose={handleClose} />
-          </PortalProvider>
-        )
-      )
+      await renderPanel({
+        onClose: handleClose,
+      })
       await waitFor(() => userEvent.dblClick(screen.getByRole("button")))
       expect(handleClose).toHaveBeenCalledTimes(2)
     })
