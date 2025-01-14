ea0543f96fddaa644c11de038b6f011c397487c5
barsukov <barsukov@users.noreply.github.com>
2025-01-14 12:39:40 +0000

    Remove preview for PR 709 🛬

--- pr-preview/pr-709/storybook/assets/MainContainer.component-CEaVW4UM.js
+++
@@ -1,6 +0,0 @@
-import{j as s}from"./jsx-runtime-D6fbYt3N.js";const o=`
-  jn-flex
-  jn-grow
-  jn-bg-theme-global-bg
-`,n=({className:e="",children:a=null,...t})=>s.jsx("main",{className:`juno-main ${o} ${e}`,...t,children:a});try{n.displayName="MainContainer",n.__docgenInfo={description:`Only needed if you want to build your app's scaffold manually. In most cases it is better to use the AppShell component instead.
-The main container for app content.`,displayName:"MainContainer",props:{className:{defaultValue:{value:""},description:"Add custom class name",name:"className",required:!1,type:{name:"string"}}}}}catch{}export{n as M};
