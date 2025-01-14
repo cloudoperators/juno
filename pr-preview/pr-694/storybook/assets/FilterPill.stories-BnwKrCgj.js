63e99c63d676a6023d3bd4612c61329a5155e615
barsukov <barsukov@users.noreply.github.com>
2025-01-14 13:03:10 +0000

    Deploy preview for PR 694 🛫

---
+++ pr-preview/pr-694/storybook/assets/FilterPill.stories-BnwKrCgj.js
@@ -0,0 +1,8 @@
+import{F as l}from"./FilterPill.component-BPbXn-S9.js";import"./jsx-runtime-D6fbYt3N.js";import"./index-DysCNOs_.js";import"./index-BHU3UH5b.js";import"./Icon.component-BXxTB2S-.js";import"./widgets-BR6-ubtP.js";const n={title:"Deprecated/Filter/FilterPill",component:l,argTypes:{}},e={args:{filterKey:"os",filterKeyLabel:"OS",filterValue:"mac_os",filterValueLabel:"Mac OS"}};var r,t,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    filterKey: "os",
+    filterKeyLabel: "OS",
+    filterValue: "mac_os",
+    filterValueLabel: "Mac OS"
+  }
+}`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const f=["Default"];export{e as Default,f as __namedExportsOrder,n as default};
