74b344a5a9b421502906240a97759314793dee59
taymoor89 <taymoor89@users.noreply.github.com>
2025-01-20 16:31:30 +0000

    Deploy preview for PR 729 🛫

---
+++ pr-preview/pr-729/storybook/assets/ButtonRow.stories-D9QDr6lA.js
@@ -0,0 +1,7 @@
+import{j as e}from"./jsx-runtime-D6fbYt3N.js";import{B as l}from"./ButtonRow.component-bDhN_D-k.js";import{B as r}from"./Button.component-BgfJ-nCv.js";import"./index-DysCNOs_.js";import"./Stack.component-B4IgElTG.js";import"./Icon.component-AKbg8iWE.js";import"./widgets-BR6-ubtP.js";import"./Spinner.component-COLjSzIA.js";const v={title:"Forms/ButtonRow",component:l,argTypes:{items:{table:{disable:!0}},children:{control:!1}}},m=({children:s,...i})=>e.jsx(l,{...i,children:s}),t={render:m,args:{name:"Default ButtonRow",children:[e.jsx(r,{label:"Cancel",title:"Cancel"},"1"),e.jsx(r,{label:"Save",title:"Save",variant:"primary"},"2")]}};var a,o,n;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    name: "Default ButtonRow",
+    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
+  }
+}`,...(n=(o=t.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};const R=["Default"];export{t as Default,R as __namedExportsOrder,v as default};
