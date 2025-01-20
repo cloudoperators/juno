44f4c5b4602aef3b9df8bda3d4dbe35d20a7d4c5
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-01-20 09:31:32 +0000

    Deploy preview for PR 726 🛫

---
+++ pr-preview/pr-726/storybook/assets/DataGridFoot.stories-C9KmlXqb.js
@@ -0,0 +1,11 @@
+import{j as t}from"./jsx-runtime-D6fbYt3N.js";import"./index-DysCNOs_.js";const a=({className:e="",children:d,...i})=>t.jsx("tfoot",{className:`juno-datagrid-foot ${e}`,...i,children:d});try{a.displayName="DataGridFoot",a.__docgenInfo={description:"",displayName:"DataGridFoot",props:{children:{defaultValue:null,description:"Children to render in the DataGridFoot",name:"children",required:!1,type:{name:"Element | null"}},className:{defaultValue:{value:""},description:"Add a classname",name:"className",required:!1,type:{name:"string"}}}}}catch{}const m={title:"WiP/DataGrid/DataGridFoot",component:a,argTypes:{children:{control:!1}},decorators:[e=>t.jsx("table",{children:e()})],parameters:{docs:{source:{excludeDecorators:!1}}}},c=({...e})=>t.jsx(a,{...e}),r={render:c,parameters:{docs:{description:{story:"Juno DataGridFoot for use in DataGrid"}}},args:{}};var o,s,n;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: Template,
+  parameters: {
+    docs: {
+      description: {
+        story: "Juno DataGridFoot for use in DataGrid"
+      }
+    }
+  },
+  args: {}
+}`,...(n=(s=r.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,m as default};
---
+++ pr-preview/pr-726/storybook/assets/MenuItem.stories-BSueyswR.js
@@ -0,0 +1,54 @@
+import{j as i}from"./jsx-runtime-D6fbYt3N.js";import{a as P,M as R}from"./MenuItem.component-BHUQZn7e.js";import{B as T}from"./Button.component-BgfJ-nCv.js";import"./index-DysCNOs_.js";import"./keyboard-BayCKsX5.js";import"./use-text-value-CqB0ebPQ.js";import"./use-tree-walker-sKJwDSu4.js";import"./Icon.component-AKbg8iWE.js";import"./widgets-BR6-ubtP.js";import"./Spinner.component-COLjSzIA.js";const $={title:"WiP/Menu/MenuItem",component:P,argTypes:{children:{control:!1}},decorators:[(E,N)=>{const{menuVariant:O}=N.parameters;return i.jsx(R,{variant:O,children:i.jsx(i.Fragment,{children:E()})})}],parameters:{menuVariant:"normal"}},e={args:{label:"A menu item",onClick:null}},r={args:{label:"A small menu item",onClick:null},parameters:{menuVariant:"small"}},a={args:{label:"A menu item with icon",icon:"deleteForever",onClick:null}},n={args:{label:"A small menu item with icon",icon:"deleteForever",onClick:null},parameters:{menuVariant:"small"}},s={args:{label:"A menu item as a link",href:"https://github.com/cloudoperators/juno"}},t={args:{label:"Menu Item as Button",onClick:()=>{}}},o={args:{children:[i.jsx(T,{label:"Delete",size:"small",variant:"subdued",icon:"deleteForever",className:"jn-w-full"},"1")]}},l={args:{disabled:!0,label:"Disabled menu item"}},m={args:{disabled:!0,label:"Disabled menu item as link",href:"https://github.com/cloudoperators/juno"}};var c,u,d;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    label: "A menu item",
+    onClick: null
+  }
+}`,...(d=(u=e.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var p,b,g;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    label: "A small menu item",
+    onClick: null
+  },
+  parameters: {
+    menuVariant: "small"
+  }
+}`,...(g=(b=r.parameters)==null?void 0:b.docs)==null?void 0:g.source}}};var h,I,M;a.parameters={...a.parameters,docs:{...(h=a.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    label: "A menu item with icon",
+    icon: "deleteForever",
+    onClick: null
+  }
+}`,...(M=(I=a.parameters)==null?void 0:I.docs)==null?void 0:M.source}}};var k,A,f;n.parameters={...n.parameters,docs:{...(k=n.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    label: "A small menu item with icon",
+    icon: "deleteForever",
+    onClick: null
+  },
+  parameters: {
+    menuVariant: "small"
+  }
+}`,...(f=(A=n.parameters)==null?void 0:A.docs)==null?void 0:f.source}}};var S,C,D;s.parameters={...s.parameters,docs:{...(S=s.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    label: "A menu item as a link",
+    href: "https://github.com/cloudoperators/juno"
+  }
+}`,...(D=(C=s.parameters)==null?void 0:C.docs)==null?void 0:D.source}}};var j,v,x;t.parameters={...t.parameters,docs:{...(j=t.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    label: "Menu Item as Button",
+    onClick: () => {}
+  }
+}`,...(x=(v=t.parameters)==null?void 0:v.docs)==null?void 0:x.source}}};var B,F,W;o.parameters={...o.parameters,docs:{...(B=o.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    children: [<Button label="Delete" size="small" variant="subdued" icon="deleteForever" className="jn-w-full" key="1" />]
+  }
+}`,...(W=(F=o.parameters)==null?void 0:F.docs)==null?void 0:W.source}}};var w,V,L;l.parameters={...l.parameters,docs:{...(w=l.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    label: "Disabled menu item"
+  }
+}`,...(L=(V=l.parameters)==null?void 0:V.docs)==null?void 0:L.source}}};var _,y,z;m.parameters={...m.parameters,docs:{...(_=m.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    label: "Disabled menu item as link",
+    href: "https://github.com/cloudoperators/juno"
+  }
+}`,...(z=(y=m.parameters)==null?void 0:y.docs)==null?void 0:z.source}}};const ee=["Default","SmallMenuItem","WithIcon","SmallWithIcon","MenuItemAsLink","MenuItemAsButton","MenuItemWithChildren","DisabledMenuItem","DisabledMenuItemAsLink"];export{e as Default,l as DisabledMenuItem,m as DisabledMenuItemAsLink,t as MenuItemAsButton,s as MenuItemAsLink,o as MenuItemWithChildren,r as SmallMenuItem,n as SmallWithIcon,a as WithIcon,ee as __namedExportsOrder,$ as default};
