9b9f8dc620945a54e2a5ceafb1d8f7d54d755fc2
guoda-puidokaite <121792659+guoda-puidokaite@users.noreply.github.com>
2025-01-22 07:49:36 +0000

    Deploying to gh-pages from @ cloudoperators/juno@851d17b5824a0f93ebf518593b7f09c13a3c018c 🚀

---
+++ assets/DataGridCell.stories-COmncAey.js
@@ -0,0 +1,39 @@
+import{j as o}from"./jsx-runtime-D6fbYt3N.js";import{b as G,D as h,a as C}from"./DataGridCell.component-DTHwRBd8.js";import"./index-DysCNOs_.js";const x={title:"Components/DataGrid/DataGridCell",component:G,argTypes:{children:{control:!1}},decorators:[n=>o.jsx(h,{columns:3,children:o.jsx(C,{children:n()})})],parameters:{docs:{source:{excludeDecorators:!1}}}},s=n=>o.jsx(G,{...n}),r={render:s,parameters:{docs:{description:{story:"Juno DataGridCell for use in DataGrid"}}},args:{children:["DataGridCell"]}},a={render:s,parameters:{docs:{description:{story:"Juno DataGridCell with nowrap option (content has white-space: nowrap;)"}}},args:{nowrap:!0,children:["DataGridCell does not wrap"]}},e={render:s,parameters:{docs:{description:{story:"Juno DataGridCell with colspan"}}},args:{colSpan:3,children:["DataGridCell with colspan"]}};var t,i,d;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: Template,
+  parameters: {
+    docs: {
+      description: {
+        story: "Juno DataGridCell for use in DataGrid"
+      }
+    }
+  },
+  args: {
+    children: ["DataGridCell"]
+  }
+}`,...(d=(i=r.parameters)==null?void 0:i.docs)==null?void 0:d.source}}};var c,l,p;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: Template,
+  parameters: {
+    docs: {
+      description: {
+        story: "Juno DataGridCell with nowrap option (content has white-space: nowrap;)"
+      }
+    }
+  },
+  args: {
+    nowrap: true,
+    children: ["DataGridCell does not wrap"]
+  }
+}`,...(p=(l=a.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var m,u,D;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: Template,
+  parameters: {
+    docs: {
+      description: {
+        story: "Juno DataGridCell with colspan"
+      }
+    }
+  },
+  args: {
+    colSpan: 3,
+    children: ["DataGridCell with colspan"]
+  }
+}`,...(D=(u=e.parameters)==null?void 0:u.docs)==null?void 0:D.source}}};const y=["Default","NoWrap","ColSpan"];export{e as ColSpan,r as Default,a as NoWrap,y as __namedExportsOrder,x as default};
