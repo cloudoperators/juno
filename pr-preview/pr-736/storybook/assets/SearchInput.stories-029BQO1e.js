3eea8e55b08b2ccee98513befd871e69b6b4bd2e
andypf <andypf@users.noreply.github.com>
2025-01-22 15:25:38 +0000

    Deploy preview for PR 736 🛫

---
+++ pr-preview/pr-736/storybook/assets/SearchInput.stories-029BQO1e.js
@@ -0,0 +1,15 @@
+import{S as h}from"./SearchInput.component-C7zij72C.js";import"./jsx-runtime-D6fbYt3N.js";import"./index-DysCNOs_.js";import"./Icon.component-B476qt6W.js";import"./widgets-BR6-ubtP.js";import"./Stack.component-B4IgElTG.js";const x={title:"Components/SearchInput",component:h,argTypes:{}},r={args:{}},e={args:{variant:"rounded"}},a={args:{variant:"hero"}},s={args:{disabled:!0}};var o,t,n;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {}
+}`,...(n=(t=r.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};var c,p,d;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    variant: "rounded"
+  }
+}`,...(d=(p=e.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};var m,i,u;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    variant: "hero"
+  }
+}`,...(u=(i=a.parameters)==null?void 0:i.docs)==null?void 0:u.source}}};var g,l,S;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...(S=(l=s.parameters)==null?void 0:l.docs)==null?void 0:S.source}}};const H=["Default","Rounded","Hero","Disabled"];export{r as Default,s as Disabled,a as Hero,e as Rounded,H as __namedExportsOrder,x as default};
---
+++ pr-preview/pr-736/storybook/assets/SideNavigationItem.component-CBZW01IB.js
@@ -0,0 +1,21 @@
+import{j as d}from"./jsx-runtime-D6fbYt3N.js";import{N as h,a as v}from"./NavigationItem.component-YThh0Ouh.js";const f=`
+  jn-min-w-[7.5rem]
+  jn-max-w-[20rem]
+`,o=({activeItem:e="",ariaLabel:a,children:i,className:t="",disabled:n=!1,onActiveItemChange:l,...s})=>d.jsx(h,{activeItem:e,ariaLabel:a,className:`juno-sidenavigation ${f} ${t}`,disabled:n,onActiveItemChange:l,...s,children:i});try{o.displayName="SideNavigation",o.__docgenInfo={description:`A generic vertical side navigation component.
+Place SideNavigationItem components as children.`,displayName:"SideNavigation",props:{activeItem:{defaultValue:{value:""},description:"The active navigation item by label",name:"activeItem",required:!1,type:{name:"string"}},ariaLabel:{defaultValue:null,description:'The aria-label of the navigation. Specify when there are more than one elements with an implicit or explicit `role="navigation"` on a page/view.',name:"ariaLabel",required:!1,type:{name:"string"}},children:{defaultValue:null,description:"The children of the Navigation. Typically these should be SideNavigationItem(s)",name:"children",required:!1,type:{name:"ReactNode"}},className:{defaultValue:{value:""},description:"Pass custom classname.",name:"className",required:!1,type:{name:"string"}},disabled:{defaultValue:{value:"false"},description:"Whether the navigation is disabled",name:"disabled",required:!1,type:{name:"boolean"}},onActiveItemChange:{defaultValue:null,description:"Handler to execute when the active item changes",name:"onActiveItemChange",required:!1,type:{name:"ActiveItemChangeHandler"}}}}}catch{}const g=`
+  jn-flex
+  jn-items-center
+  jn-w-full
+  jn-py-1.5
+  jn-px-8
+  jn-text-theme-default
+  jn-font-bold
+  jn-cursor-pointer
+  focus-visible:jn-outline-none
+  focus-visible:jn-ring-2
+  focus-visible:jn-ring-theme-focus
+`,b=`
+  jn-font-bold
+  jn-text-theme-sidenavigation-item-active
+  jn-bg-theme-sidenavigation-item-active
+`,r=({active:e=!1,ariaLabel:a="",children:i,className:t="",disabled:n=!1,icon:l=null,label:s="",href:c="",onClick:m,value:u="",...p})=>d.jsx(v,{active:e,activeItemStyles:b,ariaLabel:a,className:`juno-sidenavigation-item ${g} ${t}`,disabled:n,icon:l,label:s,href:c,onClick:m,value:u,...p,children:i});try{r.displayName="SideNavigationItem",r.__docgenInfo={description:"A SideNavigation item. To be used inside SideNavigation.",displayName:"SideNavigationItem",props:{active:{defaultValue:{value:"false"},description:"Whether the item is the currently active item",name:"active",required:!1,type:{name:"boolean"}},ariaLabel:{defaultValue:{value:""},description:"The aria label of the item",name:"ariaLabel",required:!1,type:{name:"string"}},children:{defaultValue:null,description:"The children to render. In order to make the navigation work, you also need to pass a `value` or `label` prop, or both.",name:"children",required:!1,type:{name:"ReactNode"}},disabled:{defaultValue:{value:"false"},description:"Whether the item is disabled",name:"disabled",required:!1,type:{name:"boolean"}},icon:{defaultValue:{value:"null"},description:"pass an icon name",name:"icon",required:!1,type:{name:'"accessTime" | "accountCircle" | "addCircle" | "autoAwesomeMosaic" | "autoAwesomeMotion" | "bolt" | "calendarToday" | "cancel" | "check" | "checkCircle" | "chevronLeft" | "chevronRight" | ... 40 more ...'}},label:{defaultValue:{value:""},description:"The label of the item",name:"label",required:!1,type:{name:"string"}},className:{defaultValue:{value:""},description:"Pass a custom className",name:"className",required:!1,type:{name:"string"}},href:{defaultValue:{value:""},description:"The link the item should point to. Will render the item as an anchor if passed",name:"href",required:!1,type:{name:"string"}},onClick:{defaultValue:null,description:"A handler to execute once the item is clicked. Will render the item as a button element if passed",name:"onClick",required:!1,type:{name:"MouseEventHandler<EventTarget>"}},value:{defaultValue:{value:""},description:"An optional technical identifier fort the tab. If not passed, the label will be used to identify the tab. NOTE: If value is passed, the value of the active tab MUST be used when setting the activeItem prop on the parent SideNavigation.",name:"value",required:!1,type:{name:"string"}}}}}catch{}export{o as S,r as a};
