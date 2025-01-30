74a75e563e63c80df78ff8f10392ba4eb40a963f
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-01-30 09:21:22 +0000

    Deploy preview for PR 748 🛫

--- pr-preview/pr-748/storybook/assets/ContentAreaWrapper.stories-BeM_xvZW.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./jsx-runtime-D6fbYt3N.js";import"./index-DysCNOs_.js";import{P as n}from"./index-BHU3UH5b.js";import{C as d}from"./ContentArea.component-vRrdWHDb.js";import{C as l}from"./ContentAreaToolbar.component-C_f9pJgm.js";import{B as m}from"./Button.component-BNWzKwuN.js";import"./withDeprecationWarning.component-Cd49MbBc.js";import"./Icon.component-BXxTB2S-.js";import"./widgets-BR6-ubtP.js";import"./Spinner.component-CxM1ZFOh.js";const f=`
-  jn-relative
-  jn-grow
-  jn-flex
-  jn-flex-col
-  jn-overflow-hidden
-`,t=({className:o="",children:i,...c})=>e.jsx("div",{className:`juno-content-area-wrapper ${f} ${o}`,...c,children:i});t.propTypes={className:n.string,children:n.node};t.__docgenInfo={description:"Deprecated: Will be deleted!",methods:[],displayName:"ContentAreaWrapper",props:{className:{defaultValue:{value:'""',computed:!1},description:"Add custom class name",type:{name:"string"},required:!1},children:{description:"",type:{name:"node"},required:!1}}};const v={title:"Deprecated/ContentAreaWrapper",component:t,argTypes:{children:{control:!1}}},u=o=>e.jsxs(t,{...o,children:[e.jsx(l,{children:e.jsx(m,{children:"Example"})}),e.jsx(d,{children:"Content goes here"})]}),r={render:u};var a,s,p;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  render: Template
-}`,...(p=(s=r.parameters)==null?void 0:s.docs)==null?void 0:p.source}}};const B=["Basic"];export{r as Basic,B as __namedExportsOrder,v as default};
