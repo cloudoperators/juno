4ce81be2207f8a1989b9360b3acb7f71bab680d8
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-01-30 10:22:16 +0000

    Deploy preview for PR 748 🛫

--- pr-preview/pr-748/storybook/assets/ShadowRoot.stories-CDvIPbWh.js
+++
@@ -1,6 +0,0 @@
-import{j as o}from"./jsx-runtime-D6fbYt3N.js";import{S as a}from"./ShadowRoot.component-CLRhA7vg.js";import"./index-DysCNOs_.js";import"./index-DxCGxluL.js";const c=n=>o.jsx(a,{...n,children:o.jsx("h1",{children:"Welcome"})}),i={title:"Layout/ShadowRoot",component:a,argTypes:{children:{control:!1}}},e={render:c,args:{mode:"closed"}};var r,s,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  render: Template,
-  args: {
-    mode: "closed"
-  }
-}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const u=["EncapsulateStyles"];export{e as EncapsulateStyles,u as __namedExportsOrder,i as default};
