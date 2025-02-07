4e605787bc571e45c30b218bdbaa5d1d2f883943
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-02-07 15:18:46 +0000

    Deploy preview for PR 759 🛫

---
+++ pr-preview/pr-759/storybook/assets/SelectOption.stories-RNsM2U5o.js
@@ -0,0 +1,23 @@
+import{j as n}from"./jsx-runtime-D6fbYt3N.js";import{a as v,S as x}from"./SelectOption.component-Cc6WHLn3.js";import{P as f}from"./PortalProvider.component-DJiEgPYG.js";import"./index-DysCNOs_.js";import"./Label.component-B3thB2lY.js";import"./Icon.component-tHQbZIM7.js";import"./widgets-BR6-ubtP.js";import"./Spinner.component-DGrCFjaQ.js";import"./FormHint.component-Bac6eIcI.js";import"./headlessui-float-CRaAej_Z.js";import"./index-DxCGxluL.js";import"./keyboard-BayCKsX5.js";import"./floating-ui.react-dom-t1-XbZXo.js";import"./listbox-Dqb8rs3Q.js";import"./form-DbO9dA4O.js";import"./use-text-value-CqB0ebPQ.js";const B={title:"Forms/Select/SelectOption",component:v,argTypes:{children:{control:{type:"string"},table:{type:{summary:"string"}}}},decorators:[s=>n.jsx(f,{children:s()})]},a=s=>n.jsx(x,{open:!0,children:n.jsx(v,{...s})}),e={render:a,args:{value:"Option 1"}},r={render:a,args:{disabled:!0,value:"Disabled Option"}},t={render:a,args:{children:["Option 1"]}},o={render:a,args:{value:"option-1",label:"Option 1"}};var i,p,l;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    value: "Option 1"
+  }
+}`,...(l=(p=e.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var m,c,d;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    disabled: true,
+    value: "Disabled Option"
+  }
+}`,...(d=(c=r.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var u,g,O;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    children: ["Option 1"]
+  }
+}`,...(O=(g=t.parameters)==null?void 0:g.docs)==null?void 0:O.source}}};var b,S,h;o.parameters={...o.parameters,docs:{...(b=o.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    value: "option-1",
+    label: "Option 1"
+  }
+}`,...(h=(S=o.parameters)==null?void 0:S.docs)==null?void 0:h.source}}};const G=["Default","Disabled","ChildrenOnly","withLabel"];export{t as ChildrenOnly,e as Default,r as Disabled,G as __namedExportsOrder,B as default,o as withLabel};
