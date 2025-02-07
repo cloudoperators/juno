0ba3dc1e955d45acc76116ba109020bd4457afd7
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-02-07 11:09:59 +0000

    Deploy preview for PR 759 🛫

---
+++ pr-preview/pr-759/storybook/assets/Navigation.stories-D7YJIsB_.js
@@ -0,0 +1,73 @@
+import{j as e}from"./jsx-runtime-D6fbYt3N.js";import{P as F}from"./index-BHU3UH5b.js";import{N as R,a}from"./NavigationItem.component-D2ci-UlN.js";import"./index-DysCNOs_.js";import"./Icon.component-Bb8hz3SE.js";import"./widgets-BR6-ubtP.js";const U={title:"Internal/Navigation",component:R,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}},role:{options:["TabNavigation","TopNavigation","SideNavigation"],control:{type:"select"}}}},t=({children:q,...z})=>e.jsx(R,{...z,children:q});t.propTypes={children:F.node};const i={render:t,args:{activeItem:"Item 1",children:[e.jsx(a,{children:"Item 1"},"i-1"),e.jsx(a,{children:"Item 2"},"i-2"),e.jsx(a,{children:"Item 3"},"i-3"),e.jsx(a,{disabled:!0,children:"Item 4"},"i-4")]}},r={render:t,args:{children:[e.jsx(a,{value:"i-1",label:"Item 1"},"i-1"),e.jsx(a,{value:"i-2",label:"Item 2"},"i-2"),e.jsx(a,{value:"i-3",label:"Item 3"},"i-3")]}},n={render:t,args:{children:[e.jsx(a,{value:"i-1",label:"Item 1 Label",children:"Item 1"},"i-1"),e.jsx(a,{value:"i-2",label:"Item 2 Label",children:"Item 2"},"i-2"),e.jsx(a,{value:"i-3",label:"Item 3 Label",children:"Item 3"},"i-3")]}},m={render:t,args:{children:[e.jsx(a,{value:"Item 1"},"i-1"),e.jsx(a,{value:"Item 2",active:!0},"i-2"),e.jsx(a,{value:"Item 3"},"i-3")]}},s={render:t,args:{activeItem:"item-2",children:[e.jsx(a,{value:"item-1",label:"Item 1"},"i-1"),e.jsx(a,{value:"item-2",label:"Item 2"},"i-2"),e.jsx(a,{value:"item-3",label:"Item 3"},"i-3")]}},l={render:t,args:{activeItem:"Item 2",children:[e.jsx(a,{value:"item-1",label:"Item 1"},"i-1"),e.jsx(a,{value:"item-2",label:"Item 2"},"i-2"),e.jsx(a,{value:"item-3",label:"Item 3"},"i-3")]}},o={render:t,args:{activeItem:"Item 2",children:[e.jsx(a,{value:"itm-1",children:"Item 1"},"i-1"),e.jsx(a,{value:"itm-2",children:"Item 2"},"i-2"),e.jsx(a,{value:"itm-3",children:"Item 3"},"i-3")]}},c={render:t,args:{disabled:!0,children:[e.jsx(a,{children:"Item 1"},"i-1"),e.jsx(a,{active:!0,children:"Item 2"},"i-2"),e.jsx(a,{children:"Item 3"},"i-3")]}},I={render:t,args:{children:[e.jsx(a,{href:"https://www.sap.com",children:"Link 1"},"i-1"),e.jsx(a,{href:"https://www.sap.com",children:"Link 2"},"i-2"),e.jsx(a,{href:"https://www.sap.com",children:"Link 3"},"i-3")]}};var d,v,u;i.parameters={...i.parameters,docs:{...(d=i.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    activeItem: "Item 1",
+    children: [<NavigationItem key="i-1">Item 1</NavigationItem>, <NavigationItem key="i-2">Item 2</NavigationItem>, <NavigationItem key="i-3">Item 3</NavigationItem>, <NavigationItem key="i-4" disabled>
+        Item 4
+      </NavigationItem>]
+  }
+}`,...(u=(v=i.parameters)==null?void 0:v.docs)==null?void 0:u.source}}};var g,p,h;r.parameters={...r.parameters,docs:{...(g=r.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    children: [<NavigationItem key="i-1" value="i-1" label="Item 1" />, <NavigationItem key="i-2" value="i-2" label="Item 2" />, <NavigationItem key="i-3" value="i-3" label="Item 3" />]
+  }
+}`,...(h=(p=r.parameters)==null?void 0:p.docs)==null?void 0:h.source}}};var N,b,y;n.parameters={...n.parameters,docs:{...(N=n.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    children: [<NavigationItem key="i-1" value="i-1" label="Item 1 Label">
+        Item 1
+      </NavigationItem>, <NavigationItem key="i-2" value="i-2" label="Item 2 Label">
+        Item 2
+      </NavigationItem>, <NavigationItem key="i-3" value="i-3" label="Item 3 Label">
+        Item 3
+      </NavigationItem>]
+  }
+}`,...(y=(b=n.parameters)==null?void 0:b.docs)==null?void 0:y.source}}};var k,x,j;m.parameters={...m.parameters,docs:{...(k=m.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    children: [<NavigationItem key="i-1" value="Item 1" />, <NavigationItem key="i-2" value="Item 2" active />, <NavigationItem key="i-3" value="Item 3" />]
+  }
+}`,...(j=(x=m.parameters)==null?void 0:x.docs)==null?void 0:j.source}}};var L,w,T;s.parameters={...s.parameters,docs:{...(L=s.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    activeItem: "item-2",
+    children: [<NavigationItem key="i-1" value="item-1" label="Item 1" />, <NavigationItem key="i-2" value="item-2" label="Item 2" />, <NavigationItem key="i-3" value="item-3" label="Item 3" />]
+  }
+}`,...(T=(w=s.parameters)==null?void 0:w.docs)==null?void 0:T.source}}};var f,A,W;l.parameters={...l.parameters,docs:{...(f=l.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    activeItem: "Item 2",
+    children: [<NavigationItem key="i-1" value="item-1" label="Item 1" />, <NavigationItem key="i-2" value="item-2" label="Item 2" />, <NavigationItem key="i-3" value="item-3" label="Item 3" />]
+  }
+}`,...(W=(A=l.parameters)==null?void 0:A.docs)==null?void 0:W.source}}};var S,V,B;o.parameters={...o.parameters,docs:{...(S=o.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    activeItem: "Item 2",
+    children: [<NavigationItem key="i-1" value="itm-1">
+        Item 1
+      </NavigationItem>, <NavigationItem key="i-2" value="itm-2">
+        Item 2
+      </NavigationItem>, <NavigationItem key="i-3" value="itm-3">
+        Item 3
+      </NavigationItem>]
+  }
+}`,...(B=(V=o.parameters)==null?void 0:V.docs)==null?void 0:B.source}}};var C,D,O;c.parameters={...c.parameters,docs:{...(C=c.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    disabled: true,
+    children: [<NavigationItem key="i-1">Item 1</NavigationItem>, <NavigationItem key="i-2" active>
+        Item 2
+      </NavigationItem>, <NavigationItem key="i-3">Item 3</NavigationItem>]
+  }
+}`,...(O=(D=c.parameters)==null?void 0:D.docs)==null?void 0:O.source}}};var _,E,P;I.parameters={...I.parameters,docs:{...(_=I.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    children: [<NavigationItem href="https://www.sap.com" key="i-1">
+        Link 1
+      </NavigationItem>, <NavigationItem href="https://www.sap.com" key="i-2">
+        Link 2
+      </NavigationItem>, <NavigationItem href="https://www.sap.com" key="i-3">
+        Link 3
+      </NavigationItem>]
+  }
+}`,...(P=(E=I.parameters)==null?void 0:E.docs)==null?void 0:P.source}}};const X=["DefaultWithChildren","WithValuesAndLabels","WithValuesLabelsAndChildren","ValuesOnly","WithActiveItemByValue","WithActiveItemByLabel","WithActiveItemByChild","Disabled","ItemsAsLinks"];export{i as DefaultWithChildren,c as Disabled,I as ItemsAsLinks,m as ValuesOnly,o as WithActiveItemByChild,l as WithActiveItemByLabel,s as WithActiveItemByValue,r as WithValuesAndLabels,n as WithValuesLabelsAndChildren,X as __namedExportsOrder,U as default};
