61f4745c9f662866701e5da80ef2424b1fb31b02
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-02-07 16:28:06 +0000

    Deploy preview for PR 759 🛫

---
+++ pr-preview/pr-759/storybook/assets/NavigationItem.stories-BZATvJQm.js
@@ -0,0 +1,43 @@
+import{j}from"./jsx-runtime-D6fbYt3N.js";import{P as C}from"./index-BHU3UH5b.js";import{N as y,a as E}from"./NavigationItem.component-B41_fazE.js";import"./index-DysCNOs_.js";import"./Icon.component-DteN3msQ.js";import"./widgets-BR6-ubtP.js";const B={title:"Internal/Navigation/NavigationItem",component:y,argTypes:{},decorators:[c=>j.jsx(E,{children:c()})]},e=({children:c,..._})=>j.jsx(y,{..._,children:c});e.propTypes={children:C.node};const r={render:e,args:{children:"Navigation Item"}},a={render:e,args:{value:"item-1",label:"Navigation Item 1"}},s={render:e,args:{active:!0,children:"Active Item"}},t={render:e,args:{disabled:!0,children:"Disabled Item"}},n={render:e,args:{href:"https://www.sap.com",children:"This item is a link"}},i={render:e,args:{href:"https://www.sap.com",children:"This item is a link",disabled:!0}},o={render:e,args:{label:"With Icon",icon:"warning"}};var m,d,l;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    children: "Navigation Item"
+  }
+}`,...(l=(d=r.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};var p,u,h;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    value: "item-1",
+    label: "Navigation Item 1"
+  }
+}`,...(h=(u=a.parameters)==null?void 0:u.docs)==null?void 0:h.source}}};var g,I,v;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    active: true,
+    children: "Active Item"
+  }
+}`,...(v=(I=s.parameters)==null?void 0:I.docs)==null?void 0:v.source}}};var b,T,w;t.parameters={...t.parameters,docs:{...(b=t.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    disabled: true,
+    children: "Disabled Item"
+  }
+}`,...(w=(T=t.parameters)==null?void 0:T.docs)==null?void 0:w.source}}};var f,N,k;n.parameters={...n.parameters,docs:{...(f=n.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    href: "https://www.sap.com",
+    children: "This item is a link"
+  }
+}`,...(k=(N=n.parameters)==null?void 0:N.docs)==null?void 0:k.source}}};var A,D,W;i.parameters={...i.parameters,docs:{...(A=i.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    href: "https://www.sap.com",
+    children: "This item is a link",
+    disabled: true
+  }
+}`,...(W=(D=i.parameters)==null?void 0:D.docs)==null?void 0:W.source}}};var S,x,L;o.parameters={...o.parameters,docs:{...(S=o.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    label: "With Icon",
+    icon: "warning"
+  }
+}`,...(L=(x=o.parameters)==null?void 0:x.docs)==null?void 0:L.source}}};const F=["DefaultWithChildren","WithValueAndLabel","ActiveItem","DisabledItem","ItemAsLink","DisabledLinkItem","WithIcon"];export{s as ActiveItem,r as DefaultWithChildren,t as DisabledItem,i as DisabledLinkItem,n as ItemAsLink,o as WithIcon,a as WithValueAndLabel,F as __namedExportsOrder,B as default};
