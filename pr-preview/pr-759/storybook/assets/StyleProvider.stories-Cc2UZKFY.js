a0d6782f9afeb2cac5b5cbb62f842ab94f642084
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-02-10 09:23:40 +0000

    Deploy preview for PR 759 🛫

---
+++ pr-preview/pr-759/storybook/assets/StyleProvider.stories-Cc2UZKFY.js
@@ -0,0 +1,23 @@
+import{j as W}from"./jsx-runtime-D6fbYt3N.js";import{S as T}from"./StyleProvider.component-DqlnkOkF.js";import"./index-DysCNOs_.js";import"./ShadowRoot.component-CLRhA7vg.js";import"./index-DxCGxluL.js";const L={title:"Layout/StyleProvider",component:T,argTypes:{stylesWrapper:{options:["head","inline","shadowRoot"],control:{type:"radio"}},children:{control:!1}}},o=a=>W.jsx(T,{...a,children:a.children||a.stylesWrapper||"undefined"}),e={render:o,args:{stylesWrapper:"head"}},r={render:o,args:{stylesWrapper:"inline"}},s={render:o,args:{stylesWrapper:"shadowRoot"}},t={render:o,args:{stylesWrapper:"shadowRoot",theme:"theme-light",children:"Light Theme"}};var n,d,p;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    stylesWrapper: "head"
+  }
+}`,...(p=(d=e.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};var l,i,c;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    stylesWrapper: "inline"
+  }
+}`,...(c=(i=r.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};var m,h,y;s.parameters={...s.parameters,docs:{...(m=s.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    stylesWrapper: "shadowRoot"
+  }
+}`,...(y=(h=s.parameters)==null?void 0:h.docs)==null?void 0:y.source}}};var g,u,S;t.parameters={...t.parameters,docs:{...(g=t.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    stylesWrapper: "shadowRoot",
+    theme: "theme-light",
+    children: "Light Theme"
+  }
+}`,...(S=(u=t.parameters)==null?void 0:u.docs)==null?void 0:S.source}}};const j=["AddStylesToHead","AddInlineStyles","AddStylesToShadowRoot","WithLightTheme"];export{r as AddInlineStyles,e as AddStylesToHead,s as AddStylesToShadowRoot,t as WithLightTheme,j as __namedExportsOrder,L as default};
