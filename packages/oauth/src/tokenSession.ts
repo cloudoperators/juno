ae440bcad7e19bb3876b3bd675df8a73cde0cb57
Arturo Reuschenbach Puncernau <reuschenbach@gmail.com>
2025-02-12 10:24:21 +0100

    chore(oauth): adds method documentation and types refactoring

--- packages/oauth/src/tokenSession.ts
+++ packages/oauth/src/tokenSession.ts
@@ -5,7 +5,7 @@
 
 import { parseIdTokenData, decodeIDToken } from "./tokenHelpers"
 
-interface AuthData {
+type AuthData = {
   JWT: string
   raw: Record<string, unknown>
   refreshToken: string
@@ -47,26 +47,57 @@ export const composeAuthData = (token: string, options: any): { authData: AuthDa
   }
 }
 
-interface TokenSessionState {
+type TokenSessionState = {
   auth: AuthData | null
   error: unknown
   loggedIn: boolean
   isProcessing: boolean
 }
 
-interface TokenSessionReturnType {
+type TokenSessionReturnType = {
   login: () => void
   logout: () => void
   currentState: () => TokenSessionState
 }
 
-interface TokenSessionParams {
+type TokenSessionParams = {
   token: string
   options?: Record<string, any>
   initialLogin?: boolean
   onUpdate: (_state: TokenSessionState) => void
 }
 
+/**
+ * Creates a token-based session where the provided token is validated, parsed,
+ * and extended with the specified options. The resulting authentication state
+ * is returned when the callback method is triggered.
+ *
+ * @param params.token - The token to be validated, parsed, and extended with the given options.
+ * @param params.onUpdate - A callback function invoked on login and logout events.
+ * @param params.initialLogin - A boolean indicating whether the login should be performed immediately
+ *                              on the first call, triggering the callback method.
+ * @param params.options - Additional options to extend the token data.
+ *                         See the following documentation for details on the returned auth data structure
+ *                         and usage of the `options` parameter:
+ *                         [Juno Auth Data Documentation](https://github.com/cloudoperators/juno/blob/main/packages/oauth/README.md#auth-data)
+ *
+ * @returns An object containing the following methods:
+ * - `login()`: Triggers the login process.
+ * - `logout()`: Triggers the logout process.
+ * - `currentState()`: Returns the current authentication state.
+ *
+ * @example
+ * ```typescript
+ * const session = tokenSession({
+ *   token: jwtToken,
+ *   options: {
+ *     groups: [`organization:${demoOrg}`],
+ *   },
+ *   onUpdate: (authData) => {
+ *     saveAuthData(authData);
+ *   },
+ * });
+ */
 export default function tokenSession(params: TokenSessionParams): TokenSessionReturnType {
   const { token, onUpdate, initialLogin, options } = params || {}
 
