cc59a99c05015e260bcf2c8dc0cf852279081334
TilmanHaupt <TilmanHaupt@users.noreply.github.com>
2025-02-28 10:02:57 +0000

    Deploy preview for PR 788 🛫

---
+++ pr-preview/pr-788/storybook/assets/FormRow.stories-Dqn1brHZ.js
@@ -0,0 +1,6 @@
+import{j as a}from"./jsx-runtime-BP6H2k8O.js";import{F as s}from"./FormRow.component-DiHZwysQ.js";import{T as p}from"./TextInput.component-gKCLoznM.js";import"./index-DysCNOs_.js";import"./Label.component-DhLm9IiA.js";import"./Icon.component-BTYDzMmf.js";import"./widgets-BR6-ubtP.js";import"./FormHint.component-DtRGMMjY.js";const F={title:"Forms/FormRow",component:s,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},l=({children:m,...n})=>a.jsx(s,{...n,children:m}),e={render:l,args:{children:[a.jsx(p,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
+  }
+}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const R=["Default"];export{e as Default,R as __namedExportsOrder,F as default};
---
+++ pr-preview/pr-788/storybook/assets/SelectOption.stories-BJdVQcSY.js
@@ -0,0 +1,23 @@
+import{j as n}from"./jsx-runtime-BP6H2k8O.js";import{a as v,S as x}from"./SelectOption.component-DO5IDQ63.js";import{P as f}from"./PortalProvider.component-C7NEyQzV.js";import"./index-DysCNOs_.js";import"./Label.component-DhLm9IiA.js";import"./Icon.component-BTYDzMmf.js";import"./widgets-BR6-ubtP.js";import"./Spinner.component-D_tPrRjo.js";import"./FormHint.component-DtRGMMjY.js";import"./headlessui-float-DiqDQ9Vb.js";import"./index-BIiW_AJL.js";import"./keyboard-RYHMaleb.js";import"./floating-ui.react-dom-DW-4-pTb.js";import"./listbox-FgpIuTn9.js";import"./form-CGPENT-o.js";import"./use-text-value-BSk7WQuc.js";const B={title:"Forms/Select/SelectOption",component:v,argTypes:{children:{control:{type:"string"},table:{type:{summary:"string"}}}},decorators:[s=>n.jsx(f,{children:s()})]},a=s=>n.jsx(x,{open:!0,children:n.jsx(v,{...s})}),e={render:a,args:{value:"Option 1"}},r={render:a,args:{disabled:!0,value:"Disabled Option"}},t={render:a,args:{children:["Option 1"]}},o={render:a,args:{value:"option-1",label:"Option 1"}};var i,p,l;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    value: "Option 1"
+  }
+}`,...(l=(p=e.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var m,c,d;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    disabled: true,
+    value: "Disabled Option"
+  }
+}`,...(d=(c=r.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var u,g,O;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    children: ["Option 1"]
+  }
+}`,...(O=(g=t.parameters)==null?void 0:g.docs)==null?void 0:O.source}}};var b,S,h;o.parameters={...o.parameters,docs:{...(b=o.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    value: "option-1",
+    label: "Option 1"
+  }
+}`,...(h=(S=o.parameters)==null?void 0:S.docs)==null?void 0:h.source}}};const G=["Default","Disabled","ChildrenOnly","withLabel"];export{t as ChildrenOnly,e as Default,r as Disabled,G as __namedExportsOrder,B as default,o as withLabel};
