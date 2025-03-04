a19af552aa9cbb6d4e96ace338497df3cf7241d7
andypf <andypf@users.noreply.github.com>
2025-03-04 10:40:13 +0000

    Remove preview for PR 788 🛬

--- pr-preview/pr-788/storybook/assets/AppIntro.stories-DvME9Ye0.js
+++
@@ -1,9 +0,0 @@
-import{j as n}from"./jsx-runtime-BP6H2k8O.js";import"./index-DysCNOs_.js";const m=`
-  jn-pt-16
-  jn-pb-14
-  jn-text-xl
-  in-pr-[45%]
-`,r=({className:e="",children:p,...c})=>n.jsx("div",{className:`juno-app-intro ${m} ${e}`,...c,children:p});try{r.displayName="AppIntro",r.__docgenInfo={description:"DEPRECATED: Will be deleted!",displayName:"AppIntro",props:{className:{defaultValue:{value:""},description:"Add custom class name",name:"className",required:!1,type:{name:"string"}}}}}catch{}try{AppIntrocomponent.displayName="AppIntrocomponent",AppIntrocomponent.__docgenInfo={description:"",displayName:"AppIntrocomponent",props:{className:{defaultValue:{value:""},description:"Add custom class name",name:"className",required:!1,type:{name:"string"}}}}}catch{}const u={title:"Deprecated/AppIntro",component:r,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},d=e=>n.jsx(r,{...e,children:"Intro text here. Explain what this app is in a short lead text. Lorem ipsum dolor sit amet. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est. Lorem ipsum dolor sit amet."}),t={render:d,args:{}};var a,o,s;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  render: Template,
-  args: {}
-}`,...(s=(o=t.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const _=["Intro"];export{t as Intro,_ as __namedExportsOrder,u as default};
