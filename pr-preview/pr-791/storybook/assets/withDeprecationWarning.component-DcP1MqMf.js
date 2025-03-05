c574210c5002ab94c73b17a6239005e3afdcfe98
andypf <andypf@users.noreply.github.com>
2025-03-05 07:43:41 +0000

    Remove preview for PR 791 🛬

--- pr-preview/pr-791/storybook/assets/withDeprecationWarning.component-DcP1MqMf.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./jsx-runtime-D6fbYt3N.js";import{r as e}from"./index-DysCNOs_.js";function s(n,r){return t=>(e.useEffect(()=>{console.warn(r)},[]),o.jsx(n,{...t}))}export{s as w};
