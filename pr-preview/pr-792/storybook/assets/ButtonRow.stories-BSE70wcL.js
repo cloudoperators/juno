c32f8e09db27b1967e6e1ca024e9413f2f351dbe
hgw77 <hgw77@users.noreply.github.com>
2025-03-07 11:06:01 +0000

    Remove preview for PR 792 🛬

--- pr-preview/pr-792/storybook/assets/ButtonRow.stories-BSE70wcL.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./jsx-runtime-D6fbYt3N.js";import{B as s}from"./ButtonRow.component-CbUzWAno.js";import{B as r}from"./Button.component-B2dCEIVR.js";import"./index-DysCNOs_.js";import"./Stack.component-Cte3SL04.js";import"./Icon.component-CP9cv3L0.js";import"./widgets-BR6-ubtP.js";import"./Spinner.component-CcUhSsaF.js";const v={title:"Forms/ButtonRow",component:s,argTypes:{items:{table:{disable:!0}},children:{control:!1}}},m=({children:l,...i})=>t.jsx(s,{...i,children:l}),e={render:m,args:{children:[t.jsx(r,{label:"Cancel",title:"Cancel"},"1"),t.jsx(r,{label:"Save",title:"Save",variant:"primary"},"2")]}};var a,o,n;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  render: Template,
-  args: {
-    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
-  }
-}`,...(n=(o=e.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};const j=["Default"];export{e as Default,j as __namedExportsOrder,v as default};
