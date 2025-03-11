a6ad4843c005276defd10e71204d2ec360204228
taymoor89 <taymoor89@users.noreply.github.com>
2025-03-11 08:05:59 +0000

    Deploy preview for PR 790 🛫

---
+++ pr-preview/pr-790/storybook/assets/ContentAreaToolbar.component-C_f9pJgm.js
@@ -0,0 +1,9 @@
+import{j as o}from"./jsx-runtime-D6fbYt3N.js";const r=`
+  jn-bg-theme-background-lvl-1
+  jn-py-3
+  jn-px-6
+  jn-flex
+  jn-items-center
+  jn-justify-end
+`,t=({className:e="",children:n,...a})=>o.jsx("div",{className:`juno-content-area-toolbar ${r} ${e}`,...a,children:n});try{t.displayName="ContentAreaToolbar",t.__docgenInfo={description:`ContentAreaToolbar represents the main toolbar within the content area.
+It is designed to hold main actions relevant to the current page.`,displayName:"ContentAreaToolbar",props:{className:{defaultValue:{value:""},description:"Additional CSS classes for custom styling.",name:"className",required:!1,type:{name:"string"}},children:{defaultValue:null,description:"Custom content to render within the content area toolbar.",name:"children",required:!1,type:{name:"ReactNode"}}}}}catch{}export{t as C};
