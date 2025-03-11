28187c3bb8ffcdcfa4cea99720ef5d7479073fe6
I531348 <guoda.puidokaite@sap.com>
2025-03-11 10:25:15 +0100

    update toolbar

--- apps/example/src/components/peaks/list/PeaksFilterToolbar.tsx
+++ apps/example/src/components/peaks/list/PeaksFilterToolbar.tsx
@@ -64,16 +64,61 @@ const PeaksFilterToolbar: React.FC<PeaksFilterToolbarProps> = ({
   viewType,
   setViewType,
 }) => (
-  <DataGridToolbar>
-    <Stack direction="horizontal" distribution="evenly" alignment="center" gap="5">
-      <SearchInput placeholder="Search by Name..." style={{ flexGrow: 1, minWidth: "300px" }} />
-      <InputGroup>
+  <>
+    <DataGridToolbar>
+      <Stack direction="horizontal" alignment="center" gap="5">
+        <SearchInput placeholder="Search by Name..." style={{ flexGrow: 1, minWidth: "240px" }} />
+        <InputGroup>
+          <Select
+            style={{ minWidth: "150px" }}
+            placeholder="Filter by"
+            label={selectedFilterKey ? "Filter by" : ""}
+            value={selectedFilterKey || ""}
+            onChange={(value) => setSelectedFilterKey(value as string)}
+          >
+            {filterKeys.map((filterKey) => (
+              <SelectOption key={filterKey} value={filterKey}>
+                {filterKey.charAt(0).toUpperCase() + filterKey.slice(1)}
+              </SelectOption>
+            ))}
+          </Select>
+
+          {selectedFilterKey && !["minHeight", "maxHeight"].includes(selectedFilterKey) && (
+            <Select
+              value={droplistSelections[selectedFilterKey] || ""}
+              onChange={(value) => addFilter(selectedFilterKey, value as string)}
+              placeholder="Choose filter value"
+              style={{ minWidth: "150px" }}
+            >
+              {(availableOptions[selectedFilterKey] || []).map((option) => (
+                <SelectOption
+                  key={option}
+                  value={option}
+                  disabled={filterSelections[selectedFilterKey].includes(option)}
+                >
+                  {option}
+                </SelectOption>
+              ))}
+            </Select>
+          )}
+
+          {["minHeight", "maxHeight"].includes(selectedFilterKey || "") && (
+            <TextInput
+              placeholder={`Enter ${selectedFilterKey!.charAt(0).toUpperCase() + selectedFilterKey!.slice(1)}`}
+              value={selectedFilterKey === "minHeight" ? minHeight : maxHeight}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                selectedFilterKey === "minHeight" ? setMinHeight(e.target.value) : setMaxHeight(e.target.value)
+              }
+              style={{ minWidth: "150px" }}
+            />
+          )}
+        </InputGroup>
         <Select
           style={{ minWidth: "150px" }}
-          placeholder="Filter by"
-          label={selectedFilterKey ? "Filter by" : ""}
-          value={selectedFilterKey || ""}
-          onChange={(value) => setSelectedFilterKey(value as string)}
+          label="Sort by"
+          placeholder="Sort by"
+          value={selectedSortKey || ""}
+          onChange={(value) => setSelectedSortKey(value as string)}
         >
           {filterKeys.map((filterKey) => (
             <SelectOption key={filterKey} value={filterKey}>
@@ -81,88 +126,56 @@ const PeaksFilterToolbar: React.FC<PeaksFilterToolbarProps> = ({
             </SelectOption>
           ))}
         </Select>
-
-        {selectedFilterKey && !["minHeight", "maxHeight"].includes(selectedFilterKey) && (
-          <Select
-            value={droplistSelections[selectedFilterKey] || ""}
-            onChange={(value) => addFilter(selectedFilterKey, value as string)}
-            placeholder="Choose filter value"
-            style={{ minWidth: "200px" }}
-          >
-            {(availableOptions[selectedFilterKey] || []).map((option) => (
-              <SelectOption key={option} value={option} disabled={filterSelections[selectedFilterKey].includes(option)}>
-                {option}
-              </SelectOption>
-            ))}
-          </Select>
-        )}
-
-        {["minHeight", "maxHeight"].includes(selectedFilterKey || "") && (
-          <TextInput
-            placeholder={`Enter ${selectedFilterKey!.charAt(0).toUpperCase() + selectedFilterKey!.slice(1)}`}
-            value={selectedFilterKey === "minHeight" ? minHeight : maxHeight}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              selectedFilterKey === "minHeight" ? setMinHeight(e.target.value) : setMaxHeight(e.target.value)
-            }
-            style={{ minWidth: "200px" }}
-          />
-        )}
-      </InputGroup>
-      <Select
-        style={{ minWidth: "150px" }}
-        label="Sort by"
-        placeholder="Sort by"
-        value={selectedSortKey || ""}
-        onChange={(value) => setSelectedSortKey(value as string)}
-      >
-        {filterKeys.map((filterKey) => (
-          <SelectOption key={filterKey} value={filterKey}>
-            {filterKey.charAt(0).toUpperCase() + filterKey.slice(1)}
-          </SelectOption>
-        ))}
-      </Select>
-      <InputGroup>
-        <Button onClick={() => setSortDirection("asc")} disabled={sortDirection === "asc"} variant="subdued">
-          Asc
-        </Button>
-        <Button onClick={() => setSortDirection("desc")} disabled={sortDirection === "desc"} variant="subdued">
-          Desc
-        </Button>
-      </InputGroup>
-      <ViewToggleButtons currentView={viewType} toggleView={setViewType} />
-    </Stack>
-    {(Object.entries(filterSelections).some(([_, values]) => values.length > 0) || minHeight || maxHeight) && (
-      <Stack direction="horizontal" gap="4" alignment="center">
-        <Stack direction="horizontal" gap="1">
-          {Object.entries(filterSelections).map(
-            ([key, values]) =>
-              values.length > 0 && (
-                <div key={key} style={{ display: "flex", alignItems: "center" }}>
-                  <span style={{ fontSize: "0.85rem", fontWeight: "normal", color: "#555", marginRight: "8px" }}>
-                    {key.charAt(0).toUpperCase() + key.slice(1)}:
-                  </span>
-                  <Stack direction="horizontal" gap="1" wrap style={{ justifyContent: "flex-start" }}>
-                    {values.map((value) => (
-                      <Pill
-                        key={`${key}:${value}`}
-                        pillValue={value}
-                        closeable
-                        onClose={() => removeFilter(key, value)}
-                      />
-                    ))}
-                  </Stack>
-                </div>
-              )
-          )}
-          {minHeight && <Pill pillValue={`Min Height: ${minHeight}`} closeable onClose={() => setMinHeight("")} />}
-          {maxHeight && <Pill pillValue={`Max Height: ${maxHeight}`} closeable onClose={() => setMaxHeight("")} />}
-        </Stack>
-        <Button variant="subdued" onClick={clearAllFilters}>
-          Clear All Filters
-        </Button>
+        <InputGroup style={{ flexShrink: 1 }}>
+          <Button onClick={() => setSortDirection("asc")} disabled={sortDirection === "asc"} variant="subdued">
+            Asc
+          </Button>
+          <Button onClick={() => setSortDirection("desc")} disabled={sortDirection === "desc"} variant="subdued">
+            Desc
+          </Button>
+        </InputGroup>
+        <ViewToggleButtons currentView={viewType} toggleView={setViewType} />
       </Stack>
+    </DataGridToolbar>
+    {(Object.entries(filterSelections).some(([_, values]) => values.length > 0) || minHeight || maxHeight) && (
+      <DataGridToolbar>
+        <Stack
+          direction="horizontal"
+          gap="4"
+          alignment="center"
+          style={{ justifyContent: "space-between", width: "100%" }}
+        >
+          <Stack direction="horizontal" gap="1" wrap style={{ justifyContent: "flex-start", flexWrap: "wrap" }}>
+            {Object.entries(filterSelections).map(
+              ([key, values]) =>
+                values.length > 0 && (
+                  <div key={key} style={{ display: "flex", alignItems: "center" }}>
+                    <span style={{ fontSize: "0.85rem", fontWeight: "normal", color: "#555", marginRight: "8px" }}>
+                      {key.charAt(0).toUpperCase() + key.slice(1)}:
+                    </span>
+                    <Stack direction="horizontal" gap="1" wrap style={{ justifyContent: "flex-start" }}>
+                      {values.map((value) => (
+                        <Pill
+                          key={`${key}:${value}`}
+                          pillValue={value}
+                          closeable
+                          onClose={() => removeFilter(key, value)}
+                        />
+                      ))}
+                    </Stack>
+                  </div>
+                )
+            )}
+            {minHeight && <Pill pillValue={`Min Height: ${minHeight}`} closeable onClose={() => setMinHeight("")} />}
+            {maxHeight && <Pill pillValue={`Max Height: ${maxHeight}`} closeable onClose={() => setMaxHeight("")} />}
+          </Stack>
+          <Button variant="subdued" onClick={clearAllFilters}>
+            Clear All Filters
+          </Button>
+        </Stack>
+      </DataGridToolbar>
     )}
-  </DataGridToolbar>
+  </>
 )
 
 export default PeaksFilterToolbar
