6c74f4b7c53bb1b3450bb9c3ef17e497bd6d2023
TilmanHaupt <tilman.haupt@sap.com>
2025-03-13 13:49:01 +0100

    chore(greenhouse): add header

--- apps/greenhouse/src/components/core-apps/org-admin/components/plugins/plugins/components/Toolbar.tsx
+++ apps/greenhouse/src/components/core-apps/org-admin/components/plugins/plugins/components/Toolbar.tsx
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import React, { useState, useEffect } from "react"
 import { SearchInput, Select, SelectOption, Stack, InputGroup, Pill } from "@cloudoperators/juno-ui-components"
 import { usePluginActions, usePluginConfig, useLabelValuesFilters } from "./StoreProvider"
