f377a4d2d1cd465a81306c95c4cb6a39d37f81c1
taymoor89 <taymoor89@users.noreply.github.com>
2025-03-17 07:16:27 +0000

    Deploy preview for PR 816 🛫

---
+++ pr-preview/pr-816/storybook/assets/DataList.stories-QLL2yd0q.js
@@ -0,0 +1,112 @@
+import{j as p}from"./jsx-runtime-D6fbYt3N.js";import{r as E}from"./index-DysCNOs_.js";import{P as _}from"./index-BHU3UH5b.js";import{D as x,a,S as r,A as e,P as s,G as o,b as z}from"./DataListRow.stories-CNA0BitM.js";import{D as k}from"./DataListCell.component-DqXRgVj9.js";import"./DataListCheckboxCell.component-Cv2mUUoa.js";import"./FormHint.component-Dbm6hcqV.js";import"./Icon.component-BXxTB2S-.js";import"./widgets-BR6-ubtP.js";import"./DataListCell.stories-CKDbddRW.js";const Q={title:"Deprecated/DataList/DataList",component:x,argTypes:{items:{table:{disable:!0}},children:{control:!1}},parameters:{docs:{description:{component:"DataList is deprecated and will be removed. Please use DataGrid instead."}}}},t=({items:A,...T})=>p.jsx(x,{...T,children:A.map((F,g)=>p.jsx(z,{children:F.items.map(($,j)=>E.createElement(k,{...$,key:`${g}_${j}`}))},`${g}`))});t.propTypes={items:_.array};const n={render:t,parameters:{docs:{description:{story:"Default Juno DataList for displaying data"}}},args:{items:[{...a.args},{...a.args},{...a.args},{...a.args},{...a.args}]}},i={render:t,parameters:{docs:{description:{story:"Selectable Juno DataList for displaying and selecting data"}}},args:{selectable:!0,items:[{...r.args},{...r.args},{...r.args},{...r.args},{...r.args}]}},d={render:t,parameters:{docs:{description:{story:"Juno DataList with one column set to 'auto' to maximize its width"}}},args:{items:[{...e.args},{...e.args},{...e.args},{...e.args},{...e.args}]}},c={render:t,parameters:{docs:{description:{story:"Juno DataList with percentage-based column widths"}}},args:{items:[{...s.args},{...s.args},{...s.args},{...s.args},{...s.args}]}},m={render:t,parameters:{docs:{description:{story:"Juno DataList with grid column-based column widths"}}},args:{items:[{...o.args},{...o.args},{...o.args},{...o.args},{...o.args}]}};var l,u,D;n.parameters={...n.parameters,docs:{...(l=n.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: Template,
+  parameters: {
+    docs: {
+      description: {
+        story: "Default Juno DataList for displaying data"
+      }
+    }
+  },
+  args: {
+    items: [{
+      ...DataListRowStory.args
+    }, {
+      ...DataListRowStory.args
+    }, {
+      ...DataListRowStory.args
+    }, {
+      ...DataListRowStory.args
+    }, {
+      ...DataListRowStory.args
+    }]
+  }
+}`,...(D=(u=n.parameters)==null?void 0:u.docs)==null?void 0:D.source}}};var y,L,S;i.parameters={...i.parameters,docs:{...(y=i.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: Template,
+  parameters: {
+    docs: {
+      description: {
+        story: "Selectable Juno DataList for displaying and selecting data"
+      }
+    }
+  },
+  args: {
+    selectable: true,
+    items: [{
+      ...SelectableDataListRowStory.args
+    }, {
+      ...SelectableDataListRowStory.args
+    }, {
+      ...SelectableDataListRowStory.args
+    }, {
+      ...SelectableDataListRowStory.args
+    }, {
+      ...SelectableDataListRowStory.args
+    }]
+  }
+}`,...(S=(L=i.parameters)==null?void 0:L.docs)==null?void 0:S.source}}};var w,h,R;d.parameters={...d.parameters,docs:{...(w=d.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  render: Template,
+  parameters: {
+    docs: {
+      description: {
+        story: "Juno DataList with one column set to 'auto' to maximize its width"
+      }
+    }
+  },
+  args: {
+    items: [{
+      ...AutoWidthDataListRowStory.args
+    }, {
+      ...AutoWidthDataListRowStory.args
+    }, {
+      ...AutoWidthDataListRowStory.args
+    }, {
+      ...AutoWidthDataListRowStory.args
+    }, {
+      ...AutoWidthDataListRowStory.args
+    }]
+  }
+}`,...(R=(h=d.parameters)==null?void 0:h.docs)==null?void 0:R.source}}};var b,f,P;c.parameters={...c.parameters,docs:{...(b=c.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: Template,
+  parameters: {
+    docs: {
+      description: {
+        story: "Juno DataList with percentage-based column widths"
+      }
+    }
+  },
+  args: {
+    items: [{
+      ...PercentageWidthsDataListRowStory.args
+    }, {
+      ...PercentageWidthsDataListRowStory.args
+    }, {
+      ...PercentageWidthsDataListRowStory.args
+    }, {
+      ...PercentageWidthsDataListRowStory.args
+    }, {
+      ...PercentageWidthsDataListRowStory.args
+    }]
+  }
+}`,...(P=(f=c.parameters)==null?void 0:f.docs)==null?void 0:P.source}}};var W,G,J;m.parameters={...m.parameters,docs:{...(W=m.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  render: Template,
+  parameters: {
+    docs: {
+      description: {
+        story: "Juno DataList with grid column-based column widths"
+      }
+    }
+  },
+  args: {
+    items: [{
+      ...GridFittedDataListRowStory.args
+    }, {
+      ...GridFittedDataListRowStory.args
+    }, {
+      ...GridFittedDataListRowStory.args
+    }, {
+      ...GridFittedDataListRowStory.args
+    }, {
+      ...GridFittedDataListRowStory.args
+    }]
+  }
+}`,...(J=(G=m.parameters)==null?void 0:G.docs)==null?void 0:J.source}}};const U=["Default","Selectable","Auto","Percentage","GridFitted"];export{d as Auto,n as Default,m as GridFitted,c as Percentage,i as Selectable,U as __namedExportsOrder,Q as default};
---
+++ pr-preview/pr-816/storybook/assets/MenuItem.stories-Cxt-v4Rm.js
@@ -0,0 +1,54 @@
+import{j as i}from"./jsx-runtime-D6fbYt3N.js";import{a as R,M as T}from"./MenuItem.component-Cbyaj2T_.js";import{B as O}from"./Button.component-Dr0MUxN9.js";import"./index-DysCNOs_.js";import"./withDeprecationWarning.component-DcP1MqMf.js";import"./menu-BV8S7JmC.js";import"./keyboard-BayCKsX5.js";import"./use-text-value-CqB0ebPQ.js";import"./use-tree-walker-sKJwDSu4.js";import"./Icon.component-C7L5O_8e.js";import"./widgets-BR6-ubtP.js";import"./Spinner.component-DGrCFjaQ.js";const re={title:"Deprecated/Menu/MenuItem",component:R,parameters:{menuVariant:"normal",docs:{description:{component:"DEPRECATED. Use `PopupMenu` with `PopupMenu.Item` instead."}}},argTypes:{children:{control:!1}},decorators:[(y,z)=>{const{menuVariant:N}=z.parameters;return i.jsx(T,{variant:N,children:i.jsx(i.Fragment,{children:y()})})}]},e={args:{label:"A menu item",onClick:null}},r={args:{label:"A small menu item",onClick:null},parameters:{menuVariant:"small"}},a={args:{label:"A menu item with icon",icon:"deleteForever",onClick:null}},n={args:{label:"A small menu item with icon",icon:"deleteForever",onClick:null},parameters:{menuVariant:"small"}},s={args:{label:"A menu item as a link",href:"https://github.com/cloudoperators/juno"}},t={args:{label:"Menu Item as Button",onClick:()=>{}}},o={args:{children:[i.jsx(O,{label:"Delete",size:"small",variant:"subdued",icon:"deleteForever",className:"jn-w-full"},"1")]}},l={args:{disabled:!0,label:"Disabled menu item"}},m={args:{disabled:!0,label:"Disabled menu item as link",href:"https://github.com/cloudoperators/juno"}};var c,u,p;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    label: "A menu item",
+    onClick: null
+  }
+}`,...(p=(u=e.parameters)==null?void 0:u.docs)==null?void 0:p.source}}};var d,b,g;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    label: "A small menu item",
+    onClick: null
+  },
+  parameters: {
+    menuVariant: "small"
+  }
+}`,...(g=(b=r.parameters)==null?void 0:b.docs)==null?void 0:g.source}}};var h,I,M;a.parameters={...a.parameters,docs:{...(h=a.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    label: "A menu item with icon",
+    icon: "deleteForever",
+    onClick: null
+  }
+}`,...(M=(I=a.parameters)==null?void 0:I.docs)==null?void 0:M.source}}};var k,A,D;n.parameters={...n.parameters,docs:{...(k=n.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    label: "A small menu item with icon",
+    icon: "deleteForever",
+    onClick: null
+  },
+  parameters: {
+    menuVariant: "small"
+  }
+}`,...(D=(A=n.parameters)==null?void 0:A.docs)==null?void 0:D.source}}};var f,C,S;s.parameters={...s.parameters,docs:{...(f=s.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    label: "A menu item as a link",
+    href: "https://github.com/cloudoperators/juno"
+  }
+}`,...(S=(C=s.parameters)==null?void 0:C.docs)==null?void 0:S.source}}};var j,v,w;t.parameters={...t.parameters,docs:{...(j=t.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    label: "Menu Item as Button",
+    onClick: () => {}
+  }
+}`,...(w=(v=t.parameters)==null?void 0:v.docs)==null?void 0:w.source}}};var x,B,F;o.parameters={...o.parameters,docs:{...(x=o.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    children: [<Button label="Delete" size="small" variant="subdued" icon="deleteForever" className="jn-w-full" key="1" />]
+  }
+}`,...(F=(B=o.parameters)==null?void 0:B.docs)==null?void 0:F.source}}};var V,W,E;l.parameters={...l.parameters,docs:{...(V=l.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    label: "Disabled menu item"
+  }
+}`,...(E=(W=l.parameters)==null?void 0:W.docs)==null?void 0:E.source}}};var L,P,_;m.parameters={...m.parameters,docs:{...(L=m.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    label: "Disabled menu item as link",
+    href: "https://github.com/cloudoperators/juno"
+  }
+}`,...(_=(P=m.parameters)==null?void 0:P.docs)==null?void 0:_.source}}};const ae=["Default","SmallMenuItem","WithIcon","SmallWithIcon","MenuItemAsLink","MenuItemAsButton","MenuItemWithChildren","DisabledMenuItem","DisabledMenuItemAsLink"];export{e as Default,l as DisabledMenuItem,m as DisabledMenuItemAsLink,t as MenuItemAsButton,s as MenuItemAsLink,o as MenuItemWithChildren,r as SmallMenuItem,n as SmallWithIcon,a as WithIcon,ae as __namedExportsOrder,re as default};
