045751e1655bc20cbbfbb17877842f8812fb01aa
Hoda Noori <hoda.noori@sap.com>
2025-03-18 14:40:33 +0100

    chore(heureka): removes data normalization from usefetchServiceImageVersions

--- apps/heureka/src/components/Services/useFetchServiceImageVersions.ts
+++ apps/heureka/src/components/Services/useFetchServiceImageVersions.ts
@@ -3,46 +3,6 @@ import { useEffect, useState } from "react"
 import { useGetServiceImageVersionsQuery, Page } from "../../generated/graphql"
 import { getNormalizedImageVersionsData } from "./utils"
 
-type ServiceImageVersion = {
-  version: string
-  ccrn: string
-  issueCounts: {
-    critical: number
-    high: number
-    medium: number
-    low: number
-    none: number
-  }
-}
-
-type NormalizedData = {
-  imageVersions: ServiceImageVersion[]
-  totalCount: number
-  pages: Page[]
-}
-
-const getNormalizedData = (data: any): NormalizedData => ({
-  imageVersions: isEmpty(data?.ComponentVersions?.edges)
-    ? []
-    : data?.ComponentVersions?.edges
-        ?.filter((edge: any) => edge !== null)
-        .map(
-          (edge: any): ServiceImageVersion => ({
-            version: edge?.node?.version || "",
-            ccrn: edge?.node?.component?.ccrn || "",
-            issueCounts: edge?.node?.issueCounts || {
-              critical: 0,
-              high: 0,
-              medium: 0,
-              low: 0,
-              none: 0,
-            },
-          })
-        ),
-  totalCount: data?.ComponentVersions?.totalCount || 0,
-  pages: data?.ComponentVersions?.pageInfo?.pages || [],
-})
-
 type UseFetchServiceImageVersionsProps = {
   serviceCcrn: string
   pageSize?: number
@@ -60,11 +20,9 @@ export const useFetchServiceImageVersions = ({ serviceCcrn, pageSize = 10 }: Use
     },
   })
 
-  const {
-    imageVersions,
-    totalCount,
-    pages: pagesFromApi,
-  } = getNormalizedImageVersionsData(isEmpty(data) ? previousData : data)
+  const { imageVersions, totalCount, pages: pagesFromApi } = getNormalizedImageVersionsData(
+    isEmpty(data) ? previousData : data
+  )
 
   useEffect(() => {
     if (currentPage && currentPage > Math.ceil(totalCount / pageSize)) {
