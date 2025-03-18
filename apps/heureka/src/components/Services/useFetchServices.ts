b36cfa029e1a96a290e248c6a7b1e070e1924794
Hoda Noori <hoda.noori@sap.com>
2025-03-18 14:19:44 +0100

    chore(heureka): removes unnecessary changes

--- apps/heureka/src/components/Services/useFetchServices.ts
+++ apps/heureka/src/components/Services/useFetchServices.ts
@@ -17,11 +17,10 @@ type UseFetchServicesInput = {
 export const useFetchServices = ({ filterSettings, pageSize = 10 }: UseFetchServicesInput) => {
   const pagesRef = useRef<Page[]>()
   const [currentPage, setCurrentPage] = useState<number | undefined>(1)
-  const [pages, setPages] = useState<Page[]>()
   const { data, previousData, loading, error } = useGetServicesQuery({
     variables: {
       first: pageSize,
-      after: pages?.find((page) => page?.pageNumber === currentPage)?.after,
+      after: pagesRef.current?.find((page) => page?.pageNumber === currentPage)?.after,
       filter: getActiveServiceFilter(filterSettings),
       /**
        * TODO: make it dynamic
@@ -37,14 +36,14 @@ export const useFetchServices = ({ filterSettings, pageSize = 10 }: UseFetchServ
       ],
     },
   })
-  const { services, totalCount, pages: pagesFromApi } = getNormalizedData(isEmpty(data) ? previousData : data)
+  const { services, totalCount, pages } = getNormalizedData(isEmpty(data) ? previousData : data)
 
   // reset current page if data has not much records
   useEffect(() => {
     if (currentPage && currentPage > Math.ceil(totalCount / pageSize)) {
       setCurrentPage(1)
     }
-    setPages(pagesFromApi)
+    pagesRef.current = pages
   }, [totalCount])
 
   return {
