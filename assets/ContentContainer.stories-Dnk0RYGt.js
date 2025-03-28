0cc00e484f9cfa746ba23aa05cc692404d18da00
andypf <ap@a-pfau.de>
2025-03-28 14:01:51 +0000

    Deploying to gh-pages from @ cloudoperators/juno@cde9cb54df7354043ef153b4e864a9a41d62b66b 🚀

---
+++ assets/ContentContainer.stories-Dnk0RYGt.js
@@ -0,0 +1,3 @@
+import{j as s}from"./jsx-runtime-BP6H2k8O.js";import{C as o}from"./ContentContainer.component-BOzqeEB6.js";import"./index-DysCNOs_.js";const i={title:"Internal/ContentContainer",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},c=a=>s.jsx(o,{...a,children:"content"}),e={render:c};var t,r,n;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: Template
+}`,...(n=(r=e.parameters)==null?void 0:r.docs)==null?void 0:n.source}}};const l=["Centered"];export{e as Centered,l as __namedExportsOrder,i as default};
---
+++ assets/FilterPill.stories--7DbSdWx.js
@@ -0,0 +1,8 @@
+import{F as l}from"./FilterPill.component-C7ypU0-3.js";import"./jsx-runtime-BP6H2k8O.js";import"./index-DysCNOs_.js";import"./index-BHU3UH5b.js";import"./Icon.component-D0Qkx-jp.js";import"./widgets-BR6-ubtP.js";const n={title:"Deprecated/Filter/FilterPill",component:l,argTypes:{}},e={args:{filterKey:"os",filterKeyLabel:"OS",filterValue:"mac_os",filterValueLabel:"Mac OS"}};var r,t,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    filterKey: "os",
+    filterKeyLabel: "OS",
+    filterValue: "mac_os",
+    filterValueLabel: "Mac OS"
+  }
+}`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const f=["Default"];export{e as Default,f as __namedExportsOrder,n as default};
