bde77868becba2dd5d98a2db6255c1dfa983289a
mark-karnaukh-extern-sap <mark-karnaukh-extern-sap@users.noreply.github.com>
2025-03-28 13:24:19 +0000

    Deploy preview for PR 855 🛫

---
+++ pr-preview/pr-855/storybook/assets/ContentAreaToolbar.stories--RbeRCg2.js
@@ -0,0 +1,3 @@
+import{j as e}from"./jsx-runtime-BP6H2k8O.js";import{B as m}from"./Button.component-CpJaC6Qc.js";import{C as s}from"./ContentAreaToolbar.component-DDPqWSOu.js";import"./index-DysCNOs_.js";import"./Icon.component-D9enpJBw.js";import"./widgets-BR6-ubtP.js";import"./Spinner.component-RQWIWEoX.js";const f={title:"Layout/ContentAreaToolbar",component:s,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},c=n=>e.jsx(s,{...n,children:e.jsx(m,{children:"Main Action"})}),r={render:c};var o,t,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: Template
+}`,...(a=(t=r.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const b=["Basic"];export{r as Basic,b as __namedExportsOrder,f as default};
