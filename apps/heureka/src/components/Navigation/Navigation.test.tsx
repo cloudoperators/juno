b96c2e8ea6d9a09b21ff0d4397b6ac5b22b0215b
Hoda Noori <hoda.noori@sap.com>
2025-03-28 15:14:05 +0100

    chore(heureka): fixes navigation test

--- apps/heureka/src/components/Navigation/Navigation.test.tsx
+++ apps/heureka/src/components/Navigation/Navigation.test.tsx
@@ -5,7 +5,6 @@
 
 import React from "react"
 import { render, screen } from "@testing-library/react"
-import userEvent from "@testing-library/user-event"
 import { Navigation } from "./Navigation"
 import { UserView } from "../../store/StoreProvider/types"
 
@@ -13,17 +12,5 @@ describe("Navigation", () => {
   it("should render correctly", () => {
     render(<Navigation activeItem={UserView.Services} onChange={() => {}} />)
     expect(screen.getByText("Services")).toBeInTheDocument()
-    expect(screen.getByText("Issues")).toBeInTheDocument()
-  })
-
-  it("should call onChange", async () => {
-    //setup
-    const user = userEvent.setup()
-    const onChange = vitest.fn()
-    //arrange
-    render(<Navigation activeItem={UserView.Services} onChange={onChange} />)
-    await user.click(screen.getByRole("link", { name: "issues" }))
-    //assert
-    expect(onChange).toHaveBeenCalledWith("issues")
   })
 })
