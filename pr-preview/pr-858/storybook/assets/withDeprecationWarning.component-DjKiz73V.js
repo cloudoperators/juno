bc7d7b6283793e6e339af1051359cb6fd9f998ad
mark-karnaukh-extern-sap <mark-karnaukh-extern-sap@users.noreply.github.com>
2025-03-31 10:24:48 +0000

    Deploy preview for PR 858 🛫

---
+++ pr-preview/pr-858/storybook/assets/withDeprecationWarning.component-DjKiz73V.js
@@ -0,0 +1,1 @@
+import{j as o}from"./jsx-runtime-BP6H2k8O.js";import{r as e}from"./index-DysCNOs_.js";function s(n,r){return t=>(e.useEffect(()=>{console.warn(r)},[]),o.jsx(n,{...t}))}export{s as w};
