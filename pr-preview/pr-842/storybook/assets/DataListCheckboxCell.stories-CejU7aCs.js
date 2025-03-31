cec366688bd66f3467caed6c7eaba4fc9c2aa39a
barsukov <barsukov@users.noreply.github.com>
2025-03-31 11:41:24 +0000

    Deploy preview for PR 842 🛫

---
+++ pr-preview/pr-842/storybook/assets/DataListCheckboxCell.stories-CejU7aCs.js
@@ -0,0 +1,23 @@
+import{j as l}from"./jsx-runtime-BP6H2k8O.js";import"./index-DysCNOs_.js";import{D as c}from"./DataListCheckboxCell.component-Lm4oGhJe.js";import"./index-BHU3UH5b.js";import"./FormHint.component-9UgkrTjy.js";import"./Icon.component-D0Qkx-jp.js";import"./widgets-BR6-ubtP.js";import"./DataListCell.component-H7cKwrAH.js";const h={title:"Deprecated/DataList/DataListCheckboxCell",component:c,argTypes:{children:{control:!1}},parameters:{docs:{description:{component:"DataList is deprecated and will be removed. Please use DataGrid instead."}}}},d=p=>l.jsx(c,{...p}),e={render:d,parameters:{docs:{description:{story:"Juno DataListCheckboxCell for use in DataList"}}},args:{}},r={render:d,parameters:{docs:{description:{story:"Disabled Juno DataListCheckboxCell for use in DataList"}}},args:{disabled:!0}};var s,a,t;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: Template,
+  parameters: {
+    docs: {
+      description: {
+        story: "Juno DataListCheckboxCell for use in DataList"
+      }
+    }
+  },
+  args: {}
+}`,...(t=(a=e.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};var o,n,i;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: Template,
+  parameters: {
+    docs: {
+      description: {
+        story: "Disabled Juno DataListCheckboxCell for use in DataList"
+      }
+    }
+  },
+  args: {
+    disabled: true
+  }
+}`,...(i=(n=r.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};const g=["Default","Disabled"];export{e as Default,r as Disabled,g as __namedExportsOrder,h as default};
