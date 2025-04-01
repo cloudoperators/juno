8d091e4726239558ddf9d517a316e5851fc5e993
mark-karnaukh-extern-sap <mark-karnaukh-extern-sap@users.noreply.github.com>
2025-04-01 11:31:07 +0000

    Deploy preview for PR 864 🛫

--- pr-preview/pr-864/storybook/assets/TooltipContent.stories-BvpBoi6o.js
+++
@@ -1,4 +0,0 @@
-import{j as e}from"./jsx-runtime-BP6H2k8O.js";import{T as i,a,b as p}from"./TooltipTrigger.component-DbuajSeL.js";import"./index-DysCNOs_.js";import"./Icon.component-zDvP5GVG.js";import"./floating-ui.react-dom-C789hh8f.js";import"./index-DxJeRO9x.js";const x={title:"Components/Tooltip/TooltipContent",component:i,argTypes:{},decorators:[o=>e.jsx("div",{className:"jn-my-6 jn-flex jn-justify-center",children:e.jsxs(a,{initialOpen:!0,children:[e.jsx(p,{children:"clickMe"}),e.jsx(o,{})]})})]},l=({...o})=>e.jsx(i,{...o,children:"This is a tooltip"}),t={render:l,args:{}};var r,s,n;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  render: Template,
-  args: {}
-}`,...(n=(s=t.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};const f=["Default"];export{t as Default,f as __namedExportsOrder,x as default};
