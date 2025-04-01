37bcf77117a43dd45d3c5a783bfd8ff8fecb74a7
barsukov <pro.barsukov@gmail.com>
2025-04-01 11:42:33 +0000

    Deploying to gh-pages from @ cloudoperators/juno@cdba61c84f39dd94bf661f12c2fc18ff7e5f8445 🚀

---
+++ assets/SelectDivider.stories-C80eg1r8.js
@@ -0,0 +1,8 @@
+import{j as e}from"./jsx-runtime-BP6H2k8O.js";import{e as p}from"./index-DysCNOs_.js";import{S as m,a as s}from"./SelectOption.component-CA6Zev4K.js";import{P as n}from"./PortalProvider.component-Dpl-KYct.js";import"./Label.component-DhLm9IiA.js";import"./Icon.component-Cri6u08O.js";import"./widgets-BR6-ubtP.js";import"./Spinner.component-B25dgKgo.js";import"./FormHint.component-DtRGMMjY.js";import"./headlessui-float-pPOEMtsx.js";import"./index-DxJeRO9x.js";import"./keyboard-RYHMaleb.js";import"./floating-ui.react-dom-C789hh8f.js";import"./listbox-FgpIuTn9.js";import"./form-CGPENT-o.js";import"./use-text-value-BSk7WQuc.js";const u=`
+  jn-h-px 
+  jn-w-full
+  jn-bg-theme-background-lvl-3
+`,r=p.forwardRef(({className:t="",...l},d)=>e.jsx("div",{className:`juno-select-divider ${u} ${t}`,ref:d,...l}));r.displayName="SelectDivider";try{r.displayName="SelectDivider",r.__docgenInfo={description:"A visual border/divider between SelectOptions or SelectGroups",displayName:"SelectDivider",props:{className:{defaultValue:{value:""},description:"add a custom className",name:"className",required:!1,type:{name:"string"}}}}}catch{}const R={title:"Forms/Select/SelectDivider",component:r,argTypes:{},decorators:[t=>e.jsx(n,{children:t()})]},v=()=>e.jsxs(m,{open:!0,children:[e.jsx(s,{value:"1",children:"1"}),e.jsx(r,{}),e.jsx(s,{value:"3",children:"3"})]}),o={render:v,args:{}};var i,a,c;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: Template,
+  args: {}
+}`,...(c=(a=o.parameters)==null?void 0:a.docs)==null?void 0:c.source}}};const $=["Default"];export{o as Default,$ as __namedExportsOrder,R as default};
