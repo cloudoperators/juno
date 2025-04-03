05442da918f5d57c6887f1c6b82507438baaed11
Arturo Reuschenbach Puncernau <reuschenbach@gmail.com>
2025-04-04 00:29:34 +0200

    chore(heureka): removes dynamic width for grid

--- apps/heureka/src/components/Services/common/ImageVersionDetailsPanel/ImageVersionOccurrences.tsx
+++ apps/heureka/src/components/Services/common/ImageVersionDetailsPanel/ImageVersionOccurrences.tsx
@@ -15,17 +15,23 @@ type ToolTipBoxType = {
 
 const BoxWithTooltip = ({ instance }: ToolTipBoxType) => (
   <Box className="text-sm text-theme-light">
-    <div className="whitespace-nowrap">{instance?.pod || "--"}</div>
+    <div className="text-sm whitespace-nowrap overflow-hidden text-ellipsis">{instance?.pod || "--"}</div>
     <Stack>
-      <div className="whitespace-nowrap w-full">{instance?.cluster || "--"}</div>
+      <div className="text-sm whitespace-nowrap  w-full">{instance?.cluster || "--"}</div>
       <Tooltip triggerEvent="hover">
         <TooltipTrigger>
           <Icon icon="info" size="18" className="cursor-pointer hover:text-theme-primary" />
         </TooltipTrigger>
         <TooltipContent>
-          <span>Id: {instance?.id || "--"}</span>
+          <span> Namespace: {instance?.namespace || "--"}</span>
           <br />
           <span> Region: {instance?.region || "--"}</span>
+          <br />
+          <span> Cluster: {instance?.cluster || "--"}</span>
+          <br />
+          <span> Container: {instance?.container || "--"}</span>
+          <br />
+          <span> Pod: {instance?.pod || "--"}</span>
         </TooltipContent>
       </Tooltip>
     </Stack>
@@ -61,21 +67,7 @@ const groupInstances = (instances: ComponentInstance[], maxInstances: number) =>
     }
   })
 
-  // Calculate the minWidth based on the longest pod name
-  let minWidth
-  if (maxPodLength <= 14) {
-    minWidth = 150
-  } else if (maxPodLength <= 20) {
-    minWidth = 190
-  } else if (maxPodLength <= 30) {
-    minWidth = 230
-  } else if (maxPodLength <= 50) {
-    minWidth = 240
-  } else {
-    minWidth = 300
-  }
-
-  return { grouped, minWidth }
+  return grouped
 }
 
 type ImageVersionOccurrencesProps = {
@@ -84,7 +76,7 @@ type ImageVersionOccurrencesProps = {
 
 const ImageVersionOccurrences = ({ imageVersion }: ImageVersionOccurrencesProps) => {
   const [visibleInstancesCount, setVisibleInstancesCount] = useState(MAX_VISIBLE_ITEMS)
-  const { grouped, minWidth } = groupInstances(imageVersion.componentInstances || [], visibleInstancesCount)
+  const grouped = groupInstances(imageVersion.componentInstances || [], visibleInstancesCount)
 
   useEffect(() => {
     // Reset state when myProp changes
@@ -108,7 +100,7 @@ const ImageVersionOccurrences = ({ imageVersion }: ImageVersionOccurrencesProps)
                 <div className="mb-4">
                   Namespace <b>{namespace}</b> - Container <b>{container}</b>
                 </div>
-                <div className={`grid grid-cols-[repeat(auto-fill,_minmax(${minWidth}px,_1fr))] gap-4`}>
+                <div className="grid grid-cols-[repeat(auto-fill,_minmax(240px,_1fr))] gap-2">
                   {instances.map((item, i) => (
                     <BoxWithTooltip key={i} instance={item} />
                   ))}
