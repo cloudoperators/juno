962e3b2c75c885b88c91737ea5dbfcd82949fcdc
barsukov <barsukov@users.noreply.github.com>
2025-04-09 08:58:19 +0000

    Deploy preview for PR 885 🛫

---
+++ pr-preview/pr-885/storybook/assets/ContentAreaToolbar.stories-Dn5EUSIp.js
@@ -0,0 +1,11 @@
+import{j as o}from"./jsx-runtime-BP6H2k8O.js";import{B as i}from"./Button.component-BPOIm43F.js";import"./index-DysCNOs_.js";import"./Icon.component-B8_hStz_.js";import"./Spinner.component-DWsJM9DM.js";const d=`
+  jn-bg-theme-background-lvl-1
+  jn-py-3
+  jn-px-6
+  jn-flex
+  jn-items-center
+  jn-justify-end
+`,r=({className:e="",children:l,...c})=>o.jsx("div",{className:`juno-content-area-toolbar ${d} ${e}`,...c,children:l});try{r.displayName="ContentAreaToolbar",r.__docgenInfo={description:`ContentAreaToolbar represents the main toolbar within the content area.
+It is designed to hold main actions relevant to the current page.`,displayName:"ContentAreaToolbar",props:{className:{defaultValue:{value:""},description:"Additional CSS classes for custom styling.",name:"className",required:!1,type:{name:"string"}},children:{defaultValue:null,description:"Custom content to render within the content area toolbar.",name:"children",required:!1,type:{name:"ReactNode"}}}}}catch{}const j={title:"Layout/ContentAreaToolbar",component:r,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},m=e=>o.jsx(r,{...e,children:o.jsx(i,{children:"Main Action"})}),t={render:m};var a,n,s;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: Template
+}`,...(s=(n=t.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const f=["Basic"];export{t as Basic,f as __namedExportsOrder,j as default};
