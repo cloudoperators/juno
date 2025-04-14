6409f488c2da3caf69efbbf2c4be9cb2aed41dc7
hodanoori <hodanoori@users.noreply.github.com>
2025-04-14 14:29:48 +0000

    Deploy preview for PR 892 🛫

---
+++ pr-preview/pr-892/storybook/assets/TooltipTrigger.stories-xBpf7Bpw.js
@@ -0,0 +1,14 @@
+import{j as e}from"./jsx-runtime-BP6H2k8O.js";import{a as o,T as d,b as g}from"./TooltipTrigger.component-B14M9-jD.js";import{I as h}from"./Icon.component-B8_hStz_.js";import"./index-DysCNOs_.js";import"./floating-ui.react-dom-C789hh8f.js";import"./index-DxJeRO9x.js";const A={title:"Components/Tooltip/TooltipTrigger",component:o,argTypes:{},decorators:[r=>e.jsx("div",{className:"jn-my-6 jn-flex jn-justify-center",children:e.jsxs(d,{initialOpen:!0,children:[e.jsx(r,{}),e.jsx(g,{children:"This is a tooltip"})]})})]},m=({...r})=>e.jsx(o,{...r,children:"clickMe"}),u=({...r})=>e.jsx(o,{asChild:!0,...r,children:e.jsx(h,{})}),t={render:m,args:{}},s={render:u,args:{},parameters:{docs:{description:{story:"If the asChild option is set on the TooltipTrigger the passed child element is used as the tooltip trigger. This is useful for when you want to use e.g. an Icon or Button as the trigger"}}}};var i,n,a;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: Template,
+  args: {}
+}`,...(a=(n=t.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};var p,l,c;s.parameters={...s.parameters,docs:{...(p=s.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: TemplateAsChildAnchor,
+  args: {},
+  parameters: {
+    docs: {
+      description: {
+        story: "If the asChild option is set on the TooltipTrigger the passed child element is used as the tooltip trigger. This is useful for when you want to use e.g. an Icon or Button as the trigger"
+      }
+    }
+  }
+}`,...(c=(l=s.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};const I=["Default","AsChildTooltipTrigger"];export{s as AsChildTooltipTrigger,t as Default,I as __namedExportsOrder,A as default};
