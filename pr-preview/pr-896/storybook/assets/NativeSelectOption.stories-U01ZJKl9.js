cf5aeee1808fab51aa11eb5e18065b8938d1da22
mark-karnaukh-extern-sap <mark-karnaukh-extern-sap@users.noreply.github.com>
2025-04-15 09:14:25 +0000

    Deploy preview for PR 896 🛫

---
+++ pr-preview/pr-896/storybook/assets/NativeSelectOption.stories-U01ZJKl9.js
@@ -0,0 +1,12 @@
+import{N as n}from"./NativeSelectOption.component-DmhVo9_a.js";import"./jsx-runtime-BP6H2k8O.js";import"./index-DysCNOs_.js";const m={title:"Forms/NativeSelect/NativeSelectOption",component:n,argTypes:{}},e={args:{value:"my-option-value",label:"My option"}},a={args:{value:"my-disabled-option-value",label:"My disabled option",disabled:!0}};var o,t,r;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    value: "my-option-value",
+    label: "My option"
+  }
+}`,...(r=(t=e.parameters)==null?void 0:t.docs)==null?void 0:r.source}}};var s,l,i;a.parameters={...a.parameters,docs:{...(s=a.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    value: "my-disabled-option-value",
+    label: "My disabled option",
+    disabled: true
+  }
+}`,...(i=(l=a.parameters)==null?void 0:l.docs)==null?void 0:i.source}}};const u=["Default","Disabled"];export{e as Default,a as Disabled,u as __namedExportsOrder,m as default};
