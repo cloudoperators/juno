377852b3eb04c91bc0a871391b209191c744c09c
mark-karnaukh-extern-sap <mark-karnaukh-extern-sap@users.noreply.github.com>
2025-04-16 08:34:57 +0000

    Deploy preview for PR 900 🛫

---
+++ pr-preview/pr-900/storybook/assets/SelectDivider.stories-CzjgSdSl.js
@@ -0,0 +1,8 @@
+import{j as e}from"./jsx-runtime-BP6H2k8O.js";import{e as n}from"./index-DysCNOs_.js";import{S as p,a as o}from"./SelectOption.component-CmUWcvzZ.js";import{P as m}from"./PortalProvider.component-Dpl-KYct.js";import"./portal-CKwzW6mc.js";import"./index-DxJeRO9x.js";import"./floating-ui.react-BNnl2Uqq.js";import"./frozen-EMDjdm_j.js";import"./use-text-value-W-8Y9so9.js";import"./Label.component-DhLm9IiA.js";import"./Icon.component-Bcgf_F3P.js";import"./Spinner.component-DWsJM9DM.js";import"./FormHint.component-DjeKdvkd.js";const u=`
+  jn-h-px 
+  jn-w-full
+  jn-bg-theme-background-lvl-3
+`,r=n.forwardRef(({className:t="",...l},d)=>e.jsx("div",{className:`juno-select-divider ${u} ${t}`,ref:d,...l}));r.displayName="SelectDivider";try{r.displayName="SelectDivider",r.__docgenInfo={description:"A visual border/divider between SelectOptions or SelectGroups",displayName:"SelectDivider",props:{className:{defaultValue:{value:""},description:"add a custom className",name:"className",required:!1,type:{name:"string"}}}}}catch{}const P={title:"Forms/Select/SelectDivider",component:r,argTypes:{},decorators:[t=>e.jsx(m,{children:t()})]},v=()=>e.jsxs(p,{open:!0,children:[e.jsx(o,{value:"1",children:"1"}),e.jsx(r,{}),e.jsx(o,{value:"3",children:"3"})]}),s={render:v,args:{}};var a,i,c;s.parameters={...s.parameters,docs:{...(a=s.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: Template,
+  args: {}
+}`,...(c=(i=s.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};const T=["Default"];export{s as Default,T as __namedExportsOrder,P as default};
