94276caab6fc52b692318024e0c2006a2e748c37
mark-karnaukh-extern-sap <mark-karnaukh-extern-sap@users.noreply.github.com>
2025-04-16 12:35:06 +0000

    Deploy preview for PR 900 🛫

---
+++ pr-preview/pr-900/storybook/assets/TooltipContent.stories-CuzKzXym.js
@@ -0,0 +1,4 @@
+import{j as e}from"./jsx-runtime-BP6H2k8O.js";import{b as i,T as a,a as p}from"./TooltipTrigger.component-4iFlzmWO.js";import"./index-DysCNOs_.js";import"./floating-ui.react-Ck1p4xHI.js";import"./index-DxJeRO9x.js";import"./Icon.component-A9eTkufy.js";const x={title:"Components/Tooltip/TooltipContent",component:i,argTypes:{},decorators:[o=>e.jsx("div",{className:"jn-my-6 jn-flex jn-justify-center",children:e.jsxs(a,{initialOpen:!0,children:[e.jsx(p,{children:"clickMe"}),e.jsx(o,{})]})})]},l=({...o})=>e.jsx(i,{...o,children:"This is a tooltip"}),t={render:l,args:{}};var r,s,n;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: Template,
+  args: {}
+}`,...(n=(s=t.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};const f=["Default"];export{t as Default,f as __namedExportsOrder,x as default};
