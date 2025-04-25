b40d08c4f56e35e3006a137f0ed40d85fadeb2ed
mark-karnaukh-extern-sap <mark-karnaukh-extern-sap@users.noreply.github.com>
2025-04-25 12:04:39 +0000

    Deploy preview for PR 896 🛫

---
+++ pr-preview/pr-896/storybook/assets/SelectOption.stories-DUAAk1Br.js
@@ -0,0 +1,23 @@
+import{j as n}from"./jsx-runtime-BP6H2k8O.js";import{a as v,S as x}from"./SelectOption.component-BPS_FvGL.js";import{P as f}from"./PortalProvider.component-Dpl-KYct.js";import"./index-DysCNOs_.js";import"./portal-BABn4eeb.js";import"./index-DxJeRO9x.js";import"./floating-ui.react-p8NZUn7b.js";import"./frozen-CZ7yXSSc.js";import"./use-text-value-kHf2xNJv.js";import"./Label.component-DhLm9IiA.js";import"./Icon.component-Bcgf_F3P.js";import"./Spinner.component-DWsJM9DM.js";import"./FormHint.component-DjeKdvkd.js";const q={title:"Forms/Select/SelectOption",component:v,argTypes:{children:{control:{type:"string"},table:{type:{summary:"string"}}}},decorators:[s=>n.jsx(f,{children:s()})]},o=s=>n.jsx(x,{open:!0,children:n.jsx(v,{...s})}),e={render:o,args:{value:"Option 1"}},r={render:o,args:{disabled:!0,value:"Disabled Option"}},t={render:o,args:{children:["Option 1"]}},a={render:o,args:{value:"option-1",label:"Option 1"}};var i,p,l;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    value: "Option 1"
+  }
+}`,...(l=(p=e.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var c,m,d;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    disabled: true,
+    value: "Disabled Option"
+  }
+}`,...(d=(m=r.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var u,g,O;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    children: ["Option 1"]
+  }
+}`,...(O=(g=t.parameters)==null?void 0:g.docs)==null?void 0:O.source}}};var b,S,h;a.parameters={...a.parameters,docs:{...(b=a.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    value: "option-1",
+    label: "Option 1"
+  }
+}`,...(h=(S=a.parameters)==null?void 0:S.docs)==null?void 0:h.source}}};const z=["Default","Disabled","ChildrenOnly","withLabel"];export{t as ChildrenOnly,e as Default,r as Disabled,z as __namedExportsOrder,q as default,a as withLabel};
---
+++ pr-preview/pr-896/storybook/assets/TopNavigationItem.stories-4uTkFFS2.js
@@ -0,0 +1,36 @@
+import{j as i}from"./jsx-runtime-BP6H2k8O.js";import{a as A,T as k}from"./TopNavigationItem.component-BrVYGz82.js";import{K as w}from"./Icon.component-Bcgf_F3P.js";import"./index-DysCNOs_.js";import"./NavigationItem.component-Ut54wNRH.js";const L={title:"Navigation/TopNavigation/TopNavigationItem",component:A,argTypes:{icon:{options:[null,...Object.values(w)],control:{type:"select"}},onClick:{control:!1},children:{control:!1}},parameters:{actions:{argTypesRegex:null}}},e=W=>i.jsx(k,{children:i.jsx(A,{...W})}),r={render:e,args:{label:"Navigation Item"}},a={render:e,args:{label:"Navigation Item",active:!0}},n={render:e,args:{disabled:!0,label:"Disabled Item"}},t={render:e,args:{label:"Navigation Item",icon:"warning"}},s={render:e,args:{label:"Navigation Item",href:"#"}},o={render:e,args:{value:"itm-1",children:"Item 1"}};var c,l,m;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    label: "Navigation Item"
+  }
+}`,...(m=(l=r.parameters)==null?void 0:l.docs)==null?void 0:m.source}}};var p,d,g;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    label: "Navigation Item",
+    active: true
+  }
+}`,...(g=(d=a.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var u,v,I;n.parameters={...n.parameters,docs:{...(u=n.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    disabled: true,
+    label: "Disabled Item"
+  }
+}`,...(I=(v=n.parameters)==null?void 0:v.docs)==null?void 0:I.source}}};var b,T,N;t.parameters={...t.parameters,docs:{...(b=t.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    label: "Navigation Item",
+    icon: "warning"
+  }
+}`,...(N=(T=t.parameters)==null?void 0:T.docs)==null?void 0:N.source}}};var h,f,x;s.parameters={...s.parameters,docs:{...(h=s.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    label: "Navigation Item",
+    href: "#"
+  }
+}`,...(x=(f=s.parameters)==null?void 0:f.docs)==null?void 0:x.source}}};var D,S,j;o.parameters={...o.parameters,docs:{...(D=o.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    value: "itm-1",
+    children: "Item 1"
+  }
+}`,...(j=(S=o.parameters)==null?void 0:S.docs)==null?void 0:j.source}}};const O=["Default","Active","Disabled","WithIcon","AsLink","WithChildren"];export{a as Active,s as AsLink,r as Default,n as Disabled,o as WithChildren,t as WithIcon,O as __namedExportsOrder,L as default};
