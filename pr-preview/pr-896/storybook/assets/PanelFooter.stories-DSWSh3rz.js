a1edd5529c7d711bc80695d5bb6ad906704d7699
mark-karnaukh-extern-sap <mark-karnaukh-extern-sap@users.noreply.github.com>
2025-04-28 09:35:01 +0000

    Deploy preview for PR 896 🛫

---
+++ pr-preview/pr-896/storybook/assets/PanelFooter.stories-DSWSh3rz.js
@@ -0,0 +1,11 @@
+import{j as e}from"./jsx-runtime-D-gQZ2Nk.js";import{B as i}from"./Button.component-B2evOPXG.js";import{P as d,a as l}from"./PanelBody.component-CgyEUGy3.js";import{P as s}from"./PanelFooter.component-DkhjBlB4.js";import{P as c}from"./PortalProvider.component-CHvemcrH.js";import"./index-BXnWnNu-.js";import"./Icon.component-uANO5hmX.js";import"./Spinner.component-DqgRKBmm.js";import"./index-CzvS-M9C.js";const v={title:"Layout/Panel/PanelFooter",component:s,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}},decorators:[r=>e.jsx(c,{children:e.jsx("div",{className:"jn-contrast-100",children:r()})})]},m=r=>e.jsxs("div",{children:[e.jsx(d,{heading:"My Panel",opened:!0,children:e.jsx(l,{footer:e.jsx(s,{...r,children:e.jsx(i,{children:"Do it"})}),children:"This is the panel body"})}),e.jsx("div",{className:"dummy-css-ignore jn-h-[250px]",children:"Content Area"})]}),o={render:m,parameters:{docs:{description:{story:"A container for panel footer elements, typically buttons. Add the footer to the PanelBody component via its `footer` parameter."}}},args:{}};var t,n,a;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: Template,
+  parameters: {
+    docs: {
+      description: {
+        story: "A container for panel footer elements, typically buttons. Add the footer to the PanelBody component via its \`footer\` parameter."
+      }
+    }
+  },
+  args: {}
+}`,...(a=(n=o.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};const A=["Footer"];export{o as Footer,A as __namedExportsOrder,v as default};
