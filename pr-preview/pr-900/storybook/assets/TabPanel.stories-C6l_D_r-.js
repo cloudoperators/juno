64701c76a156ef7d93dfe4399879b5dddfbcd217
mark-karnaukh-extern-sap <mark-karnaukh-extern-sap@users.noreply.github.com>
2025-04-29 14:10:38 +0000

    Deploy preview for PR 900 🛫

---
+++ pr-preview/pr-900/storybook/assets/TabPanel.stories-C6l_D_r-.js
@@ -0,0 +1,5 @@
+import{T as o}from"./TabPanel.component-DWIUlWqe.js";import"./jsx-runtime-D-gQZ2Nk.js";import"./index-BXnWnNu-.js";import"./TabPanel-B9YjS3EW.js";import"./index-BKNjMPK8.js";const m={title:"Layout/Tabs/TabPanel",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},e={args:{children:"Tab panel content goes here."}};var a,r,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: "Tab panel content goes here."
+  }
+}`,...(t=(r=e.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const i=["Default"];export{e as Default,i as __namedExportsOrder,m as default};
