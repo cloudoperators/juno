59e5f296e39f8ce63ca434abf9328c4d8b7698f7
mark-karnaukh-extern-sap <mark-karnaukh-extern-sap@users.noreply.github.com>
2025-04-29 12:59:30 +0000

    Deploy preview for PR 915 🛫

---
+++ pr-preview/pr-915/storybook/assets/FormSection.stories-BUb26hxK.js
@@ -0,0 +1,20 @@
+import{j as e}from"./jsx-runtime-D-gQZ2Nk.js";import{F as c}from"./FormSection.component-Dxu0H8EZ.js";import{T as t}from"./TextInput.component--oaZVSS8.js";import{F as s}from"./FormRow.component-DUpRiQZ9.js";import"./index-BXnWnNu-.js";import"./Label.component-Bubk172Q.js";import"./Icon.component-KLQ-3Fva.js";import"./FormHint.component-k5YuOPZg.js";const A={title:"Forms/FormSection",component:c,argTypes:{items:{table:{disable:!0}},children:{control:!1,table:{type:{summary:"ReactNode"}}}}},p=({children:x,...u})=>e.jsx(c,{...u,children:x}),r={render:p,args:{children:[e.jsx(s,{children:e.jsx(t,{label:"Address Line 1"})},"1"),e.jsx(s,{children:e.jsx(t,{label:"Address Line 2"})},"2")]}},o={render:p,args:{title:"Form Section With Title",children:[e.jsx(s,{children:e.jsx(t,{label:"Address Line 1"})},"1"),e.jsx(s,{children:e.jsx(t,{label:"Address Line 2"})},"2")]}};var n,a,i;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    children: [<FormRow key="1">
+        <TextInput label="Address Line 1" />
+      </FormRow>, <FormRow key="2">
+        <TextInput label="Address Line 2" />
+      </FormRow>]
+  }
+}`,...(i=(a=r.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};var l,m,d;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    title: "Form Section With Title",
+    children: [<FormRow key="1">
+        <TextInput label="Address Line 1" />
+      </FormRow>, <FormRow key="2">
+        <TextInput label="Address Line 2" />
+      </FormRow>]
+  }
+}`,...(d=(m=o.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};const L=["Default","WithTitle"];export{r as Default,o as WithTitle,L as __namedExportsOrder,A as default};
