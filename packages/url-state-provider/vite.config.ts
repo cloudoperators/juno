a6d87e4fd97f45389808bbcf42ca5989a22d405b
Mark Karnaukh <mark.karnaukh@sap.com>
2025-04-29 15:12:55 +0200

    chore(deps): update vite config and package.jsob (url-state-provider)

--- packages/url-state-provider/vite.config.ts
+++ packages/url-state-provider/vite.config.ts
@@ -11,10 +11,14 @@ export default defineConfig({
     lib: {
       entry: "src/index.ts", // or 'src/main.ts' if TypeScript
       name: "url-state-provider", // Replace with your library's global name
-      formats: ["es"], // Output formats: ESM and CommonJS
-      fileName: () => `index.js`, // Output file names
+      formats: ["es", "cjs"], // Output both ESM and CommonJS formats
+      fileName: (format) => (format === "es" ? "index.js" : "index.cjs"), // Different file names for different formats
     },
     outDir: "build",
+    rollupOptions: {
+      // External packages that shouldn't be bundled
+      external: ["history", "juri", "query-string"],
+    },
   },
   plugins: [
     dts({
