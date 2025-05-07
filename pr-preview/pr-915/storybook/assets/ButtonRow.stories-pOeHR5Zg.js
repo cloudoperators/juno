640bfc52277336befb56a972eb98f1b92efd4e05
mark-karnaukh-extern-sap <mark-karnaukh-extern-sap@users.noreply.github.com>
2025-05-07 09:40:40 +0000

    Deploy preview for PR 915 🛫

---
+++ pr-preview/pr-915/storybook/assets/ButtonRow.stories-pOeHR5Zg.js
@@ -0,0 +1,6 @@
+import{j as t}from"./jsx-runtime-D-gQZ2Nk.js";import{B as s}from"./ButtonRow.component-Dce-jVw2.js";import{B as r}from"./Button.component-DqypShKj.js";import"./index-BXnWnNu-.js";import"./Stack.component-BZy_3dd2.js";import"./Icon.component-KLQ-3Fva.js";import"./Spinner.component-DqgRKBmm.js";const b={title:"Forms/ButtonRow",component:s,argTypes:{items:{table:{disable:!0}},children:{control:!1}}},m=({children:l,...i})=>t.jsx(s,{...i,children:l}),e={render:m,args:{children:[t.jsx(r,{label:"Cancel",title:"Cancel"},"1"),t.jsx(r,{label:"Save",title:"Save",variant:"primary"},"2")]}};var a,o,n;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
+  }
+}`,...(n=(o=e.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};const v=["Default"];export{e as Default,v as __namedExportsOrder,b as default};
