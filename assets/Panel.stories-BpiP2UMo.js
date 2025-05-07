0a11d56a696b294d6d688c5416a2a0478f39e19f
barsukov <pro.barsukov@gmail.com>
2025-05-07 13:00:12 +0000

    Deploying to gh-pages from @ cloudoperators/juno@cdae407b59ff229664595cfc544c14f44d594a12 🚀

---
+++ assets/Panel.stories-BpiP2UMo.js
@@ -0,0 +1,19 @@
+import{j as e}from"./jsx-runtime-D-gQZ2Nk.js";import{P as l,a as p}from"./PanelBody.component-CgyEUGy3.js";import{P as h}from"./PortalProvider.component-CHvemcrH.js";import"./index-BXnWnNu-.js";import"./index-CzvS-M9C.js";import"./Icon.component-uANO5hmX.js";const j={title:"Layout/Panel/Panel",component:l,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}},heading:{table:{type:{summary:"ReactNode"}}}},decorators:[n=>e.jsx(h,{children:e.jsx("div",{className:"jn-contrast-100",children:n()})})]},m=n=>e.jsxs("div",{children:[e.jsx(l,{...n,children:e.jsx(p,{children:"Panel Body Content"})}),e.jsx("div",{className:"dummy-css-ignore jn-h-[150px]",children:"Content Area"})]}),r={render:m,parameters:{docs:{description:{story:'The panel component slides into view from the right. It is to be used as a drawer containing forms for actions on the current view, like "new item" etc. Panels should have a heading.'}}},args:{heading:"Panel Heading",opened:!0}},a={render:m,args:{opened:!0}};var t,s,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: Template,
+  parameters: {
+    docs: {
+      description: {
+        story: 'The panel component slides into view from the right. It is to be used as a drawer containing forms for actions on the current view, like "new item" etc. Panels should have a heading.'
+      }
+    }
+  },
+  args: {
+    heading: "Panel Heading",
+    opened: true
+  }
+}`,...(o=(s=r.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};var i,d,c;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    opened: true
+  }
+}`,...(c=(d=a.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};const v=["WithHeading","Plain"];export{a as Plain,r as WithHeading,v as __namedExportsOrder,j as default};
--- assets/SelectDivider.stories-CDh7Qnj7.js
+++
@@ -1,8 +0,0 @@
-import{j as e}from"./jsx-runtime-D-gQZ2Nk.js";import{e as n}from"./index-BXnWnNu-.js";import{S as p,a as o}from"./SelectOption.component-mC5eZTEr.js";import{P as m}from"./PortalProvider.component-CHvemcrH.js";import"./portal-CxRCJl3j.js";import"./index-CzvS-M9C.js";import"./floating-ui.react-BEnEHP5A.js";import"./frozen-DAEboHsG.js";import"./use-text-value-CE0LmPae.js";import"./Label.component-Bubk172Q.js";import"./Icon.component-D9q-vDfZ.js";import"./Spinner.component-DqgRKBmm.js";import"./FormHint.component-k5YuOPZg.js";const u=`
-  jn-h-px 
-  jn-w-full
-  jn-bg-theme-background-lvl-3
-`,r=n.forwardRef(({className:t="",...l},d)=>e.jsx("div",{className:`juno-select-divider ${u} ${t}`,ref:d,...l}));r.displayName="SelectDivider";try{r.displayName="SelectDivider",r.__docgenInfo={description:"A visual border/divider between SelectOptions or SelectGroups",displayName:"SelectDivider",props:{className:{defaultValue:{value:""},description:"add a custom className",name:"className",required:!1,type:{name:"string"}}}}}catch{}const P={title:"Forms/Select/SelectDivider",component:r,argTypes:{},decorators:[t=>e.jsx(m,{children:t()})]},v=()=>e.jsxs(p,{open:!0,children:[e.jsx(o,{value:"1",children:"1"}),e.jsx(r,{}),e.jsx(o,{value:"3",children:"3"})]}),s={render:v,args:{}};var a,i,c;s.parameters={...s.parameters,docs:{...(a=s.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  render: Template,
-  args: {}
-}`,...(c=(i=s.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};const T=["Default"];export{s as Default,T as __namedExportsOrder,P as default};
