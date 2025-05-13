cab41f68b259c6a6d5d75555f60140cb7ba07656
mark-karnaukh-extern-sap <mark-karnaukh-extern-sap@users.noreply.github.com>
2025-05-13 12:41:00 +0000

    Deploy preview for PR 942 🛫

--- pr-preview/pr-942/storybook/assets/FormRow.stories-DxI_hvUj.js
+++
@@ -1,6 +0,0 @@
-import{j as a}from"./jsx-runtime-BjG_zV1W.js";import{F as s}from"./FormRow.component-UNjU8TPo.js";import{T as p}from"./TextInput.component-Cvn9RTkz.js";import"./index-BdGwgT04.js";import"./Label.component-D4gjYofZ.js";import"./Icon.component-DRgdbZ5c.js";import"./FormHint.component-Bn51_8r6.js";const h={title:"Forms/FormRow",component:s,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},l=({children:n,...m})=>a.jsx(s,{...m,children:n}),e={render:l,args:{children:[a.jsx(p,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  render: Template,
-  args: {
-    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
-  }
-}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const F=["Default"];export{e as Default,F as __namedExportsOrder,h as default};
