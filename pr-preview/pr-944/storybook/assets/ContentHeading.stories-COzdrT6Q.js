dae6cb9a3bf812eeac9c7fd8c08bbe677b7f57bc
barsukov <barsukov@users.noreply.github.com>
2025-05-15 12:32:15 +0000

    Deploy preview for PR 944 🛫

--- pr-preview/pr-944/storybook/assets/ContentHeading.stories-COzdrT6Q.js
+++
@@ -1,6 +0,0 @@
-import{j as s}from"./jsx-runtime-Cf8x2fCZ.js";import{C as n}from"./ContentHeading.component-QFZbWuM7.js";import"./index-yBjzXJbu.js";const c={title:"Internal/ContentHeading",component:n,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},d=o=>s.jsx(n,{...o}),e={render:d,args:{heading:"My Page Heading"}};var a,t,r;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  render: Template,
-  args: {
-    heading: "My Page Heading"
-  }
-}`,...(r=(t=e.parameters)==null?void 0:t.docs)==null?void 0:r.source}}};const g=["Default"];export{e as Default,g as __namedExportsOrder,c as default};
