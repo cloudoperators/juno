6a2c4e0fde7a0c61593c020b53c1862bbf286f3e
mark-karnaukh-extern-sap <mark-karnaukh-extern-sap@users.noreply.github.com>
2025-05-15 11:35:26 +0000

    Deploy preview for PR 942 🛫

--- pr-preview/pr-942/storybook/assets/ButtonRow.stories-kL8g6SmL.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./jsx-runtime-BjG_zV1W.js";import{B as s}from"./ButtonRow.component-Cbvk4hRS.js";import{B as r}from"./Button.component-0Sa5b6kE.js";import"./Stack.component-C9cPsQbk.js";import"./index-BdGwgT04.js";import"./Icon.component-BcTr8sHk.js";import"./Spinner.component-C7ss7i9x.js";const b={title:"Forms/ButtonRow",component:s,argTypes:{items:{table:{disable:!0}},children:{control:!1}}},m=({children:l,...i})=>t.jsx(s,{...i,children:l}),e={render:m,args:{children:[t.jsx(r,{label:"Cancel",title:"Cancel"},"1"),t.jsx(r,{label:"Save",title:"Save",variant:"primary"},"2")]}};var a,o,n;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  render: Template,
-  args: {
-    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
-  }
-}`,...(n=(o=e.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};const v=["Default"];export{e as Default,v as __namedExportsOrder,b as default};
--- pr-preview/pr-942/storybook/assets/SelectDivider.stories-Dc9HBxWm.js
+++
@@ -1,8 +0,0 @@
-import{j as e}from"./jsx-runtime-BjG_zV1W.js";import{e as n}from"./index-BdGwgT04.js";import{S as p,a as o}from"./SelectOption.component-BgJedVi8.js";import{P as m}from"./PortalProvider.component-SN9LOs4c.js";import"./portal-SlZPv7cO.js";import"./floating-ui.react-ubu_7ndR.js";import"./index-C7VnYWJ0.js";import"./frozen-DZprLUpS.js";import"./use-text-value-BG3gIc7Y.js";import"./Label.component-D4gjYofZ.js";import"./Icon.component-BcTr8sHk.js";import"./Spinner.component-C7ss7i9x.js";import"./FormHint.component-Bn51_8r6.js";const u=`
-  jn-h-px 
-  jn-w-full
-  jn-bg-theme-background-lvl-3
-`,r=n.forwardRef(({className:t="",...l},d)=>e.jsx("div",{className:`juno-select-divider ${u} ${t}`,ref:d,...l}));r.displayName="SelectDivider";try{r.displayName="SelectDivider",r.__docgenInfo={description:"A visual border/divider between SelectOptions or SelectGroups",displayName:"SelectDivider",props:{className:{defaultValue:{value:""},description:"add a custom className",name:"className",required:!1,type:{name:"string"}}}}}catch{}const P={title:"Forms/Select/SelectDivider",component:r,argTypes:{},decorators:[t=>e.jsx(m,{children:t()})]},v=()=>e.jsxs(p,{open:!0,children:[e.jsx(o,{value:"1",children:"1"}),e.jsx(r,{}),e.jsx(o,{value:"3",children:"3"})]}),s={render:v,args:{}};var a,i,c;s.parameters={...s.parameters,docs:{...(a=s.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  render: Template,
-  args: {}
-}`,...(c=(i=s.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};const T=["Default"];export{s as Default,T as __namedExportsOrder,P as default};
