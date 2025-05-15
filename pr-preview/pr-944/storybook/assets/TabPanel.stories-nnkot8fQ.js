32f71f36d01500a82f2f15d568801b4a586f690e
barsukov <barsukov@users.noreply.github.com>
2025-05-15 14:19:06 +0000

    Deploy preview for PR 944 🛫

---
+++ pr-preview/pr-944/storybook/assets/TabPanel.stories-nnkot8fQ.js
@@ -0,0 +1,5 @@
+import{T as o}from"./TabPanel.component-GQEeGnOG.js";import"./jsx-runtime-Cf8x2fCZ.js";import"./index-yBjzXJbu.js";import"./TabPanel-B8bh5eFJ.js";import"./index-Chjiymov.js";import"./index-DN3cvKNT.js";const i={title:"Layout/Tabs/TabPanel",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},e={args:{children:"Tab panel content goes here."}};var a,r,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: "Tab panel content goes here."
+  }
+}`,...(t=(r=e.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const d=["Default"];export{e as Default,d as __namedExportsOrder,i as default};
