65bf2dd8d88a8be60ec0fa2e0da1de9d7c1655c5
mark-karnaukh-extern-sap <mark-karnaukh-extern-sap@users.noreply.github.com>
2025-05-16 10:15:49 +0000

    Deploy preview for PR 942 🛫

---
+++ pr-preview/pr-942/storybook/assets/ButtonRow.stories-DD8-OPF1.js
@@ -0,0 +1,6 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{B as s}from"./ButtonRow.component-Cz-5ITo2.js";import{B as r}from"./Button.component-CQyllTRG.js";import"./Stack.component-wcsSWG1x.js";import"./index-DI7CEG0L.js";import"./Icon.component-DwRmdXUS.js";import"./Spinner.component-NCyRlNFL.js";const b={title:"Forms/ButtonRow",component:s,argTypes:{items:{table:{disable:!0}},children:{control:!1}}},m=({children:l,...i})=>t.jsx(s,{...i,children:l}),e={render:m,args:{children:[t.jsx(r,{label:"Cancel",title:"Cancel"},"1"),t.jsx(r,{label:"Save",title:"Save",variant:"primary"},"2")]}};var a,o,n;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
+  }
+}`,...(n=(o=e.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};const v=["Default"];export{e as Default,v as __namedExportsOrder,b as default};
--- pr-preview/pr-942/storybook/assets/TabPanel.stories-DNgBahkk.js
+++
@@ -1,5 +0,0 @@
-import{T as o}from"./TabPanel.component-DuTnwr05.js";import"./jsx-runtime-BjG_zV1W.js";import"./TabPanel-BmeRdDBb.js";import"./index-BdGwgT04.js";const p={title:"Layout/Tabs/TabPanel",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},e={args:{children:"Tab panel content goes here."}};var a,r,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    children: "Tab panel content goes here."
-  }
-}`,...(t=(r=e.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const m=["Default"];export{e as Default,m as __namedExportsOrder,p as default};
