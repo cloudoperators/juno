c786e302fd2f1dda25775d717acbe4828131afd6
barsukov <barsukov@users.noreply.github.com>
2025-05-16 08:54:44 +0000

    Deploy preview for PR 944 🛫

---
+++ pr-preview/pr-944/storybook/assets/StyleProvider.stories-DsPpuMqn.js
@@ -0,0 +1,23 @@
+import{j as W}from"./jsx-runtime-Cf8x2fCZ.js";import{S as T}from"./StyleProvider.component-NQ8f7xRu.js";import"./index-yBjzXJbu.js";import"./index-DN3cvKNT.js";import"./ShadowRoot.component-BnLrQxiR.js";import"./index-FzTzjy2T.js";import"./index-fNjTmf9T.js";const v={title:"Layout/StyleProvider",component:T,argTypes:{stylesWrapper:{options:["head","inline","shadowRoot"],control:{type:"radio"}},children:{control:!1}}},o=a=>W.jsx(T,{...a,children:a.children||a.stylesWrapper||"undefined"}),e={render:o,args:{stylesWrapper:"head"}},r={render:o,args:{stylesWrapper:"inline"}},s={render:o,args:{stylesWrapper:"shadowRoot"}},t={render:o,args:{stylesWrapper:"shadowRoot",theme:"theme-light",children:"Light Theme"}};var n,d,p;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    stylesWrapper: "head"
+  }
+}`,...(p=(d=e.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};var i,l,m;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    stylesWrapper: "inline"
+  }
+}`,...(m=(l=r.parameters)==null?void 0:l.docs)==null?void 0:m.source}}};var c,h,y;s.parameters={...s.parameters,docs:{...(c=s.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    stylesWrapper: "shadowRoot"
+  }
+}`,...(y=(h=s.parameters)==null?void 0:h.docs)==null?void 0:y.source}}};var g,u,S;t.parameters={...t.parameters,docs:{...(g=t.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    stylesWrapper: "shadowRoot",
+    theme: "theme-light",
+    children: "Light Theme"
+  }
+}`,...(S=(u=t.parameters)==null?void 0:u.docs)==null?void 0:S.source}}};const P=["AddStylesToHead","AddInlineStyles","AddStylesToShadowRoot","WithLightTheme"];export{r as AddInlineStyles,e as AddStylesToHead,s as AddStylesToShadowRoot,t as WithLightTheme,P as __namedExportsOrder,v as default};
---
+++ pr-preview/pr-944/storybook/assets/Tab.stories-C8YHKV60.js
@@ -0,0 +1,11 @@
+import{T as i}from"./Tab.component-CVtl_W4f.js";import{K as l}from"./Icon.component-m3_ZDYx0.js";import"./jsx-runtime-Cf8x2fCZ.js";import"./index-yBjzXJbu.js";import"./TabPanel-B8bh5eFJ.js";import"./index-Chjiymov.js";import"./index-DN3cvKNT.js";const h={title:"Layout/Tabs/Tab",component:i,argTypes:{icon:{options:["default",...Object.keys(l)],control:{type:"select"}},children:{control:!1,table:{type:{summary:"ReactNode"}}}}},e={args:{label:"A Tab Label",children:"Tab 1"}},r={args:{children:"Tab with Icon",icon:"danger"}};var a,o,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    label: "A Tab Label",
+    children: "Tab 1"
+  }
+}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};var n,s,c;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    children: "Tab with Icon",
+    icon: "danger"
+  }
+}`,...(c=(s=r.parameters)==null?void 0:s.docs)==null?void 0:c.source}}};const f=["Default","WithIcon"];export{e as Default,r as WithIcon,f as __namedExportsOrder,h as default};
