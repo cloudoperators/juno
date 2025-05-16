3181da0b69add7ffa7a7e8a80117ec33e611b027
barsukov <barsukov@users.noreply.github.com>
2025-05-16 13:57:59 +0000

    Deploy preview for PR 924 🛫

---
+++ pr-preview/pr-924/storybook/assets/FormRow.stories-YVX8xkL5.js
@@ -0,0 +1,6 @@
+import{j as a}from"./jsx-runtime-Cf8x2fCZ.js";import{F as s}from"./FormRow.component-7eKnYO3o.js";import{T as p}from"./TextInput.component-qWzPKrif.js";import"./index-yBjzXJbu.js";import"./index-DN3cvKNT.js";import"./Label.component-BnXfZbGU.js";import"./Icon.component-JpmceVQR.js";import"./FormHint.component-DQa8ln7A.js";const F={title:"Forms/FormRow",component:s,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},l=({children:m,...n})=>a.jsx(s,{...n,children:m}),e={render:l,args:{children:[a.jsx(p,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
+  }
+}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const R=["Default"];export{e as Default,R as __namedExportsOrder,F as default};
---
+++ pr-preview/pr-924/storybook/assets/PanelFooter.stories-DlAVNZK6.js
@@ -0,0 +1,11 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{B as i}from"./Button.component-BacpW3jB.js";import{P as m,a as d}from"./PanelBody.component-C7Y12-P6.js";import{P as s}from"./PanelFooter.component-BKRrct_4.js";import{P as l}from"./PortalProvider.component-DwfadwbX.js";import"./index-yBjzXJbu.js";import"./index-DN3cvKNT.js";import"./Icon.component-JpmceVQR.js";import"./Spinner.component-BgFgW-bc.js";import"./index-FzTzjy2T.js";import"./index-fNjTmf9T.js";const B={title:"Layout/Panel/PanelFooter",component:s,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}},decorators:[r=>e.jsx(l,{children:e.jsx("div",{className:"jn-contrast-100",children:r()})})]},p=r=>e.jsxs("div",{children:[e.jsx(m,{heading:"My Panel",opened:!0,children:e.jsx(d,{footer:e.jsx(s,{...r,children:e.jsx(i,{children:"Do it"})}),children:"This is the panel body"})}),e.jsx("div",{className:"dummy-css-ignore jn-h-[250px]",children:"Content Area"})]}),o={render:p,parameters:{docs:{description:{story:"A container for panel footer elements, typically buttons. Add the footer to the PanelBody component via its `footer` parameter."}}},args:{}};var t,n,a;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: Template,
+  parameters: {
+    docs: {
+      description: {
+        story: "A container for panel footer elements, typically buttons. Add the footer to the PanelBody component via its \`footer\` parameter."
+      }
+    }
+  },
+  args: {}
+}`,...(a=(n=o.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};const F=["Footer"];export{o as Footer,F as __namedExportsOrder,B as default};
