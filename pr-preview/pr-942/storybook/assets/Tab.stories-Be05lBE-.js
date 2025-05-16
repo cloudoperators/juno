7a13f129ee6f0168445ed9b559fe1963d0d4bc54
mark-karnaukh-extern-sap <mark-karnaukh-extern-sap@users.noreply.github.com>
2025-05-16 15:05:40 +0000

    Deploy preview for PR 942 🛫

---
+++ pr-preview/pr-942/storybook/assets/Tab.stories-Be05lBE-.js
@@ -0,0 +1,11 @@
+import{T as i}from"./Tab.component-ihbJe2A8.js";import{K as l}from"./Icon.component-DdNENVKs.js";import"./jsx-runtime-D_zvdyIk.js";import"./TabPanel-BMxd_RTe.js";import"./index-DI7CEG0L.js";const T={title:"Layout/Tabs/Tab",component:i,argTypes:{icon:{options:["default",...Object.keys(l)],control:{type:"select"}},children:{control:!1,table:{type:{summary:"ReactNode"}}}}},e={args:{label:"A Tab Label",children:"Tab 1"}},a={args:{children:"Tab with Icon",icon:"danger"}};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    label: "A Tab Label",
+    children: "Tab 1"
+  }
+}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};var n,s,c;a.parameters={...a.parameters,docs:{...(n=a.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    children: "Tab with Icon",
+    icon: "danger"
+  }
+}`,...(c=(s=a.parameters)==null?void 0:s.docs)==null?void 0:c.source}}};const g=["Default","WithIcon"];export{e as Default,a as WithIcon,g as __namedExportsOrder,T as default};
