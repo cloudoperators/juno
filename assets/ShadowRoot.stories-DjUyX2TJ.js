5a3d2d4e7429698d97442fd28eafb625484f6751
barsukov <pro.barsukov@gmail.com>
2025-05-19 11:24:58 +0000

    Deploying to gh-pages from @ cloudoperators/juno@439a1b8006f7146090c8d5c35db092c9eec27cef 🚀

---
+++ assets/ShadowRoot.stories-DjUyX2TJ.js
@@ -0,0 +1,6 @@
+import{j as o}from"./jsx-runtime-Cf8x2fCZ.js";import{S as a}from"./ShadowRoot.component-GIUGqqBF.js";import"./index-yBjzXJbu.js";import"./index-FzTzjy2T.js";import"./index-DN3cvKNT.js";import"./index-fNjTmf9T.js";const c=n=>o.jsx(a,{...n,children:o.jsx("h1",{children:"Welcome"})}),h={title:"Layout/ShadowRoot",component:a,argTypes:{children:{control:!1}}},e={render:c,args:{mode:"closed"}};var r,t,s;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    mode: "closed"
+  }
+}`,...(s=(t=e.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const S=["EncapsulateStyles"];export{e as EncapsulateStyles,S as __namedExportsOrder,h as default};
