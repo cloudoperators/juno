0afcf6aa9e5f98422fdb1cef091e2f098d9d09fa
barsukov <barsukov@users.noreply.github.com>
2025-05-19 11:21:56 +0000

    Deploy preview for PR 930 🛫

---
+++ pr-preview/pr-930/storybook/assets/PageFooter.stories-D0Xlu6Uu.js
@@ -0,0 +1,11 @@
+import{j as n}from"./jsx-runtime-Cf8x2fCZ.js";import{P as a}from"./PageFooter.component-CBOZWkHR.js";import"./index-yBjzXJbu.js";import"./index-DN3cvKNT.js";const l={title:"Layout/PageFooter",component:a,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},p=s=>n.jsx(a,{...s}),e={render:p,parameters:{docs:{description:{story:"The page footer component renders a footer at the bottom of the website. Place as last child of AppBody."}}},args:{}};var o,t,r;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: Template,
+  parameters: {
+    docs: {
+      description: {
+        story: "The page footer component renders a footer at the bottom of the website. Place as last child of AppBody."
+      }
+    }
+  },
+  args: {}
+}`,...(r=(t=e.parameters)==null?void 0:t.docs)==null?void 0:r.source}}};const f=["Simple"];export{e as Simple,f as __namedExportsOrder,l as default};
