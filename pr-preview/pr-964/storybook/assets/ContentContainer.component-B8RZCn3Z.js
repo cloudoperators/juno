398a55eb636194959a2ed9d7d0658160d8be640a
ArtieReus <ArtieReus@users.noreply.github.com>
2025-05-22 13:15:17 +0000

    Deploy preview for PR 964 🛫

---
+++ pr-preview/pr-964/storybook/assets/ContentContainer.component-B8RZCn3Z.js
@@ -0,0 +1,11 @@
+import{j as o}from"./jsx-runtime-Cf8x2fCZ.js";const r=`
+  jn-flex-col
+  jn-grow
+  jn-bg-[right_top_1rem]
+  jn-bg-no-repeat
+  jn-bg-theme-content-area-bg
+  jn-relative
+`,n=({className:e="",children:t,...a})=>o.jsx("div",{className:`juno-content-container ${r} ${e}`,...a,children:t});try{n.displayName="ContentContainer",n.__docgenInfo={description:`ContentContainer serves as a wrapper for app content.
+It is intended for manual app scaffold creation. In most cases, using the AppShell component instead is recommended.
+
+Note: When the browser window is wider than the max breakpoint width, the content within this container will be centered on the screen.`,displayName:"ContentContainer",props:{className:{defaultValue:{value:""},description:"Additional CSS classes for custom styling.",name:"className",required:!1,type:{name:"string"}},children:{defaultValue:null,description:"Custom content to render within the container.",name:"children",required:!1,type:{name:"ReactNode"}}}}}catch{}export{n as C};
---
+++ pr-preview/pr-964/storybook/assets/NativeSelectOptionGroup.stories-CDz6tsGj.js
@@ -0,0 +1,14 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{a as c,N as b}from"./NativeSelectOptionGroup.component-C6YmFTrk.js";import{N as t}from"./NativeSelectOption.component-CiSWzXQ8.js";import"./index-yBjzXJbu.js";import"./index-DN3cvKNT.js";import"./Icon.component-cS6TiWAp.js";import"./Spinner.component-BgFgW-bc.js";const j={title:"Forms/NativeSelect/NativeSelectOptionGroup",component:c,argTypes:{options:{table:{disable:!0}},children:{control:!1,table:{type:{summary:"ReactNode"}}}}},u=({children:d,...m})=>e.jsx(b,{children:e.jsx(c,{...m,children:d})}),a={render:u,args:{label:"My option group",children:[e.jsx(t,{value:"1",label:"Option 1"},0),e.jsx(t,{value:"2",label:"Option 2"},1),e.jsx(t,{value:"3",label:"Option 3"},2)]}},l={render:u,args:{label:"My disabled option group",children:[e.jsx(t,{value:"1",label:"Option 1"},0),e.jsx(t,{value:"2",label:"Option 2"},1),e.jsx(t,{value:"3",label:"Option 3"},2)],disabled:!0}};var o,r,i;a.parameters={...a.parameters,docs:{...(o=a.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    label: "My option group",
+    children: [<NativeSelectOption key={0} value="1" label="Option 1" />, <NativeSelectOption key={1} value="2" label="Option 2" />, <NativeSelectOption key={2} value="3" label="Option 3" />]
+  }
+}`,...(i=(r=a.parameters)==null?void 0:r.docs)==null?void 0:i.source}}};var n,p,s;l.parameters={...l.parameters,docs:{...(n=l.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    label: "My disabled option group",
+    children: [<NativeSelectOption key={0} value="1" label="Option 1" />, <NativeSelectOption key={1} value="2" label="Option 2" />, <NativeSelectOption key={2} value="3" label="Option 3" />],
+    disabled: true
+  }
+}`,...(s=(p=l.parameters)==null?void 0:p.docs)==null?void 0:s.source}}};const f=["Default","Disabled"];export{a as Default,l as Disabled,f as __namedExportsOrder,j as default};
