6e2168c90bc2016fc88d60d1174b123d8a628716
barsukov <barsukov@users.noreply.github.com>
2025-06-02 10:37:42 +0000

    Remove preview for PR 965 🛬

--- pr-preview/pr-965/storybook/assets/ShadowRoot.stories-BnAHYIz_.js
+++
@@ -1,6 +0,0 @@
-import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{S as a}from"./ShadowRoot.component-jmOzOo4k.js";import"./index-DnsdnBHL.js";import"./index-DI7CEG0L.js";import"./index-BMl_lxmD.js";const c=n=>o.jsx(a,{...n,children:o.jsx("h1",{children:"Welcome"})}),u={title:"Layout/ShadowRoot",component:a,argTypes:{children:{control:!1}}},e={render:c,args:{mode:"closed"}};var r,s,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  render: Template,
-  args: {
-    mode: "closed"
-  }
-}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const h=["EncapsulateStyles"];export{e as EncapsulateStyles,h as __namedExportsOrder,u as default};
