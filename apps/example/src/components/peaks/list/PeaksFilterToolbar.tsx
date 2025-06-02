4838e76b2219c1facf66f881725e0254e767a6b5
I531348 <guoda.puidokaite@sap.com>
2025-06-02 11:06:21 +0200

    add lisences

--- apps/example/src/components/peaks/list/PeaksFilterToolbar.tsx
+++ apps/example/src/components/peaks/list/PeaksFilterToolbar.tsx
@@ -1,4 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Juno contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 /* eslint-disable no-unused-vars */
+
 import React from "react"
 import {
   DataGridToolbar,
