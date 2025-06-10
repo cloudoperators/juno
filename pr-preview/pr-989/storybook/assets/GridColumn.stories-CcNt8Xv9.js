c87fc1509787cf8bdd3210cb38a68b576fc62dcd
ArtieReus <ArtieReus@users.noreply.github.com>
2025-06-10 11:09:15 +0000

    Deploy preview for PR 989 🛫

---
+++ pr-preview/pr-989/storybook/assets/GridColumn.stories-CcNt8Xv9.js
@@ -0,0 +1,18 @@
+import{j as s}from"./jsx-runtime-D_zvdyIk.js";import{G as g}from"./GridColumn.component-Ca1LjLbT.js";const f={title:"Layout/Grid/GridColumn",component:g,argTypes:{children:{control:!1}},decorators:[(r,h)=>s.jsx("div",{className:"juno-container jn-px-6 jn-py-6",children:s.jsx(r,{...h.args})})]},a=r=>s.jsx(g,{...r,className:`${r.className} jn-bg-juno-blue-3 jn-text-juno-grey-blue`}),e={render:a,args:{children:"Column"}},n={render:a,args:{auto:!0,children:"Auto Column"}},o={render:a,args:{width:50,children:"Column 50%"}};var t,u,c;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    children: "Column"
+  }
+}`,...(c=(u=e.parameters)==null?void 0:u.docs)==null?void 0:c.source}}};var l,m,d;n.parameters={...n.parameters,docs:{...(l=n.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    auto: true,
+    children: "Auto Column"
+  }
+}`,...(d=(m=n.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var i,p,C;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    width: 50,
+    children: "Column 50%"
+  }
+}`,...(C=(p=o.parameters)==null?void 0:p.docs)==null?void 0:C.source}}};const G=["Default","AutoColumn","WidthColumn"];export{n as AutoColumn,e as Default,o as WidthColumn,G as __namedExportsOrder,f as default};
