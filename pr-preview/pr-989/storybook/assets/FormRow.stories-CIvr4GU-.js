518dffe9b56cabe34bb2eaceb358fe0ddbb10e95
ArtieReus <ArtieReus@users.noreply.github.com>
2025-06-10 14:12:34 +0000

    Deploy preview for PR 989 🛫

--- pr-preview/pr-989/storybook/assets/FormRow.stories-CIvr4GU-.js
+++
@@ -1,6 +0,0 @@
-import{j as a}from"./jsx-runtime-D_zvdyIk.js";import{F as s}from"./FormRow.component-D7mQYlhI.js";import{T as p}from"./TextInput.component-BK4DpXf6.js";import"./index-DI7CEG0L.js";import"./Label.component-DZ9JGTj_.js";import"./Icon.component-Ruh8QuLa.js";import"./FormHint.component-CxYdfrh1.js";const h={title:"Forms/FormRow",component:s,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},l=({children:n,...m})=>a.jsx(s,{...m,children:n}),e={render:l,args:{children:[a.jsx(p,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  render: Template,
-  args: {
-    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
-  }
-}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const F=["Default"];export{e as Default,F as __namedExportsOrder,h as default};
