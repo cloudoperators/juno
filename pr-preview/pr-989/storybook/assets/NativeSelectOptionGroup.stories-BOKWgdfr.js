b61d113b55e621c10d1f198490fd101e6f3c60b5
ArtieReus <ArtieReus@users.noreply.github.com>
2025-06-11 12:04:49 +0000

    Deploy preview for PR 989 🛫

---
+++ pr-preview/pr-989/storybook/assets/NativeSelectOptionGroup.stories-BOKWgdfr.js
@@ -0,0 +1,14 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{a as c,N as b}from"./NativeSelectOptionGroup.component-D75R5KjZ.js";import{N as t}from"./NativeSelectOption.component-CxPT0Exj.js";import"./index-DI7CEG0L.js";import"./Icon.component-BJIU5AI2.js";import"./Spinner.component-NCyRlNFL.js";const g={title:"Forms/NativeSelect/NativeSelectOptionGroup",component:c,argTypes:{options:{table:{disable:!0}},children:{control:!1,table:{type:{summary:"ReactNode"}}}}},u=({children:d,...m})=>e.jsx(b,{children:e.jsx(c,{...m,children:d})}),a={render:u,args:{label:"My option group",children:[e.jsx(t,{value:"1",label:"Option 1"},0),e.jsx(t,{value:"2",label:"Option 2"},1),e.jsx(t,{value:"3",label:"Option 3"},2)]}},l={render:u,args:{label:"My disabled option group",children:[e.jsx(t,{value:"1",label:"Option 1"},0),e.jsx(t,{value:"2",label:"Option 2"},1),e.jsx(t,{value:"3",label:"Option 3"},2)],disabled:!0}};var o,r,i;a.parameters={...a.parameters,docs:{...(o=a.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    label: "My option group",
+    children: [<NativeSelectOption key={0} value="1" label="Option 1" />, <NativeSelectOption key={1} value="2" label="Option 2" />, <NativeSelectOption key={2} value="3" label="Option 3" />]
+  }
+}`,...(i=(r=a.parameters)==null?void 0:r.docs)==null?void 0:i.source}}};var n,p,s;l.parameters={...l.parameters,docs:{...(n=l.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    label: "My disabled option group",
+    children: [<NativeSelectOption key={0} value="1" label="Option 1" />, <NativeSelectOption key={1} value="2" label="Option 2" />, <NativeSelectOption key={2} value="3" label="Option 3" />],
+    disabled: true
+  }
+}`,...(s=(p=l.parameters)==null?void 0:p.docs)==null?void 0:s.source}}};const j=["Default","Disabled"];export{a as Default,l as Disabled,j as __namedExportsOrder,g as default};
---
+++ pr-preview/pr-989/storybook/assets/Pill.stories-Id0Ph8F5.js
@@ -0,0 +1,54 @@
+import{j as l}from"./jsx-runtime-D_zvdyIk.js";import{I as P}from"./Icon.component-BJIU5AI2.js";import"./index-DI7CEG0L.js";const N=e=>`
+    jn-inline-flex
+    jn-basis-auto
+    jn-shrink
+    jn-items-center
+    jn-flex-nowrap
+    jn-text-xs
+    jn-p-px
+    jn-border
+    jn-rounded
+    jn-border-theme-background-lvl-4
+    jn-group
+    ${e?"jn-cursor-pointer":""}
+  `,E=e=>`
+    jn-bg-theme-background-lvl-4
+    jn-text-theme-high
+    ${e?"group-hover:jn-text-theme-highest":""}
+    jn-px-1
+    jn-py-0.5
+    jn-rounded-sm
+    jn-inline-block
+  `,h=e=>`
+    jn-px-1
+    jn-py-0.5
+    jn-text-theme-high
+    ${e?"group-hover:jn-text-theme-highest":""}
+    jn-inline-block
+  `,c=({uid:e="",pillKey:n="",pillKeyLabel:p="",pillValue:s="",pillValueLabel:d="",closeable:L=!1,onClick:a,onClose:m,className:K="",...M})=>{const q=u=>{m&&m(u,e||n||s)},b=u=>{a&&a(u,e||n||s)};return l.jsxs("div",{className:`juno-pill ${N(!!a)} ${K}`,...M,children:[!s&&!d?l.jsx("span",{className:`${h(!1)}`,children:"set pillValue or pillValueLabel"}):l.jsxs(l.Fragment,{children:[(p||n)&&l.jsx("span",{className:`pill-key ${E(!!a)}`,onClick:b,children:p||n}),l.jsx("span",{className:`pill-value ${h(!!a)}`,onClick:b,children:d||s})]}),L&&l.jsx(P,{icon:"close",size:18,onClick:q})]})};try{c.displayName="Pill",c.__docgenInfo={description:"A Pill to represent a value, or key and value. Can e.g. be used to represent selected filter values in a filter component. Can optionally be closed. On close the uid, if provided, or the pillKey is returned in the callback.",displayName:"Pill",props:{uid:{defaultValue:{value:""},description:"The unique identifier of the pill. Returned by the onClose callback",name:"uid",required:!1,type:{name:"string"}},pillKey:{defaultValue:{value:""},description:"The key of the filter the pill represents. Returned by the onClose callback if uid undefined. Optional.",name:"pillKey",required:!1,type:{name:"string"}},pillKeyLabel:{defaultValue:{value:""},description:"The visible label to describe the pill key. If not set pillKey is used. Optional.",name:"pillKeyLabel",required:!1,type:{name:"string"}},pillValue:{defaultValue:{value:""},description:"The value of filter the pill represents. Returned by the onClose callback if uid and pillKey undefined",name:"pillValue",required:!1,type:{name:"string"}},pillValueLabel:{defaultValue:{value:""},description:"The visible label to describe the pill value. If not set pillValue is used. Optional.",name:"pillValueLabel",required:!1,type:{name:"string"}},className:{defaultValue:{value:""},description:"Add custom classNames",name:"className",required:!1,type:{name:"string"}},closeable:{defaultValue:{value:"false"},description:"Whether the pill should be closeable",name:"closeable",required:!1,type:{name:"boolean"}},onClose:{defaultValue:null,description:"Pass a handler to be executed when closing the Pill. Also returns the event and the uid (fallback: pillKey -> fallback: pillValue)",name:"onClose",required:!1,type:{name:"EventHandler"}},onClick:{defaultValue:null,description:"Pass a handler to be executed when clicking on the Pill (but not on the close button). Also returns the event and the uid (fallback: pillKey -> fallback: pillValue)",name:"onClick",required:!1,type:{name:"EventHandler"}}}}}catch{}const I={title:"Components/Pill",component:c,argTypes:{onClose:{table:{type:{summary:"(event: React.MouseEvent<HTMLElement>, id: string) => void"}}},onClick:{table:{type:{summary:"(event: React.MouseEvent<HTMLElement>, id: string) => void"}}}}},t={args:{pillKey:"os",pillKeyLabel:"OS",pillValue:"mac_os",pillValueLabel:"Mac OS"}},r={args:{pillKey:"os",pillKeyLabel:"OS",pillValue:"mac_os",pillValueLabel:"Mac OS",closeable:!0}},i={args:{pillValue:"mac_os",pillValueLabel:"Mac OS"}},o={args:{pillValue:"mac_os",pillValueLabel:"Mac OS",closeable:!0}};var y,f,g;t.parameters={...t.parameters,docs:{...(y=t.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    pillKey: "os",
+    pillKeyLabel: "OS",
+    pillValue: "mac_os",
+    pillValueLabel: "Mac OS"
+  }
+}`,...(g=(f=t.parameters)==null?void 0:f.docs)==null?void 0:g.source}}};var V,j,v;r.parameters={...r.parameters,docs:{...(V=r.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    pillKey: "os",
+    pillKeyLabel: "OS",
+    pillValue: "mac_os",
+    pillValueLabel: "Mac OS",
+    closeable: true
+  }
+}`,...(v=(j=r.parameters)==null?void 0:j.docs)==null?void 0:v.source}}};var x,k,O;i.parameters={...i.parameters,docs:{...(x=i.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    pillValue: "mac_os",
+    pillValueLabel: "Mac OS"
+  }
+}`,...(O=(k=i.parameters)==null?void 0:k.docs)==null?void 0:O.source}}};var S,_,C;o.parameters={...o.parameters,docs:{...(S=o.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    pillValue: "mac_os",
+    pillValueLabel: "Mac OS",
+    closeable: true
+  }
+}`,...(C=(_=o.parameters)==null?void 0:_.docs)==null?void 0:C.source}}};const A=["Default","Closeable","ValueOnly","ValueOnlyCloseable"];export{r as Closeable,t as Default,i as ValueOnly,o as ValueOnlyCloseable,A as __namedExportsOrder,I as default};
