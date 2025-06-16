2b825106b2bbe97292966c84d31ea54fc3129954
andypf <ap@a-pfau.de>
2025-06-16 13:25:08 +0000

    Deploying to gh-pages from @ cloudoperators/juno@5488568d86732191054c3fb1776e02041bbc43be 🚀

---
+++ assets/FormRow.stories-Bjzcl6ue.js
@@ -0,0 +1,5 @@
+import{j as a}from"./iframe-ZvJmXgkJ.js";import{F as s}from"./FormRow.component-C38MSF4t.js";import{T as m}from"./TextInput.component-DLxmABuH.js";import"./Label.component-CpKNg1px.js";import"./Icon.component-CB4jBGkN.js";import"./FormHint.component-Dh3qEzNM.js";const d={title:"Forms/FormRow",component:s,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},e={args:{children:[a.jsx(m,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
+  }
+}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const x=["Default"];export{e as Default,x as __namedExportsOrder,d as default};
