c960e3cb1fb050b465c3094c4c8e3e0b5d2fded5
Taimoor Aslam <taimoor.aslam@sap.com>
2025-06-16 17:40:12 +0200

    chore(heureka): add license header

--- apps/heureka/src/components/common/CursorPagination.tsx
+++ apps/heureka/src/components/common/CursorPagination.tsx
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Juno contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import React, { use, useCallback } from "react"
 import { Pagination, Stack } from "@cloudoperators/juno-ui-components"
 import { ApolloQueryResult } from "@apollo/client"
