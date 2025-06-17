ee38c86c414c8fb3afa5dc33282ca5647ca0c73c
mark-karnaukh-extern-sap <mark-karnaukh-extern-sap@users.noreply.github.com>
2025-06-17 10:18:02 +0000

    Deploy preview for PR 956 🛫

---
+++ pr-preview/pr-956/storybook/assets/SelectOption.stories-CDuIOwfk.js
@@ -0,0 +1,20 @@
+import{j as e}from"./iframe-BJQzcJBh.js";import{a as n,S}from"./SelectOption.component-0JRie4XK.js";import{P as v}from"./PortalProvider.component-DhOJ6SBC.js";import"./portal-BRr_6k_6.js";import"./floating-ui.react-B2usp79P.js";import"./frozen-B_g9poF9.js";import"./use-text-value-B3EcsX6K.js";import"./Label.component-BxJBEllw.js";import"./Icon.component-B0zhLXRx.js";import"./Spinner.component-DXcFHL5G.js";import"./FormHint.component--Mj7Oo5n.js";const R={title:"Forms/Select/SelectOption",component:n,argTypes:{children:{control:{type:"text"},table:{type:{summary:"string"}}}},decorators:[s=>e.jsx(v,{children:e.jsx(s,{})})],render:s=>e.jsx(S,{open:!0,children:e.jsx(n,{...s})})},r={args:{value:"Option 1"}},a={args:{disabled:!0,value:"Disabled Option"}},o={args:{children:["Option 1"]}},t={args:{value:"option-1",label:"Option 1"}};var i,p,c;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    value: "Option 1"
+  }
+}`,...(c=(p=r.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};var l,m,d;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    value: "Disabled Option"
+  }
+}`,...(d=(m=a.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var u,g,O;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    // @ts-ignore
+    children: ["Option 1"]
+  }
+}`,...(O=(g=o.parameters)==null?void 0:g.docs)==null?void 0:O.source}}};var b,h,x;t.parameters={...t.parameters,docs:{...(b=t.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    value: "option-1",
+    label: "Option 1"
+  }
+}`,...(x=(h=t.parameters)==null?void 0:h.docs)==null?void 0:x.source}}};const T=["Default","Disabled","ChildrenOnly","WithLabel"];export{o as ChildrenOnly,r as Default,a as Disabled,t as WithLabel,T as __namedExportsOrder,R as default};
