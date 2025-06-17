6892673fcc5132699e683407f42f54a2268d3da3
barsukov <barsukov@users.noreply.github.com>
2025-06-17 08:49:17 +0000

    Remove preview for PR 1005 🛬

--- pr-preview/pr-1005/storybook/assets/FormRow.stories-1VPklTig.js
+++
@@ -1,5 +0,0 @@
-import{j as a}from"./iframe-CLlYSCdk.js";import{F as s}from"./FormRow.component-Cu5A4-hT.js";import{T as m}from"./TextInput.component-D5vGayaV.js";import"./Label.component-ZJ31sctE.js";import"./Icon.component-DdDTKhO_.js";import"./FormHint.component-c_8nw10a.js";const d={title:"Forms/FormRow",component:s,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},e={args:{children:[a.jsx(m,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
-  }
-}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const x=["Default"];export{e as Default,x as __namedExportsOrder,d as default};
