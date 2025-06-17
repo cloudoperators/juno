73e4862a89007aff3d81a757d4d851a541cd5c51
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-06-17 08:41:24 +0000

    Deploy preview for PR 1005 🛫

---
+++ pr-preview/pr-1005/storybook/assets/CheckboxGroup.stories--gA01bKR.js
@@ -0,0 +1,77 @@
+import{j as e}from"./iframe-CLlYSCdk.js";import{a as X,C as a}from"./Checkbox.component-DysCUpcw.js";import"./Label.component-ZJ31sctE.js";import"./Icon.component-DdDTKhO_.js";import"./FormHint.component-c_8nw10a.js";const le={title:"Forms/Checkbox/CheckboxGroup",component:X,argTypes:{errortext:{control:!1},helptext:{control:!1},successtext:{control:!1},children:{control:!1},selected:{control:!1},onChange:{table:{type:{summary:"(value: string) => void"}}}}},l={args:{children:[e.jsx(a,{value:"val-1",label:"Option 1"},"1"),e.jsx(a,{value:"val-2",label:"Option 2"},"2"),e.jsx(a,{value:"val-3",label:"Option 3"},"3")]}},o={args:{selected:["val-2"],children:[e.jsx(a,{value:"val-1",label:"Option 1"},"1"),e.jsx(a,{value:"val-2",label:"Option 2"},"2"),e.jsx(a,{value:"val-3",label:"Option 3"},"3")]}},r={args:{children:[e.jsx(a,{value:"val-1",label:"Option 1"},"1"),e.jsx(a,{value:"val-2",label:"Option 2",checked:!0},"2"),e.jsx(a,{value:"val-3",label:"Option 3",checked:!0},"3"),e.jsx(a,{value:"val-4",label:"Option 4"},"4")]}},c={args:{name:"Labelled ChechboxGroup",label:"A Labelled CheckboxGroup",children:[e.jsx(a,{value:"val-1",id:"c-1",label:"Option 1"},"1"),e.jsx(a,{value:"val-2",id:"c-2",label:"Option 2"},"2"),e.jsx(a,{value:"val-3",id:"c-3",label:"Option 3"},"3")]}},i={args:{name:"Required Labelled ChechboxGroup",label:"A Required, Labelled CheckboxGroup",required:!0,children:[e.jsx(a,{value:"val-1",id:"c-1",label:"Option 1"},"1"),e.jsx(a,{value:"val-2",id:"c-2",label:"Option 2"},"2"),e.jsx(a,{value:"val-3",id:"c-3",label:"Option 3"},"3")]}},t={args:{label:"A disabled CheckboxGroup",disabled:!0,children:[e.jsx(a,{value:"val-1",id:"c-1",label:"Option 1"},"1"),e.jsx(a,{value:"val-2",id:"c-2",label:"Option 2"},"2"),e.jsx(a,{value:"val-3",id:"c-3",label:"Option 3"},"3")]}},n={args:{name:"valid-checkbox-group",label:"A valid CheckboxGroup",valid:!0,children:[e.jsx(a,{value:"val-1",id:"c-1",label:"Option 1"},"1"),e.jsx(a,{value:"val-2",id:"c-2",label:"Option 2"},"2"),e.jsx(a,{value:"val-3",id:"c-3",label:"Option 3"},"3")]}},s={args:{name:"checkbox-group-with-helptext",label:"A CheckboxGroup with helptext",helptext:"A helptext",children:[e.jsx(a,{value:"val-1",id:"c-1",label:"Option 1"},"1"),e.jsx(a,{value:"val-2",id:"c-2",label:"Option 2"},"2"),e.jsx(a,{value:"val-3",id:"c-3",label:"Option 3"},"3")]}},p={args:{name:"checkbox-group-with-helptext-as-node",label:"A CheckboxGroup with helptext as node",helptext:e.jsxs(e.Fragment,{children:["This is a helptext with a ",e.jsx("a",{href:"#",children:"Link"})]}),children:[e.jsx(a,{value:"val-1",id:"c-1",label:"Option 1"},"1"),e.jsx(a,{value:"val-2",id:"c-2",label:"Option 2"},"2"),e.jsx(a,{value:"val-3",id:"c-3",label:"Option 3"},"3")]}},u={args:{name:"checkbox-group-with-success",label:"A CheckboxGroup with successful validation",successtext:"This group is valid.",children:[e.jsx(a,{value:"val-1",id:"c-1",label:"Option 1"},"1"),e.jsx(a,{value:"val-2",id:"c-2",label:"Option 2"},"2"),e.jsx(a,{value:"val-3",id:"c-3",label:"Option 3"},"3")]}},b={args:{name:"invalid-checkbox-group",label:"An invalid CheckboxGroup",invalid:!0,children:[e.jsx(a,{value:"val-1",id:"c-1",label:"Option 1"},"1"),e.jsx(a,{value:"val-2",id:"c-2",label:"Option 2"},"2"),e.jsx(a,{value:"val-3",id:"c-3",label:"Option 3"},"3")]}},d={args:{name:"checkbox-group-with-error",label:"A CheckboxGroup with an Error",errortext:"This group has an error.",children:[e.jsx(a,{value:"val-1",id:"c-1",label:"Option 1"},"1"),e.jsx(a,{value:"val-2",id:"c-2",label:"Option 2"},"2"),e.jsx(a,{value:"val-3",id:"c-3",label:"Option 3"},"3")]}};var h,v,x;l.parameters={...l.parameters,docs:{...(h=l.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    children: [<Checkbox value="val-1" label="Option 1" key="1" />, <Checkbox value="val-2" label="Option 2" key="2" />, <Checkbox value="val-3" label="Option 3" key="3" />]
+  }
+}`,...(x=(v=l.parameters)==null?void 0:v.docs)==null?void 0:x.source}}};var k,m,O;o.parameters={...o.parameters,docs:{...(k=o.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    selected: ["val-2"],
+    children: [<Checkbox value="val-1" label="Option 1" key="1" />, <Checkbox value="val-2" label="Option 2" key="2" />, <Checkbox value="val-3" label="Option 3" key="3" />]
+  }
+}`,...(O=(m=o.parameters)==null?void 0:m.docs)==null?void 0:O.source}}};var C,g,y;r.parameters={...r.parameters,docs:{...(C=r.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    children: [<Checkbox value="val-1" label="Option 1" key="1" />, <Checkbox value="val-2" label="Option 2" key="2" checked />, <Checkbox value="val-3" label="Option 3" key="3" checked />, <Checkbox value="val-4" label="Option 4" key="4" />]
+  }
+}`,...(y=(g=r.parameters)==null?void 0:g.docs)==null?void 0:y.source}}};var j,G,A;c.parameters={...c.parameters,docs:{...(j=c.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    name: "Labelled ChechboxGroup",
+    label: "A Labelled CheckboxGroup",
+    children: [<Checkbox value="val-1" id="c-1" label="Option 1" key="1" />, <Checkbox value="val-2" id="c-2" label="Option 2" key="2" />, <Checkbox value="val-3" id="c-3" label="Option 3" key="3" />]
+  }
+}`,...(A=(G=c.parameters)==null?void 0:G.docs)==null?void 0:A.source}}};var w,S,f;i.parameters={...i.parameters,docs:{...(w=i.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    name: "Required Labelled ChechboxGroup",
+    label: "A Required, Labelled CheckboxGroup",
+    required: true,
+    children: [<Checkbox value="val-1" id="c-1" label="Option 1" key="1" />, <Checkbox value="val-2" id="c-2" label="Option 2" key="2" />, <Checkbox value="val-3" id="c-3" label="Option 3" key="3" />]
+  }
+}`,...(f=(S=i.parameters)==null?void 0:S.docs)==null?void 0:f.source}}};var L,W,q;t.parameters={...t.parameters,docs:{...(L=t.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    label: "A disabled CheckboxGroup",
+    disabled: true,
+    children: [<Checkbox value="val-1" id="c-1" label="Option 1" key="1" />, <Checkbox value="val-2" id="c-2" label="Option 2" key="2" />, <Checkbox value="val-3" id="c-3" label="Option 3" key="3" />]
+  }
+}`,...(q=(W=t.parameters)==null?void 0:W.docs)==null?void 0:q.source}}};var R,T,E;n.parameters={...n.parameters,docs:{...(R=n.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    name: "valid-checkbox-group",
+    label: "A valid CheckboxGroup",
+    valid: true,
+    children: [<Checkbox value="val-1" id="c-1" label="Option 1" key="1" />, <Checkbox value="val-2" id="c-2" label="Option 2" key="2" />, <Checkbox value="val-3" id="c-3" label="Option 3" key="3" />]
+  }
+}`,...(E=(T=n.parameters)==null?void 0:T.docs)==null?void 0:E.source}}};var D,H,I;s.parameters={...s.parameters,docs:{...(D=s.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    name: "checkbox-group-with-helptext",
+    label: "A CheckboxGroup with helptext",
+    helptext: "A helptext",
+    children: [<Checkbox value="val-1" id="c-1" label="Option 1" key="1" />, <Checkbox value="val-2" id="c-2" label="Option 2" key="2" />, <Checkbox value="val-3" id="c-3" label="Option 3" key="3" />]
+  }
+}`,...(I=(H=s.parameters)==null?void 0:H.docs)==null?void 0:I.source}}};var F,N,V;p.parameters={...p.parameters,docs:{...(F=p.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  args: {
+    name: "checkbox-group-with-helptext-as-node",
+    label: "A CheckboxGroup with helptext as node",
+    helptext: <>
+        This is a helptext with a <a href="#">Link</a>
+      </>,
+    children: [<Checkbox value="val-1" id="c-1" label="Option 1" key="1" />, <Checkbox value="val-2" id="c-2" label="Option 2" key="2" />, <Checkbox value="val-3" id="c-3" label="Option 3" key="3" />]
+  }
+}`,...(V=(N=p.parameters)==null?void 0:N.docs)==null?void 0:V.source}}};var _,z,B;u.parameters={...u.parameters,docs:{...(_=u.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    name: "checkbox-group-with-success",
+    label: "A CheckboxGroup with successful validation",
+    successtext: "This group is valid.",
+    children: [<Checkbox value="val-1" id="c-1" label="Option 1" key="1" />, <Checkbox value="val-2" id="c-2" label="Option 2" key="2" />, <Checkbox value="val-3" id="c-3" label="Option 3" key="3" />]
+  }
+}`,...(B=(z=u.parameters)==null?void 0:z.docs)==null?void 0:B.source}}};var J,K,M;b.parameters={...b.parameters,docs:{...(J=b.parameters)==null?void 0:J.docs,source:{originalSource:`{
+  args: {
+    name: "invalid-checkbox-group",
+    label: "An invalid CheckboxGroup",
+    invalid: true,
+    children: [<Checkbox value="val-1" id="c-1" label="Option 1" key="1" />, <Checkbox value="val-2" id="c-2" label="Option 2" key="2" />, <Checkbox value="val-3" id="c-3" label="Option 3" key="3" />]
+  }
+}`,...(M=(K=b.parameters)==null?void 0:K.docs)==null?void 0:M.source}}};var P,Q,U;d.parameters={...d.parameters,docs:{...(P=d.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    name: "checkbox-group-with-error",
+    label: "A CheckboxGroup with an Error",
+    errortext: "This group has an error.",
+    children: [<Checkbox value="val-1" id="c-1" label="Option 1" key="1" />, <Checkbox value="val-2" id="c-2" label="Option 2" key="2" />, <Checkbox value="val-3" id="c-3" label="Option 3" key="3" />]
+  }
+}`,...(U=(Q=d.parameters)==null?void 0:Q.docs)==null?void 0:U.source}}};const oe=["Default","Selected","IndividuallyChecked","WithLabel","Required","Disabled","ValidCheckboxGroup","WithHelptext","WithHelptextAsNode","WithSuccesstext","InvalidCheckboxGroup","WithErrortext"];export{l as Default,t as Disabled,r as IndividuallyChecked,b as InvalidCheckboxGroup,i as Required,o as Selected,n as ValidCheckboxGroup,d as WithErrortext,s as WithHelptext,p as WithHelptextAsNode,c as WithLabel,u as WithSuccesstext,oe as __namedExportsOrder,le as default};
