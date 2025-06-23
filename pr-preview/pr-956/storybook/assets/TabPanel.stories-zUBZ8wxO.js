f5b16e02871a8c018f45625c4f0db29e09070c54
barsukov <barsukov@users.noreply.github.com>
2025-06-23 12:10:47 +0000

    Deploy preview for PR 956 🛫

---
+++ pr-preview/pr-956/storybook/assets/TabPanel.stories-zUBZ8wxO.js
@@ -0,0 +1,5 @@
+import{T as o}from"./TabPanel.component-CYUw0HPD.js";import"./iframe-DD2RYS5_.js";import"./TabPanel-qZALRHG9.js";const l={title:"Layout/Tabs/TabPanel",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},e={args:{children:"Tab panel content goes here."}};var a,r,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: "Tab panel content goes here."
+  }
+}`,...(t=(r=e.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const p=["Default"];export{e as Default,p as __namedExportsOrder,l as default};
