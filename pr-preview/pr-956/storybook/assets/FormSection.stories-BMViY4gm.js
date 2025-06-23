de0c17331b5740d8c59d04001538555501e0c2dd
mark-karnaukh-extern-sap <mark-karnaukh-extern-sap@users.noreply.github.com>
2025-06-23 11:09:30 +0000

    Deploy preview for PR 956 🛫

---
+++ pr-preview/pr-956/storybook/assets/FormSection.stories-BMViY4gm.js
@@ -0,0 +1,18 @@
+import{j as e}from"./iframe-DD2RYS5_.js";import{F as c}from"./FormSection.component-BuuVMpKL.js";import{T as s}from"./TextInput.component-DlgTKjv3.js";import{F as t}from"./FormRow.component-ASkcvPtU.js";import"./Label.component-tFZ5X6QO.js";import"./Icon.component-DLXr1Oas.js";import"./FormHint.component-BhbjIswW.js";const j={title:"Forms/FormSection",component:c,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{children:e.jsx(s,{label:"Address Line 1"})},"1"),e.jsx(t,{children:e.jsx(s,{label:"Address Line 2"})},"2")]}},o={args:{title:"Form Section With Title",children:[e.jsx(t,{children:e.jsx(s,{label:"Address Line 1"})},"1"),e.jsx(t,{children:e.jsx(s,{label:"Address Line 2"})},"2")]}};var n,a,i;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    children: [<FormRow key="1">
+        <TextInput label="Address Line 1" />
+      </FormRow>, <FormRow key="2">
+        <TextInput label="Address Line 2" />
+      </FormRow>]
+  }
+}`,...(i=(a=r.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};var l,m,d;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    title: "Form Section With Title",
+    children: [<FormRow key="1">
+        <TextInput label="Address Line 1" />
+      </FormRow>, <FormRow key="2">
+        <TextInput label="Address Line 2" />
+      </FormRow>]
+  }
+}`,...(d=(m=o.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};const b=["Default","WithTitle"];export{r as Default,o as WithTitle,b as __namedExportsOrder,j as default};
