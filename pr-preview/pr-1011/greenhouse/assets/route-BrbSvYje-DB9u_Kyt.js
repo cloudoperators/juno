9f3c55d851cfe3a74604d635a56018eb814c4ba7
barsukov <barsukov@users.noreply.github.com>
2025-06-24 12:10:17 +0000

    Remove preview for PR 1011 🛬

--- pr-preview/pr-1011/greenhouse/assets/route-BrbSvYje-DB9u_Kyt.js
+++
@@ -1,1 +0,0 @@
-import{O as o}from"./App-0HyVOZoP-DD8H8vFs.js";import"./index-oakbOWOt.js";import"./index-CR7LFbZ9.js";const r=o;export{r as component};
--- pr-preview/pr-1011/heureka/assets/route-H2vjWxD3.js
+++
@@ -1,1 +0,0 @@
-import{O as o}from"./App-MASXeF_7.js";import"./index-Cd_uWheP.js";const m=o;export{m as component};
