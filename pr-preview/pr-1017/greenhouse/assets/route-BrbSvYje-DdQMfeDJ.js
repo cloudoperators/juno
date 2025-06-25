5b7dd1a9bb12a049dca91e2e6deef274449b8f06
taymoor89 <taymoor89@users.noreply.github.com>
2025-06-25 07:37:34 +0000

    Remove preview for PR 1017 🛬

--- pr-preview/pr-1017/greenhouse/assets/route-BrbSvYje-DdQMfeDJ.js
+++
@@ -1,1 +0,0 @@
-import{O as o}from"./App-0HyVOZoP-BrHm2y5Q.js";import"./index-DSJOM-Kq.js";import"./index-izZ6DFM-.js";const r=o;export{r as component};
--- pr-preview/pr-1017/heureka/assets/route-BBaNSevY.js
+++
@@ -1,1 +0,0 @@
-import{O as o}from"./App-0QXGJJFe.js";import"./index-DcTZAFLo.js";const m=o;export{m as component};
