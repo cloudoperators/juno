2205ead1e01e6eb253af6c67d51c45b268783812
I531348 <guoda.puidokaite@sap.com>
2025-06-27 10:17:40 +0200

    remove comment

--- apps/example/src/App.tsx
+++ apps/example/src/App.tsx
@@ -16,8 +16,6 @@ import Content from "./components/app-shell/Content"
 import Header from "./components/app-shell/header/Header"
 import TopNavigationBar from "./components/app-shell/Navigation"
 
-//TRIGGER DEPLOYMENT
-
 import styles from "./styles.scss?inline"
 
 interface AppProps {
