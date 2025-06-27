26afeedae552f07cf2c7b371a89634b7a00909e7
mark-karnaukh-extern-sap <mark-karnaukh-extern-sap@users.noreply.github.com>
2025-06-27 08:49:21 +0000

    Deploy preview for PR 1034 🛫

---
+++ pr-preview/pr-1034/storybook/assets/ButtonRow.stories-DsSSYgpb.js
@@ -0,0 +1,5 @@
+import{j as e}from"./iframe-Dj_ANc-G.js";import{B as l}from"./ButtonRow.component-FHA5cYuB.js";import{B as a}from"./Button.component-Ct3Zj1Eu.js";import"./Stack.component-DzdMa6q8.js";import"./Icon.component-xg4SWuNe.js";import"./Spinner.component-B9Tx3jlY.js";const d={title:"Forms/ButtonRow",component:l,argTypes:{children:{control:!1}}},t={args:{children:[e.jsx(a,{label:"Cancel",title:"Cancel"},"1"),e.jsx(a,{label:"Save",title:"Save",variant:"primary"},"2")]}};var r,o,n;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
+  }
+}`,...(n=(o=t.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};const f=["Default"];export{t as Default,f as __namedExportsOrder,d as default};
---
+++ pr-preview/pr-1034/storybook/assets/ComboBoxOption.stories-bF70y82Z.js
@@ -0,0 +1,14 @@
+import{j as s}from"./iframe-Dj_ANc-G.js";import{a,C as b}from"./ComboBoxOption.component-Dgx1UdL6.js";import"./floating-ui.react-Cpy7nuHM.js";import"./Label.component-Dsbf57s3.js";import"./FormHint.component-Cvdqz5Ck.js";import"./Icon.component-xg4SWuNe.js";import"./Spinner.component-B9Tx3jlY.js";import"./PortalProvider.component-BpRh08yL.js";import"./portal-DA8LjGb1.js";import"./frozen-DhGedZjh.js";import"./use-tree-walker-Ba2Jsss8.js";const E={title:"Forms/ComboBox/ComboBoxOption",component:a,argTypes:{},render:O=>s.jsx(b,{children:s.jsx(a,{...O})})},r={args:{value:"Option 1"}},o={args:{disabled:!0,value:"Disabled Option"}},e={args:{children:"Option 1"}};var t,n,i;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    value: "Option 1"
+  }
+}`,...(i=(n=r.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};var p,m,c;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    value: "Disabled Option"
+  }
+}`,...(c=(m=o.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};var d,l,u;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    children: "Option 1"
+  }
+}`,...(u=(l=e.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};const _=["Default","Disabled","ChildrenOnly"];export{e as ChildrenOnly,r as Default,o as Disabled,_ as __namedExportsOrder,E as default};
