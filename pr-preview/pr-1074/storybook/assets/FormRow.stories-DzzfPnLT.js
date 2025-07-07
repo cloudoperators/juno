1f73bab579ac695d7b0271af2fbd381e5e709546
mark-karnaukh-extern-sap <mark-karnaukh-extern-sap@users.noreply.github.com>
2025-07-07 09:44:33 +0000

    Deploy preview for PR 1074 🛫

---
+++ pr-preview/pr-1074/storybook/assets/FormRow.stories-DzzfPnLT.js
@@ -0,0 +1,5 @@
+import{j as a}from"./iframe-DL2z27nb.js";import{F as s}from"./FormRow.component-BPaDQiei.js";import{T as m}from"./TextInput.component-DSF4U2wW.js";import"./Label.component-ndRtHtbJ.js";import"./Icon.component-BaIV3QHy.js";import"./FormHint.component-BYtadDDl.js";const d={title:"Forms/FormRow",component:s,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},e={args:{children:[a.jsx(m,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
+  }
+}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const x=["Default"];export{e as Default,x as __namedExportsOrder,d as default};
