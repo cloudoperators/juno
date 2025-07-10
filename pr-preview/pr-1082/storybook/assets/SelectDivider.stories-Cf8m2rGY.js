8d18901882b01224ef03d72ca53e4d3524b14d89
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-07-10 13:31:12 +0000

    Remove preview for PR 1082 🛬

--- pr-preview/pr-1082/storybook/assets/SelectDivider.stories-Cf8m2rGY.js
+++
@@ -1,7 +0,0 @@
-import{e as n,j as e}from"./iframe-Co0hevCM.js";import{S as p,a}from"./SelectOption.component-BKjecahH.js";import{P as m}from"./PortalProvider.component-D2y7Ftuz.js";import"./portal-Do3njrQB.js";import"./floating-ui.react-DuwZ_LrB.js";import"./frozen-B4u__BWK.js";import"./use-text-value-fEDQExqQ.js";import"./Label.component-DnLxQYQc.js";import"./Icon.component-IJHM0szB.js";import"./Spinner.component-QdQKvfRc.js";import"./FormHint.component-Cl_VhGuw.js";const u=`
-  jn:h-px 
-  jn:w-full
-  jn:bg-theme-background-lvl-3
-`,r=n.forwardRef(({className:t="",...l},d)=>e.jsx("div",{className:`juno-select-divider ${u} ${t}`,ref:d,...l}));r.displayName="SelectDivider";try{r.displayName="SelectDivider",r.__docgenInfo={description:"A visual border/divider between SelectOptions or SelectGroups",displayName:"SelectDivider",props:{className:{defaultValue:{value:""},description:"add a custom className",name:"className",required:!1,type:{name:"string"}}}}}catch{}const b={title:"Forms/Select/SelectDivider",component:r,argTypes:{},decorators:[t=>e.jsx(m,{children:e.jsx(t,{})})],render:()=>e.jsxs(p,{open:!0,children:[e.jsx(a,{value:"1",children:"1"}),e.jsx(r,{}),e.jsx(a,{value:"3",children:"3"})]})},s={args:{}};var o,i,c;s.parameters={...s.parameters,docs:{...(o=s.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {}
-}`,...(c=(i=s.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};const w=["Default"];export{s as Default,w as __namedExportsOrder,b as default};
--- pr-preview/pr-1082/storybook/assets/TooltipContent.stories-CTq2Nycb.js
+++
@@ -1,4 +0,0 @@
-import{j as e}from"./iframe-Co0hevCM.js";import{b as n,T as i,a as l}from"./TooltipTrigger.component-MTBP8L43.js";import"./floating-ui.react-DuwZ_LrB.js";import"./Icon.component-IJHM0szB.js";const j={title:"Components/Tooltip/TooltipContent",component:n,argTypes:{},decorators:[r=>e.jsx("div",{className:"jn:my-6 jn:flex jn:justify-center",children:e.jsxs(i,{initialOpen:!0,children:[e.jsx(l,{children:"clickMe"}),e.jsx(r,{})]})})]},p=r=>e.jsx(n,{...r,children:"This is a tooltip"}),t={args:{},render:p};var o,s,a;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {},
-  render: DefaultTemplate
-}`,...(a=(s=t.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const T=["Default"];export{t as Default,T as __namedExportsOrder,j as default};
