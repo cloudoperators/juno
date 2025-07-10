50cd168bb3917450b3fd1c7af4911bfc1a47790e
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-07-10 09:55:55 +0000

    Deploy preview for PR 1082 🛫

---
+++ pr-preview/pr-1082/storybook/assets/ButtonRow.stories-B5ZJR8sb.js
@@ -0,0 +1,5 @@
+import{j as e}from"./iframe-DMkIl3Ib.js";import{B as l}from"./ButtonRow.component-DJ7ZHlI-.js";import{B as a}from"./Button.component-B5j6jEwJ.js";import"./Stack.component-DmHVi0ak.js";import"./Icon.component-_QpXXvLQ.js";import"./Spinner.component-BlPEWZvb.js";const d={title:"Forms/ButtonRow",component:l,argTypes:{children:{control:!1}}},t={args:{children:[e.jsx(a,{label:"Cancel",title:"Cancel"},"1"),e.jsx(a,{label:"Save",title:"Save",variant:"primary"},"2")]}};var r,o,n;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
+  }
+}`,...(n=(o=t.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};const f=["Default"];export{t as Default,f as __namedExportsOrder,d as default};
