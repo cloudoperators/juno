fd85284c1b96ed575fda4b6b79ad371a202232de
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-07-14 13:32:39 +0000

    Deploy preview for PR 1084 🛫

---
+++ pr-preview/pr-1084/storybook/assets/ComboBoxOption.stories-S3iua5C5.js
@@ -0,0 +1,14 @@
+import{j as s}from"./iframe-Da2nAnW2.js";import{a,C as n}from"./ComboBoxOption.component-B4hqPHm-.js";import"./floating-ui.react-D1V8yblC.js";import"./Label.component-BxRhUr3P.js";import"./FormHint.component-C3yjEjJw.js";import"./Icon.component-b4kYVQ7U.js";import"./Spinner.component-BQ8joNj2.js";import"./PortalProvider.component-C8lvNiYh.js";import"./portal-DCrtyBC5.js";import"./frozen-C6BVeZWi.js";import"./use-tree-walker-DhcROk_X.js";const C={title:"Forms/ComboBox/ComboBoxOption",component:a,argTypes:{},render:t=>s.jsx(n,{children:s.jsx(a,{...t})})},r={args:{value:"Option 1"}},o={args:{disabled:!0,value:"Disabled Option"}},e={args:{children:"Option 1"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: "Option 1"
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    value: "Disabled Option"
+  }
+}`,...o.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: "Option 1"
+  }
+}`,...e.parameters?.docs?.source}}};const D=["Default","Disabled","ChildrenOnly"];export{e as ChildrenOnly,r as Default,o as Disabled,D as __namedExportsOrder,C as default};
---
+++ pr-preview/pr-1084/storybook/assets/GridRow.component-BGh2kHqx.js
@@ -0,0 +1,7 @@
+import{j as n}from"./iframe-Da2nAnW2.js";const i=`
+    jn:flex
+    jn:flex-wrap
+    jn:m-grid-row
+`,r=({children:e,className:o="",...t})=>n.jsx("div",{className:`juno-grid-row ${i} ${o}`,...t,children:e});try{r.displayName="GridRow",r.__docgenInfo={description:`A grid row container to hold GridColumn elements inside a Grid.
+This component ensures that its children are wrapped correctly in a flexbox layout.`,displayName:"GridRow",props:{children:{defaultValue:null,description:`Elements to be rendered within the grid row.
+Typically, these would be GridColumn components.`,name:"children",required:!1,type:{name:"ReactNode"}},className:{defaultValue:{value:""},description:"Additional CSS classes to apply to the grid row for custom styling.",name:"className",required:!1,type:{name:"string"}}}}}catch{}export{r as G};
