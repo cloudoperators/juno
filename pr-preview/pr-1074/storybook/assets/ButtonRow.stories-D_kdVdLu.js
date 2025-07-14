6358db6abae50efe1f456f7c9760b2337c1f22ac
mark-karnaukh-extern-sap <mark-karnaukh-extern-sap@users.noreply.github.com>
2025-07-14 08:57:09 +0000

    Deploy preview for PR 1074 🛫

--- pr-preview/pr-1074/storybook/assets/ButtonRow.stories-D_kdVdLu.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-BF07MaSR.js";import{B as l}from"./ButtonRow.component-MFSdXnLP.js";import{B as a}from"./Button.component-D9wdYe8C.js";import"./Stack.component-CF4sKDrg.js";import"./Icon.component-CGT6HHfX.js";import"./Spinner.component-DpLiDhsq.js";const d={title:"Forms/ButtonRow",component:l,argTypes:{children:{control:!1}}},t={args:{children:[e.jsx(a,{label:"Cancel",title:"Cancel"},"1"),e.jsx(a,{label:"Save",title:"Save",variant:"primary"},"2")]}};var r,o,n;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
-  }
-}`,...(n=(o=t.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};const f=["Default"];export{t as Default,f as __namedExportsOrder,d as default};
