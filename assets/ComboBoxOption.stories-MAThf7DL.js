6448ab31627333b1145428fe139d2d21f46465e0
andypf <ap@a-pfau.de>
2025-07-15 10:46:22 +0000

    Deploying to gh-pages from @ cloudoperators/juno@ca7489793f4d8f4d91f25e2975d19a5201393599 🚀

---
+++ assets/ComboBoxOption.stories-MAThf7DL.js
@@ -0,0 +1,14 @@
+import{j as s}from"./iframe-zQHwQpPp.js";import{a,C as n}from"./ComboBoxOption.component-plNdThd7.js";import"./floating-ui.react-D0J-D4sO.js";import"./Label.component-fj7RC_PX.js";import"./FormHint.component-BQjFNQBI.js";import"./Icon.component-FhB_jwry.js";import"./Spinner.component-DNyVm1ru.js";import"./PortalProvider.component-C80SquJp.js";import"./portal-D2BCQ2xG.js";import"./frozen-BZrLgXyc.js";import"./use-tree-walker-D-OfthFc.js";const C={title:"Forms/ComboBox/ComboBoxOption",component:a,argTypes:{},render:t=>s.jsx(n,{children:s.jsx(a,{...t})})},r={args:{value:"Option 1"}},o={args:{disabled:!0,value:"Disabled Option"}},e={args:{children:"Option 1"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: "Option 1"
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    value: "Disabled Option"
+  }
+}`,...o.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: "Option 1"
+  }
+}`,...e.parameters?.docs?.source}}};const D=["Default","Disabled","ChildrenOnly"];export{e as ChildrenOnly,r as Default,o as Disabled,D as __namedExportsOrder,C as default};
--- assets/FormRow.stories-m6ma0y8Z.js
+++
@@ -1,5 +0,0 @@
-import{j as r}from"./iframe-B9TuM_je.js";import{F as o}from"./FormRow.component-B9DKNp_e.js";import{T as t}from"./TextInput.component-DZlTsDx-.js";import"./Label.component-DihJZkX6.js";import"./Icon.component-CKBZsNoP.js";import"./FormHint.component-BD6IR7AD.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},e={args:{children:[r.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
-  }
-}`,...e.parameters?.docs?.source}}};const i=["Default"];export{e as Default,i as __namedExportsOrder,c as default};
