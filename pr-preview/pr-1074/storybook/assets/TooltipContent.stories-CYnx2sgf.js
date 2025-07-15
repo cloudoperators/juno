77a9adab769fc644f0dddac4de784297c2e73f79
andypf <andypf@users.noreply.github.com>
2025-07-15 10:46:29 +0000

    Remove preview for PR 1074 🛬

--- pr-preview/pr-1074/storybook/assets/TooltipContent.stories-CYnx2sgf.js
+++
@@ -1,4 +0,0 @@
-import{j as e}from"./iframe-zQHwQpPp.js";import{b as o,T as s,a}from"./TooltipTrigger.component-DzLI9otp.js";import"./floating-ui.react-D0J-D4sO.js";import"./Icon.component-FhB_jwry.js";const m={title:"Components/Tooltip/TooltipContent",component:o,argTypes:{},decorators:[r=>e.jsx("div",{className:"jn:my-6 jn:flex jn:justify-center",children:e.jsxs(s,{initialOpen:!0,children:[e.jsx(a,{children:"clickMe"}),e.jsx(r,{})]})})]},n=r=>e.jsx(o,{...r,children:"This is a tooltip"}),t={args:{},render:n};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {},
-  render: DefaultTemplate
-}`,...t.parameters?.docs?.source}}};const d=["Default"];export{t as Default,d as __namedExportsOrder,m as default};
