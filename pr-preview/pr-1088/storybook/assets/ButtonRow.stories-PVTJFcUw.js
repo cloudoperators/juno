b226f0cc846ca9726ac8c8429029adbbd970e775
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-07-18 16:50:02 +0000

    Remove preview for PR 1088 🛬

--- pr-preview/pr-1088/storybook/assets/ButtonRow.stories-PVTJFcUw.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-P61G9-iG.js";import{B as r}from"./ButtonRow.component-C_XPhah5.js";import{B as a}from"./Button.component-DX84J5ua.js";import"./Stack.component-D0VVztX5.js";import"./Icon.component-DeYor7za.js";import"./Spinner.component-sQszOBR0.js";const c={title:"Forms/ButtonRow",component:r,argTypes:{children:{control:!1}}},t={args:{children:[e.jsx(a,{label:"Cancel",title:"Cancel"},"1"),e.jsx(a,{label:"Save",title:"Save",variant:"primary"},"2")]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
-  }
-}`,...t.parameters?.docs?.source}}};const p=["Default"];export{t as Default,p as __namedExportsOrder,c as default};
--- pr-preview/pr-1088/storybook/assets/SelectDivider.stories-DP5W341j.js
+++
@@ -1,7 +0,0 @@
-import{e as c,j as e}from"./iframe-P61G9-iG.js";import{S as l,a}from"./SelectOption.component-DM9v8Jjw.js";import{P as d}from"./PortalProvider.component-Bc3DL5Ls.js";import"./portal-oOYqBe7g.js";import"./frozen-BD7onkn6.js";import"./use-text-value-DR_Qgkcn.js";import"./Label.component-DrpdqCKq.js";import"./Icon.component-DeYor7za.js";import"./Spinner.component-sQszOBR0.js";import"./FormHint.component-CNpNuCcM.js";import"./floating-ui.react-D4xN6_YI.js";const n=`
-  jn:h-px 
-  jn:w-full
-  jn:bg-theme-background-lvl-3
-`,r=c.forwardRef(({className:t="",...o},i)=>e.jsx("div",{className:`juno-select-divider ${n} ${t}`,ref:i,...o}));r.displayName="SelectDivider";try{r.displayName="SelectDivider",r.__docgenInfo={description:"A visual border/divider between SelectOptions or SelectGroups",displayName:"SelectDivider",props:{className:{defaultValue:{value:""},description:"add a custom className",name:"className",required:!1,type:{name:"string"}}}}}catch{}const h={title:"Forms/Select/SelectDivider",component:r,argTypes:{},decorators:[t=>e.jsx(d,{children:e.jsx(t,{})})],render:()=>e.jsxs(l,{open:!0,children:[e.jsx(a,{value:"1",children:"1"}),e.jsx(r,{}),e.jsx(a,{value:"3",children:"3"})]})},s={args:{}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...s.parameters?.docs?.source}}};const D=["Default"];export{s as Default,D as __namedExportsOrder,h as default};
