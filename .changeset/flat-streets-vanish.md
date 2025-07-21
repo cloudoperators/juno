6d972a52688022803cba89e07d05a98fc67268c7
Hoda <107242553+hodanoori@users.noreply.github.com>
2025-07-21 15:06:34 +0200

    chore(heureka):  displays occurences in service details page (#1093)
    * chore(heureka): displayes occurences directly in service details page
    
    * chore(heureka): adds changeset
    
    * fix(heureka): fix typo

---
+++ .changeset/flat-streets-vanish.md
@@ -0,0 +1,6 @@
+---
+"@cloudoperators/juno-app-heureka": patch
+"@cloudoperators/juno-app-greenhouse": patch
+---
+
+Displays number of service image version instances in service details page (this count is also available in service image version details panel)
--- apps/heureka/src/components/Services/utils.ts
+++ apps/heureka/src/components/Services/utils.ts
@@ -158,7 +158,7 @@ export type ServiceImageVersion = {
   repository: string
   ccrn: string
   issueCounts: IssuesCountsType
-  componetInstancesCount: number
+  componentInstancesCount: number
   componentInstances?: ComponentInstance[]
 }
 
@@ -193,7 +193,7 @@ export const getNormalizedImageVersionsResponse = (
               none: 0,
               total: 0,
             },
-            componetInstancesCount: edge?.node?.componentInstances?.totalCount ?? 0,
+            componentInstancesCount: edge?.node?.componentInstances?.totalCount ?? 0,
             componentInstances:
               edge?.node?.componentInstances?.edges
                 ?.filter((edge) => edge?.node) // Remove null edges
