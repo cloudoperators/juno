5e16cc94420009840b4000116bd7958ec81b4c7f
Hoda Noori <hoda.noori@sap.com>
2025-07-22 15:37:22 +0200

    chore(heureka): fixes license header in graphql.ts file

--- apps/heureka/src/generated/graphql.ts
+++ apps/heureka/src/generated/graphql.ts
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: [year] SAP SE or an SAP affiliate company and Juno contributors
+ * SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Juno contributors
  * SPDX-License-Identifier: Apache-2.0
  */
 
