28af1fb7f9a53075257f94b1a1a7cde9b0a69eff
andypf <andypf@users.noreply.github.com>
2025-07-23 07:19:39 +0000

    Deploy preview for PR 1092 🛫

---
+++ pr-preview/pr-1092/heureka/assets/route-DZwDTugg.js
@@ -0,0 +1,1 @@
+import{O as o}from"./App-k87nbXyE.js";import"./index-BdQ7vFHI.js";const m=o;export{m as component};
---
+++ pr-preview/pr-1092/storybook/assets/ButtonRow.stories-Bh51Pd1H.js
@@ -0,0 +1,5 @@
+import{j as e}from"./iframe-C_cKnsOz.js";import{B as r}from"./ButtonRow.component-DC9loCn9.js";import{B as a}from"./Button.component-D7iuRHC2.js";import"./Stack.component-5NC-lLQV.js";import"./Icon.component-CMZDKm3R.js";import"./Spinner.component-Bo07_wcE.js";const c={title:"Forms/ButtonRow",component:r,argTypes:{children:{control:!1}}},t={args:{children:[e.jsx(a,{label:"Cancel",title:"Cancel"},"1"),e.jsx(a,{label:"Save",title:"Save",variant:"primary"},"2")]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
+  }
+}`,...t.parameters?.docs?.source}}};const p=["Default"];export{t as Default,p as __namedExportsOrder,c as default};
