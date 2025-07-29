e4e44ee18eb0a229d92a6b557e87795516df9ce4
Taimoor Aslam <taimoor.aslam@sap.com>
2025-07-29 09:57:59 +0200

    chore(juno): add missing license header

--- apps/greenhouse/src/routes/__root.tsx
+++ apps/greenhouse/src/routes/__root.tsx
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Juno contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import React, { useEffect } from "react"
 import { Outlet, createRootRouteWithContext } from "@tanstack/react-router"
 import { useActions } from "@cloudoperators/juno-messages-provider"
