611c9bf2c2b6b71bd1f2d2079f1ac79f5c7fe18d
Taimoor Aslam <taimoor.aslam@sap.com>
2025-07-30 13:49:16 +0200

    feat(supernova): setup tanstack router

--- apps/supernova/src/App.tsx
+++ apps/supernova/src/App.tsx
@@ -5,8 +5,9 @@
 
 import React from "react"
 
+import { createBrowserHistory, createHashHistory, createRouter, RouterProvider } from "@tanstack/react-router"
+import { decodeV2, encodeV2 } from "@cloudoperators/juno-url-state-provider"
 import { AppShellProvider, CodeBlock } from "@cloudoperators/juno-ui-components"
-import AppContent from "./AppContent"
 import styles from "./styles.css?inline"
 import { StoreProvider } from "./components/StoreProvider"
 import { MessagesProvider } from "@cloudoperators/juno-messages-provider"
@@ -16,6 +17,37 @@ import { ErrorBoundary } from "react-error-boundary"
 import useUrlState from "./hooks/useUrlState"
 import useUrlQueryState from "./hooks/useUrlQueryState"
 import { z } from "zod"
+import { routeTree } from "./routeTree.gen"
+
+// Create a new router instance
+const router = createRouter({
+  routeTree,
+  context: {
+    appProps: undefined!,
+    queryClient: undefined!,
+  },
+})
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router
+  }
+}
+
+export type AppProps = {
+  endpoint: string
+  filterLabels: string[]
+  initialFilters: Record<string, any>
+  predefinedFilters: Record<string, any>
+  silenceExcludedLabels: string[]
+  silenceTemplates: any[]
+  theme: "theme-dark" | "theme-light"
+  username: string
+  enableHashedRouting?: boolean
+  basePath?: string
+}
+
 const AppShellTheme = z.enum(["theme-dark", "theme-light"])
 
 const queryClient = new QueryClient({
@@ -47,13 +79,47 @@ const fallbackRender = ({ error }: any) => {
   )
 }
 
-function App() {
+function App(props: AppProps) {
+  /*
+   * Dynamically change the type of history on the router
+   * based on the enableHashedRouting prop. This ensures that
+   * the correct history type is used when A Shell app does not
+   * want the app to use browser history.
+   */
+  router.update({
+    routeTree,
+    context: { appProps: props, queryClient },
+    history: props.enableHashedRouting ? createHashHistory() : createBrowserHistory(),
+    stringifySearch: encodeV2,
+    parseSearch: (searchString) => {
+      if (!props.enableHashedRouting) {
+        return decodeV2(searchString)
+      }
+
+      /*
+       * In case of hashed routing Tanstack router returns URL search params of the entire URL rather than just from the hashed part.
+       * We'll have to extract the query part from the hash because otherwise in embedded mode the app will be taking search params from the shell app as well.
+       * Sanitize the search string by extracting the substring between the first '?' and the next '?' (if any), keeping the first '?'.
+       * https://github.com/TanStack/router/issues/4370
+       * http://localhost:3000/?preHashParam=prehashtest#/services?postHashParam1=test1?preHashParam=prehashtest
+       * searchString = "?postHashParam1=test1?preHashParam=prehashtest"
+       * searchStringFromHash = "?postHashParam1=test1"
+       */
+      const postHashParams = searchString.indexOf("?")
+      if (postHashParams === -1) return {} // If no query part is found, return an empty object
+      const preHashParams = searchString.indexOf("?", postHashParams + 1)
+      const searchStringFromHash = searchString.slice(postHashParams, preHashParams === -1 ? undefined : preHashParams)
+
+      return decodeV2(searchStringFromHash)
+    },
+  })
+
   return (
     <ErrorBoundary fallbackRender={fallbackRender}>
       <MessagesProvider>
         <CustomAppShell>
           <QueryClientProvider client={queryClient}>
-            <AppContent />
+            <RouterProvider basepath={props.basePath || "/"} router={router} />
           </QueryClientProvider>
         </CustomAppShell>
       </MessagesProvider>
