375a3a8ced693fb4648f4d9601bff57750f9ada2
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-08-15 16:06:59 +0000

    Deploy preview for PR 1128 🛫

---
+++ pr-preview/pr-1128/storybook/assets/Checkbox.stories-lrAKJRUx.js
@@ -0,0 +1,52 @@
+import{C as h}from"./Checkbox.component-Ccl2WSja.js";import"./iframe-BsvsW6Bj.js";import"./Label.component-BES_YWKZ.js";import"./Icon.component-CublxHfD.js";import"./FormHint.component-u0kCBxwk.js";const g={title:"Forms/Checkbox/Checkbox",component:h,argTypes:{errortext:{control:!1},helptext:{control:!1},successtext:{control:!1}}},e={args:{}},a={args:{checked:!0}},r={args:{label:"Checkbox with Label"}},t={args:{required:!0,label:"Required Checkbox"}},s={args:{disabled:!0}},o={args:{indeterminate:!0}},n={args:{valid:!0}},i={args:{invalid:!0}},c={args:{valid:!0,label:"Validated checkbox with label and icon",successtext:"This option is valid.",helptext:"Validation icons will only show when there is a label on the Checkbox"}},l={args:{invalid:!0,label:"Invalidated checkbox with label and icon",errortext:"This option is invalid.",helptext:"Validation icons will only show when there is a label on the Checkbox"}},d={args:{indeterminate:!0,label:"Indeterminate checkbox with label",helptext:"A checkbox can be indeterminate as parent of multiple checkboxes with mixed checked states"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...e.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    checked: true
+  }
+}`,...a.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Checkbox with Label"
+  }
+}`,...r.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    required: true,
+    label: "Required Checkbox"
+  }
+}`,...t.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...s.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    indeterminate: true
+  }
+}`,...o.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    valid: true
+  }
+}`,...n.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    invalid: true
+  }
+}`,...i.parameters?.docs?.source}}};c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    valid: true,
+    label: "Validated checkbox with label and icon",
+    successtext: "This option is valid.",
+    helptext: "Validation icons will only show when there is a label on the Checkbox"
+  }
+}`,...c.parameters?.docs?.source}}};l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    invalid: true,
+    label: "Invalidated checkbox with label and icon",
+    errortext: "This option is invalid.",
+    helptext: "Validation icons will only show when there is a label on the Checkbox"
+  }
+}`,...l.parameters?.docs?.source}}};d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    indeterminate: true,
+    label: "Indeterminate checkbox with label",
+    helptext: "A checkbox can be indeterminate as parent of multiple checkboxes with mixed checked states"
+  }
+}`,...d.parameters?.docs?.source}}};const k=["Default","Checked","WithLabel","Required","Disabled","Indeterminate","Valid","Invalid","ValidWithLabel","InvalidWithLabel","IndeterminateWithLabel"];export{a as Checked,e as Default,s as Disabled,o as Indeterminate,d as IndeterminateWithLabel,i as Invalid,l as InvalidWithLabel,t as Required,n as Valid,c as ValidWithLabel,r as WithLabel,k as __namedExportsOrder,g as default};
---
+++ pr-preview/pr-1128/storybook/assets/FormRow.stories-BGeMJdsR.js
@@ -0,0 +1,5 @@
+import{j as r}from"./iframe-BsvsW6Bj.js";import{F as o}from"./FormRow.component-B6AXo2Wq.js";import{T as t}from"./TextInput.component-BqD45hKn.js";import"./Label.component-BES_YWKZ.js";import"./Icon.component-CublxHfD.js";import"./FormHint.component-u0kCBxwk.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},e={args:{children:[r.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
+  }
+}`,...e.parameters?.docs?.source}}};const i=["Default"];export{e as Default,i as __namedExportsOrder,c as default};
