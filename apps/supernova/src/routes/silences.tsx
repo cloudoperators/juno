f81b11710386c6fc6872545b21e3480e00087174
Taimoor Aslam <taimoor.aslam@sap.com>
2025-08-20 10:40:50 +0500

    chore(supernova): add license header

--- apps/supernova/src/routes/silences.tsx
+++ apps/supernova/src/routes/silences.tsx
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Juno contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import React from "react"
 import { createFileRoute } from "@tanstack/react-router"
 import SilencesList from "../components/silences/SilencesList"
