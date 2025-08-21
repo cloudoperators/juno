d5820bbc34d1613442580ed7802a9665f3bcce03
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-08-21 09:32:59 +0000

    Remove preview for PR 1130 🛬

--- pr-preview/pr-1130/storybook/assets/ButtonRow.stories-Db_aX0Fe.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-8mFGz_IY.js";import{B as r}from"./ButtonRow.component-BBBTkvsN.js";import{B as a}from"./Button.component-BQ6_SXme.js";import"./Stack.component-CKu_UKiX.js";import"./Icon.component-wwPg9qoH.js";import"./Spinner.component-xF8t0Aoy.js";const c={title:"Forms/ButtonRow",component:r,argTypes:{children:{control:!1}}},t={args:{children:[e.jsx(a,{label:"Cancel",title:"Cancel"},"1"),e.jsx(a,{label:"Save",title:"Save",variant:"primary"},"2")]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
-  }
-}`,...t.parameters?.docs?.source}}};const p=["Default"];export{t as Default,p as __namedExportsOrder,c as default};
