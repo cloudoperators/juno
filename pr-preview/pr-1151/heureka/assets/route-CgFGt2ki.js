5d5281d34094f0494c5b85df05dd93900a840405
TilmanHaupt <TilmanHaupt@users.noreply.github.com>
2025-09-09 10:11:38 +0000

    Remove preview for PR 1151 🛬

--- pr-preview/pr-1151/heureka/assets/route-CgFGt2ki.js
+++
@@ -1,1 +0,0 @@
-import{O as o}from"./App-B6Z405WW.js";import"./index-DLpO8FGH.js";const m=o;export{m as component};
--- pr-preview/pr-1151/storybook/assets/FormRow.stories-v8zNbN2o.js
+++
@@ -1,5 +0,0 @@
-import{j as r}from"./iframe-4Fo5DqIm.js";import{F as o}from"./FormRow.component-CTbDV4Jq.js";import{T as t}from"./TextInput.component-h1KrXEGO.js";import"./Label.component-CDplEs1n.js";import"./Icon.component-Brwxs4YX.js";import"./FormHint.component-Cg1I6QZt.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},e={args:{children:[r.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
-  }
-}`,...e.parameters?.docs?.source}}};const i=["Default"];export{e as Default,i as __namedExportsOrder,c as default};
