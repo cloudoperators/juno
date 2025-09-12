000f4284a9bc659b31fb58c6112fff9358c63ec6
guoda-puidokaite <121792659+guoda-puidokaite@users.noreply.github.com>
2025-09-12 15:24:53 +0000

    Deploying to gh-pages from @ cloudoperators/juno@fe485e41f3f8e408b60d9eb9927cd7cad32c4f05 🚀

---
+++ assets/ComboBoxOption.stories-B-ztE2F7.js
@@ -0,0 +1,14 @@
+import{j as s}from"./iframe-B4f83Mxe.js";import{a,C as n}from"./ComboBoxOption.component-BQ35XNNx.js";import"./floating-ui.react-D1jQ47Vk.js";import"./Label.component-DNYiDg9f.js";import"./FormHint.component-DyYyRAmp.js";import"./Icon.component-BgeY6u9c.js";import"./Spinner.component-YWJI0m26.js";import"./PortalProvider.component-63Amamwq.js";import"./portal-zUUQVJty.js";import"./frozen-CB0INIIt.js";import"./use-tree-walker-CviqVa_U.js";const C={title:"Forms/ComboBox/ComboBoxOption",component:a,argTypes:{},render:t=>s.jsx(n,{children:s.jsx(a,{...t})})},r={args:{value:"Option 1"}},o={args:{disabled:!0,value:"Disabled Option"}},e={args:{children:"Option 1"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: "Option 1"
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    value: "Disabled Option"
+  }
+}`,...o.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: "Option 1"
+  }
+}`,...e.parameters?.docs?.source}}};const D=["Default","Disabled","ChildrenOnly"];export{e as ChildrenOnly,r as Default,o as Disabled,D as __namedExportsOrder,C as default};
---
+++ assets/SelectDivider.stories-DM6tK0Wi.js
@@ -0,0 +1,7 @@
+import{e as c,j as e}from"./iframe-B4f83Mxe.js";import{S as l,a}from"./SelectOption.component-NeVNkOKA.js";import{P as d}from"./PortalProvider.component-63Amamwq.js";import"./portal-zUUQVJty.js";import"./frozen-CB0INIIt.js";import"./use-text-value-CrKmRQgw.js";import"./Label.component-DNYiDg9f.js";import"./Icon.component-BgeY6u9c.js";import"./Spinner.component-YWJI0m26.js";import"./FormHint.component-DyYyRAmp.js";import"./floating-ui.react-D1jQ47Vk.js";const n=`
+  jn:h-px 
+  jn:w-full
+  jn:bg-theme-background-lvl-3
+`,r=c.forwardRef(({className:t="",...o},i)=>e.jsx("div",{className:`juno-select-divider ${n} ${t}`,ref:i,...o}));r.displayName="SelectDivider";try{r.displayName="SelectDivider",r.__docgenInfo={description:"A visual border/divider between SelectOptions or SelectGroups",displayName:"SelectDivider",props:{className:{defaultValue:{value:""},description:"add a custom className",name:"className",required:!1,type:{name:"string"}}}}}catch{}const h={title:"Forms/Select/SelectDivider",component:r,argTypes:{},decorators:[t=>e.jsx(d,{children:e.jsx(t,{})})],render:()=>e.jsxs(l,{open:!0,children:[e.jsx(a,{value:"1",children:"1"}),e.jsx(r,{}),e.jsx(a,{value:"3",children:"3"})]})},s={args:{}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...s.parameters?.docs?.source}}};const D=["Default"];export{s as Default,D as __namedExportsOrder,h as default};
