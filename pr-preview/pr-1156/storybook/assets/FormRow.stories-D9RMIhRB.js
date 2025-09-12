6bff3eecf3c83434d4ec40c812b8f64c2371189c
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-09-12 15:25:25 +0000

    Remove preview for PR 1156 🛬

--- pr-preview/pr-1156/storybook/assets/FormRow.stories-D9RMIhRB.js
+++
@@ -1,5 +0,0 @@
-import{j as r}from"./iframe-B4f83Mxe.js";import{F as o}from"./FormRow.component-Bi79gRHt.js";import{T as t}from"./TextInput.component-BnCNPAA1.js";import"./Label.component-DNYiDg9f.js";import"./Icon.component-BgeY6u9c.js";import"./FormHint.component-DyYyRAmp.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},e={args:{children:[r.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
-  }
-}`,...e.parameters?.docs?.source}}};const i=["Default"];export{e as Default,i as __namedExportsOrder,c as default};
