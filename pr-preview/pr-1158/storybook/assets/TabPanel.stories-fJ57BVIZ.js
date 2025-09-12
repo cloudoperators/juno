ed31a464ea0e0ef3f286324cf821c12e8842225d
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-09-12 15:08:57 +0000

    Remove preview for PR 1158 🛬

--- pr-preview/pr-1158/storybook/assets/TabPanel.stories-fJ57BVIZ.js
+++
@@ -1,5 +0,0 @@
-import{T as a}from"./TabPanel.component-DNOT4LLH.js";import"./iframe-D3C7SVFw.js";import"./TabPanel-CbelcGch.js";const n={title:"Layout/Tabs/TabPanel",component:a,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},e={args:{children:"Tab panel content goes here."}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: "Tab panel content goes here."
-  }
-}`,...e.parameters?.docs?.source}}};const s=["Default"];export{e as Default,s as __namedExportsOrder,n as default};
--- pr-preview/pr-1158/storybook/assets/TooltipContent.stories-BOeskO-z.js
+++
@@ -1,4 +0,0 @@
-import{j as e}from"./iframe-D3C7SVFw.js";import{b as o,T as s,a}from"./TooltipTrigger.component-CsG-N9y6.js";import"./floating-ui.react-CPA6ZQSB.js";import"./Icon.component-DA9Oml3C.js";const m={title:"Components/Tooltip/TooltipContent",component:o,argTypes:{},decorators:[r=>e.jsx("div",{className:"jn:my-6 jn:flex jn:justify-center",children:e.jsxs(s,{initialOpen:!0,children:[e.jsx(a,{children:"clickMe"}),e.jsx(r,{})]})})]},n=r=>e.jsx(o,{...r,children:"This is a tooltip"}),t={args:{},render:n};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {},
-  render: DefaultTemplate
-}`,...t.parameters?.docs?.source}}};const d=["Default"];export{t as Default,d as __namedExportsOrder,m as default};
