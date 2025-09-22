0bf3edfe089030828b7786d531b87ca82d659cdd
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-09-22 06:06:59 +0000

    Deploy preview for PR 1165 🛫

---
+++ pr-preview/pr-1165/storybook/assets/PanelBody.stories-B9Vmrqqr.js
@@ -0,0 +1,21 @@
+import{j as e}from"./iframe-CrIr1Ity.js";import{B as a}from"./Button.component-BczIxEjG.js";import{a as n,P as s}from"./PanelBody.component-BUiHdDGb.js";import{P as c}from"./PanelFooter.component-CXHdnOLp.js";import{P as i}from"./PortalProvider.component-C4I_esLS.js";import"./Icon.component-BkjhvmXK.js";import"./Spinner.component-DL7_T10B.js";const x={title:"Layout/Panel/PanelBody",component:n,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}},footer:{control:!1,table:{type:{summary:"ReactElement"}}}},decorators:[r=>e.jsx(i,{children:e.jsx("div",{className:"jn:contrast-100",children:e.jsx(r,{})})})],parameters:{docs:{source:{transform:r=>r.replace(/jn:/g,"")}}},render:r=>e.jsxs("div",{children:[e.jsx(s,{heading:"My Panel",opened:!0,children:e.jsx(n,{...r,children:"This is the panel body"})}),e.jsx("div",{className:"dummy-css-ignore jn:h-[250px]",children:"Content Area"})]})},d=e.jsx(c,{children:e.jsx(a,{label:"Click me"})}),o={parameters:{docs:{description:{story:"A container for panel content."}}},args:{}},t={parameters:{docs:{description:{story:"A container for panel content with footer."}}},args:{footer:d}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: "A container for panel content."
+      }
+    }
+  },
+  args: {}
+}`,...o.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: "A container for panel content with footer."
+      }
+    }
+  },
+  args: {
+    footer: FooterExample
+  }
+}`,...t.parameters?.docs?.source}}};const j=["Body","BodyWithFooter"];export{o as Body,t as BodyWithFooter,j as __namedExportsOrder,x as default};
