8aed8b0d1874fffa189b3fb21260e0fadcc059ca
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-09-26 10:22:43 +0000

    Deploy preview for PR 1180 🛫

---
+++ pr-preview/pr-1180/storybook/assets/ComboBoxOption.stories-Cx4a7YiJ.js
@@ -0,0 +1,14 @@
+import{j as s}from"./iframe-BQ-cpnA3.js";import{a,C as n}from"./ComboBoxOption.component-DUs2lvgr.js";import"./preload-helper-PPVm8Dsz.js";import"./floating-ui.react-QJfz5bnx.js";import"./Label.component-BVw4khP5.js";import"./FormHint.component-bf3_yV41.js";import"./Icon.component-CPtFKpL7.js";import"./Spinner.component-D_tFgP6h.js";import"./PortalProvider.component-Br1imvgd.js";import"./portal-DUxkOHQG.js";import"./frozen-Cv0Or-Wg.js";import"./use-tree-walker-DyLvwC9N.js";const D={title:"Forms/ComboBox/ComboBoxOption",component:a,argTypes:{},render:t=>s.jsx(n,{children:s.jsx(a,{...t})})},r={args:{value:"Option 1"}},o={args:{disabled:!0,value:"Disabled Option"}},e={args:{children:"Option 1"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: "Option 1"
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    value: "Disabled Option"
+  }
+}`,...o.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: "Option 1"
+  }
+}`,...e.parameters?.docs?.source}}};const f=["Default","Disabled","ChildrenOnly"];export{e as ChildrenOnly,r as Default,o as Disabled,f as __namedExportsOrder,D as default};
---
+++ pr-preview/pr-1180/storybook/assets/ShadowRoot.stories-BEQUMNOC.js
@@ -0,0 +1,5 @@
+import{j as o,S as s}from"./iframe-BQ-cpnA3.js";import"./preload-helper-PPVm8Dsz.js";const c={title:"Layout/ShadowRoot",component:s,argTypes:{children:{control:!1}},render:r=>o.jsx(s,{...r,children:o.jsx("h1",{children:"Welcome"})})},e={args:{mode:"closed"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    mode: "closed"
+  }
+}`,...e.parameters?.docs?.source}}};const n=["EncapsulateStyles"];export{e as EncapsulateStyles,n as __namedExportsOrder,c as default};
