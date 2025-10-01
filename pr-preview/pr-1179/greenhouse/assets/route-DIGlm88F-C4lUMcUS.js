731d099e6c706f019d7bf8ebdc453845e187fff9
ArtieReus <ArtieReus@users.noreply.github.com>
2025-10-01 12:55:37 +0000

    Remove preview for PR 1179 🛬

--- pr-preview/pr-1179/greenhouse/assets/route-DIGlm88F-C4lUMcUS.js
+++
@@ -1,1 +0,0 @@
-import{O as o}from"./App-Pk76pVFJ-baXRgl0m.js";import"./index-BqrWCDpe.js";import"./_extensionId._-NE8Fni2w.js";import"./Shell-DKRuD1KC.js";import"./index-BacmSEz2.js";const n=o;export{n as component};
--- pr-preview/pr-1179/heureka/assets/route-BoG4xGJ2.js
+++
@@ -1,1 +0,0 @@
-import{O as o}from"./App-CtFLoLYk.js";import"./index-kmmukl_M.js";const m=o;export{m as component};
