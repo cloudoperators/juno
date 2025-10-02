2551fcd93a2c4563da2278f5bfe3e1af904f99df
taymoor89 <taymoor89@users.noreply.github.com>
2025-10-02 11:32:25 +0000

    Remove preview for PR 1181 🛬

--- pr-preview/pr-1181/storybook/assets/SelectDivider.stories-7YSnkYAu.js
+++
@@ -1,7 +0,0 @@
-import{e as c,j as e}from"./iframe-DiDJ7HYD.js";import{S as l,a}from"./SelectOption.component-ilPQIFSg.js";import{P as d}from"./PortalProvider.component-mpR_iwt_.js";import"./preload-helper-PPVm8Dsz.js";import"./portal-CPshFOXL.js";import"./frozen-D5yJOJeS.js";import"./use-text-value-ByhEVUO5.js";import"./Label.component-DEIytD50.js";import"./Icon.component-DQiPtm68.js";import"./Spinner.component-g3_ZkYxL.js";import"./FormHint.component-BaTiOowt.js";import"./floating-ui.react-CeA92Vx7.js";const n=`
-  jn:h-px 
-  jn:w-full
-  jn:bg-theme-background-lvl-3
-`,r=c.forwardRef(({className:t="",...o},i)=>e.jsx("div",{className:`juno-select-divider ${n} ${t}`,ref:i,...o}));r.displayName="SelectDivider";try{r.displayName="SelectDivider",r.__docgenInfo={description:"A visual border/divider between SelectOptions or SelectGroups",displayName:"SelectDivider",props:{className:{defaultValue:{value:""},description:"add a custom className",name:"className",required:!1,type:{name:"string"}}}}}catch{}const D={title:"Forms/Select/SelectDivider",component:r,argTypes:{},decorators:[t=>e.jsx(d,{children:e.jsx(t,{})})],render:()=>e.jsxs(l,{open:!0,children:[e.jsx(a,{value:"1",children:"1"}),e.jsx(r,{}),e.jsx(a,{value:"3",children:"3"})]})},s={args:{}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...s.parameters?.docs?.source}}};const N=["Default"];export{s as Default,N as __namedExportsOrder,D as default};
--- pr-preview/pr-1181/storybook/assets/ShadowRoot.stories-bG2TFRAf.js
+++
@@ -1,5 +0,0 @@
-import{j as o,S as s}from"./iframe-DiDJ7HYD.js";import"./preload-helper-PPVm8Dsz.js";const c={title:"Layout/ShadowRoot",component:s,argTypes:{children:{control:!1}},render:r=>o.jsx(s,{...r,children:o.jsx("h1",{children:"Welcome"})})},e={args:{mode:"closed"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    mode: "closed"
-  }
-}`,...e.parameters?.docs?.source}}};const n=["EncapsulateStyles"];export{e as EncapsulateStyles,n as __namedExportsOrder,c as default};
