e90af676b3b14c42991c50fa0b0d2dfc96eafcda
taymoor89 <taymoor89@users.noreply.github.com>
2025-10-09 15:46:44 +0000

    Deploy preview for PR 1197 🛫

---
+++ pr-preview/pr-1197/greenhouse/assets/index-CkmfYmCM.js
@@ -0,0 +1,1 @@
+import{r as c}from"./index-3KZP7c56.js";import{u as n,R as a,a as u}from"./Shell-B7n4WsG1.js";import"./index-D3ilvMTw.js";function v(){const s=n({from:"/"}),{activeApp:t}=a.useRouteContext(),e=u().active(),{__s:p}=a.useSearch();return c.useLayoutEffect(()=>{const o=t||e[0];o&&s({to:`/${o}`,search:r=>({...r,__s:p})})},[t,e]),null}export{v as component};
