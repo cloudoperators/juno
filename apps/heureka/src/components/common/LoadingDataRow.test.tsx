be19f56a03cd880cf838ac7b45ef1b63ba79fb27
Taimoor Aslam <taimoor.aslam@sap.com>
2025-10-09 16:12:50 +0200

    test(heureka): add tests for new components

---
+++ apps/heureka/src/components/common/LoadingDataRow.test.tsx
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Juno contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import { LoadingDataRow } from "./LoadingDataRow"
+
+describe("LoadingDataRow", () => {
+  it("should correctly render", () => {
+    render(<LoadingDataRow colSpan={5} />)
+    const loadingText = screen.getByText("Loading")
+    const spinner = screen.getByRole("progressbar")
+    expect(loadingText).toBeInTheDocument()
+    expect(spinner).toBeInTheDocument()
+  })
+})
