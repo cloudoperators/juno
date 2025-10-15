ee839f8301f798afd3bb2c9f97813ea49f86821e
TilmanHaupt <tilman.haupt@sap.com>
2025-10-15 14:29:49 +0200

    chore(supernova): redo date type

--- apps/supernova/src/components/alerts/shared/AlertSilencesList.tsx
+++ apps/supernova/src/components/alerts/shared/AlertSilencesList.tsx
@@ -27,16 +27,15 @@ const AlertSilencesList = ({ alert }: any) => {
 
   const formatDateTime = (timestamp: VarDate) => {
     const date = new Date(timestamp)
-    return date.toLocaleString('en-GB', {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-      hour: '2-digit',
-      minute: '2-digit',
-      hour12: false
+    return date.toLocaleString("en-GB", {
+      year: "numeric",
+      month: "short",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
     })
   }
-  
 
   return (
     <>
