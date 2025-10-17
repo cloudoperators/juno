04b567652f31c1d5471de51cef39b7626e1fbd7a
Guoda <121792659+guoda-puidokaite@users.noreply.github.com>
2025-10-17 10:48:58 +0200

    chore(carbon): remove wouter (#1211)
    * remove wouter
    
    * remove wouter from lock
    
    * remove routing logiv
    
    * remove routing logiv

--- apps/carbon/src/components/AppContent/AppContent.tsx
+++ apps/carbon/src/components/AppContent/AppContent.tsx
@@ -7,7 +7,6 @@
  */
 
 import React from "react"
-import { Redirect, Route, Switch } from "wouter"
 import Navigation from "../Navigation"
 import Extension from "../Extension/Extension"
 import NoExtensionFound from "./NoExtensionFound"
@@ -17,19 +16,10 @@ const AppContent = ({ extensions }: { extensions: Extension[] }) => (
   <div className="h-screen flex">
     <Navigation extensions={extensions} />
     <div className="flex-1 bg-theme-global-bg" data-testid="extension-container">
-      <Switch>
-        <Route path="/">
-          <Redirect to={extensions[0].url} />
-        </Route>
-        {extensions.map((extension) => (
-          <Route key={extension.name} path={extension.url}>
-            <Extension extension={extension} />
-          </Route>
-        ))}
-        <Route>
-          <NoExtensionFound />
-        </Route>
-      </Switch>
+      {extensions.map((extension) => (
+        <Extension key={extension.name} extension={extension} />
+      ))}
+      <NoExtensionFound />
     </div>
   </div>
 )
