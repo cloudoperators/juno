890690a59cfa78134ad3ce90a17041c2f382a8c5
taymoor89 <taymoor89@users.noreply.github.com>
2025-10-27 09:19:33 +0000

    Remove preview for PR 1250 🛬

--- pr-preview/pr-1250/heureka/assets/route-CcxsOCbn.js
+++
@@ -1,1 +0,0 @@
-import{O as o}from"./App-DBKior3s.js";import"./index-DaSNsbVB.js";const m=o;export{m as component};
--- pr-preview/pr-1250/storybook/assets/ButtonRow.stories-CdBiY69y.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-Dplk4Hgf.js";import{B as a}from"./ButtonRow.component-BVDj-bhW.js";import{B as r}from"./Button.component-eYY6FF0V.js";import"./preload-helper-PPVm8Dsz.js";import"./Stack.component-CsuHYka0.js";import"./Icon.component-CgYNMenf.js";import"./Spinner.component-BxiiG1_7.js";const p={title:"Forms/ButtonRow",component:a,argTypes:{children:{control:!1}}},t={args:{children:[e.jsx(r,{label:"Cancel",title:"Cancel"},"1"),e.jsx(r,{label:"Save",title:"Save",variant:"primary"},"2")]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
-  }
-}`,...t.parameters?.docs?.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,p as default};
