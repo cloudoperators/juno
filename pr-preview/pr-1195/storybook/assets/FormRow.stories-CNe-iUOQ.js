d30c1abb4c6324f91f3b8ebc6d483f7089369f99
taymoor89 <taymoor89@users.noreply.github.com>
2025-10-29 08:48:40 +0000

    Remove preview for PR 1195 🛬

--- pr-preview/pr-1195/storybook/assets/FormRow.stories-CNe-iUOQ.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-atcX-KBD.js";import{F as o}from"./FormRow.component-Cc7RDvb9.js";import{T as t}from"./TextInput.component-bTvB72m3.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-ftzlIinz.js";import"./Icon.component-DxYvZgJ5.js";import"./FormHint.component-Cmh1BJJ5.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
-  }
-}`,...r.parameters?.docs?.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,c as default};
