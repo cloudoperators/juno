08d44282601c30eedd68d08a720b246a28b618a7
taymoor89 <taymooraslam@gmail.com>
2025-10-30 13:05:07 +0000

    Deploying to gh-pages from @ cloudoperators/juno@2ffd9a0a1b138dc911776b112ce2da1f2c029dbb 🚀

--- assets/TabPanel.stories-lDOCg1KW.js
+++
@@ -1,5 +0,0 @@
-import{T as a}from"./TabPanel.component-v1zh_ofK.js";import"./iframe-DoiDq9pO.js";import"./preload-helper-PPVm8Dsz.js";import"./TabPanel-Cf5xiWO8.js";const s={title:"Layout/Tabs/TabPanel",component:a,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},e={args:{children:"Tab panel content goes here."}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: "Tab panel content goes here."
-  }
-}`,...e.parameters?.docs?.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,s as default};
