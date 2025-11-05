4121c41dccab6ea0cd41491bd857737441cf7635
taymoor89 <taymooraslam@gmail.com>
2025-11-05 13:19:53 +0000

    Deploying to gh-pages from @ cloudoperators/juno@416d83f62fe04548d396f2ac44e8ed3a790e7014 🚀

---
+++ assets/FormRow.stories-B7vtBVGt.js
@@ -0,0 +1,5 @@
+import{j as e}from"./iframe-Bt2Z7YT7.js";import{F as o}from"./FormRow.component-3OhmzWTE.js";import{T as t}from"./TextInput.component-CS0MEdxe.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-BZUNBgCi.js";import"./Icon.component-DiHCNYRw.js";import"./FormHint.component-Dg7OXbIS.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
+  }
+}`,...r.parameters?.docs?.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,c as default};
