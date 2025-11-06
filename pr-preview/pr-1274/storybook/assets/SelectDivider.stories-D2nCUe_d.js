c60d86a6bb792d9ac4fd5f9eb0c54ec84cd20be4
taymoor89 <taymoor89@users.noreply.github.com>
2025-11-06 14:32:33 +0000

    Deploy preview for PR 1274 🛫

---
+++ pr-preview/pr-1274/storybook/assets/SelectDivider.stories-D2nCUe_d.js
@@ -0,0 +1,7 @@
+import{e as c,j as e}from"./iframe-CyOU5sEG.js";import{S as l,a}from"./SelectOption.component-D38DZPRK.js";import{P as d}from"./PortalProvider.component-BLaV-ZRl.js";import"./preload-helper-PPVm8Dsz.js";import"./element-movement-9ufEjwnM.js";import"./floating-ui.react-B8hhkJYi.js";import"./frozen-BMih8Bcp.js";import"./use-text-value-CZXiRSP7.js";import"./Label.component-BT3zz1uW.js";import"./Icon.component-CeMA4N15.js";import"./Spinner.component-pee9lqVC.js";import"./FormHint.component-CwOd-nnt.js";const n=`
+  jn:h-px 
+  jn:w-full
+  jn:bg-theme-background-lvl-3
+`,r=c.forwardRef(({className:t="",...o},i)=>e.jsx("div",{className:`juno-select-divider ${n} ${t}`,ref:i,...o}));r.displayName="SelectDivider";try{r.displayName="SelectDivider",r.__docgenInfo={description:"A visual border/divider between SelectOptions or SelectGroups",displayName:"SelectDivider",props:{className:{defaultValue:{value:""},description:"add a custom className",name:"className",required:!1,type:{name:"string"}}}}}catch{}const D={title:"Forms/Select/SelectDivider",component:r,argTypes:{},decorators:[t=>e.jsx(d,{children:e.jsx(t,{})})],render:()=>e.jsxs(l,{open:!0,children:[e.jsx(a,{value:"1",children:"1"}),e.jsx(r,{}),e.jsx(a,{value:"3",children:"3"})]})},s={args:{}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...s.parameters?.docs?.source}}};const N=["Default"];export{s as Default,N as __namedExportsOrder,D as default};
