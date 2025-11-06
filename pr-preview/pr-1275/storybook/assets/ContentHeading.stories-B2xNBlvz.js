4edfa2bc912e40fd07226ed44e62dbb465da6536
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-11-06 19:29:03 +0000

    Deploy preview for PR 1275 🛫

---
+++ pr-preview/pr-1275/storybook/assets/ContentHeading.stories-B2xNBlvz.js
@@ -0,0 +1,5 @@
+import{C as a}from"./ContentHeading.component-VNTKZ5HL.js";import"./iframe-D0Cy4-OT.js";import"./preload-helper-PPVm8Dsz.js";const o={title:"Internal/ContentHeading",component:a,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},e={args:{heading:"My Page Heading"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    heading: "My Page Heading"
+  }
+}`,...e.parameters?.docs?.source}}};const s=["Default"];export{e as Default,s as __namedExportsOrder,o as default};
---
+++ pr-preview/pr-1275/storybook/assets/FormSection.stories-BPLqPSlt.js
@@ -0,0 +1,18 @@
+import{j as e}from"./iframe-D0Cy4-OT.js";import{F as n}from"./FormSection.component-Bt6Zep0-.js";import{T as s}from"./TextInput.component-DgzZnj0l.js";import{F as t}from"./FormRow.component-D3siexjz.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-MjIEzXfz.js";import"./Icon.component-DvMIj3Yg.js";import"./FormHint.component-D0821BcI.js";const u={title:"Forms/FormSection",component:n,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{children:e.jsx(s,{label:"Address Line 1"})},"1"),e.jsx(t,{children:e.jsx(s,{label:"Address Line 2"})},"2")]}},o={args:{title:"Form Section With Title",children:[e.jsx(t,{children:e.jsx(s,{label:"Address Line 1"})},"1"),e.jsx(t,{children:e.jsx(s,{label:"Address Line 2"})},"2")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<FormRow key="1">
+        <TextInput label="Address Line 1" />
+      </FormRow>, <FormRow key="2">
+        <TextInput label="Address Line 2" />
+      </FormRow>]
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: "Form Section With Title",
+    children: [<FormRow key="1">
+        <TextInput label="Address Line 1" />
+      </FormRow>, <FormRow key="2">
+        <TextInput label="Address Line 2" />
+      </FormRow>]
+  }
+}`,...o.parameters?.docs?.source}}};const F=["Default","WithTitle"];export{r as Default,o as WithTitle,F as __namedExportsOrder,u as default};
