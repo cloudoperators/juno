849f7be787090bd7c4dc8fcd134b755efda37118
TilmanHaupt <TilmanHaupt@users.noreply.github.com>
2025-11-07 10:22:11 +0000

    Deploy preview for PR 1301 🛫

--- pr-preview/pr-1301/storybook/assets/ButtonRow.stories-BSxMU5Rz.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-g4gVqToi.js";import{B as a}from"./ButtonRow.component--hxWyw7m.js";import{B as r}from"./Button.component-BSdHdFgm.js";import"./preload-helper-PPVm8Dsz.js";import"./Stack.component-DJ0DryvF.js";import"./Icon.component-B1qrznhP.js";import"./Spinner.component-DmAxTs6g.js";const p={title:"Forms/ButtonRow",component:a,argTypes:{children:{control:!1}}},t={args:{children:[e.jsx(r,{label:"Cancel",title:"Cancel"},"1"),e.jsx(r,{label:"Save",title:"Save",variant:"primary"},"2")]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
-  }
-}`,...t.parameters?.docs?.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,p as default};
--- pr-preview/pr-1301/storybook/assets/FormRow.stories-DghrsDNn.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-g4gVqToi.js";import{F as o}from"./FormRow.component-DVtJnpPH.js";import{T as t}from"./TextInput.component-By6jz9YC.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-Db-Ykpp2.js";import"./Icon.component-B1qrznhP.js";import"./FormHint.component-BCLvFeCS.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
-  }
-}`,...r.parameters?.docs?.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,c as default};
