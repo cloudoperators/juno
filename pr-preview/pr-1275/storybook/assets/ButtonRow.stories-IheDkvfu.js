bfe2b4d4b6c884e37d9070048c05cf0887ee97eb
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-11-13 11:29:45 +0000

    Deploy preview for PR 1275 🛫

--- pr-preview/pr-1275/storybook/assets/ButtonRow.stories-IheDkvfu.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-B3RTbhHZ.js";import{B as a}from"./ButtonRow.component-Cn8O-mpP.js";import{B as r}from"./Button.component-HcrzaB8g.js";import"./preload-helper-PPVm8Dsz.js";import"./Stack.component-C9Pnd9Ym.js";import"./Icon.component-CZQUfeez.js";import"./Spinner.component-Bc8jPabh.js";const p={title:"Forms/ButtonRow",component:a,argTypes:{children:{control:!1}}},t={args:{children:[e.jsx(r,{label:"Cancel",title:"Cancel"},"1"),e.jsx(r,{label:"Save",title:"Save",variant:"primary"},"2")]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
-  }
-}`,...t.parameters?.docs?.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,p as default};
