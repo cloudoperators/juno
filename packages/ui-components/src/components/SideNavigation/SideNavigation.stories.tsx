651192d6530e06899b29f9c805eaf78ae4f45520
I531348 <guoda.puidokaite@sap.com>
2025-11-28 09:25:49 +0100

    add to story

--- packages/ui-components/src/components/SideNavigation/SideNavigation.stories.tsx
+++ packages/ui-components/src/components/SideNavigation/SideNavigation.stories.tsx
@@ -63,7 +63,9 @@ export const NavigationWithGroups: Story = {
         <SideNavigationItem label="Item 1" icon="addCircle" selected={true} href="#" />
         <SideNavigationItem label="Item 2" icon="addCircle">
           <SideNavigationItem label="Sub-Child 1" />
-          <SideNavigationItem label="Sub-Child 2" />
+          <SideNavigationItem label="Sub-Child 2">
+            <SideNavigationItem label="Sub-Child 2" />
+          </SideNavigationItem>
         </SideNavigationItem>
         <SideNavigationItem label="Item 3" href="#" />
         <SideNavigationGroup label="Group Example">
