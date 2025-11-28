d45e863196496a5a88650480574e1bb7f9e352a9
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-11-28 09:45:02 +0000

    Deploy preview for PR 1339 🛫

--- pr-preview/pr-1339/storybook/assets/ButtonRow.stories-uBGLzZbm.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-BBCMFZaF.js";import{B as a}from"./ButtonRow.component-yXW9lcs-.js";import{B as r}from"./Button.component-WBSU9H0p.js";import"./preload-helper-PPVm8Dsz.js";import"./Stack.component-z2BuZich.js";import"./Icon.component-I-jjsJJw.js";import"./Spinner.component-B5XNOTdd.js";const p={title:"Forms/ButtonRow",component:a,argTypes:{children:{control:!1}}},t={args:{children:[e.jsx(r,{label:"Cancel",title:"Cancel"},"1"),e.jsx(r,{label:"Save",title:"Save",variant:"primary"},"2")]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
-  }
-}`,...t.parameters?.docs?.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,p as default};
---
+++ pr-preview/pr-1339/storybook/assets/TabList.stories-2oWmft8c.js
@@ -0,0 +1,10 @@
+import{j as a}from"./iframe-DuhFsJQK.js";import{a as l}from"./TabList.component-Nem_xnD6.js";import{T as e}from"./Tab.component-6KNpX3Vi.js";import"./preload-helper-PPVm8Dsz.js";import"./TabPanel-BEnQQOkt.js";import"./Icon.component-D4yOgYke.js";const n={title:"Layout/Tabs/TabList",component:l,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},t={args:{children:[a.jsx(e,{label:"Tab 1"},"t-1"),a.jsx(e,{label:"Tab 2",selected:!0},"t-2"),a.jsx(e,{label:"Tab 3"},"t-3"),a.jsx(e,{label:"Disabled Tab",disabled:!0},"t-4")]}},s={args:{variant:"main",children:[a.jsx(e,{label:"Tab 1"},"t-1"),a.jsx(e,{label:"Tab 2",selected:!0},"t-2"),a.jsx(e,{label:"Tab 3"},"t-3"),a.jsx(e,{label:"Disabled Tab",disabled:!0},"t-4")]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<Tab key="t-1" label="Tab 1" />, <Tab key="t-2" label="Tab 2" selected />, <Tab key="t-3" label="Tab 3" />, <Tab key="t-4" label="Disabled Tab" disabled />]
+  }
+}`,...t.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: "main",
+    children: [<Tab key="t-1" label="Tab 1" />, <Tab key="t-2" label="Tab 2" selected />, <Tab key="t-3" label="Tab 3" />, <Tab key="t-4" label="Disabled Tab" disabled />]
+  }
+}`,...s.parameters?.docs?.source}}};const c=["Default","MainTabList"];export{t as Default,s as MainTabList,c as __namedExportsOrder,n as default};
