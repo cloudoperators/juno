0e0bbb9ca598f7e96121913bb6046c9987f1f6bb
Hoda Noori <hoda.noori@sap.com>
2025-12-02 17:06:44 +0100

    chore(heureka): removes image version related files

--- apps/heureka/src/mocks/promises.tsx
+++ apps/heureka/src/mocks/promises.tsx
@@ -7,7 +7,6 @@ import { ApolloQueryResult } from "@apollo/client"
 import {
   GetImagesQuery,
   GetServiceFiltersQuery,
-  GetServiceImageVersionsQuery,
   GetServicesQuery,
   GetVulnerabilitiesQuery,
   GetVulnerabilityFiltersQuery,
@@ -110,73 +109,6 @@ export const mockImagesPromise: Promise<ApolloQueryResult<GetImagesQuery>> = Pro
         __typename: "PageInfo",
       },
       __typename: "ImageConnection",
-    },
-  },
-  loading: false,
-  networkStatus: 7,
-  error: undefined,
-  partial: false,
-})
-
-// This mock simulates the response for service image versions query (legacy)
-export const mockImageVersionsPromise: Promise<ApolloQueryResult<GetServiceImageVersionsQuery>> = Promise.resolve({
-  data: {
-    ComponentVersions: {
-      totalCount: 1,
-      edges: [
-        {
-          node: {
-            tag: "v1.0.0",
-            repository: "repo1",
-            version: "1.0.0",
-            issueCounts: {
-              critical: 0,
-              high: 0,
-              medium: 0,
-              low: 0,
-              none: 1,
-              total: 1,
-              __typename: "SeverityCounts",
-            },
-            component: {
-              ccrn: "alpha",
-              __typename: "Component",
-            },
-            componentInstances: {
-              totalCount: 1,
-              edges: [
-                {
-                  node: {
-                    id: "ci-1",
-                    ccrn: "alpha",
-                    region: "eu",
-                    cluster: "cluster-1",
-                    namespace: "ns1",
-                    pod: "pod-1",
-                    container: "container-1",
-                    __typename: "ComponentInstance",
-                  },
-                  __typename: "ComponentInstanceEdge",
-                },
-              ],
-              pageInfo: {
-                pageNumber: 1,
-                pages: [],
-                __typename: "PageInfo",
-              },
-              __typename: "ComponentInstanceConnection",
-            },
-            __typename: "ComponentVersion",
-          },
-          __typename: "ComponentVersionEdge",
-        },
-      ],
-      pageInfo: {
-        pageNumber: 1,
-        pages: [],
-        __typename: "PageInfo",
-      },
-      __typename: "ComponentVersionConnection",
     },
   },
   loading: false,
