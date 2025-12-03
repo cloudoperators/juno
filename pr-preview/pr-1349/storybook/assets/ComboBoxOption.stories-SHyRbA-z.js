427e3532d6e0bf7e6fde82d304e91c4a2fd74fe6
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-12-03 09:01:56 +0000

    Deploy preview for PR 1349 🛫

---
+++ pr-preview/pr-1349/storybook/assets/ComboBoxOption.stories-SHyRbA-z.js
@@ -0,0 +1,14 @@
+import{j as s}from"./iframe-CQVeaZdR.js";import{a,C as n}from"./ComboBoxOption.component-BGXdS_f3.js";import"./preload-helper-PPVm8Dsz.js";import"./floating-ui.react-Djnpjrk-.js";import"./Label.component-Db4_-IJX.js";import"./FormHint.component-CbtLUehm.js";import"./Icon.component-Ck8zX28e.js";import"./Spinner.component-BGCxK8IX.js";import"./PortalProvider.component-BrRdjFNc.js";import"./element-movement-BJ0OTSpY.js";import"./frozen-SG0CLaWY.js";import"./use-tree-walker-DM_SCAsf.js";const D={title:"Forms/ComboBox/ComboBoxOption",component:a,argTypes:{},render:t=>s.jsx(n,{children:s.jsx(a,{...t})})},r={args:{value:"Option 1"}},o={args:{disabled:!0,value:"Disabled Option"}},e={args:{children:"Option 1"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: "Option 1"
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    value: "Disabled Option"
+  }
+}`,...o.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: "Option 1"
+  }
+}`,...e.parameters?.docs?.source}}};const f=["Default","Disabled","ChildrenOnly"];export{e as ChildrenOnly,r as Default,o as Disabled,f as __namedExportsOrder,D as default};
--- pr-preview/pr-1349/storybook/assets/FormRow.stories-BxMdMUFc.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-CpLPiwR6.js";import{F as o}from"./FormRow.component-BM-yTUbY.js";import{T as t}from"./TextInput.component-Bb9kkH8A.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-BU8KX09U.js";import"./Icon.component-D5LjExGT.js";import"./FormHint.component-y1XXzZHi.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
-  }
-}`,...r.parameters?.docs?.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,c as default};
