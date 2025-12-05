b620672b63e130007b224a686667f9a3f4b5bd0e
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-12-05 09:42:09 +0000

    Deploy preview for PR 1329 🛫

--- pr-preview/pr-1329/storybook/assets/ButtonRow.stories-DL-HxYy8.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-D-5bv9fe.js";import{B as a}from"./ButtonRow.component-CbW72f0V.js";import{B as r}from"./Button.component-0joBOMWB.js";import"./preload-helper-PPVm8Dsz.js";import"./Stack.component-DdGPgQI8.js";import"./Icon.component-Cl58n4vU.js";import"./Spinner.component-DRMYaAsY.js";const p={title:"Forms/ButtonRow",component:a,argTypes:{children:{control:!1}}},t={args:{children:[e.jsx(r,{label:"Cancel",title:"Cancel"},"1"),e.jsx(r,{label:"Save",title:"Save",variant:"primary"},"2")]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
-  }
-}`,...t.parameters?.docs?.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,p as default};
---
+++ pr-preview/pr-1329/storybook/assets/FormSection.stories-BA3Eh5t7.js
@@ -0,0 +1,18 @@
+import{j as e}from"./iframe-BdHkkWkD.js";import{F as n}from"./FormSection.component-lDVxgPov.js";import{T as s}from"./TextInput.component-C4Kwd91Z.js";import{F as t}from"./FormRow.component-CqBmX6BV.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-ChW76x_3.js";import"./Icon.component-55PG-Ml5.js";import"./FormHint.component-By1pOlYb.js";const u={title:"Forms/FormSection",component:n,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{children:e.jsx(s,{label:"Address Line 1"})},"1"),e.jsx(t,{children:e.jsx(s,{label:"Address Line 2"})},"2")]}},o={args:{title:"Form Section With Title",children:[e.jsx(t,{children:e.jsx(s,{label:"Address Line 1"})},"1"),e.jsx(t,{children:e.jsx(s,{label:"Address Line 2"})},"2")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<FormRow key="1">
+        <TextInput label="Address Line 1" />
+      </FormRow>, <FormRow key="2">
+        <TextInput label="Address Line 2" />
+      </FormRow>]
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: "Form Section With Title",
+    children: [<FormRow key="1">
+        <TextInput label="Address Line 1" />
+      </FormRow>, <FormRow key="2">
+        <TextInput label="Address Line 2" />
+      </FormRow>]
+  }
+}`,...o.parameters?.docs?.source}}};const F=["Default","WithTitle"];export{r as Default,o as WithTitle,F as __namedExportsOrder,u as default};
