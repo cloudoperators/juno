32dfe5b35e7391502dcdce6e01e17ed7f3a60b41
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-12-08 10:13:32 +0000

    Deploy preview for PR 1363 🛫

---
+++ pr-preview/pr-1363/storybook/assets/ComboBoxOption.stories-BynOh0Fw.js
@@ -0,0 +1,14 @@
+import{j as s}from"./iframe-wvtdDadm.js";import{a,C as n}from"./ComboBoxOption.component-B__SLFbd.js";import"./preload-helper-PPVm8Dsz.js";import"./floating-ui.react-D0sN8Pbl.js";import"./Label.component-DCkRQyDj.js";import"./FormHint.component-D77Kg5H4.js";import"./Icon.component-qY6SGgLL.js";import"./Spinner.component-DP5ljbAY.js";import"./PortalProvider.component-_fpchibN.js";import"./element-movement-DA7nkoK3.js";import"./frozen-DzbedhnK.js";import"./use-tree-walker-Bvs9Vzt9.js";const D={title:"Forms/ComboBox/ComboBoxOption",component:a,argTypes:{},render:t=>s.jsx(n,{children:s.jsx(a,{...t})})},r={args:{value:"Option 1"}},o={args:{disabled:!0,value:"Disabled Option"}},e={args:{children:"Option 1"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: "Option 1"
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    value: "Disabled Option"
+  }
+}`,...o.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: "Option 1"
+  }
+}`,...e.parameters?.docs?.source}}};const f=["Default","Disabled","ChildrenOnly"];export{e as ChildrenOnly,r as Default,o as Disabled,f as __namedExportsOrder,D as default};
---
+++ pr-preview/pr-1363/storybook/assets/SelectOption.stories-e3qSSE_g.js
@@ -0,0 +1,20 @@
+import{j as r}from"./iframe-wvtdDadm.js";import{a as n,S as i}from"./SelectOption.component-Beq7eKVD.js";import{P as p}from"./PortalProvider.component-_fpchibN.js";import"./preload-helper-PPVm8Dsz.js";import"./element-movement-DA7nkoK3.js";import"./floating-ui.react-D0sN8Pbl.js";import"./frozen-DzbedhnK.js";import"./use-text-value-Rz_SVrgr.js";import"./Label.component-DCkRQyDj.js";import"./Icon.component-qY6SGgLL.js";import"./Spinner.component-DP5ljbAY.js";import"./FormHint.component-D77Kg5H4.js";const f={title:"Forms/Select/SelectOption",component:n,argTypes:{children:{control:{type:"text"},table:{type:{summary:"string"}}}},decorators:[s=>r.jsx(p,{children:r.jsx(s,{})})],render:s=>r.jsx(i,{open:!0,children:r.jsx(n,{...s})})},e={args:{value:"Option 1"}},o={args:{disabled:!0,value:"Disabled Option"}},t={args:{children:["Option 1"]}},a={args:{value:"option-1",label:"Option 1"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: "Option 1"
+  }
+}`,...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    value: "Disabled Option"
+  }
+}`,...o.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    // @ts-ignore
+    children: ["Option 1"]
+  }
+}`,...t.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: "option-1",
+    label: "Option 1"
+  }
+}`,...a.parameters?.docs?.source}}};const j=["Default","Disabled","ChildrenOnly","WithLabel"];export{t as ChildrenOnly,e as Default,o as Disabled,a as WithLabel,j as __namedExportsOrder,f as default};
