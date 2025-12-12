b6263892814ba5978ba5e556407750428013aa26
MartinS-git <MartinS-git@users.noreply.github.com>
2025-12-12 13:24:41 +0000

    Deploy preview for PR 1381 🛫

---
+++ pr-preview/pr-1381/storybook/assets/ButtonRow.stories-CJo-tAqa.js
@@ -0,0 +1,5 @@
+import{j as e}from"./iframe-Pv0NskM9.js";import{B as a}from"./ButtonRow.component-Ch7W7oDc.js";import{B as r}from"./Button.component-CzwTTUPR.js";import"./preload-helper-PPVm8Dsz.js";import"./Stack.component-_bKkwY-5.js";import"./Icon.component-DL2jn4IZ.js";import"./Spinner.component-XsWUIv_-.js";const p={title:"Forms/ButtonRow",component:a,argTypes:{children:{control:!1}}},t={args:{children:[e.jsx(r,{label:"Cancel",title:"Cancel"},"1"),e.jsx(r,{label:"Save",title:"Save",variant:"primary"},"2")]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
+  }
+}`,...t.parameters?.docs?.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,p as default};
---
+++ pr-preview/pr-1381/storybook/assets/SearchInput.stories-skDTtgB0.js
@@ -0,0 +1,15 @@
+import{S as o}from"./SearchInput.component-B1zjg9z7.js";import"./iframe-Pv0NskM9.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon.component-DL2jn4IZ.js";import"./Stack.component-_bKkwY-5.js";const m={title:"Components/SearchInput",component:o,argTypes:{}},r={args:{}},e={args:{variant:"rounded"}},a={args:{variant:"hero"}},s={args:{disabled:!0}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: "rounded"
+  }
+}`,...e.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: "hero"
+  }
+}`,...a.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...s.parameters?.docs?.source}}};const u=["Default","Rounded","Hero","Disabled"];export{r as Default,s as Disabled,a as Hero,e as Rounded,u as __namedExportsOrder,m as default};
