26d2c245e6731afb783a7d9a873b9e96887e46b3
MartinS-git <MartinS-git@users.noreply.github.com>
2025-12-16 14:02:17 +0000

    Remove preview for PR 1381 🛬

--- pr-preview/pr-1381/storybook/assets/FormRow.stories-D0hatyqT.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-qzw7_43R.js";import{F as o}from"./FormRow.component-uqRSU3J3.js";import{T as t}from"./TextInput.component-B4NT3hec.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-66BR2JvB.js";import"./Icon.component-2-bHvDa5.js";import"./FormHint.component-B51T7Mbo.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
-  }
-}`,...r.parameters?.docs?.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,c as default};
