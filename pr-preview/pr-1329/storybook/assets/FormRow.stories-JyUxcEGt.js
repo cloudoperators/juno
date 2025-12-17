172825b77542d39523a025abb8181d8460c827ed
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2025-12-17 13:11:10 +0000

    Deploy preview for PR 1329 🛫

---
+++ pr-preview/pr-1329/storybook/assets/FormRow.stories-JyUxcEGt.js
@@ -0,0 +1,5 @@
+import{j as e}from"./iframe-DNtYy8lo.js";import{F as o}from"./FormRow.component-B1R9MLXT.js";import{T as t}from"./TextInput.component-qSh8_kPW.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-BaU9-cDK.js";import"./Icon.component-C0h9s-kc.js";import"./FormHint.component-nZzV5tQB.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
+  }
+}`,...r.parameters?.docs?.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,c as default};
