5854f40c2ad24f22a746ba7982117cac91094d08
taymoor89 <taymoor89@users.noreply.github.com>
2026-01-06 09:19:15 +0000

    Remove preview for PR 1387 🛬

--- pr-preview/pr-1387/storybook/assets/FormRow.stories-DrGmkliU.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-DR5aBBEO.js";import{F as o}from"./FormRow.component-Dn6Tmz7F.js";import{T as t}from"./TextInput.component-B2c2PFLZ.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-DYJhdqBK.js";import"./Icon.component-BFxpQTbX.js";import"./FormHint.component-_bgTHoNZ.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
-  }
-}`,...r.parameters?.docs?.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,c as default};
