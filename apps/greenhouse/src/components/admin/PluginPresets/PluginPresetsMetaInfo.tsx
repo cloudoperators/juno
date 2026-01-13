6d921d58838c144783f8f5a56171108d0ea28123
Taimoor Aslam <taimoor.aslam@sap.com>
2026-01-13 17:10:52 +0100

    chore(greenhouse): add missing license header

--- apps/greenhouse/src/components/admin/PluginPresets/PluginPresetsMetaInfo.tsx
+++ apps/greenhouse/src/components/admin/PluginPresets/PluginPresetsMetaInfo.tsx
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Juno contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import React from "react"
 import { useLoaderData, useRouter } from "@tanstack/react-router"
 import { Stack, Button } from "@cloudoperators/juno-ui-components/index"
--- apps/greenhouse/src/components/admin/constants.ts
+++ apps/greenhouse/src/components/admin/constants.ts
@@ -1,1 +1,6 @@
+/*
+ * SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Juno contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 export const SELECTED_FILTER_PREFIX = "f_"
