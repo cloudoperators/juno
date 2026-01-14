a2927bdf2fbd1a9147e1e48a8ae0df0c4acfbccc
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2026-01-14 19:18:55 +0000

    Deploy preview for PR 1411 🛫

---
+++ pr-preview/pr-1411/storybook/assets/ButtonRow.stories-B1Lj57rC.js
@@ -0,0 +1,5 @@
+import{j as e}from"./iframe-RL4qp5RK.js";import{B as a}from"./ButtonRow.component-YkWq4SJl.js";import{B as r}from"./Button.component-BnPWFGV0.js";import"./preload-helper-PPVm8Dsz.js";import"./Stack.component-CC6-3mOo.js";import"./Icon.component-u1z391l1.js";import"./Spinner.component-SDMeR5s-.js";const p={title:"Forms/ButtonRow",component:a,argTypes:{children:{control:!1}}},t={args:{children:[e.jsx(r,{label:"Cancel",title:"Cancel"},"1"),e.jsx(r,{label:"Save",title:"Save",variant:"primary"},"2")]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
+  }
+}`,...t.parameters?.docs?.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,p as default};
---
+++ pr-preview/pr-1411/storybook/assets/ComboBoxOption.stories-DGOkgRtV.js
@@ -0,0 +1,14 @@
+import{j as s}from"./iframe-RL4qp5RK.js";import{a,C as n}from"./ComboBoxOption.component-BBHkEcHZ.js";import"./preload-helper-PPVm8Dsz.js";import"./floating-ui.react-5Uc2NgrU.js";import"./Label.component-DV5ISie8.js";import"./FormHint.component-Bb4v5CQn.js";import"./Icon.component-u1z391l1.js";import"./Spinner.component-SDMeR5s-.js";import"./PortalProvider.component-yiqXKVlP.js";import"./element-movement-DC5wZUfI.js";import"./frozen-DjKez5dy.js";import"./use-tree-walker-7PPe3o7F.js";const D={title:"Forms/ComboBox/ComboBoxOption",component:a,argTypes:{},render:t=>s.jsx(n,{children:s.jsx(a,{...t})})},r={args:{value:"Option 1"}},o={args:{disabled:!0,value:"Disabled Option"}},e={args:{children:"Option 1"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: "Option 1"
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    value: "Disabled Option"
+  }
+}`,...o.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: "Option 1"
+  }
+}`,...e.parameters?.docs?.source}}};const f=["Default","Disabled","ChildrenOnly"];export{e as ChildrenOnly,r as Default,o as Disabled,f as __namedExportsOrder,D as default};
