38dfdc74f564bc90eb42542a68d9fb5556d0868a
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2026-01-16 11:56:55 +0000

    Remove preview for PR 1411 🛬

--- pr-preview/pr-1411/storybook/assets/ButtonRow.stories-Be5jYFPY.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-DeA9zV8u.js";import{B as a}from"./ButtonRow.component-BDQg7W_H.js";import{B as r}from"./Button.component-CHRKYJ2p.js";import"./preload-helper-PPVm8Dsz.js";import"./Stack.component-wZVKZES9.js";import"./Icon.component-DLA0dFlg.js";import"./Spinner.component-ByFKqOoQ.js";const p={title:"Forms/ButtonRow",component:a,argTypes:{children:{control:!1}}},t={args:{children:[e.jsx(r,{label:"Cancel",title:"Cancel"},"1"),e.jsx(r,{label:"Save",title:"Save",variant:"primary"},"2")]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
-  }
-}`,...t.parameters?.docs?.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,p as default};
--- pr-preview/pr-1411/storybook/assets/FormRow.stories-DOcWNXaY.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-DeA9zV8u.js";import{F as o}from"./FormRow.component-BRQBPPml.js";import{T as t}from"./TextInput.component-D5apCrEY.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-QS8GQPSI.js";import"./Icon.component-DLA0dFlg.js";import"./FormHint.component-Csazt7Mm.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
-  }
-}`,...r.parameters?.docs?.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,c as default};
