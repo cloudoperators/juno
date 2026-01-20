d8af674e16156b6fc5a0fe8ab0b7077dd833093f
taymoor89 <taymoor89@users.noreply.github.com>
2026-01-20 09:55:51 +0000

    Remove preview for PR 1413 🛬

--- pr-preview/pr-1413/greenhouse/assets/clusters-DRyMgqL4.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./Shell-DPBugr0q.js";import"./index-cCT__0Ca.js";import"./index-CoaPAS7r.js";function e(){return o.jsx("div",{children:'Hello "/admin/clusters"!'})}export{e as component};
--- pr-preview/pr-1413/greenhouse/assets/teams-DQRbmBiZ.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./Shell-DPBugr0q.js";import"./index-cCT__0Ca.js";import"./index-CoaPAS7r.js";function m(){return o.jsx("div",{children:'Hello "/admin/teams"!'})}export{m as component};
