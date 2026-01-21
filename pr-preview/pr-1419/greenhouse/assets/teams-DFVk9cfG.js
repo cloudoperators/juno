e0896b2e159f15306fcc12c871f88c391157418d
taymoor89 <taymoor89@users.noreply.github.com>
2026-01-21 14:37:04 +0000

    Deploy preview for PR 1419 🛫

---
+++ pr-preview/pr-1419/greenhouse/assets/teams-DFVk9cfG.js
@@ -0,0 +1,1 @@
+import{j as o}from"./Shell-DR-G2ppQ.js";import"./index-YKfwnDI1.js";import"./index-D9iZIrW1.js";function m(){return o.jsx("div",{children:'Hello "/admin/teams"!'})}export{m as component};
