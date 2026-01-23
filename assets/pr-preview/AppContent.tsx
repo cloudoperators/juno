9815ae85490c859e35dfb33609659d283387a59b
Arturo Reuschenbach Puncernau <reuschenbach@gmail.com>
2026-01-23 15:14:23 +0100

    chore(greenhouse): adds AppContent component for pr-preview

---
+++ assets/pr-preview/AppContent.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+const AppContent: React.FC = () => {
+  return (
+    <div>
+      Following previews are available:
+      <br />
+      <br />
+      <ul>
+        <li>
+          <a href="/greenhouse/index.html#/demo">Greenhouse</a>
+        </li>
+        <li>
+          <a href="/supernova/index.html">Supernova</a>
+        </li>
+        <li>
+          <a href="/doop/index.html">Doop</a>
+        </li>
+        <li>
+          <a href="/heureka/index.html">Heureka</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default AppContent
