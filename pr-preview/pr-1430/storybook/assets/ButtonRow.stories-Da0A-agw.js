fe81d4799015d12e178e3d346b1bbd8236a50d7d
ArtieReus <ArtieReus@users.noreply.github.com>
2026-01-27 15:07:49 +0000

    Remove preview for PR 1430 🛬

--- pr-preview/pr-1430/storybook/assets/ButtonRow.stories-Da0A-agw.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-BdXPUYtd.js";import{B as a}from"./ButtonRow.component-puotWH-g.js";import{B as r}from"./Button.component-BfEQLRym.js";import"./preload-helper-PPVm8Dsz.js";import"./Stack.component-CnS1SMfc.js";import"./Icon.component-D_QlQ5rT.js";import"./Spinner.component-ByYWunee.js";const p={title:"Forms/ButtonRow",component:a,argTypes:{children:{control:!1}}},t={args:{children:[e.jsx(r,{label:"Cancel",title:"Cancel"},"1"),e.jsx(r,{label:"Save",title:"Save",variant:"primary"},"2")]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
-  }
-}`,...t.parameters?.docs?.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,p as default};
