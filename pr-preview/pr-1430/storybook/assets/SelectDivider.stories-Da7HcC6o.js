2fb8794184be328c5378d91ac29a77916d92a401
ArtieReus <ArtieReus@users.noreply.github.com>
2026-01-27 14:38:29 +0000

    Deploy preview for PR 1430 🛫

--- pr-preview/pr-1430/storybook/assets/SelectDivider.stories-Da7HcC6o.js
+++
@@ -1,7 +0,0 @@
-import{e as c,j as e}from"./iframe-B5sM-Prq.js";import{S as l,a}from"./SelectOption.component-Deyg-Ruh.js";import{P as d}from"./PortalProvider.component-zRKRAJWB.js";import"./preload-helper-PPVm8Dsz.js";import"./element-movement-CvPbzJoX.js";import"./floating-ui.react-CJowmlmK.js";import"./frozen-BNad_J6X.js";import"./use-text-value-SpcPvf-5.js";import"./Label.component-BAXEpNio.js";import"./Icon.component-ChYY3iQH.js";import"./Spinner.component-CgnJGw_F.js";import"./FormHint.component-D9uz6LMn.js";const n=`
-  jn:h-px 
-  jn:w-full
-  jn:bg-theme-background-lvl-3
-`,r=c.forwardRef(({className:t="",...o},i)=>e.jsx("div",{className:`juno-select-divider ${n} ${t}`,ref:i,...o}));r.displayName="SelectDivider";try{r.displayName="SelectDivider",r.__docgenInfo={description:"A visual border/divider between SelectOptions or SelectGroups",displayName:"SelectDivider",props:{className:{defaultValue:{value:""},description:"add a custom className",name:"className",required:!1,type:{name:"string"}}}}}catch{}const D={title:"Forms/Select/SelectDivider",component:r,argTypes:{},decorators:[t=>e.jsx(d,{children:e.jsx(t,{})})],render:()=>e.jsxs(l,{open:!0,children:[e.jsx(a,{value:"1",children:"1"}),e.jsx(r,{}),e.jsx(a,{value:"3",children:"3"})]})},s={args:{}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...s.parameters?.docs?.source}}};const N=["Default"];export{s as Default,N as __namedExportsOrder,D as default};
---
+++ pr-preview/pr-1430/storybook/assets/TabPanel.stories-DdJxS1jY.js
@@ -0,0 +1,5 @@
+import{T as a}from"./TabPanel.component-BCZCZl5o.js";import"./iframe-BdXPUYtd.js";import"./preload-helper-PPVm8Dsz.js";import"./TabPanel-yk41nDaj.js";const s={title:"Layout/Tabs/TabPanel",component:a,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},e={args:{children:"Tab panel content goes here."}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: "Tab panel content goes here."
+  }
+}`,...e.parameters?.docs?.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,s as default};
