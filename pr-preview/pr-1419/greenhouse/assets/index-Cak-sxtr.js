17354eef952493eb1e3624f17f68bd4630f0f2e8
ArtieReus <ArtieReus@users.noreply.github.com>
2026-01-27 08:27:58 +0000

    Remove preview for PR 1419 🛬

--- pr-preview/pr-1419/greenhouse/assets/index-Cak-sxtr.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index-CxGYU1Ea.js";import{u as n,R as a,c as u}from"./Shell-BqWBQhBO.js";import"./index-DH4_aeCm.js";function v(){const s=n({from:"/"}),{activeApp:t}=a.useRouteContext(),e=u().active(),{__s:c}=a.useSearch();return r.useLayoutEffect(()=>{const o=t||e[0];o&&s({to:`/${o}`,search:p=>({...p,__s:c})})},[t,e]),null}export{v as component};
--- pr-preview/pr-1419/greenhouse/assets/teams-2d2vJh-w.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./Shell-BqWBQhBO.js";import"./index-CxGYU1Ea.js";import"./index-DH4_aeCm.js";function m(){return o.jsx("div",{children:'Hello "/admin/teams"!'})}export{m as component};
