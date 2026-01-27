a4e3fe89e7231bb45f11fbf87168e61ff91192dd
Arturo Reuschenbach Puncernau <reuschenbach@gmail.com>
2026-01-27 11:39:00 +0100

    chore(greenhouse): adds comment to the appContent

--- apps/greenhouse/README.md
+++ apps/greenhouse/README.md
@@ -1,4 +1,4 @@
-# Greenhouse App
+# Greenhouse
 
 [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](LICENSE)
 [![Built with Juno](https://cloudoperators.github.io/juno/built-with-juno.svg)](https://github.com/cloudoperators/juno)
--- assets/pr-preview/AppContent.tsx
+++ assets/pr-preview/AppContent.tsx
@@ -1,3 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: 2026 SAP SE or an SAP affiliate company and Juno contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*
+ * Used by the Juno PR Preview app to render the list of available previews.
+ *
+ * During the preview build, this file is copied into the template app
+ * (apps/template/src/components/AppContent.tsx), after which the template
+ * is built as a static app.
+ */
+
 import React from "react"
 
 const AppContent: React.FC = () => {
