c2e3952e4b1c8eab687dde2908721a847754083d
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2026-01-30 10:14:23 +0000

    Remove preview for PR 1426 🛬

--- pr-preview/pr-1426/storybook/assets/TabPanel.stories-BzAIyfmP.js
+++
@@ -1,5 +0,0 @@
-import{T as a}from"./TabPanel.component-L9z39Vf5.js";import"./iframe-CypRd6Jd.js";import"./preload-helper-PPVm8Dsz.js";import"./TabPanel-0MMUwfo1.js";const s={title:"Layout/Tabs/TabPanel",component:a,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},e={args:{children:"Tab panel content goes here."}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: "Tab panel content goes here."
-  }
-}`,...e.parameters?.docs?.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,s as default};
