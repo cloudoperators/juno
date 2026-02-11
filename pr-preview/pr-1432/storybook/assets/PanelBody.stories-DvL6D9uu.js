919847119909c40f0c146e91f336a1030110de63
ArtieReus <ArtieReus@users.noreply.github.com>
2026-02-11 12:07:17 +0000

    Deploy preview for PR 1432 🛫

---
+++ pr-preview/pr-1432/storybook/assets/PanelBody.stories-DvL6D9uu.js
@@ -0,0 +1,21 @@
+import{j as e}from"./iframe-Cf7Ra4UQ.js";import{B as a}from"./Button.component-Ce30Hq5i.js";import{a as n,P as s}from"./PanelBody.component-49BWgIiC.js";import{P as c}from"./PanelFooter.component-DO7L2wmI.js";import{P as i}from"./PortalProvider.component-DGH1dKH6.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon.component-Co1aF76C.js";import"./Spinner.component-BH_aHfQ6.js";const j={title:"Layout/Panel/PanelBody",component:n,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}},footer:{control:!1,table:{type:{summary:"ReactElement"}}}},decorators:[r=>e.jsx(i,{children:e.jsx("div",{className:"jn:contrast-100",children:e.jsx(r,{})})})],parameters:{docs:{source:{transform:r=>r.replace(/jn:/g,"")}}},render:r=>e.jsxs("div",{children:[e.jsx(s,{heading:"My Panel",opened:!0,children:e.jsx(n,{...r,children:"This is the panel body"})}),e.jsx("div",{className:"dummy-css-ignore jn:h-[250px]",children:"Content Area"})]})},d=e.jsx(c,{children:e.jsx(a,{label:"Click me"})}),o={parameters:{docs:{description:{story:"A container for panel content."}}},args:{}},t={parameters:{docs:{description:{story:"A container for panel content with footer."}}},args:{footer:d}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: "A container for panel content."
+      }
+    }
+  },
+  args: {}
+}`,...o.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: "A container for panel content with footer."
+      }
+    }
+  },
+  args: {
+    footer: FooterExample
+  }
+}`,...t.parameters?.docs?.source}}};const P=["Body","BodyWithFooter"];export{o as Body,t as BodyWithFooter,P as __namedExportsOrder,j as default};
