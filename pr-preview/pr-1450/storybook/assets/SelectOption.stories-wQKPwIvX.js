6b7524191ea0864168453035060bd31995d54775
vlad-schur-external-sap <vlad-schur-external-sap@users.noreply.github.com>
2026-02-12 10:31:08 +0000

    Deploy preview for PR 1450 🛫

---
+++ pr-preview/pr-1450/storybook/assets/SelectOption.stories-wQKPwIvX.js
@@ -0,0 +1,20 @@
+import{j as r}from"./iframe-DFNSQ5tH.js";import{a as n,S as i}from"./SelectOption.component-bozE5bji.js";import{P as p}from"./PortalProvider.component-COlvFgKL.js";import"./preload-helper-PPVm8Dsz.js";import"./element-movement-BYFZudEW.js";import"./floating-ui.react-CBBv2Cbb.js";import"./frozen-uED_qrEW.js";import"./use-text-value-DhkgBfo1.js";import"./Label.component-BPXcNqci.js";import"./Icon.component-B6Foepcp.js";import"./Spinner.component-fSxKraxX.js";import"./FormHint.component-CLN_2ipP.js";const f={title:"Forms/Select/SelectOption",component:n,argTypes:{children:{control:{type:"text"},table:{type:{summary:"string"}}}},decorators:[s=>r.jsx(p,{children:r.jsx(s,{})})],render:s=>r.jsx(i,{open:!0,children:r.jsx(n,{...s})})},e={args:{value:"Option 1"}},o={args:{disabled:!0,value:"Disabled Option"}},t={args:{children:["Option 1"]}},a={args:{value:"option-1",label:"Option 1"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: "Option 1"
+  }
+}`,...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    value: "Disabled Option"
+  }
+}`,...o.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    // @ts-ignore
+    children: ["Option 1"]
+  }
+}`,...t.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: "option-1",
+    label: "Option 1"
+  }
+}`,...a.parameters?.docs?.source}}};const j=["Default","Disabled","ChildrenOnly","WithLabel"];export{t as ChildrenOnly,e as Default,o as Disabled,a as WithLabel,j as __namedExportsOrder,f as default};
