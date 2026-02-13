ab6f834097cac88c86f91da29bafe35c51e21d03
vlad-schur-external-sap <vlad-schur-external-sap@users.noreply.github.com>
2026-02-13 14:22:24 +0000

    Remove preview for PR 1450 🛬

--- pr-preview/pr-1450/storybook/assets/ButtonRow.stories-QIRrc6Gd.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-Cfp4nl02.js";import{B as a}from"./ButtonRow.component-wn0wRTRC.js";import{B as r}from"./Button.component-bQVtXmgy.js";import"./preload-helper-PPVm8Dsz.js";import"./Stack.component-BFpf_viQ.js";import"./Icon.component-DLq0r8Sb.js";import"./Spinner.component-CH6bMBSm.js";const p={title:"Forms/ButtonRow",component:a,argTypes:{children:{control:!1}}},t={args:{children:[e.jsx(r,{label:"Cancel",title:"Cancel"},"1"),e.jsx(r,{label:"Save",title:"Save",variant:"primary"},"2")]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
-  }
-}`,...t.parameters?.docs?.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,p as default};
