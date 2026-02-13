8a001102755cfe71f33f7331db0824723ff3b5e8
Arturo Reuschenbach Puncernau <reuschenbach@gmail.com>
2026-02-13 17:26:53 +0100

    chore(greenhouse): remove unused imports

--- apps/greenhouse/src/components/admin/common/YamlViewer.test.tsx
+++ apps/greenhouse/src/components/admin/common/YamlViewer.test.tsx
@@ -4,7 +4,7 @@
  */
 
 import React from "react"
-import { render, screen, waitFor } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { describe, it, expect } from "vitest"
 import YamlViewer from "./YamlViewer"
 
