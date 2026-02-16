7713101948ce9aa62f6324d51ee825c26617ff94
KirylSAP <KirylSAP@users.noreply.github.com>
2026-02-16 09:23:42 +0000

    Remove preview for PR 1445 🛬

--- pr-preview/pr-1445/storybook/assets/ButtonRow.stories-BUexqMx-.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-CFex6Aaq.js";import{B as a}from"./ButtonRow.component-BjFnxzyq.js";import{B as r}from"./Button.component-Dpo75Q_G.js";import"./preload-helper-PPVm8Dsz.js";import"./Stack.component-CH0qH-m6.js";import"./Icon.component-BxXj_gNE.js";import"./Spinner.component-B1qu_6oa.js";const p={title:"Forms/ButtonRow",component:a,argTypes:{children:{control:!1}}},t={args:{children:[e.jsx(r,{label:"Cancel",title:"Cancel"},"1"),e.jsx(r,{label:"Save",title:"Save",variant:"primary"},"2")]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
-  }
-}`,...t.parameters?.docs?.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,p as default};
--- pr-preview/pr-1445/storybook/assets/FormRow.stories-DoD9xVxs.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-CFex6Aaq.js";import{F as o}from"./FormRow.component-DOV7eCBs.js";import{T as t}from"./TextInput.component-CYBi3Y6a.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-Dx3ZoZV-.js";import"./Icon.component-BxXj_gNE.js";import"./FormHint.component-CxnJHpMU.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
-  }
-}`,...r.parameters?.docs?.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,c as default};
