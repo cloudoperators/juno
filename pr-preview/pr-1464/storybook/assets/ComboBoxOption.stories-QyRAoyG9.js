babc54a402c468019c7890f4080611c39282395a
ArtieReus <ArtieReus@users.noreply.github.com>
2026-02-16 11:31:14 +0000

    Deploy preview for PR 1464 🛫

---
+++ pr-preview/pr-1464/storybook/assets/ComboBoxOption.stories-QyRAoyG9.js
@@ -0,0 +1,14 @@
+import{j as s}from"./iframe-Qozk-ahj.js";import{a,C as n}from"./ComboBoxOption.component-B_nzedBw.js";import"./preload-helper-PPVm8Dsz.js";import"./floating-ui.react-B8RodcDu.js";import"./Label.component-CKcpKI-i.js";import"./FormHint.component-DAdhxumX.js";import"./Icon.component-Cw8odOJb.js";import"./Spinner.component-DiRtE4SA.js";import"./PortalProvider.component-HLgnyf4U.js";import"./element-movement-BJpuGFer.js";import"./frozen-CHaxnLr_.js";import"./use-tree-walker-BBn2C-Dq.js";const D={title:"Forms/ComboBox/ComboBoxOption",component:a,argTypes:{},render:t=>s.jsx(n,{children:s.jsx(a,{...t})})},r={args:{value:"Option 1"}},o={args:{disabled:!0,value:"Disabled Option"}},e={args:{children:"Option 1"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: "Option 1"
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    value: "Disabled Option"
+  }
+}`,...o.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: "Option 1"
+  }
+}`,...e.parameters?.docs?.source}}};const f=["Default","Disabled","ChildrenOnly"];export{e as ChildrenOnly,r as Default,o as Disabled,f as __namedExportsOrder,D as default};
---
+++ pr-preview/pr-1464/storybook/assets/FormSection.stories-DIGgpVNA.js
@@ -0,0 +1,18 @@
+import{j as e}from"./iframe-Qozk-ahj.js";import{F as n}from"./FormSection.component-CmdfZYut.js";import{T as s}from"./TextInput.component-Cez2sFiG.js";import{F as t}from"./FormRow.component-BJO1hkE8.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-CKcpKI-i.js";import"./Icon.component-Cw8odOJb.js";import"./FormHint.component-DAdhxumX.js";const u={title:"Forms/FormSection",component:n,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{children:e.jsx(s,{label:"Address Line 1"})},"1"),e.jsx(t,{children:e.jsx(s,{label:"Address Line 2"})},"2")]}},o={args:{title:"Form Section With Title",children:[e.jsx(t,{children:e.jsx(s,{label:"Address Line 1"})},"1"),e.jsx(t,{children:e.jsx(s,{label:"Address Line 2"})},"2")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<FormRow key="1">
+        <TextInput label="Address Line 1" />
+      </FormRow>, <FormRow key="2">
+        <TextInput label="Address Line 2" />
+      </FormRow>]
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: "Form Section With Title",
+    children: [<FormRow key="1">
+        <TextInput label="Address Line 1" />
+      </FormRow>, <FormRow key="2">
+        <TextInput label="Address Line 2" />
+      </FormRow>]
+  }
+}`,...o.parameters?.docs?.source}}};const F=["Default","WithTitle"];export{r as Default,o as WithTitle,F as __namedExportsOrder,u as default};
