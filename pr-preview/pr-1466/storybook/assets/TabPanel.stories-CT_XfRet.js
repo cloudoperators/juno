b875ae325f9ab9457215aa6f26f0ec606883d930
edda <edda@users.noreply.github.com>
2026-02-17 14:43:55 +0000

    Remove preview for PR 1466 🛬

--- pr-preview/pr-1466/storybook/assets/TabPanel.stories-CT_XfRet.js
+++
@@ -1,5 +0,0 @@
-import{T as a}from"./TabPanel.component-DXP2M7IM.js";import"./iframe-OZUm7ZIu.js";import"./preload-helper-PPVm8Dsz.js";import"./TabPanel-CL5AGbhp.js";const s={title:"Layout/Tabs/TabPanel",component:a,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},e={args:{children:"Tab panel content goes here."}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: "Tab panel content goes here."
-  }
-}`,...e.parameters?.docs?.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,s as default};
