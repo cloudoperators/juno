502f44e0437cb727f9c05464d4971b53fcf80340
taymoor89 <taymoor89@users.noreply.github.com>
2026-02-23 12:01:37 +0000

    Remove preview for PR 1478 🛬

--- pr-preview/pr-1478/storybook/assets/ButtonRow.stories-9MXazj9z.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-Db5E3M_Q.js";import{B as a}from"./ButtonRow.component-D5-UvX5r.js";import{B as r}from"./Button.component-CR-jC3m0.js";import"./preload-helper-PPVm8Dsz.js";import"./Stack.component-OWYzocYZ.js";import"./Icon.component-K1XWv2_8.js";import"./Spinner.component-COXZdxp0.js";const p={title:"Forms/ButtonRow",component:a,argTypes:{children:{control:!1}}},t={args:{children:[e.jsx(r,{label:"Cancel",title:"Cancel"},"1"),e.jsx(r,{label:"Save",title:"Save",variant:"primary"},"2")]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
-  }
-}`,...t.parameters?.docs?.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,p as default};
