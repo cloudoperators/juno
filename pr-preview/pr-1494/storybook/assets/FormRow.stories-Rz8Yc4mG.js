5a203b73ba931f1b47fb498fe8e18cef198f98b8
ArtieReus <ArtieReus@users.noreply.github.com>
2026-02-27 12:48:02 +0000

    Deploy preview for PR 1494 🛫

---
+++ pr-preview/pr-1494/storybook/assets/FormRow.stories-Rz8Yc4mG.js
@@ -0,0 +1,5 @@
+import{j as e}from"./iframe-CKpxSCCg.js";import{F as o}from"./FormRow.component-BaToDgd8.js";import{T as t}from"./TextInput.component-CFmI_n_w.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-CMzpr85l.js";import"./Icon.component-BxwOeuaq.js";import"./FormHint.component-4V11byYD.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
+  }
+}`,...r.parameters?.docs?.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,c as default};
