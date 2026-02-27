8c4a4d4e2b8028887f559483016985e09edebe88
andypf <andypf@users.noreply.github.com>
2026-02-27 09:31:38 +0000

    Deploy preview for PR 1491 🛫

---
+++ pr-preview/pr-1491/storybook/assets/FormRow.stories-Dt-J30Bn.js
@@ -0,0 +1,5 @@
+import{j as e}from"./iframe-DH-YFaYq.js";import{F as o}from"./FormRow.component-CqmqYNO_.js";import{T as t}from"./TextInput.component-C-4EQHRO.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-Bdmhf0Cw.js";import"./Icon.component-AfSKyJLq.js";import"./FormHint.component-BQ1wI912.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
+  }
+}`,...r.parameters?.docs?.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,c as default};
---
+++ pr-preview/pr-1491/storybook/assets/SelectDivider.stories-BPqzk7vn.js
@@ -0,0 +1,7 @@
+import{r as c,j as e}from"./iframe-DH-YFaYq.js";import{S as l,a}from"./SelectOption.component-BjCafvG7.js";import{P as d}from"./PortalProvider.component-NxqRqCvz.js";import"./preload-helper-PPVm8Dsz.js";import"./element-movement-laiIhMeX.js";import"./floating-ui.react-C0qDJfNI.js";import"./frozen-CnDGoasn.js";import"./use-text-value-D5-v6rcg.js";import"./Label.component-Bdmhf0Cw.js";import"./Icon.component-AfSKyJLq.js";import"./Spinner.component-DjeMyPS8.js";import"./FormHint.component-BQ1wI912.js";const p=`
+  jn:h-px 
+  jn:w-full
+  jn:bg-theme-background-lvl-3
+`,r=c.forwardRef(({className:t="",...o},i)=>e.jsx("div",{className:`juno-select-divider ${p} ${t}`,ref:i,...o}));r.displayName="SelectDivider";try{r.displayName="SelectDivider",r.__docgenInfo={description:"A visual border/divider between SelectOptions or SelectGroups",displayName:"SelectDivider",props:{className:{defaultValue:{value:""},description:"add a custom className",name:"className",required:!1,type:{name:"string"}}}}}catch{}const D={title:"Forms/Select/SelectDivider",component:r,argTypes:{},decorators:[t=>e.jsx(d,{children:e.jsx(t,{})})],render:()=>e.jsxs(l,{open:!0,children:[e.jsx(a,{value:"1",children:"1"}),e.jsx(r,{}),e.jsx(a,{value:"3",children:"3"})]})},s={args:{}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...s.parameters?.docs?.source}}};const N=["Default"];export{s as Default,N as __namedExportsOrder,D as default};
