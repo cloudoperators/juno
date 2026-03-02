7333427becd240725bb4ed29a73a7611ebb1e9df
Taimoor Aslam <taimoor.aslam@sap.com>
2026-03-02 13:25:04 +0100

    chore(greenhouse): reroute old admin area to new one (#1483)
    Co-authored-by: Arturo Reuschenbach <reuschenbach@gmail.com>

---
+++ apps/greenhouse/src/routes/org-admin.tsx
@@ -0,0 +1,15 @@
+/*
+ * SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Juno contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { createFileRoute, redirect } from "@tanstack/react-router"
+
+export const Route = createFileRoute("/org-admin")({
+  beforeLoad: () => {
+    // eslint-disable-next-line @typescript-eslint/only-throw-error
+    throw redirect({
+      to: "/admin",
+    })
+  },
+})
