4f33a41a31d66e7c4a38b08e3da53c2c35699520
ArtieReus <ArtieReus@users.noreply.github.com>
2026-03-04 11:09:22 +0000

    Remove preview for PR 1501 🛬

--- pr-preview/pr-1501/storybook/assets/FormRow.stories-91tHJQMy.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-Dubado1M.js";import{F as o}from"./FormRow.component-B3YEyhNq.js";import{T as t}from"./TextInput.component-6yenX3y4.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-CSn9SNum.js";import"./Icon.component-BH7yTDqg.js";import"./FormHint.component-4d6mfGZf.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
-  }
-}`,...r.parameters?.docs?.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,c as default};
