0e444a390033b820b56f0d69e3d28c4d2431118c
I531348 <guoda.puidokaite@sap.com>
2026-03-09 11:19:24 +0100

    mock issue

--- packages/ui-components/src/docs/ColorPalette/JunoColorPalette.tsx
+++ packages/ui-components/src/docs/ColorPalette/JunoColorPalette.tsx
@@ -1,6 +1,6 @@
 /*
  * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Juno contributors
- * SPDX-License-Identifier: Apache-2.0
+ * SPDX-License-Identifier: Apache-2.0X
  */
 
 import React from "react"
