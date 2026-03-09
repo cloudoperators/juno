94362e2c604847cd7bba45e75cc77f7be403e944
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2026-03-09 10:18:08 +0000

    Deploy preview for PR 1503 🛫

--- pr-preview/pr-1503/storybook/assets/ButtonRow.stories-COJKGUuG.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-DUHEE__M.js";import{B as a}from"./ButtonRow.component-MS8L-GiS.js";import{B as r}from"./Button.component-C8LNPWhT.js";import"./preload-helper-PPVm8Dsz.js";import"./Stack.component-DpLUSoTr.js";import"./Icon.component-BHjXFvwD.js";import"./Spinner.component-Dk1xpJFW.js";const p={title:"Forms/ButtonRow",component:a,argTypes:{children:{control:!1}}},t={args:{children:[e.jsx(r,{label:"Cancel",title:"Cancel"},"1"),e.jsx(r,{label:"Save",title:"Save",variant:"primary"},"2")]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
-  }
-}`,...t.parameters?.docs?.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,p as default};
---
+++ pr-preview/pr-1503/storybook/assets/ButtonRow.stories-DBw-fM8J.js
@@ -0,0 +1,5 @@
+import{j as e}from"./iframe-B3Gxqdf9.js";import{B as a}from"./ButtonRow.component-CnpsenL_.js";import{B as r}from"./Button.component-D0WL3Xj2.js";import"./preload-helper-PPVm8Dsz.js";import"./Stack.component-MUikLhvY.js";import"./Icon.component-CLeKmsBV.js";import"./Spinner.component-sne8Vmqg.js";const p={title:"Forms/ButtonRow",component:a,argTypes:{children:{control:!1}}},t={args:{children:[e.jsx(r,{label:"Cancel",title:"Cancel"},"1"),e.jsx(r,{label:"Save",title:"Save",variant:"primary"},"2")]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
+  }
+}`,...t.parameters?.docs?.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,p as default};
