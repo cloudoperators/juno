ae6c7771a8426cc183460b86cba92b06c9af758b
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2026-03-10 06:38:14 +0000

    Deploy preview for PR 1515 🛫

---
+++ pr-preview/pr-1515/storybook/assets/FormSection.stories-CGcX1sPh.js
@@ -0,0 +1,18 @@
+import{j as e}from"./iframe-BQ9k-GBc.js";import{F as n}from"./FormSection.component-ltFSiD4M.js";import{T as s}from"./TextInput.component-C8p2HGRe.js";import{F as t}from"./FormRow.component-jg6P2RdY.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-B94md4wO.js";import"./Icon.component-vs_FJ61w.js";import"./FormHint.component-DybdAKvt.js";const u={title:"Forms/FormSection",component:n,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{children:e.jsx(s,{label:"Address Line 1"})},"1"),e.jsx(t,{children:e.jsx(s,{label:"Address Line 2"})},"2")]}},o={args:{title:"Form Section With Title",children:[e.jsx(t,{children:e.jsx(s,{label:"Address Line 1"})},"1"),e.jsx(t,{children:e.jsx(s,{label:"Address Line 2"})},"2")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<FormRow key="1">
+        <TextInput label="Address Line 1" />
+      </FormRow>, <FormRow key="2">
+        <TextInput label="Address Line 2" />
+      </FormRow>]
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: "Form Section With Title",
+    children: [<FormRow key="1">
+        <TextInput label="Address Line 1" />
+      </FormRow>, <FormRow key="2">
+        <TextInput label="Address Line 2" />
+      </FormRow>]
+  }
+}`,...o.parameters?.docs?.source}}};const F=["Default","WithTitle"];export{r as Default,o as WithTitle,F as __namedExportsOrder,u as default};
