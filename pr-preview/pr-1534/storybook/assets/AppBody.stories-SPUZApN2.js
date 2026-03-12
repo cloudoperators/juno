4f4a613090bf1e1b7e0ce90240f5c27a259d156e
franzheidl <franzheidl@users.noreply.github.com>
2026-03-12 17:10:05 +0000

    Remove preview for PR 1534 🛬

--- pr-preview/pr-1534/storybook/assets/AppBody.stories-SPUZApN2.js
+++
@@ -1,4 +0,0 @@
-import{A as e}from"./AppBody.component-DDsW7S2o.js";import"./iframe-BYKp5Kmi.js";import"./preload-helper-PPVm8Dsz.js";const t={title:"Internal/AppBody",component:e,argTypes:{children:{control:!1}}},r={parameters:{},args:{}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  parameters: {},
-  args: {}
-}`,...r.parameters?.docs?.source}}};const p=["Body"];export{r as Body,p as __namedExportsOrder,t as default};
