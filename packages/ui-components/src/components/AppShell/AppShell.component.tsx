4c238457203a25ebaef961e544412f3431d27199
Vladislav Schur <u.shchur@sap.com>
2026-03-16 14:23:42 +0100

    refactor(ui): replace hardcode values with tailwind predefined

--- packages/ui-components/src/components/AppShell/AppShell.component.tsx
+++ packages/ui-components/src/components/AppShell/AppShell.component.tsx
@@ -37,7 +37,7 @@ export const AppShell: React.FC<AppShellProps> = ({
         <>
           {topNavigation && <HeaderContainer fullWidth={fullWidthOrDefault}>{topNavigation}</HeaderContainer>}
           <MainContainer>
-            <MainContainerInner fullWidth={fullWidthOrDefault} className={`${topNavigation ? "jn:mt-[3.875rem]" : ""}`}>
+            <MainContainerInner fullWidth={fullWidthOrDefault} className={`${topNavigation ? "jn:mt-15.5" : ""}`}>
               {sideNavigation && sideNavigation}
               <ContentContainer>{children}</ContentContainer>
             </MainContainerInner>
@@ -51,7 +51,7 @@ export const AppShell: React.FC<AppShellProps> = ({
           </HeaderContainer>
           {/* Wrap everything except page header and footer and navigations in a main container. Add top margin to MainContainerInner as we are not in embedded mode here. */}
           <MainContainer>
-            <MainContainerInner fullWidth={fullWidthOrDefault} className="jn:mt-[3.875rem]">
+            <MainContainerInner fullWidth={fullWidthOrDefault} className="jn:mt-15.5">
               {sideNavigation}
               <ContentContainer>{children}</ContentContainer>
             </MainContainerInner>
