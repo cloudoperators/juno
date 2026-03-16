d0893e32606f9256d3130e08f6adb90b42fa2a50
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2026-03-16 11:34:47 +0000

    Remove preview for PR 1536 🛬

--- pr-preview/pr-1536/storybook/assets/FormRow.stories-qZFWyfDm.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-Dz8m2SVh.js";import{F as o}from"./FormRow.component--ka0rcTx.js";import{T as t}from"./TextInput.component-vol8Qonc.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-DqqwCn4z.js";import"./Icon.component-CgXjljCb.js";import"./FormHint.component-DJU_IJCR.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
-  }
-}`,...r.parameters?.docs?.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,c as default};
--- pr-preview/pr-1536/storybook/assets/TooltipContent.stories-Co151QDG.js
+++
@@ -1,4 +0,0 @@
-import{j as e}from"./iframe-Dz8m2SVh.js";import{b as o,T as s,a}from"./TooltipTrigger.component-CHnm0Sk-.js";import"./preload-helper-PPVm8Dsz.js";import"./floating-ui.react-CbVSyrRD.js";import"./Icon.component-CgXjljCb.js";const d={title:"Components/Tooltip/TooltipContent",component:o,argTypes:{},decorators:[r=>e.jsx("div",{className:"jn:my-6 jn:flex jn:justify-center",children:e.jsxs(s,{initialOpen:!0,children:[e.jsx(a,{children:"clickMe"}),e.jsx(r,{})]})})]},n=r=>e.jsx(o,{...r,children:"This is a tooltip"}),t={args:{},render:n};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {},
-  render: DefaultTemplate
-}`,...t.parameters?.docs?.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,d as default};
