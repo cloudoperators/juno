850cb089024ad2b1698e0feb385971b18a0393cc
vlad-schur-external-sap <vlad-schur-external-sap@users.noreply.github.com>
2026-03-16 16:36:14 +0000

    Remove preview for PR 1541 🛬

--- pr-preview/pr-1541/storybook/assets/SelectDivider.stories-C2CIDU6q.js
+++
@@ -1,7 +0,0 @@
-import{r as c,j as e}from"./iframe-ZMVnSADv.js";import{S as l,a}from"./SelectOption.component-DrjpB_Bf.js";import{P as d}from"./PortalProvider.component-B5cOEgrQ.js";import"./preload-helper-PPVm8Dsz.js";import"./element-movement-CNcOiOaE.js";import"./floating-ui.react-CTgJMc8O.js";import"./frozen-OsyfrLlB.js";import"./use-text-value-Cu7ZiXlE.js";import"./Label.component-DxmW9GhK.js";import"./Icon.component-CLlWf5Lx.js";import"./Spinner.component-BSeIEHxz.js";import"./FormHint.component-jXjY4E1B.js";const p=`
-  jn:h-px 
-  jn:w-full
-  jn:bg-theme-background-lvl-3
-`,r=c.forwardRef(({className:t="",...o},i)=>e.jsx("div",{className:`juno-select-divider ${p} ${t}`,ref:i,...o}));r.displayName="SelectDivider";try{r.displayName="SelectDivider",r.__docgenInfo={description:"A visual border/divider between SelectOptions or SelectGroups",displayName:"SelectDivider",props:{className:{defaultValue:{value:""},description:"add a custom className",name:"className",required:!1,type:{name:"string"}}}}}catch{}const D={title:"Forms/Select/SelectDivider",component:r,argTypes:{},decorators:[t=>e.jsx(d,{children:e.jsx(t,{})})],render:()=>e.jsxs(l,{open:!0,children:[e.jsx(a,{value:"1",children:"1"}),e.jsx(r,{}),e.jsx(a,{value:"3",children:"3"})]})},s={args:{}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...s.parameters?.docs?.source}}};const N=["Default"];export{s as Default,N as __namedExportsOrder,D as default};
