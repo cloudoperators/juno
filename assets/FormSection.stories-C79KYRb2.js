75c513f10f3c5cb33112585fe38c2192ddc5ade0
ArtieReus <reuschenbach@gmail.com>
2026-03-17 08:34:09 +0000

    Deploying to gh-pages from @ cloudoperators/juno@f3572180fe02b1f52bb8a2f1baf3a40b0067d441 🚀

---
+++ assets/FormSection.stories-C79KYRb2.js
@@ -0,0 +1,18 @@
+import{j as e}from"./iframe-CopHY7TA.js";import{F as n}from"./FormSection.component-Cc7cfZaD.js";import{T as s}from"./TextInput.component-sWo4heIy.js";import{F as t}from"./FormRow.component-Ce6EB0I1.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-BLsJv7Ip.js";import"./Icon.component-Bzbz4R49.js";import"./FormHint.component-BIpopmGw.js";const u={title:"Forms/FormSection",component:n,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{children:e.jsx(s,{label:"Address Line 1"})},"1"),e.jsx(t,{children:e.jsx(s,{label:"Address Line 2"})},"2")]}},o={args:{title:"Form Section With Title",children:[e.jsx(t,{children:e.jsx(s,{label:"Address Line 1"})},"1"),e.jsx(t,{children:e.jsx(s,{label:"Address Line 2"})},"2")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<FormRow key="1">
+        <TextInput label="Address Line 1" />
+      </FormRow>, <FormRow key="2">
+        <TextInput label="Address Line 2" />
+      </FormRow>]
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: "Form Section With Title",
+    children: [<FormRow key="1">
+        <TextInput label="Address Line 1" />
+      </FormRow>, <FormRow key="2">
+        <TextInput label="Address Line 2" />
+      </FormRow>]
+  }
+}`,...o.parameters?.docs?.source}}};const F=["Default","WithTitle"];export{r as Default,o as WithTitle,F as __namedExportsOrder,u as default};
--- assets/SelectDivider.stories-CG5spjU2.js
+++
@@ -1,7 +0,0 @@
-import{r as c,j as e}from"./iframe-DNheHJOW.js";import{S as l,a}from"./SelectOption.component-NkrAiR4h.js";import{P as d}from"./PortalProvider.component-C4IKIicA.js";import"./preload-helper-PPVm8Dsz.js";import"./element-movement-DMttNKLn.js";import"./floating-ui.react-Cbwc4hQP.js";import"./frozen-Bu5tLWJ4.js";import"./use-text-value-CJUTH0Ee.js";import"./Label.component-evu2h3Ql.js";import"./Icon.component-C-RXN8bN.js";import"./Spinner.component-DTO8lSTP.js";import"./FormHint.component-DCdlBGzi.js";const p=`
-  jn:h-px 
-  jn:w-full
-  jn:bg-theme-background-lvl-3
-`,r=c.forwardRef(({className:t="",...o},i)=>e.jsx("div",{className:`juno-select-divider ${p} ${t}`,ref:i,...o}));r.displayName="SelectDivider";try{r.displayName="SelectDivider",r.__docgenInfo={description:"A visual border/divider between SelectOptions or SelectGroups",displayName:"SelectDivider",props:{className:{defaultValue:{value:""},description:"add a custom className",name:"className",required:!1,type:{name:"string"}}}}}catch{}const D={title:"Forms/Select/SelectDivider",component:r,argTypes:{},decorators:[t=>e.jsx(d,{children:e.jsx(t,{})})],render:()=>e.jsxs(l,{open:!0,children:[e.jsx(a,{value:"1",children:"1"}),e.jsx(r,{}),e.jsx(a,{value:"3",children:"3"})]})},s={args:{}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...s.parameters?.docs?.source}}};const N=["Default"];export{s as Default,N as __namedExportsOrder,D as default};
