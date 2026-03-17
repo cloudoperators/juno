dac772ca0b2f08f15ea020f159d4cef689698aee
Guoda <121792659+guoda-puidokaite@users.noreply.github.com>
2026-03-17 15:52:50 +0100

    feat(ui): add alignRight to DataGridToolbar (#1515)
    * add alignRight
    
    * remove unused import
    
    * remove unused const
    
    * remove search and update story
    
    * tidy
    
    * tidy
    
    * fix storybook

--- packages/ui-components/src/components/DataGridToolbar/DataGridToolbar.component.tsx
+++ packages/ui-components/src/components/DataGridToolbar/DataGridToolbar.component.tsx
@@ -3,19 +3,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import React, { HTMLAttributes, ReactElement, ReactNode } from "react"
+import React, { HTMLAttributes, ReactNode } from "react"
 
-const datagridtoolbarstyles = `
-	jn:flex
-	jn:items-center
+const baseToolbarStyles = `
 	jn:bg-theme-background-lvl-1
 	jn:py-3
 	jn:px-6
 	jn:mb-px
-`
-
-const childrenWrapperStyles = `
-	jn:ml-auto
 `
 
 /**
@@ -24,21 +18,22 @@ const childrenWrapperStyles = `
  * @see https://cloudoperators.github.io/juno/?path=/docs/components-datagrid-datagridtoolbar--docs
  * @see {@link DataGridToolbarProps}
  */
-export const DataGridToolbar = ({ search, className = "", children, ...props }: DataGridToolbarProps): ReactNode => {
+export const DataGridToolbar = ({
+  className = "",
+  children,
+  alignRight = true,
+  ...props
+}: DataGridToolbarProps): ReactNode => {
+  const childrenWrapperStyles = alignRight ? "jn:ml-auto" : ""
+  const alignmentToolbarStyles = alignRight ? "jn:flex jn:items-center" : ""
   return (
-    <div className={`juno-datagrid-toolbar ${datagridtoolbarstyles} ${className}`} {...props}>
-      {search && <div>{search}</div>}
+    <div className={`juno-datagrid-toolbar ${baseToolbarStyles} ${alignmentToolbarStyles} ${className}`} {...props}>
       <div className={childrenWrapperStyles}>{children}</div>
     </div>
   )
 }
 
 export interface DataGridToolbarProps extends HTMLAttributes<HTMLDivElement> {
-  /**
-   * An optional `SearchInput` component for inclusion in the toolbar.
-   */
-  search?: ReactElement
-
   /**
    * Elements or components to render within the DataGridToolbar.
    */
@@ -49,4 +44,12 @@ export interface DataGridToolbarProps extends HTMLAttributes<HTMLDivElement> {
    * @default ""
    */
   className?: string
+
+  /**
+   * Determines whether the children are automatically aligned to the right side within the toolbar.
+   * When true, applies `ml-auto` to the children wrapper, pushing content right.
+   * When false, no automatic alignment is applied, allowing for custom layouts.
+   * @default true
+   */
+  alignRight?: boolean
 }
