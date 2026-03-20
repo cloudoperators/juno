18c13ff5f396a811a1430ba8f147bb9ed93efd5e
ArtieReus <ArtieReus@users.noreply.github.com>
2026-03-20 11:59:54 +0000

    Deploy preview for PR 1557 🛫

---
+++ pr-preview/pr-1557/storybook/assets/Pill.component-BGLYRvS0.js
@@ -0,0 +1,28 @@
+import{j as l}from"./iframe-BYbA6OhF.js";import{I as b}from"./Icon.component-BIv8NNH9.js";const j=e=>`
+    jn:inline-flex
+    jn:basis-auto
+    jn:shrink
+    jn:items-center
+    jn:flex-nowrap
+    jn:text-xs
+    jn:p-px
+    jn:border
+    jn:rounded
+    jn:border-theme-background-lvl-4
+    jn:group
+    ${e?"jn:cursor-pointer":""}
+  `,y=e=>`
+    jn:bg-theme-background-lvl-4
+    jn:text-theme-high
+    ${e?"jn:group-hover:text-theme-highest":""}
+    jn:px-1
+    jn:py-0.5
+    jn:rounded-sm
+    jn:inline-block
+  `,d=e=>`
+    jn:px-1
+    jn:py-0.5
+    jn:text-theme-high
+    ${e?"jn:group-hover:text-theme-highest":""}
+    jn:inline-block
+  `,p=({uid:e="",pillKey:a="",pillKeyLabel:s="",pillValue:t="",pillValueLabel:r="",closeable:c=!1,onClick:n,onClose:o,className:h="",...f})=>{const m=i=>{o&&o(i,e||a||t)},u=i=>{n&&n(i,e||a||t)};return l.jsxs("div",{className:`juno-pill ${j(!!n)} ${h}`,...f,children:[!t&&!r?l.jsx("span",{className:`${d(!1)}`,children:"set pillValue or pillValueLabel"}):l.jsxs(l.Fragment,{children:[(s||a)&&l.jsx("span",{className:`pill-key ${y(!!n)}`,onClick:u,children:s||a}),l.jsx("span",{className:`pill-value ${d(!!n)}`,onClick:u,children:r||t})]}),c&&l.jsx(b,{icon:"close",size:18,onClick:m})]})};try{p.displayName="Pill",p.__docgenInfo={description:"A Pill to represent a value, or key and value. Can e.g. be used to represent selected filter values in a filter component. Can optionally be closed. On close the uid, if provided, or the pillKey is returned in the callback.",displayName:"Pill",props:{uid:{defaultValue:{value:""},description:"The unique identifier of the pill. Returned by the onClose callback",name:"uid",required:!1,type:{name:"string"}},pillKey:{defaultValue:{value:""},description:"The key of the filter the pill represents. Returned by the onClose callback if uid undefined. Optional.",name:"pillKey",required:!1,type:{name:"string"}},pillKeyLabel:{defaultValue:{value:""},description:"The visible label to describe the pill key. If not set pillKey is used. Optional.",name:"pillKeyLabel",required:!1,type:{name:"string"}},pillValue:{defaultValue:{value:""},description:"The value of filter the pill represents. Returned by the onClose callback if uid and pillKey undefined",name:"pillValue",required:!1,type:{name:"string"}},pillValueLabel:{defaultValue:{value:""},description:"The visible label to describe the pill value. If not set pillValue is used. Optional.",name:"pillValueLabel",required:!1,type:{name:"string"}},className:{defaultValue:{value:""},description:"Add custom classNames",name:"className",required:!1,type:{name:"string"}},closeable:{defaultValue:{value:"false"},description:"Whether the pill should be closeable",name:"closeable",required:!1,type:{name:"boolean"}},onClose:{defaultValue:null,description:"Pass a handler to be executed when closing the Pill. Also returns the event and the uid (fallback: pillKey -> fallback: pillValue)",name:"onClose",required:!1,type:{name:"EventHandler"}},onClick:{defaultValue:null,description:"Pass a handler to be executed when clicking on the Pill (but not on the close button). Also returns the event and the uid (fallback: pillKey -> fallback: pillValue)",name:"onClick",required:!1,type:{name:"EventHandler"}}}}}catch{}export{p as P};
---
+++ pr-preview/pr-1557/storybook/assets/SelectOption.stories-DUqR1NGO.js
@@ -0,0 +1,20 @@
+import{j as r}from"./iframe-BYbA6OhF.js";import{a as n,S as i}from"./SelectOption.component-B9xxDJIP.js";import{P as p}from"./PortalProvider.component-EEOIH_EH.js";import"./preload-helper-PPVm8Dsz.js";import"./element-movement--AcWDzbr.js";import"./floating-ui.react-NxRPogu0.js";import"./frozen-BVd0YrR8.js";import"./use-text-value-RIyBkDMt.js";import"./Label.component-CIviI1bg.js";import"./Icon.component-BIv8NNH9.js";import"./Spinner.component-B0xwrUwp.js";import"./FormHint.component-CYDw1jv6.js";const f={title:"Forms/Select/SelectOption",component:n,argTypes:{children:{control:{type:"text"},table:{type:{summary:"string"}}}},decorators:[s=>r.jsx(p,{children:r.jsx(s,{})})],render:s=>r.jsx(i,{open:!0,children:r.jsx(n,{...s})})},e={args:{value:"Option 1"}},o={args:{disabled:!0,value:"Disabled Option"}},t={args:{children:["Option 1"]}},a={args:{value:"option-1",label:"Option 1"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: "Option 1"
+  }
+}`,...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    value: "Disabled Option"
+  }
+}`,...o.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    // @ts-ignore
+    children: ["Option 1"]
+  }
+}`,...t.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: "option-1",
+    label: "Option 1"
+  }
+}`,...a.parameters?.docs?.source}}};const j=["Default","Disabled","ChildrenOnly","WithLabel"];export{t as ChildrenOnly,e as Default,o as Disabled,a as WithLabel,j as __namedExportsOrder,f as default};
