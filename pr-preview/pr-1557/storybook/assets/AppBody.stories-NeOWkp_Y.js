b26e501637a99da9ef68554fa740582279dae410
ArtieReus <ArtieReus@users.noreply.github.com>
2026-03-20 11:44:09 +0000

    Deploy preview for PR 1557 🛫

--- pr-preview/pr-1557/storybook/assets/AppBody.stories-NeOWkp_Y.js
+++
@@ -1,4 +0,0 @@
-import{A as e}from"./AppBody.component-CY_jCaff.js";import"./iframe-B2DPEw16.js";import"./preload-helper-PPVm8Dsz.js";const t={title:"Internal/AppBody",component:e,argTypes:{children:{control:!1}}},r={parameters:{},args:{}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  parameters: {},
-  args: {}
-}`,...r.parameters?.docs?.source}}};const p=["Body"];export{r as Body,p as __namedExportsOrder,t as default};
---
+++ pr-preview/pr-1557/storybook/assets/SelectDivider.stories-XM_EfFPG.js
@@ -0,0 +1,7 @@
+import{r as c,j as e}from"./iframe-BDGuxNIU.js";import{S as l,a}from"./SelectOption.component-5IgcQwRY.js";import{P as d}from"./PortalProvider.component-Du64UeTk.js";import"./preload-helper-PPVm8Dsz.js";import"./element-movement-Dzv2Nlgh.js";import"./floating-ui.react-D74fnFx3.js";import"./frozen-BG5mvKCV.js";import"./use-text-value-CY8BNeRQ.js";import"./Label.component-DBsUhjwv.js";import"./Icon.component-Ct7IZmnO.js";import"./Spinner.component-BjC3kHSq.js";import"./FormHint.component-vCamc5hb.js";const p=`
+  jn:h-px 
+  jn:w-full
+  jn:bg-theme-background-lvl-3
+`,r=c.forwardRef(({className:t="",...o},i)=>e.jsx("div",{className:`juno-select-divider ${p} ${t}`,ref:i,...o}));r.displayName="SelectDivider";try{r.displayName="SelectDivider",r.__docgenInfo={description:"A visual border/divider between SelectOptions or SelectGroups",displayName:"SelectDivider",props:{className:{defaultValue:{value:""},description:"add a custom className",name:"className",required:!1,type:{name:"string"}}}}}catch{}const D={title:"Forms/Select/SelectDivider",component:r,argTypes:{},decorators:[t=>e.jsx(d,{children:e.jsx(t,{})})],render:()=>e.jsxs(l,{open:!0,children:[e.jsx(a,{value:"1",children:"1"}),e.jsx(r,{}),e.jsx(a,{value:"3",children:"3"})]})},s={args:{}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...s.parameters?.docs?.source}}};const N=["Default"];export{s as Default,N as __namedExportsOrder,D as default};
