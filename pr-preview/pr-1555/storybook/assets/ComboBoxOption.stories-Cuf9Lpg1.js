9fe91939a9df42d4010b939139c2937d7ed8e774
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2026-03-20 10:57:53 +0000

    Deploy preview for PR 1555 🛫

---
+++ pr-preview/pr-1555/storybook/assets/ComboBoxOption.stories-Cuf9Lpg1.js
@@ -0,0 +1,14 @@
+import{j as s}from"./iframe-B2DPEw16.js";import{a,C as n}from"./ComboBoxOption.component-C63xvxG9.js";import"./preload-helper-PPVm8Dsz.js";import"./floating-ui.react-OOwfs064.js";import"./Label.component-Vo7w2G9b.js";import"./FormHint.component-CLKeFdCI.js";import"./Icon.component-bIGbcEoD.js";import"./Spinner.component-CE_KUrDo.js";import"./PortalProvider.component-C6TEvvww.js";import"./element-movement-Cv5qP9_A.js";import"./frozen-8TDeSBy2.js";import"./use-tree-walker-DsjN0Ckr.js";const D={title:"Forms/ComboBox/ComboBoxOption",component:a,argTypes:{},render:t=>s.jsx(n,{children:s.jsx(a,{...t})})},r={args:{value:"Option 1"}},o={args:{disabled:!0,value:"Disabled Option"}},e={args:{children:"Option 1"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: "Option 1"
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    value: "Disabled Option"
+  }
+}`,...o.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: "Option 1"
+  }
+}`,...e.parameters?.docs?.source}}};const f=["Default","Disabled","ChildrenOnly"];export{e as ChildrenOnly,r as Default,o as Disabled,f as __namedExportsOrder,D as default};
