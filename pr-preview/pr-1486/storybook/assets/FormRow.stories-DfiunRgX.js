9c2b8576303880c92f68c2baf1b33a162a2c57ba
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2026-03-24 12:48:37 +0000

    Deploy preview for PR 1486 🛫

--- pr-preview/pr-1486/storybook/assets/FormRow.stories-DfiunRgX.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-BJOUvBkO.js";import{F as o}from"./FormRow.component-DdNKIQnP.js";import{T as t}from"./TextInput.component-CDAd0tS7.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component--Ha-i43n.js";import"./Icon.component-BFlE0xBD.js";import"./FormHint.component-BPQny1SM.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
-  }
-}`,...r.parameters?.docs?.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,c as default};
---
+++ pr-preview/pr-1486/storybook/assets/SelectOption.stories-DdyeilG9.js
@@ -0,0 +1,20 @@
+import{j as r}from"./iframe-C0xlVtux.js";import{a as n,S as i}from"./SelectOption.component--UQsjCEP.js";import{P as p}from"./PortalProvider.component-DthSlbze.js";import"./preload-helper-PPVm8Dsz.js";import"./element-movement-CWBR7H7L.js";import"./floating-ui.react-B-A0irht.js";import"./frozen-DxafTsNw.js";import"./use-text-value-BZsp8c44.js";import"./Label.component-BpeRc1zX.js";import"./Icon.component-DSCrHV08.js";import"./Spinner.component-BFm-aCBz.js";import"./FormHint.component-BgUCPWQr.js";const f={title:"Forms/Select/SelectOption",component:n,argTypes:{children:{control:{type:"text"},table:{type:{summary:"string"}}}},decorators:[s=>r.jsx(p,{children:r.jsx(s,{})})],render:s=>r.jsx(i,{open:!0,children:r.jsx(n,{...s})})},e={args:{value:"Option 1"}},o={args:{disabled:!0,value:"Disabled Option"}},t={args:{children:["Option 1"]}},a={args:{value:"option-1",label:"Option 1"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: "Option 1"
+  }
+}`,...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    value: "Disabled Option"
+  }
+}`,...o.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    // @ts-ignore
+    children: ["Option 1"]
+  }
+}`,...t.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: "option-1",
+    label: "Option 1"
+  }
+}`,...a.parameters?.docs?.source}}};const j=["Default","Disabled","ChildrenOnly","WithLabel"];export{t as ChildrenOnly,e as Default,o as Disabled,a as WithLabel,j as __namedExportsOrder,f as default};
