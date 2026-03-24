dbe6c129341343f7e51b0cf3e4ff51be81478856
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2026-03-24 09:58:07 +0000

    Deploy preview for PR 1486 🛫

---
+++ pr-preview/pr-1486/storybook/assets/SelectDivider.stories-B4I64tVt.js
@@ -0,0 +1,7 @@
+import{r as c,j as e}from"./iframe-BJOUvBkO.js";import{S as l,a}from"./SelectOption.component-CInmJwXf.js";import{P as d}from"./PortalProvider.component-CT7yhIx1.js";import"./preload-helper-PPVm8Dsz.js";import"./element-movement-L3-zvF9-.js";import"./floating-ui.react-CUoi9BDc.js";import"./frozen-BG0LSQSq.js";import"./use-text-value-B1spt2Xw.js";import"./Label.component--Ha-i43n.js";import"./Icon.component-BFlE0xBD.js";import"./Spinner.component-DbbaBl4g.js";import"./FormHint.component-BPQny1SM.js";const p=`
+  jn:h-px 
+  jn:w-full
+  jn:bg-theme-background-lvl-3
+`,r=c.forwardRef(({className:t="",...o},i)=>e.jsx("div",{className:`juno-select-divider ${p} ${t}`,ref:i,...o}));r.displayName="SelectDivider";try{r.displayName="SelectDivider",r.__docgenInfo={description:"A visual border/divider between SelectOptions or SelectGroups",displayName:"SelectDivider",props:{className:{defaultValue:{value:""},description:"add a custom className",name:"className",required:!1,type:{name:"string"}}}}}catch{}const D={title:"Forms/Select/SelectDivider",component:r,argTypes:{},decorators:[t=>e.jsx(d,{children:e.jsx(t,{})})],render:()=>e.jsxs(l,{open:!0,children:[e.jsx(a,{value:"1",children:"1"}),e.jsx(r,{}),e.jsx(a,{value:"3",children:"3"})]})},s={args:{}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...s.parameters?.docs?.source}}};const N=["Default"];export{s as Default,N as __namedExportsOrder,D as default};
---
+++ pr-preview/pr-1486/storybook/assets/SelectOption.stories-fVupGqxU.js
@@ -0,0 +1,20 @@
+import{j as r}from"./iframe-BJOUvBkO.js";import{a as n,S as i}from"./SelectOption.component-CInmJwXf.js";import{P as p}from"./PortalProvider.component-CT7yhIx1.js";import"./preload-helper-PPVm8Dsz.js";import"./element-movement-L3-zvF9-.js";import"./floating-ui.react-CUoi9BDc.js";import"./frozen-BG0LSQSq.js";import"./use-text-value-B1spt2Xw.js";import"./Label.component--Ha-i43n.js";import"./Icon.component-BFlE0xBD.js";import"./Spinner.component-DbbaBl4g.js";import"./FormHint.component-BPQny1SM.js";const f={title:"Forms/Select/SelectOption",component:n,argTypes:{children:{control:{type:"text"},table:{type:{summary:"string"}}}},decorators:[s=>r.jsx(p,{children:r.jsx(s,{})})],render:s=>r.jsx(i,{open:!0,children:r.jsx(n,{...s})})},e={args:{value:"Option 1"}},o={args:{disabled:!0,value:"Disabled Option"}},t={args:{children:["Option 1"]}},a={args:{value:"option-1",label:"Option 1"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: "Option 1"
+  }
+}`,...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    value: "Disabled Option"
+  }
+}`,...o.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    // @ts-ignore
+    children: ["Option 1"]
+  }
+}`,...t.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: "option-1",
+    label: "Option 1"
+  }
+}`,...a.parameters?.docs?.source}}};const j=["Default","Disabled","ChildrenOnly","WithLabel"];export{t as ChildrenOnly,e as Default,o as Disabled,a as WithLabel,j as __namedExportsOrder,f as default};
