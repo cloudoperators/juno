66ba128d2747b24354a016284fb2985252c4c221
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2026-03-27 10:25:16 +0000

    Deploy preview for PR 1574 🛫

---
+++ pr-preview/pr-1574/storybook/assets/FormRow.stories-CsNjmbOk.js
@@ -0,0 +1,5 @@
+import{j as e}from"./iframe-2dps-xUY.js";import{F as o}from"./FormRow.component-D_ySwTb-.js";import{T as t}from"./TextInput.component-BOC4QoSu.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-Dvc6eBCj.js";import"./Icon.component-NvrSsj3N.js";import"./FormHint.component-BRIuDXJZ.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
+  }
+}`,...r.parameters?.docs?.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,c as default};
---
+++ pr-preview/pr-1574/storybook/assets/TabPanel.stories-BEJglNXl.js
@@ -0,0 +1,5 @@
+import{T as a}from"./TabPanel.component-C9ePksjd.js";import"./iframe-2dps-xUY.js";import"./preload-helper-PPVm8Dsz.js";import"./TabPanel-B5-oOICw.js";const s={title:"Layout/Tabs/TabPanel",component:a,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},e={args:{children:"Tab panel content goes here."}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: "Tab panel content goes here."
+  }
+}`,...e.parameters?.docs?.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,s as default};
