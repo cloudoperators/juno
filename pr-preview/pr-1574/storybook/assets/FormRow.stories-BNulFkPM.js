29d3a6ecdc3446d954e716bb78000bde4dea284b
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2026-03-27 10:23:06 +0000

    Deploy preview for PR 1574 🛫

--- pr-preview/pr-1574/storybook/assets/FormRow.stories-BNulFkPM.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-5y8ctERw.js";import{F as o}from"./FormRow.component-GxKzgSp_.js";import{T as t}from"./TextInput.component-CXDDrOk3.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-Bg4avpNR.js";import"./Icon.component-zsdOUBzj.js";import"./FormHint.component-BxdApV_T.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
-  }
-}`,...r.parameters?.docs?.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,c as default};
---
+++ pr-preview/pr-1574/storybook/assets/FormRow.stories-BimEFuMg.js
@@ -0,0 +1,5 @@
+import{j as e}from"./iframe-BPOMoef1.js";import{F as o}from"./FormRow.component-DMPv2fB8.js";import{T as t}from"./TextInput.component-DgfPg7tz.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-CBT92-02.js";import"./Icon.component-BTa9UpoW.js";import"./FormHint.component-D7-GRwEJ.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
+  }
+}`,...r.parameters?.docs?.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,c as default};
