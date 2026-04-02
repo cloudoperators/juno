1ed2ce47e7244adb1f560db17599aced2054a191
MartinS-git <MartinS-git@users.noreply.github.com>
2026-04-02 14:52:33 +0000

    Remove preview for PR 1583 🛬

--- pr-preview/pr-1583/storybook/assets/FormRow.stories-BakaPxDa.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-DgZEaafe.js";import{F as o}from"./FormRow.component-DgQmyRRD.js";import{T as t}from"./TextInput.component-CeCfRoDr.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-DHO80CRU.js";import"./Icon.component-Cw3UgLfz.js";import"./FormHint.component-wn5Taudb.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
-  }
-}`,...r.parameters?.docs?.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,c as default};
