8ccb382e4762c3a653ce98e4211fd1dc9f30f404
taymoor89 <taymoor89@users.noreply.github.com>
2026-04-07 07:53:09 +0000

    Deploy preview for PR 1584 🛫

---
+++ pr-preview/pr-1584/storybook/assets/FormRow.stories-BkiQjFA-.js
@@ -0,0 +1,5 @@
+import{j as e}from"./iframe-B9EDNK39.js";import{F as o}from"./FormRow.component-CI4f5vIq.js";import{T as t}from"./TextInput.component-CnAn0LZq.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-B8RFu1yc.js";import"./Icon.component-zHcJ_I5s.js";import"./FormHint.component-BXW-hsLf.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
+  }
+}`,...r.parameters?.docs?.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,c as default};
