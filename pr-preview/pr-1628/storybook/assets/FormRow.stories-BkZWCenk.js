8aefea566ef68df79f50b50ed3c9a9eb73a40cd8
edda <edda@users.noreply.github.com>
2026-04-24 17:04:03 +0000

    Deploy preview for PR 1628 🛫

--- pr-preview/pr-1628/storybook/assets/FormRow.stories-BkZWCenk.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-JFvu19Wa.js";import{F as o}from"./FormRow.component-CtzbD6R0.js";import{T as t}from"./TextInput.component-DtOGgnnB.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-a9C_JmOH.js";import"./Icon.component-BbqvmxDP.js";import"./FormHint.component-CvL2HNw8.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
-  }
-}`,...r.parameters?.docs?.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,c as default};
