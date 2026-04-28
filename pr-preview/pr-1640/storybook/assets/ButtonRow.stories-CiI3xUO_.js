b7d82dbc8581ad6d856113b43d34166cf0cb9a1b
franzheidl <franzheidl@users.noreply.github.com>
2026-04-28 16:00:24 +0000

    Deploy preview for PR 1640 🛫

---
+++ pr-preview/pr-1640/storybook/assets/ButtonRow.stories-CiI3xUO_.js
@@ -0,0 +1,5 @@
+import{j as e}from"./iframe-M8DONBoS.js";import{B as a}from"./ButtonRow.component-uECWCPD_.js";import{B as r}from"./Button.component-BRhq6fhf.js";import"./preload-helper-PPVm8Dsz.js";import"./Stack.component-qbl1T_5h.js";import"./Icon.component-tQGVnqJQ.js";import"./Spinner.component-CmtlNlTY.js";const p={title:"Forms/ButtonRow",component:a,argTypes:{children:{control:!1}}},t={args:{children:[e.jsx(r,{label:"Cancel",title:"Cancel"},"1"),e.jsx(r,{label:"Save",title:"Save",variant:"primary"},"2")]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
+  }
+}`,...t.parameters?.docs?.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,p as default};
---
+++ pr-preview/pr-1640/storybook/assets/SelectOption.stories-W23QKINR.js
@@ -0,0 +1,20 @@
+import{j as r}from"./iframe-M8DONBoS.js";import{a as n,S as i}from"./SelectOption.component-B_3lAV86.js";import{P as p}from"./PortalProvider.component-RQi1Aw5o.js";import"./preload-helper-PPVm8Dsz.js";import"./element-movement-Cepzhj2V.js";import"./floating-ui.react-6sxQtCSl.js";import"./frozen-B29LHOGk.js";import"./use-text-value-dVKKha24.js";import"./Label.component-DFe8PvfJ.js";import"./Icon.component-tQGVnqJQ.js";import"./Spinner.component-CmtlNlTY.js";import"./FormHint.component-Du7x9hMo.js";const f={title:"Forms/Select/SelectOption",component:n,argTypes:{children:{control:{type:"text"},table:{type:{summary:"string"}}}},decorators:[s=>r.jsx(p,{children:r.jsx(s,{})})],render:s=>r.jsx(i,{open:!0,children:r.jsx(n,{...s})})},e={args:{value:"Option 1"}},o={args:{disabled:!0,value:"Disabled Option"}},t={args:{children:["Option 1"]}},a={args:{value:"option-1",label:"Option 1"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: "Option 1"
+  }
+}`,...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    value: "Disabled Option"
+  }
+}`,...o.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    // @ts-ignore
+    children: ["Option 1"]
+  }
+}`,...t.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: "option-1",
+    label: "Option 1"
+  }
+}`,...a.parameters?.docs?.source}}};const j=["Default","Disabled","ChildrenOnly","WithLabel"];export{t as ChildrenOnly,e as Default,o as Disabled,a as WithLabel,j as __namedExportsOrder,f as default};
