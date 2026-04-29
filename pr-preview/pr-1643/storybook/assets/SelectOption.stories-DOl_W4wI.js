efa617ba6c37f1d73bd6c6d4ebeb95972907ad56
franzheidl <franzheidl@users.noreply.github.com>
2026-04-29 14:32:41 +0000

    Deploy preview for PR 1643 🛫

---
+++ pr-preview/pr-1643/storybook/assets/SelectOption.stories-DOl_W4wI.js
@@ -0,0 +1,20 @@
+import{j as r}from"./iframe-uWfseNJC.js";import{a as n,S as i}from"./SelectOption.component-DrvHC79y.js";import{P as p}from"./PortalProvider.component-DW_CZhSy.js";import"./preload-helper-PPVm8Dsz.js";import"./element-movement-DaHfs0Jw.js";import"./floating-ui.react-Do_UgzcN.js";import"./frozen-BQFkVy4H.js";import"./use-text-value-Bh26V301.js";import"./Label.component-CmO3CSvC.js";import"./Icon.component-Ben__1cl.js";import"./Spinner.component-Bj1PP07I.js";import"./FormHint.component--jsPyFaL.js";const f={title:"Forms/Select/SelectOption",component:n,argTypes:{children:{control:{type:"text"},table:{type:{summary:"string"}}}},decorators:[s=>r.jsx(p,{children:r.jsx(s,{})})],render:s=>r.jsx(i,{open:!0,children:r.jsx(n,{...s})})},e={args:{value:"Option 1"}},o={args:{disabled:!0,value:"Disabled Option"}},t={args:{children:["Option 1"]}},a={args:{value:"option-1",label:"Option 1"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: "Option 1"
+  }
+}`,...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    value: "Disabled Option"
+  }
+}`,...o.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    // @ts-ignore
+    children: ["Option 1"]
+  }
+}`,...t.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: "option-1",
+    label: "Option 1"
+  }
+}`,...a.parameters?.docs?.source}}};const j=["Default","Disabled","ChildrenOnly","WithLabel"];export{t as ChildrenOnly,e as Default,o as Disabled,a as WithLabel,j as __namedExportsOrder,f as default};
---
+++ pr-preview/pr-1643/storybook/assets/TabPanel.stories-COvZRSua.js
@@ -0,0 +1,5 @@
+import{T as a}from"./TabPanel.component-Cvz8Hkjl.js";import"./iframe-uWfseNJC.js";import"./preload-helper-PPVm8Dsz.js";import"./TabPanel-DeYVcRwS.js";const s={title:"Layout/Tabs/TabPanel",component:a,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},e={args:{children:"Tab panel content goes here."}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: "Tab panel content goes here."
+  }
+}`,...e.parameters?.docs?.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,s as default};
