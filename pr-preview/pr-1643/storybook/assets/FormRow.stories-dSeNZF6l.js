714fbf3ed73ef41d746d259383c6f578e578fb84
franzheidl <franzheidl@users.noreply.github.com>
2026-04-30 08:49:23 +0000

    Remove preview for PR 1643 🛬

--- pr-preview/pr-1643/storybook/assets/FormRow.stories-dSeNZF6l.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-BxLC_HoJ.js";import{F as o}from"./FormRow.component-BCJNfZ0F.js";import{T as t}from"./TextInput.component-DEiAXcsZ.js";import"./preload-helper-PPVm8Dsz.js";import"./Label.component-CGl2jrpx.js";import"./Icon.component-DJJqI4p2.js";import"./FormHint.component-BeYtRt2m.js";const c={title:"Forms/FormRow",component:o,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}}},r={args:{children:[e.jsx(t,{label:"TextInput in a FormRow",placeholder:"Your input here…"},"1")]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
-  }
-}`,...r.parameters?.docs?.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,c as default};
