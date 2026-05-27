ab6996aa510bfec33884995809a01aa1d702ecd2
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2026-05-27 07:51:29 +0000

    Deploy preview for PR 1711 🛫

---
+++ pr-preview/pr-1711/storybook/assets/Spinner-ByLQ5mDO.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./Spinner.component-UJEVs6Ya.js";var n=e((()=>{t()}));export{n as t};
--- pr-preview/pr-1711/storybook/assets/TabList-BHGIVEiB.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{i as t,n}from"./TabList.component-CYH3-5zj.js";var r=e((()=>{t()})),i=e((()=>{n()}));export{r as n,i as t};
