9a4d828736db285cd87860b6df51a77aa9330e15
edda <edda@users.noreply.github.com>
2026-05-29 08:23:55 +0000

    Remove preview for PR 1725 🛬

--- pr-preview/pr-1725/storybook/assets/TooltipContent-CY_s9Qnc.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{a as t}from"./TooltipTrigger-q2_hd9P_.js";var n=e((()=>{t()}));export{n as t};
--- pr-preview/pr-1725/storybook/assets/TopNavigationItem-C4VqNVtx.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./TopNavigationItem.component-4ISW61Wd.js";var n=e((()=>{t()}));export{n as t};
