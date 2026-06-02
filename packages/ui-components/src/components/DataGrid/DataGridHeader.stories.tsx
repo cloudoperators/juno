ad95535393e7b5ab5692aee56b5d1e3a689e72db
Franz Heidl <f.heidl@sap.com>
2026-06-02 12:01:13 +0200

    fix(docs): fix static code example for fully-featured DataGrid story (#1736)
    * fix(docs): fix static code example for fully-featured DataGrid story
    
    Signed-off-by: Franz Heidl <f.heidl@sap.com>
    
    * fix(docs): fix static code yet again
    
    Signed-off-by: Franz Heidl <f.heidl@sap.com>
    
    ---------
    
    Signed-off-by: Franz Heidl <f.heidl@sap.com>

--- packages/ui-components/src/components/DataGrid/DataGridHeader.stories.tsx
+++ packages/ui-components/src/components/DataGrid/DataGridHeader.stories.tsx
@@ -338,7 +338,9 @@ export const FullyFeatured: Story = {
       <Stack gap="2" alignment="center">
         <Checkbox />
         <PopupMenu className="flex items-center">
-          <PopupMenuToggle {...({ as: Button, size: "xs", icon: "moreVert", label: "Actions" } as any)} />
+          <PopupMenuToggle as={React.Fragment}>
+            <Button size="xs" icon="moreVert" label="Actions" />
+          </PopupMenuToggle>
           <PopupMenuOptions>
             <PopupMenuItem label="Download" />
             <PopupMenuItem label="Delete" />
