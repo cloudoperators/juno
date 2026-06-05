ed3a7172e80ff3b955d935cc34a51045c57bcd44
edda <edda@users.noreply.github.com>
2026-06-05 13:25:42 +0000

    Deploy preview for PR 1740 🛫

---
+++ pr-preview/pr-1740/storybook/assets/DataGridCell.stories-CMyNy7vK.js
@@ -0,0 +1,36 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{U as t,tt as n}from"./iframe-ooB7dZsI.js";import{i as r,o as i,r as a,t as o}from"./DataGridCell.component-DQlL-r9R.js";import{t as s}from"./DataGridCell-CGqAQy2G.js";import{t as c}from"./DataGrid-DDYt5DX_.js";var l,u,d,f,p,m,h=e((()=>{n(),c(),a(),s(),l=t(),u={title:`Components/DataGrid/DataGridCell`,component:o,argTypes:{children:{control:!1,table:{type:{summary:`ReactNode`}}}},decorators:[e=>(0,l.jsx)(i,{columns:3,children:(0,l.jsx)(r,{children:(0,l.jsx)(e,{})})})],parameters:{docs:{source:{excludeDecorators:!1}}}},d={parameters:{docs:{description:{story:`Juno DataGridCell for use in DataGrid`}}},args:{children:[`DataGridCell`]}},f={parameters:{docs:{description:{story:`Juno DataGridCell with nowrap option (content has white-space: nowrap;)`}}},args:{nowrap:!0,children:[`DataGridCell does not wrap`]}},p={parameters:{docs:{description:{story:`Juno DataGridCell with colspan`}}},args:{colSpan:3,children:[`DataGridCell with colspan`]}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: "Juno DataGridCell for use in DataGrid"
+      }
+    }
+  },
+  args: {
+    children: ["DataGridCell"]
+  }
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: "Juno DataGridCell with nowrap option (content has white-space: nowrap;)"
+      }
+    }
+  },
+  args: {
+    nowrap: true,
+    children: ["DataGridCell does not wrap"]
+  }
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: "Juno DataGridCell with colspan"
+      }
+    }
+  },
+  args: {
+    colSpan: 3,
+    children: ["DataGridCell with colspan"]
+  }
+}`,...p.parameters?.docs?.source}}},m=[`Default`,`NoWrap`,`ColSpan`]}));h();export{p as ColSpan,d as Default,f as NoWrap,m as __namedExportsOrder,u as default,h as t};
