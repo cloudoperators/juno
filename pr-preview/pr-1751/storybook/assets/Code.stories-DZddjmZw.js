da0f998f8b2966b344882a148e45450fc40c15e8
TilmanHaupt <TilmanHaupt@users.noreply.github.com>
2026-06-09 12:22:34 +0000

    Deploy preview for PR 1751 🛫

---
+++ pr-preview/pr-1751/storybook/assets/Code.stories-DZddjmZw.js
@@ -0,0 +1,27 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{U as t,tt as n}from"./iframe-COIN0Adj.js";var r,i,a,o=e((()=>{n(),r=t(),i=`
+  jn:bg-theme-code-block
+  jn:text-sm
+`,a=({content:e=``,children:t,className:n=``,...a})=>(0,r.jsx)(`code`,{className:`juno-code ${i} ${n}`,...a,children:e||t});try{a.displayName=`Code`,a.__docgenInfo={description:"The `Code` component is a lightweight inline `<code>` element used for displaying code snippets or text.\nIt can accept content directly through the `content` prop or render children encapsulated within it.",displayName:`Code`,filePath:`/home/runner/work/juno/juno/packages/ui-components/src/components/Code/Code.component.tsx`,methods:[],props:{content:{defaultValue:{value:``},declarations:[{fileName:`ui-components/src/components/Code/Code.component.tsx`,name:`CodeProps`}],description:"Text content to render within the code element. Overrides `children`.",name:`content`,parent:{fileName:`ui-components/src/components/Code/Code.component.tsx`,name:`CodeProps`},required:!1,tags:{default:`""`},type:{name:`string`}},className:{defaultValue:{value:``},declarations:[{fileName:`ui-components/src/components/Code/Code.component.tsx`,name:`CodeProps`}],description:`Additional CSS class names for styling the code element.`,name:`className`,parent:{fileName:`ui-components/src/components/Code/Code.component.tsx`,name:`CodeProps`},required:!1,tags:{default:`""`},type:{name:`string`}},children:{defaultValue:null,declarations:[{fileName:`ui-components/src/components/Code/Code.component.tsx`,name:`CodeProps`}],description:`Elements or text to render inside the code element.`,name:`children`,parent:{fileName:`ui-components/src/components/Code/Code.component.tsx`,name:`CodeProps`},required:!1,tags:{},type:{name:`ReactNode`}}},tags:{see:`https://cloudoperators.github.io/juno/?path=/docs/components-code--docs
+{@link CodeProps }`}}}catch{}})),s=e((()=>{o()})),c,l,u,d;e((()=>{s(),c={title:`Components/Code`,component:a,argTypes:{children:{control:!1,table:{type:{summary:`ReactNode`}}}}},l={parameters:{docs:{description:{story:`Default inline code`}}},args:{content:`<span>Some code passed as content prop.</span>`}},u={parameters:{docs:{description:{story:`Inline code with children`}}},args:{children:`<Code>Some code with children</Code>`}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: "Default inline code"
+      }
+    }
+  },
+  args: {
+    content: "<span>Some code passed as content prop.</span>"
+  }
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: "Inline code with children"
+      }
+    }
+  },
+  args: {
+    children: "<Code>Some code with children</Code>"
+  }
+}`,...u.parameters?.docs?.source}}},d=[`Default`,`WithChildren`]}))();export{l as Default,u as WithChildren,d as __namedExportsOrder,c as default};
--- pr-preview/pr-1751/storybook/assets/Stack-DFaxlGfP.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./Stack.component-Ch5Z5Zob.js";var n=e((()=>{t()}));export{n as t};
