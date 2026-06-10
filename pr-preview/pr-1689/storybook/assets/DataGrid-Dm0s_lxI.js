5f9c91342dd1ef41bb1b7e1d4cb13baab81d6e0d
vlad-schur-external-sap <vlad-schur-external-sap@users.noreply.github.com>
2026-06-10 13:32:40 +0000

    Remove preview for PR 1689 🛬

--- pr-preview/pr-1689/storybook/assets/DataGrid-Dm0s_lxI.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{s as t}from"./DataGridCell.component-Dr8cnE68.js";var n=e((()=>{t()}));export{n as t};
--- pr-preview/pr-1689/storybook/assets/Tab-L4aFwC1g.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./Tab.component-I29B7Gn5.js";var n=e((()=>{t()}));export{n as t};
