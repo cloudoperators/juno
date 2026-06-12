f61595cfefecc914126bf48a164a1e11d87433f2
MartinS-git <MartinS-git@users.noreply.github.com>
2026-06-12 12:09:28 +0000

    Deploy preview for PR 1746 🛫

---
+++ pr-preview/pr-1746/storybook/assets/FormSection.stories-DsD-KOSm.js
@@ -0,0 +1,18 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{U as t,tt as n}from"./iframe-BQV0ptDC.js";import{n as r,t as i}from"./FormRow.component-Ctf4li7G.js";import{n as a,t as o}from"./FormSection.component-BZ_AAfJw.js";import{n as s,t as c}from"./TextInput.component-IdFXmoB1.js";var l,u,d,f,p;e((()=>{n(),a(),s(),r(),l=t(),u={title:`Forms/FormSection`,component:o,argTypes:{children:{control:!1,table:{type:{summary:`ReactNode`}}}}},d={args:{children:[(0,l.jsx)(i,{children:(0,l.jsx)(c,{label:`Address Line 1`})},`1`),(0,l.jsx)(i,{children:(0,l.jsx)(c,{label:`Address Line 2`})},`2`)]}},f={args:{title:`Form Section With Title`,children:[(0,l.jsx)(i,{children:(0,l.jsx)(c,{label:`Address Line 1`})},`1`),(0,l.jsx)(i,{children:(0,l.jsx)(c,{label:`Address Line 2`})},`2`)]}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<FormRow key="1">
+        <TextInput label="Address Line 1" />
+      </FormRow>, <FormRow key="2">
+        <TextInput label="Address Line 2" />
+      </FormRow>]
+  }
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: "Form Section With Title",
+    children: [<FormRow key="1">
+        <TextInput label="Address Line 1" />
+      </FormRow>, <FormRow key="2">
+        <TextInput label="Address Line 2" />
+      </FormRow>]
+  }
+}`,...f.parameters?.docs?.source}}},p=[`Default`,`WithTitle`]}))();export{d as Default,f as WithTitle,p as __namedExportsOrder,u as default};
--- pr-preview/pr-1746/storybook/assets/TooltipContent-BlzDWrp4.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{a as t}from"./TooltipTrigger-9Mfh_lMF.js";var n=e((()=>{t()}));export{n as t};
