4e56a3c0199be1e16374137bf314c28acac61dd0
MartinS-git <MartinS-git@users.noreply.github.com>
2026-06-15 09:32:46 +0000

    Deploy preview for PR 1747 🛫

---
+++ pr-preview/pr-1747/storybook/assets/Navigation.stories-DwNC7H8J.js
@@ -0,0 +1,73 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./react-CmJWSAiZ.js";import{t as n}from"./jsx-runtime-O9QVJvLM.js";import{t as r}from"./Navigation.component-Bncdt2q9.js";import{n as i,t as a}from"./NavigationItem-DwZpuPWI.js";import{t as o}from"./Navigation-BuaW0M_s.js";var s,c,l,u,d,f,p,m,h,g,_,v,y;e((()=>{t(),o(),a(),s=n(),c=({children:e,...t})=>(0,s.jsx)(r,{...t,children:e}),l={title:`Internal/Navigation`,component:r,argTypes:{children:{control:!1,table:{type:{summary:`ReactNode`}}},onActiveItemChange:{control:!1,table:{type:{summary:`(value: ReactNode) => void`}}}}},u={render:c,args:{activeItem:`Item 1`,children:[(0,s.jsx)(i,{children:`Item 1`},`i-1`),(0,s.jsx)(i,{children:`Item 2`},`i-2`),(0,s.jsx)(i,{children:`Item 3`},`i-3`),(0,s.jsx)(i,{disabled:!0,children:`Item 4`},`i-4`)]}},d={render:c,args:{children:[(0,s.jsx)(i,{value:`i-1`,label:`Item 1`},`i-1`),(0,s.jsx)(i,{value:`i-2`,label:`Item 2`},`i-2`),(0,s.jsx)(i,{value:`i-3`,label:`Item 3`},`i-3`)]}},f={render:c,args:{children:[(0,s.jsx)(i,{value:`i-1`,label:`Item 1 Label`,children:`Item 1`},`i-1`),(0,s.jsx)(i,{value:`i-2`,label:`Item 2 Label`,children:`Item 2`},`i-2`),(0,s.jsx)(i,{value:`i-3`,label:`Item 3 Label`,children:`Item 3`},`i-3`)]}},p={render:c,args:{children:[(0,s.jsx)(i,{value:`Item 1`},`i-1`),(0,s.jsx)(i,{value:`Item 2`,active:!0},`i-2`),(0,s.jsx)(i,{value:`Item 3`},`i-3`)]}},m={render:c,args:{activeItem:`item-2`,children:[(0,s.jsx)(i,{value:`item-1`,label:`Item 1`},`i-1`),(0,s.jsx)(i,{value:`item-2`,label:`Item 2`},`i-2`),(0,s.jsx)(i,{value:`item-3`,label:`Item 3`},`i-3`)]}},h={render:c,args:{activeItem:`Item 2`,children:[(0,s.jsx)(i,{value:`item-1`,label:`Item 1`},`i-1`),(0,s.jsx)(i,{value:`item-2`,label:`Item 2`},`i-2`),(0,s.jsx)(i,{value:`item-3`,label:`Item 3`},`i-3`)]}},g={render:c,args:{activeItem:`Item 2`,children:[(0,s.jsx)(i,{value:`itm-1`,children:`Item 1`},`i-1`),(0,s.jsx)(i,{value:`itm-2`,children:`Item 2`},`i-2`),(0,s.jsx)(i,{value:`itm-3`,children:`Item 3`},`i-3`)]}},_={render:c,args:{disabled:!0,children:[(0,s.jsx)(i,{children:`Item 1`},`i-1`),(0,s.jsx)(i,{active:!0,children:`Item 2`},`i-2`),(0,s.jsx)(i,{children:`Item 3`},`i-3`)]}},v={render:c,args:{children:[(0,s.jsx)(i,{href:`https://www.sap.com`,children:`Link 1`},`i-1`),(0,s.jsx)(i,{href:`https://www.sap.com`,children:`Link 2`},`i-2`),(0,s.jsx)(i,{href:`https://www.sap.com`,children:`Link 3`},`i-3`)]}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    activeItem: "Item 1",
+    children: [<NavigationItem key="i-1">Item 1</NavigationItem>, <NavigationItem key="i-2">Item 2</NavigationItem>, <NavigationItem key="i-3">Item 3</NavigationItem>, <NavigationItem key="i-4" disabled>
+        Item 4
+      </NavigationItem>]
+  }
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    children: [<NavigationItem key="i-1" value="i-1" label="Item 1" />, <NavigationItem key="i-2" value="i-2" label="Item 2" />, <NavigationItem key="i-3" value="i-3" label="Item 3" />]
+  }
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    children: [<NavigationItem key="i-1" value="i-1" label="Item 1 Label">
+        Item 1
+      </NavigationItem>, <NavigationItem key="i-2" value="i-2" label="Item 2 Label">
+        Item 2
+      </NavigationItem>, <NavigationItem key="i-3" value="i-3" label="Item 3 Label">
+        Item 3
+      </NavigationItem>]
+  }
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    children: [<NavigationItem key="i-1" value="Item 1" />, <NavigationItem key="i-2" value="Item 2" active />, <NavigationItem key="i-3" value="Item 3" />]
+  }
+}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    activeItem: "item-2",
+    children: [<NavigationItem key="i-1" value="item-1" label="Item 1" />, <NavigationItem key="i-2" value="item-2" label="Item 2" />, <NavigationItem key="i-3" value="item-3" label="Item 3" />]
+  }
+}`,...m.parameters?.docs?.source}}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    activeItem: "Item 2",
+    children: [<NavigationItem key="i-1" value="item-1" label="Item 1" />, <NavigationItem key="i-2" value="item-2" label="Item 2" />, <NavigationItem key="i-3" value="item-3" label="Item 3" />]
+  }
+}`,...h.parameters?.docs?.source}}},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    activeItem: "Item 2",
+    children: [<NavigationItem key="i-1" value="itm-1">
+        Item 1
+      </NavigationItem>, <NavigationItem key="i-2" value="itm-2">
+        Item 2
+      </NavigationItem>, <NavigationItem key="i-3" value="itm-3">
+        Item 3
+      </NavigationItem>]
+  }
+}`,...g.parameters?.docs?.source}}},_.parameters={..._.parameters,docs:{..._.parameters?.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    disabled: true,
+    children: [<NavigationItem key="i-1">Item 1</NavigationItem>, <NavigationItem key="i-2" active>
+        Item 2
+      </NavigationItem>, <NavigationItem key="i-3">Item 3</NavigationItem>]
+  }
+}`,..._.parameters?.docs?.source}}},v.parameters={...v.parameters,docs:{...v.parameters?.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    children: [<NavigationItem href="https://www.sap.com" key="i-1">
+        Link 1
+      </NavigationItem>, <NavigationItem href="https://www.sap.com" key="i-2">
+        Link 2
+      </NavigationItem>, <NavigationItem href="https://www.sap.com" key="i-3">
+        Link 3
+      </NavigationItem>]
+  }
+}`,...v.parameters?.docs?.source}}},y=[`DefaultWithChildren`,`WithValuesAndLabels`,`WithValuesLabelsAndChildren`,`ValuesOnly`,`WithActiveItemByValue`,`WithActiveItemByLabel`,`WithActiveItemByChild`,`Disabled`,`ItemsAsLinks`]}))();export{u as DefaultWithChildren,_ as Disabled,v as ItemsAsLinks,p as ValuesOnly,g as WithActiveItemByChild,h as WithActiveItemByLabel,m as WithActiveItemByValue,d as WithValuesAndLabels,f as WithValuesLabelsAndChildren,y as __namedExportsOrder,l as default};
--- pr-preview/pr-1747/storybook/assets/TextInput-CAIpmBf1.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./TextInput.component-aiT4mxkB.js";var n=e((()=>{t()}));export{n as t};
