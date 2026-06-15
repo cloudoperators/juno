bc0323814b413c42072afb23c0f4ce439acc6dc1
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2026-06-15 07:43:52 +0000

    Deploy preview for PR 1757 🛫

---
+++ pr-preview/pr-1757/storybook/assets/FormHint-D4PhX6-D.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./FormHint.component-BAzOo5_X.js";var n=e((()=>{t()}));export{n as t};
---
+++ pr-preview/pr-1757/storybook/assets/FormRow.stories-BAyQwa-i.js
@@ -0,0 +1,5 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./react-CmJWSAiZ.js";import{t as n}from"./jsx-runtime-O9QVJvLM.js";import{n as r,t as i}from"./FormRow.component-CNMOnuAs.js";import{n as a,t as o}from"./TextInput.component-BwKEvDma.js";var s,c,l,u;e((()=>{t(),r(),a(),s=n(),c={title:`Forms/FormRow`,component:i,argTypes:{children:{control:!1,table:{type:{summary:`ReactNode`}}}}},l={args:{children:[(0,s.jsx)(o,{label:`TextInput in a FormRow`,placeholder:`Your input here…`},`1`)]}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<TextInput label="TextInput in a FormRow" placeholder="Your input here…" key="1" />]
+  }
+}`,...l.parameters?.docs?.source}}},u=[`Default`]}))();export{l as Default,u as __namedExportsOrder,c as default};
