1795fa5d50c2fc0f6850a9ec29a9979ff27a2351
franzheidl <franzheidl@users.noreply.github.com>
2026-06-18 13:13:30 +0000

    Deploy preview for PR 1772 🛫

---
+++ pr-preview/pr-1772/storybook/assets/StyleProvider.stories-DBjlk1Vn.js
@@ -0,0 +1,23 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./react-CmJWSAiZ.js";import{t as n}from"./jsx-runtime-O9QVJvLM.js";import{F as r,N as i}from"./iframe-CIq5HNVg.js";var a,o,s,c,l,u,d,f;e((()=>{t(),i(),a=n(),o={title:`Layout/StyleProvider`,component:r,argTypes:{stylesWrapper:{options:[`head`,`inline`,`shadowRoot`],control:{type:`radio`}},children:{control:!1}}},s=e=>(0,a.jsx)(r,{...e,children:e.children||e.stylesWrapper||`undefined`}),c={render:s,args:{stylesWrapper:`head`}},l={render:s,args:{stylesWrapper:`inline`}},u={render:s,args:{stylesWrapper:`shadowRoot`}},d={render:s,args:{stylesWrapper:`shadowRoot`,theme:`theme-light`,children:`Light Theme`}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    stylesWrapper: "head"
+  }
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    stylesWrapper: "inline"
+  }
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    stylesWrapper: "shadowRoot"
+  }
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    stylesWrapper: "shadowRoot",
+    theme: "theme-light",
+    children: "Light Theme"
+  }
+}`,...d.parameters?.docs?.source}}},f=[`AddStylesToHead`,`AddInlineStyles`,`AddStylesToShadowRoot`,`WithLightTheme`]}))();export{l as AddInlineStyles,c as AddStylesToHead,u as AddStylesToShadowRoot,d as WithLightTheme,f as __namedExportsOrder,o as default};
