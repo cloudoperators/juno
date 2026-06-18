26155a3f3ed127e42a4d02c18ae6acd919d80b65
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2026-06-18 12:25:31 +0000

    Deploy preview for PR 1756 🛫

--- pr-preview/pr-1756/storybook/assets/Checkbox-D69eWDoa.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./Checkbox.component-Duq-2yjl.js";var n=e((()=>{t()}));export{n as t};
--- pr-preview/pr-1756/storybook/assets/Label-BhwO8a9v.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./Label.component-D3Kt9Dfi.js";var n=e((()=>{t()}));export{n as t};
