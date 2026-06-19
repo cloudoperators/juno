4fd3fce1f14eddf9ef4395e665e1dee3e4bf90eb
guoda-puidokaite <guoda-puidokaite@users.noreply.github.com>
2026-06-19 14:01:00 +0000

    Deploy preview for PR 1756 🛫

---
+++ pr-preview/pr-1756/storybook/assets/PortalProvider.stories-DECkRKru.js
@@ -0,0 +1,24 @@
+import{c as e,i as t}from"./preload-helper-usAeo7Bx.js";import{t as n}from"./react-IGKVQItS.js";import{t as r}from"./react-dom-Ey92KDtl.js";import{t as i}from"./jsx-runtime-O9QVJvLM.js";import{n as a,r as o,t as s}from"./PortalProvider.component-Do0ZvEjj.js";import{t as c}from"./Button.component-tICnU_N-.js";import{t as l}from"./Button-UZ9hYW25.js";var u,d,f,p,m,h,g,_;t((()=>{n(),u=e(r()),a(),l(),d=i(),f={title:`WiP/PortalProvider`,component:s,subcomponents:{"PortalProvider.Portal":s.Portal},tags:[`autodocs`],argTypes:{children:{control:!1}}},p=()=>{let e=o();return e?(0,u.createPortal)((0,d.jsx)(c,{label:`I'm inside a portal using the usePortalref hook in a custom component.`}),e):null},m={args:{children:(0,d.jsx)(s.Portal,{children:(0,d.jsx)(c,{label:`I'm inside a portal using the Portal component as provided by PortalProvider.`})})}},h={args:{children:(0,d.jsxs)(d.Fragment,{children:[(0,d.jsx)(`span`,{children:` Some non-portalled content`}),(0,d.jsx)(p,{})]})}},g={args:{children:(0,d.jsxs)(d.Fragment,{children:[(0,d.jsx)(`div`,{children:`Some non-portaled content.`}),(0,d.jsx)(s.Portal,{children:(0,d.jsx)(c,{label:`I'm inside a portal using the Portal component as provided by PortalProvider.`})}),(0,d.jsx)(p,{})]})}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <PortalProvider.Portal>
+        <Button label="I'm inside a portal using the Portal component as provided by PortalProvider." />
+      </PortalProvider.Portal>
+  }
+}`,...m.parameters?.docs?.source}}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <>
+        <span> Some non-portalled content</span>
+        <PortalMessage />
+      </>
+  }
+}`,...h.parameters?.docs?.source}}},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <>
+        <div>Some non-portaled content.</div>
+        <PortalProvider.Portal>
+          <Button label="I'm inside a portal using the Portal component as provided by PortalProvider." />
+        </PortalProvider.Portal>
+        <PortalMessage />
+      </>
+  }
+}`,...g.parameters?.docs?.source}}},_=[`WithPortalComponent`,`WithHook`,`MultiplePortals`]}))();export{g as MultiplePortals,h as WithHook,m as WithPortalComponent,_ as __namedExportsOrder,f as default};
---
+++ pr-preview/pr-1756/storybook/assets/TextInput.stories-C3ntv1rG.js
@@ -0,0 +1,70 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./react-IGKVQItS.js";import{t as n}from"./jsx-runtime-O9QVJvLM.js";import{t as r}from"./TextInput.component-ColqtPbl.js";import{t as i}from"./TextInput-iGtu2Hdv.js";var a,o,s,c,l,u,d,f,p,m,h,g,_,v,y,b,x,S,C,w,T;e((()=>{t(),i(),a=n(),{fn:o}=__STORYBOOK_MODULE_TEST__,s={title:`Forms/TextInput`,component:r,args:{onFocus:o()},argTypes:{errortext:{control:!1},helptext:{control:!1},successtext:{control:!1},value:{control:{type:`text`}}}},c={args:{}},l={args:{label:`Text Input`}},u={args:{label:`Required Text Input`,required:!0}},d={args:{invalid:!0}},f={args:{valid:!0}},p={args:{autoFocus:!0}},m={args:{disabled:!0}},h={args:{readOnly:!0}},g={args:{type:`email`}},_={args:{type:`tel`}},v={args:{type:`url`}},y={args:{type:`number`}},b={args:{type:`password`}},x={args:{helptext:`This is an explanatory text referring to the input`}},S={args:{helptext:(0,a.jsxs)(a.Fragment,{children:[`This is a helptext with a `,(0,a.jsx)(`a`,{href:`#`,children:`Link`})]})}},C={args:{successtext:`This field is a great success!`}},w={args:{errortext:`This field has an error`}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Text Input"
+  }
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Required Text Input",
+    required: true
+  }
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    invalid: true
+  }
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    valid: true
+  }
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    autoFocus: true
+  }
+}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...m.parameters?.docs?.source}}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
+  args: {
+    readOnly: true
+  }
+}`,...h.parameters?.docs?.source}}},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{
+  args: {
+    type: "email"
+  }
+}`,...g.parameters?.docs?.source}}},_.parameters={..._.parameters,docs:{..._.parameters?.docs,source:{originalSource:`{
+  args: {
+    type: "tel"
+  }
+}`,..._.parameters?.docs?.source}}},v.parameters={...v.parameters,docs:{...v.parameters?.docs,source:{originalSource:`{
+  args: {
+    type: "url"
+  }
+}`,...v.parameters?.docs?.source}}},y.parameters={...y.parameters,docs:{...y.parameters?.docs,source:{originalSource:`{
+  args: {
+    type: "number"
+  }
+}`,...y.parameters?.docs?.source}}},b.parameters={...b.parameters,docs:{...b.parameters?.docs,source:{originalSource:`{
+  args: {
+    type: "password"
+  }
+}`,...b.parameters?.docs?.source}}},x.parameters={...x.parameters,docs:{...x.parameters?.docs,source:{originalSource:`{
+  args: {
+    helptext: "This is an explanatory text referring to the input"
+  }
+}`,...x.parameters?.docs?.source}}},S.parameters={...S.parameters,docs:{...S.parameters?.docs,source:{originalSource:`{
+  args: {
+    helptext: <>
+        This is a helptext with a <a href="#">Link</a>
+      </>
+  }
+}`,...S.parameters?.docs?.source}}},C.parameters={...C.parameters,docs:{...C.parameters?.docs,source:{originalSource:`{
+  args: {
+    successtext: "This field is a great success!"
+  }
+}`,...C.parameters?.docs?.source}}},w.parameters={...w.parameters,docs:{...w.parameters?.docs,source:{originalSource:`{
+  args: {
+    errortext: "This field has an error"
+  }
+}`,...w.parameters?.docs?.source}}},T=[`Default`,`WithLabel`,`RequiredWithLabel`,`Invalid`,`Valid`,`Autofocus`,`Disabled`,`ReadOnly`,`Email`,`Tel`,`Url`,`Number`,`Password`,`WithHelpText`,`WithHelpTextAsNode`,`WithSuccessText`,`WithErrorText`]}))();export{p as Autofocus,c as Default,m as Disabled,g as Email,d as Invalid,y as Number,b as Password,h as ReadOnly,u as RequiredWithLabel,_ as Tel,v as Url,f as Valid,w as WithErrorText,x as WithHelpText,S as WithHelpTextAsNode,l as WithLabel,C as WithSuccessText,T as __namedExportsOrder,s as default};
