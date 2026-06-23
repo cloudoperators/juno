9bd248887cd32db7cbc03ff0714c4ca9215fa363
franzheidl <franzheidl@users.noreply.github.com>
2026-06-23 16:52:06 +0000

    Deploy preview for PR 1792 🛫

--- pr-preview/pr-1792/storybook/assets/ShadowRoot.stories-CdIvN3WO.js
+++
@@ -1,5 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./react-IGKVQItS.js";import{t as n}from"./jsx-runtime-O9QVJvLM.js";import{n as r,t as i}from"./ShadowRoot.component-DahHHJGJ.js";var a,o,s,c;e((()=>{t(),r(),a=n(),o={title:`Layout/ShadowRoot`,component:i,argTypes:{children:{control:!1}},render:e=>(0,a.jsx)(i,{...e,children:(0,a.jsx)(`h1`,{children:`Welcome`})})},s={args:{mode:`closed`}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
-  args: {
-    mode: "closed"
-  }
-}`,...s.parameters?.docs?.source}}},c=[`EncapsulateStyles`]}))();export{s as EncapsulateStyles,c as __namedExportsOrder,o as default};
