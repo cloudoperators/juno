1f148bb9fd78607ef6c9ec90f70e4d58d80a6bdb
franzheidl <franzheidl@users.noreply.github.com>
2026-06-24 15:00:13 +0000

    Deploy preview for PR 1792 🛫

--- pr-preview/pr-1792/storybook/assets/ThemeToggle.stories-ByB_PtlF.js
+++
@@ -1,8 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t,t as n}from"./ThemeToggle.component-BU07Oqwt.js";var r,i,a,o;e((()=>{t(),r={title:`WIP/ThemeToggle`,component:n,argTypes:{}},i={parameters:{},args:{}},a={args:{disabled:!0}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
-  parameters: {},
-  args: {}
-}`,...i.parameters?.docs?.source}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
-  args: {
-    disabled: true
-  }
-}`,...a.parameters?.docs?.source}}},o=[`Default`,`Disabled`]}))();export{i as Default,a as Disabled,o as __namedExportsOrder,r as default};
