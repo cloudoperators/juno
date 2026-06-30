e5190ef8f223a3aaf1818b953743d756a058b546
TilmanHaupt <147151040+TilmanHaupt@users.noreply.github.com>
2026-06-30 12:17:01 +0000

    Deploying to gh-pages from @ cloudoperators/juno@ba757eb6ca53470316b4ae3a54dbc0f1e40e6cba 🚀

---
+++ assets/DataGridHeadCell.stories-C0uwoHFD.js
@@ -0,0 +1,36 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./react-IGKVQItS.js";import{t as n}from"./jsx-runtime-O9QVJvLM.js";import{i as r,o as i,r as a}from"./DataGridCell.component-B0ina1gO.js";import{n as o,t as s}from"./DataGridHeadCell-D8xx4Go6.js";import{t as c}from"./DataGrid-BFhoekIU.js";var l,u,d,f,p,m;e((()=>{t(),c(),a(),s(),l=n(),u={title:`Components/DataGrid/DataGridHeadCell`,component:o,argTypes:{children:{control:!1,table:{type:{summary:`ReactNode`}}}},decorators:[e=>(0,l.jsx)(i,{columns:3,children:(0,l.jsx)(r,{children:(0,l.jsx)(e,{})})})],parameters:{docs:{source:{excludeDecorators:!1}}}},d={parameters:{docs:{description:{story:`Juno DataGridHeadCell for use in DataGrid`}}},args:{children:[`DataGridHeadCell`]}},f={parameters:{docs:{description:{story:`Juno DataGridHeadCell with nowrap option (content has white-space: nowrap;)`}}},args:{nowrap:!0,children:[`DataGridHeadCell does not wrap`]}},p={parameters:{docs:{description:{story:`Juno DataGridHeadCell with colspan`}}},args:{colSpan:3,children:[`DataGridHeadCell with colspan`]}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: "Juno DataGridHeadCell for use in DataGrid"
+      }
+    }
+  },
+  args: {
+    children: ["DataGridHeadCell"]
+  }
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: "Juno DataGridHeadCell with nowrap option (content has white-space: nowrap;)"
+      }
+    }
+  },
+  args: {
+    nowrap: true,
+    children: ["DataGridHeadCell does not wrap"]
+  }
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: "Juno DataGridHeadCell with colspan"
+      }
+    }
+  },
+  args: {
+    colSpan: 3,
+    children: ["DataGridHeadCell with colspan"]
+  }
+}`,...p.parameters?.docs?.source}}},m=[`Default`,`NoWrap`,`ColSpan`]}))();export{p as ColSpan,d as Default,f as NoWrap,m as __namedExportsOrder,u as default};
