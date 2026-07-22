2048f138103dc197372a48f32aa83a73fa693ee8
Franz Heidl <franz.heidl@sap.com>
2026-07-22 08:44:41 +0200

    feat(ui): remove exact error message from jsdoc string
    just refer to „default error message“.
    
    Signed-off-by: Franz Heidl <franz.heidl@sap.com>

--- packages/ui-components/src/components/SignInForm/SignInForm.component.tsx
+++ packages/ui-components/src/components/SignInForm/SignInForm.component.tsx
@@ -31,7 +31,7 @@ export interface SignInFormProps extends Omit<FormHTMLAttributes<HTMLFormElement
   /**
    * Error message to display when authentication fails.
    * Pass a string for a custom error message.
-   * Pass `true` to display the default error message "Unable to authenticate the provided credentials.".
+   * Pass `true` to display the default error message.
    * Pass `false` or omit to hide the error message.
    */
   error?: string | boolean
