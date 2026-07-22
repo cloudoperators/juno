46122b6687fc6ccfbff8cae36726b51eaaffa2ac
franzheidl <franzheidl@users.noreply.github.com>
2026-07-22 07:31:59 +0000

    Deploy preview for PR 1822 🛫

--- pr-preview/pr-1822/storybook/assets/SideNavigation-CspxrkvE.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{a as t}from"./SideNavigationItem.component-CweUf2GX.js";var n=e((()=>{t()}));export{n as t};
--- pr-preview/pr-1822/storybook/assets/TooltipContent-Chdlq8O3.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{a as t}from"./TooltipTrigger-CBw_7p3G.js";var n=e((()=>{t()}));export{n as t};
