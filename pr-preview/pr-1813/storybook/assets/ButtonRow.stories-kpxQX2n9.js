d3a39d371ca0194e105813d3f4ae9ff4ec4186b3
hodanoori <hodanoori@users.noreply.github.com>
2026-07-29 11:03:38 +0000

    Remove preview for PR 1813 🛬

--- pr-preview/pr-1813/storybook/assets/ButtonRow.stories-kpxQX2n9.js
+++
@@ -1,5 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./react-IGKVQItS.js";import{t as n}from"./jsx-runtime-O9QVJvLM.js";import{t as r}from"./ButtonRow.component-D6U_-NV1.js";import{t as i}from"./ButtonRow-BZNAchld.js";import{t as a}from"./Button.component-DySZA1uT.js";import{t as o}from"./Button-CLPe4pDE.js";var s,c,l,u;e((()=>{t(),i(),o(),s=n(),c={title:`Forms/ButtonRow`,component:r,argTypes:{children:{control:!1}}},l={args:{children:[(0,s.jsx)(a,{label:`Cancel`,title:`Cancel`},`1`),(0,s.jsx)(a,{label:`Save`,title:`Save`,variant:`primary`},`2`)]}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
-  }
-}`,...l.parameters?.docs?.source}}},u=[`Default`]}))();export{l as Default,u as __namedExportsOrder,c as default};
