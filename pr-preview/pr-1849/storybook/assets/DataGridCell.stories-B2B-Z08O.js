b3046f0efdceb0f7e032b835da7f1cc885901e8c
franzheidl <franzheidl@users.noreply.github.com>
2026-08-03 14:35:26 +0000

    Deploy preview for PR 1849 🛫

---
+++ pr-preview/pr-1849/storybook/assets/DataGridCell.stories-B2B-Z08O.js
@@ -0,0 +1,36 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./react-IGKVQItS.js";import{t as n}from"./jsx-runtime-O9QVJvLM.js";import{a as r,r as i,t as a}from"./DataGridCell.component-BmJLE3Ur.js";import{t as o}from"./DataGridRow-B3hx3MHm.js";import{t as s}from"./DataGridCell-DinIQtS3.js";import{t as c}from"./DataGrid-C5IX8wdX.js";var l,u,d,f,p,m,h=e((()=>{t(),c(),o(),s(),l=n(),u={title:`Components/DataGrid/DataGridCell`,component:a,argTypes:{children:{control:!1,table:{type:{summary:`ReactNode`}}}},decorators:[e=>(0,l.jsx)(r,{columns:3,children:(0,l.jsx)(i,{children:(0,l.jsx)(e,{})})})],parameters:{docs:{source:{excludeDecorators:!1}}}},d={parameters:{docs:{description:{story:`Juno DataGridCell for use in DataGrid`}}},args:{children:[`DataGridCell`]}},f={parameters:{docs:{description:{story:`Juno DataGridCell with nowrap option (content has white-space: nowrap;)`}}},args:{nowrap:!0,children:[`DataGridCell does not wrap`]}},p={parameters:{docs:{description:{story:`Juno DataGridCell with colspan`}}},args:{colSpan:3,children:[`DataGridCell with colspan`]}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: "Juno DataGridCell for use in DataGrid"
+      }
+    }
+  },
+  args: {
+    children: ["DataGridCell"]
+  }
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: "Juno DataGridCell with nowrap option (content has white-space: nowrap;)"
+      }
+    }
+  },
+  args: {
+    nowrap: true,
+    children: ["DataGridCell does not wrap"]
+  }
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: "Juno DataGridCell with colspan"
+      }
+    }
+  },
+  args: {
+    colSpan: 3,
+    children: ["DataGridCell with colspan"]
+  }
+}`,...p.parameters?.docs?.source}}},m=[`Default`,`NoWrap`,`ColSpan`]}));h();export{p as ColSpan,d as Default,f as NoWrap,m as __namedExportsOrder,u as default,h as t};
---
+++ pr-preview/pr-1849/storybook/assets/DataGridHeadCell.stories-Cst7Al2E.js
@@ -0,0 +1,36 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./react-IGKVQItS.js";import{t as n}from"./jsx-runtime-O9QVJvLM.js";import{a as r,r as i}from"./DataGridCell.component-BmJLE3Ur.js";import{t as a}from"./DataGridRow-B3hx3MHm.js";import{t as o}from"./DataGridHeadCell.component-LP27RLmU.js";import{t as s}from"./DataGridHeadCell-oGNHnL3C.js";import{t as c}from"./DataGrid-C5IX8wdX.js";var l,u,d,f,p,m;e((()=>{t(),c(),a(),s(),l=n(),u={title:`Components/DataGrid/DataGridHeadCell`,component:o,argTypes:{children:{control:!1,table:{type:{summary:`ReactNode`}}}},decorators:[e=>(0,l.jsx)(r,{columns:3,children:(0,l.jsx)(i,{children:(0,l.jsx)(e,{})})})],parameters:{docs:{source:{excludeDecorators:!1}}}},d={parameters:{docs:{description:{story:`Juno DataGridHeadCell for use in DataGrid`}}},args:{children:[`DataGridHeadCell`]}},f={parameters:{docs:{description:{story:`Juno DataGridHeadCell with nowrap option (content has white-space: nowrap;)`}}},args:{nowrap:!0,children:[`DataGridHeadCell does not wrap`]}},p={parameters:{docs:{description:{story:`Juno DataGridHeadCell with colspan`}}},args:{colSpan:3,children:[`DataGridHeadCell with colspan`]}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: "Juno DataGridHeadCell for use in DataGrid"
+      }
+    }
+  },
+  args: {
+    children: ["DataGridHeadCell"]
+  }
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: "Juno DataGridHeadCell with nowrap option (content has white-space: nowrap;)"
+      }
+    }
+  },
+  args: {
+    nowrap: true,
+    children: ["DataGridHeadCell does not wrap"]
+  }
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: "Juno DataGridHeadCell with colspan"
+      }
+    }
+  },
+  args: {
+    colSpan: 3,
+    children: ["DataGridHeadCell with colspan"]
+  }
+}`,...p.parameters?.docs?.source}}},m=[`Default`,`NoWrap`,`ColSpan`]}))();export{p as ColSpan,d as Default,f as NoWrap,m as __namedExportsOrder,u as default};
