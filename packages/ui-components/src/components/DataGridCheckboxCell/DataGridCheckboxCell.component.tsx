9b62cbf886f558bc7444db986633f6cccd8646e3
Franz Heidl <franz.heidl@sap.com>
2026-08-12 15:32:27 +0200

    feat(ui): use string literal for styles
    Signed-off-by: Franz Heidl <franz.heidl@sap.com>

--- packages/ui-components/src/components/DataGridCheckboxCell/DataGridCheckboxCell.component.tsx
+++ packages/ui-components/src/components/DataGridCheckboxCell/DataGridCheckboxCell.component.tsx
@@ -6,6 +6,12 @@
 import React, { HTMLAttributes, ReactNode } from "react"
 import { DataGridCell } from "../DataGridCell/DataGridCell.component"
 
+const checkboxCellStyles = `
+  jn:flex
+  jn:flex-col
+  jn:justify-center
+`
+
 /**
  * `DataGridCheckboxCell` is a `DataGrid` cell pre-configured for holding a `Checkbox` component.
  * Place a `Checkbox` as a child and wire up selection state and event handlers directly on it.
@@ -15,10 +21,7 @@ import { DataGridCell } from "../DataGridCell/DataGridCell.component"
 export const DataGridCheckboxCell = ({ className = "", children, ...props }: DataGridCheckboxCellProps): ReactNode => {
   return (
     // Always center vertically regardless of the parent DataGrid's cellVerticalAlignment context.
-    <DataGridCell
-      className={`juno-datagrid-checkbox-cell jn:flex jn:flex-col jn:justify-center ${className}`}
-      {...props}
-    >
+    <DataGridCell className={`juno-datagrid-checkbox-cell ${checkboxCellStyles} ${className}`} {...props}>
       {children}
     </DataGridCell>
   )
