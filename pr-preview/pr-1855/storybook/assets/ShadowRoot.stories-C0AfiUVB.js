510ba606ddb0b84612eb174c54c439423e739db2
hodanoori <hodanoori@users.noreply.github.com>
2026-08-27 13:17:24 +0000

    Remove preview for PR 1855 🛬

--- pr-preview/pr-1855/storybook/assets/ShadowRoot.stories-C0AfiUVB.js
+++
@@ -1,5 +0,0 @@
-import{n as e}from"./rolldown-runtime-C0FnF6B9.js";import{t}from"./react-C21x__mS.js";import{t as n}from"./jsx-runtime-BdxMnOeJ.js";import{n as r,t as i}from"./ShadowRoot.component-U0jTPh5b.js";var a,o,s,c;function l(){return(l=e((()=>{t(),r(),a=n(),o={title:`Layout/ShadowRoot`,component:i,argTypes:{children:{control:!1}},render:e=>(0,a.jsx)(i,{...e,children:(0,a.jsx)(`h1`,{children:`Welcome`})})},s={args:{mode:`closed`}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
-  args: {
-    mode: "closed"
-  }
-}`,...s.parameters?.docs?.source}}},c=[`EncapsulateStyles`]})))()}l();export{s as EncapsulateStyles,c as __namedExportsOrder,o as default};
