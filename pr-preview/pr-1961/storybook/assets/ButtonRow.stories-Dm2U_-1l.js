cb0655251615266b9771bfac77d9a44337b10c7e
ArtieReus <ArtieReus@users.noreply.github.com>
2026-09-24 12:42:41 +0000

    Remove preview for PR 1961 🛬

--- pr-preview/pr-1961/storybook/assets/ButtonRow.stories-Dm2U_-1l.js
+++
@@ -1,5 +0,0 @@
-import{n as e}from"./rolldown-runtime-C0FnF6B9.js";import{t}from"./react-DiVRNtpo.js";import{t as n}from"./jsx-runtime-BdxMnOeJ.js";import{t as r}from"./ButtonRow.component-kARoRLdk.js";import{t as i}from"./ButtonRow-DjJON_kg.js";import{t as a}from"./Button.component-0B953Rdp.js";import{t as o}from"./Button-DIe1Z6KH.js";var s,c,l,u;function d(){return(d=e((()=>{t(),i(),o(),s=n(),c={title:`Forms/ButtonRow`,component:r,argTypes:{children:{control:!1}}},l={args:{children:[(0,s.jsx)(a,{label:`Cancel`,title:`Cancel`},`1`),(0,s.jsx)(a,{label:`Save`,title:`Save`,variant:`primary`},`2`)]}},u=[`Default`],l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<Button key="1" label="Cancel" title="Cancel" />, <Button key="2" label="Save" title="Save" variant="primary" />]
-  }
-}`,...l.parameters?.docs?.source}}}})))()}d();export{l as Default,u as __namedExportsOrder,c as default};
--- pr-preview/pr-1961/storybook/assets/Navigation-CEYOktjy.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./rolldown-runtime-C0FnF6B9.js";import{r as t}from"./Navigation.component-CRrnHW7j.js";function n(){return(n=e((()=>{t()})))()}export{n as t};
