1b140e9cc539e9c42683c6f9ea605bdccfea54c8
MartinS-git <MartinS-git@users.noreply.github.com>
2026-09-25 08:38:25 +0000

    Deploy preview for PR 1983 🛫

---
+++ pr-preview/pr-1983/storybook/assets/TooltipTrigger.stories-CuCqdy_4.js
@@ -0,0 +1,14 @@
+import{n as e}from"./rolldown-runtime-C0FnF6B9.js";import{t}from"./react-DiVRNtpo.js";import{t as n}from"./jsx-runtime-BdxMnOeJ.js";import{t as r}from"./Icon.component-w6ZOgBJp.js";import{t as i}from"./Icon-ihm6ueZt.js";import{a,o,r as s,t as c}from"./TooltipContent.component-BhS5LRYK.js";import{t as l}from"./TooltipContent-DzRZuJyi.js";import{t as u}from"./TooltipTrigger-0R0j918_.js";var d,f,p,m,h,g,_;function v(){return(v=e((()=>{t(),a(),l(),u(),i(),d=n(),f={title:`Components/Tooltip/TooltipTrigger`,component:s,argTypes:{},decorators:[e=>(0,d.jsx)(`div`,{className:`jn:my-6 jn:flex jn:justify-center`,children:(0,d.jsxs)(o,{initialOpen:!0,children:[(0,d.jsx)(e,{}),(0,d.jsx)(c,{children:`This is a tooltip`})]})})]},p=e=>(0,d.jsx)(s,{...e,children:`clickMe`}),m=e=>(0,d.jsx)(s,{asChild:!0,...e,children:(0,d.jsx)(r,{})}),h={args:{},render:p},g={args:{},parameters:{docs:{description:{story:`If the asChild option is set on the TooltipTrigger the passed child element is used as the tooltip trigger. This is useful for when you want to use e.g. an Icon or Button as the trigger`}}},render:m},_=[`Default`,`AsChildTooltipTrigger`],h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
+  args: {},
+  render: DefaultTemplate
+}`,...h.parameters?.docs?.source}}},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{
+  args: {},
+  parameters: {
+    docs: {
+      description: {
+        story: "If the asChild option is set on the TooltipTrigger the passed child element is used as the tooltip trigger. This is useful for when you want to use e.g. an Icon or Button as the trigger"
+      }
+    }
+  },
+  render: AsChildTemplate
+}`,...g.parameters?.docs?.source}}}})))()}v();export{g as AsChildTooltipTrigger,h as Default,_ as __namedExportsOrder,f as default};
