ba13a3231ba41ef6c91fc776911e9c266da408b8
ArtieReus <ArtieReus@users.noreply.github.com>
2026-09-25 13:19:36 +0000

    Deploy preview for PR 2012 🛫

---
+++ pr-preview/pr-2012/storybook/assets/AppBody.stories-DwSzxqpS.js
@@ -0,0 +1,4 @@
+import{n as e}from"./rolldown-runtime-C0FnF6B9.js";import{n as t,t as n}from"./AppBody-BhAT17XX.js";var r,i,a;function o(){return(o=e((()=>{n(),r={title:`Internal/AppBody`,component:t,argTypes:{children:{control:!1}}},i={parameters:{},args:{}},a=[`Body`],i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  parameters: {},
+  args: {}
+}`,...i.parameters?.docs?.source}}}})))()}o();export{i as Body,a as __namedExportsOrder,r as default};
